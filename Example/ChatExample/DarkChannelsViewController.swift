f202158f7552483792da5ddfc6ba19230098d4b0
Alexey Bukhtin <bukhtin@gmail.com>
2019-08-27 15:05:26 +0200

    Updated the Example project with logout buttons.

---
+++ Example/ChatExample/DarkChannelsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  DarkChannelsViewController.swift
+//  ChatExample
+//
+//  Created by Alexey Bukhtin on 27/08/2019.
+//  Copyright © 2019 Stream.io Inc. All rights reserved.
+//
+
+import UIKit
+import StreamChat
+
+final class DarkChannelsViewController: ChannelsViewController {
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        style = .dark
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        style = .dark
+    }
+}
