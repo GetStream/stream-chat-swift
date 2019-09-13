25815e988520ad68fc0c57fccc4477eea45e86a3
Alexey Bukhtin <bukhtin@gmail.com>
2019-09-13 16:06:07 +0200

    Example project updated.

--- Example/ChatExample/RootViewController.swift
+++ Example/ChatExample/RootViewController.swift
@@ -12,26 +12,54 @@ import RxCocoa
 import StreamChatCore
 
 final class RootViewController: UIViewController {
-
+    
     @IBOutlet weak var badgeLabel: UILabel!
+    @IBOutlet weak var badgeSwitch: UISwitch!
     @IBOutlet weak var onlinelabel: UILabel!
+    @IBOutlet weak var onlineSwitch: UISwitch!
     
     let disposeBag = DisposeBag()
+    var badgeDisposeBag = DisposeBag()
+    var onlineDisposeBag = DisposeBag()
     let channel = Channel(type: .messaging, id: "general")
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        badgeSwitch.rx.isOn.changed
+            .subscribe(onNext: { [weak self] isOn in
+                if isOn {
+                    self?.subscribeForUnreadCount()
+                } else {
+                    self?.badgeDisposeBag = DisposeBag()
+                }
+            })
+            .disposed(by: disposeBag)
+        
+        onlineSwitch.rx.isOn.changed
+            .subscribe(onNext: { [weak self] isOn in
+                if isOn {
+                    self?.subscribeForOnlineUsers()
+                } else {
+                    self?.onlineDisposeBag = DisposeBag()
+                }
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    func subscribeForUnreadCount() {
         channel.unreadCount
             .drive(onNext: { [weak self] count in
                 self?.badgeLabel.text = "\(count == 100 ? "99+" : String(count))  "
                 UIApplication.shared.applicationIconBadgeNumber = count
             })
-            .disposed(by: disposeBag)
-        
+            .disposed(by: badgeDisposeBag)
+    }
+    
+    func subscribeForOnlineUsers() {
         channel.onlineUsers
             .drive(onNext: { [weak self] users in
-                var userNames = "0"
+                var userNames = "—"
                 
                 if users.count == 1 {
                     userNames = users[0].name
@@ -43,6 +71,6 @@ final class RootViewController: UIViewController {
                 
                 self?.onlinelabel.text = "Online: \(userNames)"
             })
-            .disposed(by: disposeBag)
+            .disposed(by: onlineDisposeBag)
     }
 }
