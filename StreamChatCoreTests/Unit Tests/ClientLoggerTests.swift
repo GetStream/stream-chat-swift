d9d166b7740a0abe41d8f013f54dd940367a6d45
Bahadir Oncel <bahadir@getstream.io>
2020-01-06 14:08:52 +0100

    Adds different log level testing, refactors test so they are clearer and more robust

--- StreamChatCoreTests/Unit Tests/ClientLoggerTests.swift
+++ StreamChatCoreTests/Unit Tests/ClientLoggerTests.swift
@@ -14,15 +14,27 @@ class ClientLoggerTests: XCTestCase {
     private let testChannel = Channel(type: .messaging, id: "test")
     private let testUrl = "getstream.io".url!
     private let testFilter = Filter.key("members", .in(["test-member"]))
-    private let testData = Data(base64Encoded: "TestData")!
+    private let testData = "{\"testKey\":\"testValue\"}".data(using: .utf8)!
 
     private var allEndpoints = [Endpoint]()
     
     private var logged = [String]()
     
-    private lazy var logger: ClientLogger = {
+    private lazy var infoLogger: ClientLogger = {
         ClientLogger.logger = { [weak self] _, _, message in self?.logged.append(message) }
         let logger = ClientLogger(icon: "🗣", level: .info)
+        return logger
+    }()
+    
+    private lazy var debugLogger: ClientLogger = {
+        ClientLogger.logger = { [weak self] _, _, message in self?.logged.append(message) }
+        let logger = ClientLogger(icon: "🗣", level: .debug)
+        return logger
+    }()
+    
+    private lazy var errorLogger: ClientLogger = {
+        ClientLogger.logger = { [weak self] _, _, message in self?.logged.append(message) }
+        let logger = ClientLogger(icon: "🗣", level: .error)
         return logger
     }()
     
@@ -75,15 +87,128 @@ class ClientLoggerTests: XCTestCase {
                         .ban(.init(user: testUser, channel: testChannel, timeoutInMinutes: 0, reason: nil))]
     }
     
-    func testLogEndpointStrings() {
+    // MARK: Info level logging
+    
+    func testLogEndpointStringsWithInfoLevel() {
         for endpoint in allEndpoints {
-            logger.log("Endpoint: \(String(describing: endpoint))", level: .debug)
+            infoLogger.log("Endpoint: \(String(describing: endpoint))", level: .debug)
+        }
+
+        XCTAssertEqual(logged.count, allEndpoints.count)
+    }
+
+    func testLogURLRequestWithInfoLevel() {
+        for endpoint in allEndpoints {
+            var urlRequest = URLRequest(url: testUrl)
+            urlRequest.httpMethod = endpoint.method.rawValue
+            urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+
+            if let body = endpoint.body {
+                let encodable = AnyEncodable(body)
+
+                do {
+                    if let httpBody = try? JSONEncoder.defaultGzip.encode(encodable) {
+                        urlRequest.httpBody = httpBody
+                        urlRequest.addValue("gzip", forHTTPHeaderField: "Content-Encoding")
+                    } else {
+                        urlRequest.httpBody = try JSONEncoder.default.encode(encodable)
+                    }
+                } catch {
+                    XCTFail(error.localizedDescription)
+                }
+            }
+
+            infoLogger.log(urlRequest)
+        }
+
+        XCTAssertFalse(logged.isEmpty)
+    }
+
+    func testLogHeadersWithInfoLevel() {
+        let headers = [
+            "X-Stream-Client": "stream-chat-swift-client-2",
+            "X-Stream-Device": "iPhone11",
+            "X-Stream-OS": "iOS13.3",
+            "Stream-Auth-Type": "anonymous"]
+
+        infoLogger.log(headers: headers)
+
+        if logged.isEmpty {
+            XCTFail("No logged message!")
+            return
+        }
+        for (key, value) in headers {
+            XCTAssert(logged[0].contains("\(key) = \(value)"))
+        }
+    }
+
+    func testLogQueryItemsWithInfoLevel() {
+        let queryItems: [URLQueryItem] = [.init(name: "test1", value: "test1"),
+                                          .init(name: "test2", value: "test2")]
+
+        infoLogger.log(queryItems)
+
+        if logged.isEmpty {
+            XCTFail("No logged message!")
+            return
+        }
+        XCTAssertEqual(logged[0], "URL query items:\n▫️ test1=test1\n▫️ test2=test2\n")
+    }
+
+    func testLogURLResponseWithInfoLevel() {
+        let urlResponse = HTTPURLResponse(url: testUrl, mimeType: nil, expectedContentLength: 1, textEncodingName: nil)
+
+        infoLogger.log(urlResponse, data: testData)
+
+        if logged.isEmpty {
+            XCTFail("No logged message!")
+            return
+        }
+        XCTAssertEqual(logged[0], "⬅️ Response 200 (23 bytes): getstream.io")
+    }
+
+    func testLogErrorWithInfoLevel() {
+        let testError = NSError(domain: "test", code: 1, userInfo: nil)
+        infoLogger.log(testError as Error, message: "test error message"); let line = #line
+
+        if logged.isEmpty {
+            XCTFail("No logged message!")
+            return
+        }
+        XCTAssertEqual(logged[0], "❌ test error message Error Domain=test Code=1 \"(null)\" in \(#function)[\(line)]")
+    }
+
+    func testLogDataWithInfoLevel() {
+        infoLogger.log(testData)
+
+        if logged.isEmpty {
+            XCTFail("No logged message!")
+            return
+        }
+        XCTAssertEqual(logged[0], "📦  {\n  \"testKey\" : \"testValue\"\n}")
+    }
+
+    func testLogMessageWithInfoLevel() {
+        infoLogger.log("Some message")
+
+        if logged.isEmpty {
+            XCTFail("No logged message!")
+            return
+        }
+        XCTAssertEqual(logged[0], "Some message")
+    }
+    
+    // MARK: Debug level logging
+    
+    func testLogEndpointStringsWithDebugLevel() {
+        for endpoint in allEndpoints {
+            debugLogger.log("Endpoint: \(String(describing: endpoint))", level: .debug)
         }
         
         XCTAssertEqual(logged.count, allEndpoints.count)
     }
     
-    func testLogURLRequest() {
+    func testLogURLRequestWithDebugLevel() {
         for endpoint in allEndpoints {
             var urlRequest = URLRequest(url: testUrl)
             urlRequest.httpMethod = endpoint.method.rawValue
@@ -104,65 +229,162 @@ class ClientLoggerTests: XCTestCase {
                 }
             }
             
-            logger.log(urlRequest)
+            debugLogger.log(urlRequest)
         }
         
         XCTAssertFalse(logged.isEmpty)
     }
     
-    func testLogHeaders() {
+    func testLogHeadersWithDebugLevel() {
         let headers = [
             "X-Stream-Client": "stream-chat-swift-client-2",
             "X-Stream-Device": "iPhone11",
             "X-Stream-OS": "iOS13.3",
             "Stream-Auth-Type": "anonymous"]
         
-        logger.log(headers: headers)
+        debugLogger.log(headers: headers)
         
-        XCTAssertFalse(logged.isEmpty)
-        for (key, value) in headers {
-            XCTAssert(logged[0].contains("\(key) = \(value)"))
-        }
+        XCTAssert(logged.isEmpty)
     }
     
-    func testLogQueryItems() {
+    func testLogQueryItemsWithDebugLevel() {
         let queryItems: [URLQueryItem] = [.init(name: "test1", value: "test1"),
                                           .init(name: "test2", value: "test2")]
         
-        logger.log(queryItems)
+        debugLogger.log(queryItems)
         
-        XCTAssertFalse(logged.isEmpty)
+        if logged.isEmpty {
+            XCTFail("No logged message!")
+            return
+        }
         XCTAssertEqual(logged[0], "URL query items:\n▫️ test1=test1\n▫️ test2=test2\n")
     }
     
-    func testLogURLResponse() {
-        let urlResponse = URLResponse(url: testUrl, mimeType: nil, expectedContentLength: 1, textEncodingName: nil)
+    func testLogURLResponseWithDebugLevel() {
+        let urlResponse = HTTPURLResponse(url: testUrl, mimeType: nil, expectedContentLength: 1, textEncodingName: nil)
         
-        logger.log(urlResponse, data: testData)
+        debugLogger.log(urlResponse, data: testData)
         
-        XCTAssertFalse(logged.isEmpty)
-        XCTAssert(logged[0].starts(with: "⬅️❔ Unknown response (6 bytes): "))
+        if logged.isEmpty {
+            XCTFail("No logged message!")
+            return
+        }
+        XCTAssertEqual(logged[0], "⬅️ Response 200 (23 bytes): getstream.io")
     }
     
-    func testLogError() {
+    func testLogErrorWithDebugLevel() {
         let testError = NSError(domain: "test", code: 1, userInfo: nil)
-        logger.log(testError as Error, message: "test error message")
+        debugLogger.log(testError as Error, message: "test error message"); let line = #line
         
-        XCTAssertFalse(logged.isEmpty)
-        XCTAssertEqual(logged[0], "❌ test error message Error Domain=test Code=1 \"(null)\" in testLogError()[144]")
+        if logged.isEmpty {
+            XCTFail("No logged message!")
+            return
+        }
+        XCTAssertEqual(logged[0], "❌ test error message Error Domain=test Code=1 \"(null)\" in \(#function)[\(line)]")
     }
     
-    func testLogData() {
-        logger.log(testData)
+    func testLogDataWithDebugLevel() {
+        debugLogger.log(testData)
         
-        XCTAssertFalse(logged.isEmpty)
-        XCTAssertEqual(logged[0], "📦  Error Domain=NSCocoaErrorDomain Code=3840 \"JSON text did not start with array or object and option to allow fragments not set.\" UserInfo={NSDebugDescription=JSON text did not start with array or object and option to allow fragments not set.}")
+        XCTAssert(logged.isEmpty)
     }
     
-    func testLogMessage() {
-        logger.log("Some message")
+    func testLogMessageWithDebugLevel() {
+        debugLogger.log("Some message")
         
-        XCTAssertFalse(logged.isEmpty)
+        if logged.isEmpty {
+            XCTFail("No logged message!")
+            return
+        }
         XCTAssertEqual(logged[0], "Some message")
+    }
+    
+    // MARK: Error level logging
+    
+    func testLogEndpointStringsWithErrorLevel() {
+        for endpoint in allEndpoints {
+            errorLogger.log("Endpoint: \(String(describing: endpoint))", level: .debug)
+        }
+        
+        XCTAssert(logged.isEmpty)
+    }
+    
+    func testLogURLRequestWithErrorLevel() {
+        for endpoint in allEndpoints {
+            var urlRequest = URLRequest(url: testUrl)
+            urlRequest.httpMethod = endpoint.method.rawValue
+            urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            
+            if let body = endpoint.body {
+                let encodable = AnyEncodable(body)
+                
+                do {
+                    if let httpBody = try? JSONEncoder.defaultGzip.encode(encodable) {
+                        urlRequest.httpBody = httpBody
+                        urlRequest.addValue("gzip", forHTTPHeaderField: "Content-Encoding")
+                    } else {
+                        urlRequest.httpBody = try JSONEncoder.default.encode(encodable)
+                    }
+                } catch {
+                    XCTFail(error.localizedDescription)
+                }
+            }
+            
+            errorLogger.log(urlRequest)
+        }
+        
+        XCTAssert(logged.isEmpty)
+    }
+    
+    func testLogHeadersWithErrorLevel() {
+        let headers = [
+            "X-Stream-Client": "stream-chat-swift-client-2",
+            "X-Stream-Device": "iPhone11",
+            "X-Stream-OS": "iOS13.3",
+            "Stream-Auth-Type": "anonymous"]
+        
+        errorLogger.log(headers: headers)
+        
+        XCTAssert(logged.isEmpty)
+    }
+    
+    func testLogQueryItemsWithErrorLevel() {
+        let queryItems: [URLQueryItem] = [.init(name: "test1", value: "test1"),
+                                          .init(name: "test2", value: "test2")]
+        
+        errorLogger.log(queryItems)
+        
+        XCTAssert(logged.isEmpty)
+    }
+    
+    func testLogURLResponseWithErrorLevel() {
+        let urlResponse = HTTPURLResponse(url: testUrl, mimeType: nil, expectedContentLength: 1, textEncodingName: nil)
+        
+        errorLogger.log(urlResponse, data: testData)
+        
+        XCTAssert(logged.isEmpty)
+    }
+    
+    func testLogErrorWithErrorLevel() {
+        let testError = NSError(domain: "test", code: 1, userInfo: nil)
+        errorLogger.log(testError as Error, message: "test error message"); let line = #line
+        
+        if logged.isEmpty {
+            XCTFail("No logged message!")
+            return
+        }
+        XCTAssertEqual(logged[0], "❌ test error message Error Domain=test Code=1 \"(null)\" in \(#function)[\(line)]")
+    }
+    
+    func testLogDataWithErrorLevel() {
+        errorLogger.log(testData)
+        
+        XCTAssert(logged.isEmpty)
+    }
+    
+    func testLogMessageWithErrorLevel() {
+        errorLogger.log("Some message")
+        
+        XCTAssert(logged.isEmpty)
     }
 }
