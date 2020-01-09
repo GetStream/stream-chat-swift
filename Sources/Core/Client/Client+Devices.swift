4ee522307c08130a1af1f84a713359719247705a
Alexey Bukhtin <bukhtin@gmail.com>
2020-01-09 16:42:31 +0100

    Added Subscription. Device API moved to rx and classic way.

---
+++ Sources/Core/Client/Client+Devices.swift
@@ -0,0 +1,49 @@
+//
+//  Client+Devices.swift
+//  StreamChatCore
+//
+//  Created by Alexey Bukhtin on 09/01/2020.
+//  Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - Devices
+
+public extension Client {
+    
+    /// Add a device for Push Notifications.
+    /// - Parameters:
+    ///   - deviceToken: a device token.
+    ///   - completion: a completion block (see `EmptyClientCompletion`).
+    /// - Returns: an subscription.
+    func addDevice(deviceToken: Data, _ completion: @escaping EmptyClientCompletion) -> Subscription {
+        return rx.addDevice(deviceToken: deviceToken).bind(to: completion)
+    }
+    
+    /// Add a device for Push Notifications.
+    /// - Parameters:
+    ///   - deviceId: a Push Notifications device identifier.
+    ///   - completion: a completion block (see `EmptyClientCompletion`).
+    /// - Returns: an observable completion.
+    func addDevice(deviceId: String, _ completion: @escaping EmptyClientCompletion) -> Subscription {
+        return rx.addDevice(deviceId: deviceId).bind(to: completion)
+    }
+    
+    /// Request az list if devices.
+    /// - Parameter completion: a completion block (see `ClientCompletion`).
+    /// - Returns: an subscription.
+    func requestDevices(_ completion: @escaping ClientCompletion<DevicesResponse>) -> Subscription {
+        return rx.requestDevices().bind(to: completion)
+    }
+    
+    /// Remove a device.
+    ///
+    /// - Parameters:
+    ///   - deviceId: a Push Notifications device identifier.
+    ///   - completion: a completion block (see `EmptyClientCompletion`).
+    /// - Returns: an subscription.
+    func removeDevice(deviceId: String, _ completion: @escaping EmptyClientCompletion) -> Subscription {
+        return rx.removeDevice(deviceId: deviceId).bind(to: completion)
+    }
+}
