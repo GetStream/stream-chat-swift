3803607d903d23aa9bf2673ec89e3bfcf7321402
Alexey Bukhtin <bukhtin@gmail.com>
2020-01-09 17:13:50 +0100

    Channels requests in rx and classic way.

---
+++ Sources/Core/Client/API/Client+Channels.swift
@@ -0,0 +1,64 @@
+//
+//  Client+Channels.swift
+//  StreamChatCore
+//
+//  Created by Alexey Bukhtin on 09/01/2020.
+//  Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+
+// MARK: Channels Requests
+
+public extension Client {
+    
+    /// A message search.
+    /// - Parameters:
+    ///   - filter: a filter for channels, e.g. .key("members", .in(["john"]))
+    ///   - query: a search query.
+    ///   - pagination: a pagination. It works via the standard limit and offset parameters.
+    ///   - completion: a completion block (see `ClientCompletion`).
+    /// - Returns: an subscription.
+    func search(filter: Filter = .none,
+                query: String,
+                pagination: Pagination = .channelsPageSize,
+                _ completion: @escaping ClientCompletion<[Message]>) -> Subscription {
+        return rx.search(filter: filter, query: query, pagination: pagination).bind(to: completion)
+    }
+    
+    /// Requests channels with a given query.
+    ///
+    /// - Parameters:
+    ///   - query: a channels query (see `ChannelsQuery`).
+    ///   - completion: a completion block (see `ClientCompletion`).
+    /// - Returns: an subscription.
+    func channels(query: ChannelsQuery, _ completion: @escaping ClientCompletion<[ChannelResponse]>) -> Subscription {
+        return rx.channels(query: query).bind(to: completion)
+    }
+    
+    /// Requests channel with a given query.
+    ///
+    /// - Parameters:
+    ///   - query: a channels query (see `ChannelsQuery`).
+    ///   - completion: a completion block (see `ClientCompletion`).
+    /// - Returns: an subscription.
+    func channel(query: ChannelQuery, _ completion: @escaping ClientCompletion<ChannelResponse>) -> Subscription {
+        return rx.channel(query: query).bind(to: completion)
+    }
+    
+    /// Get a message by id.
+    /// - Parameters:
+    ///   - messageId: a message id.
+    ///   - completion: a completion block (see `ClientCompletion`).
+    /// - Returns: an subscription.
+    func message(with messageId: String, _ completion: @escaping ClientCompletion<MessageResponse>) -> Subscription {
+        return rx.message(with: messageId).bind(to: completion)
+    }
+    
+    /// Mark all messages as readed.
+    /// - Parameter completion: a completion block (see `EmptyClientCompletion`).
+    /// - Returns: an subscription.
+    func markAllRead(_ completion: @escaping EmptyClientCompletion) -> Subscription {
+        return rx.markAllRead().bind(to: completion)
+    }
+}
--- Sources/Core/Client/API/Client+Requests.swift
+++ Sources/Core/Client/API/Client+Requests.swift
@@ -9,67 +9,6 @@
 import Foundation
 import RxSwift
 
-// MARK: - Channels Requests
-
-public extension Client {
-    
-    /// A message search.
-    /// - Parameters:
-    ///   - filter: a filter for channels, e.g. .key("members", .in(["john"]))
-    ///   - query: a search query.
-    ///   - pagination: a pagination. It works via the standard limit and offset parameters.
-    func search(filter: Filter = .none, query: String, pagination: Pagination = .channelsPageSize) -> Observable<[Message]> {
-        guard !query.isBlank else {
-            return .empty()
-        }
-        
-        let query = SearchQuery(filter: filter, query: query, pagination: pagination)
-        
-        if case .none = query.filter {
-            return .error(SearchQueryError.emptyFilter)
-        }
-        
-        let request: Observable<SearchResponse> = rx.request(endpoint: .search(query))
-        
-        return connectedRequest(request.map { $0.messages.compactMap({ $0["message"] }) })
-    }
-    
-    /// Requests channels with a given query.
-    ///
-    /// - Parameter query: a channels query (see `ChannelsQuery`).
-    /// - Returns: a list of a channel response (see `ChannelResponse`).
-    func channels(query: ChannelsQuery) -> Observable<[ChannelResponse]> {
-        let request: Observable<ChannelsResponse> = rx.request(endpoint: .channels(query))
-        return connectedRequest(request.map { $0.channels })
-            .do(onNext: { [unowned self] in self.add(channelsToDatabase: $0, query: query) })
-    }
-    
-    /// Requests channel with a given query.
-    ///
-    /// - Parameter query: a channels query (see `ChannelsQuery`).
-    /// - Returns: a list of a channel response (see `ChannelResponse`).
-    func channel(query: ChannelQuery) -> Observable<ChannelResponse> {
-        return connectedRequest(.channel(query))
-            .do(onNext: { channelResponse in
-                if query.options.contains(.state) {
-                    channelResponse.channel.add(messagesToDatabase: channelResponse.messages)
-                }
-            })
-    }
-    
-    /// Get a message by id.
-    /// - Parameter messageId: a message id.
-    func message(with messageId: String) -> Observable<MessageResponse> {
-        return connectedRequest(.message(messageId))
-    }
-    
-    /// Mark all messages as readed.
-    func markAllRead() -> Observable<Void> {
-        let request: Observable<EmptyData> = rx.request(endpoint: .markAllRead)
-        return connectedRequest(request.map({ _ in Void() }))
-    }
-}
-
 // MARK: - Users Requests
 
 public extension Client {
