36aded2137df53d69b08dff82c27dafe8bd494b1
Alexey Bukhtin <bukhtin@gmail.com>
2020-01-11 22:15:43 +0100

    Removed subscriptions for requests.

--- Sources/Core/Model/Channel/Channel+Events.swift
+++ Sources/Core/Model/Channel/Channel+Events.swift
@@ -11,22 +11,26 @@ import Foundation
 public extension Channel {
     
     /// Observe channel events.
-    ///
-    /// - Parameter eventType: an event type.
-    /// - Returns: observable events.
+    /// - Parameters:
+    ///   - eventType: an event type.
+    ///   - completion: a completion block with `Event`.
+    /// - Returns: a subscription.
     func onEvent(_ eventType: EventType, _ completion: @escaping ClientCompletion<Event>) -> Subscription {
         return rx.onEvent(eventType).bind(to: completion)
     }
     
     /// Observe a list of events with a given channel id (optional).
-    ///
-    /// - Parameter eventType: an event type.
-    /// - Returns: observable events.
+    /// - Parameters:
+    ///   - eventType: an event type.
+    ///   - completion: a completion block with `Event`.
+    /// - Returns: a subscription.
     func onEvent(_ eventTypes: [EventType] = [], _ completion: @escaping ClientCompletion<Event>) -> Subscription {
         return rx.onEvent(eventTypes).bind(to: completion)
     }
     
     /// An observable isUnread state of the channel.
+    /// - Parameter completion: a completion block with `Bool`.
+    /// - Returns: a subscription.
     func isUnread(_ completion: @escaping ClientCompletion<Bool>) -> Subscription {
         return rx.isUnread.asObservable().bind(to: completion)
     }
@@ -34,6 +38,8 @@ public extension Channel {
     /// Observe an unread count of messages in the channel.
     /// - Note: Be sure the current user is a member of the channel.
     /// - Note: 100 is the maximum unread count of messages.
+    /// - Parameter completion: a completion block with `Int`.
+    /// - Returns: a subscription.
     func unreadCount(_ completion: @escaping ClientCompletion<Int>) -> Subscription {
         return rx.unreadCount.asObservable().bind(to: completion)
     }
@@ -41,12 +47,16 @@ public extension Channel {
     /// Observe a user mentioned unread count of messages in the channel.
     /// - Note: Be sure the current user is a member of the channel.
     /// - Note: 100 is the maximum unread count of messages.
+    /// - Parameter completion: a completion block with `Int`.
+    /// - Returns: a subscription.
     func mentionedUnreadCount(_ completion: @escaping ClientCompletion<Int>) -> Subscription {
         return rx.mentionedUnreadCount.asObservable().bind(to: completion)
     }
     
     /// Online users in the channel.
     /// - Note: Be sure users are members of the channel.
+    /// - Parameter completion: a completion block with `[User]`.
+    /// - Returns: a subscription.
     func onlineUsers(_ completion: @escaping ClientCompletion<[User]>) -> Subscription {
         return rx.onlineUsers.asObservable().bind(to: completion)
     }
