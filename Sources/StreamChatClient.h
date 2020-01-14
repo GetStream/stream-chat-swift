b3ab7b54ab2122031d4f0cc0ec613e93cbb12029
Alexey Bukhtin <bukhtin@gmail.com>
2020-01-14 14:33:40 +0100

    Cleanup the project file.

---
+++ Sources/StreamChatClient.h
@@ -0,0 +1,19 @@
+//
+//  StreamChatClient.h
+//  StreamChatClient
+//
+//  Created by Alexey Bukhtin on 14/01/2020.
+//  Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StreamChatClient.
+FOUNDATION_EXPORT double StreamChatClientVersionNumber;
+
+//! Project version string for StreamChatClient.
+FOUNDATION_EXPORT const unsigned char StreamChatClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StreamChatClient/PublicHeader.h>
+
+
