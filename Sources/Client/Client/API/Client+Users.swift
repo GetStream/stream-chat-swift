54495b1731bc5ce383b4d9b835fe48d1aa640541
Alexey Bukhtin <bukhtin@gmail.com>
2020-01-17 13:15:40 +0100

    Added Client User API.

---
+++ Sources/Client/Client/API/Client+Users.swift
@@ -0,0 +1,123 @@
+//
+//  Client+Users.swift
+//  StreamChatCore
+//
+//  Created by Alexey Bukhtin on 09/01/2020.
+//  Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+
+// MARK: Users Requests
+
+public extension Client {
+    
+    /// Requests users with a given query.
+    /// - Parameters:
+    ///   - query: a users query (see `UsersQuery`).
+    ///   - completion: a completion block with `[User]`.
+    @discardableResult
+    func users(query: UsersQuery, _ completion: @escaping Client.Completion<[User]>) -> URLSessionTask {
+        return request(endpoint: .users(query)) { (result: Result<UsersResponse, ClientError>) in
+            completion(result.map({ $0.users }))
+        }
+    }
+    
+    // MARK: Update User
+    
+    /// Update or create a user.
+    /// - Parameter completion: a completion block with `[User]`.
+    @discardableResult
+    func update(users: [User], _ completion: @escaping Client.Completion<[User]>) -> URLSessionTask {
+        return request(endpoint: .updateUsers(users)) { (result: Result<UpdatedUsersResponse, ClientError>) in
+            completion(result.map({ $0.users.compactMap({ $0.value }) }))
+        }
+    }
+    
+    /// Update or create a user.
+    /// - Parameters:
+    ///   - user: a user.
+    ///   - completion: a completion block with `User`.
+    @discardableResult
+    func update(user: User, _ completion: @escaping Client.Completion<User>) -> URLSessionTask {
+        return update(users: [user]) { (result: Result<[User], ClientError>) in
+            completion(result.first(orError: .emptyUser))
+        }
+    }
+    
+    // MARK: Mute User
+    
+    /// Mute a user.
+    /// - Parameters:
+    ///   - user: a user.
+    ///   - completion: a completion block with `MutedUsersResponse`.
+    @discardableResult
+    func mute(user: User, _ completion: @escaping Client.Completion<MutedUsersResponse>) -> URLSessionTask {
+        if user.isCurrent {
+            return .empty
+        }
+        
+        return request(endpoint: .muteUser(user), addSideEffect(for: completion, before: { Client.shared.user = $0.currentUser }))
+    }
+    
+    /// Unmute a user.
+    /// - Parameters:
+    ///   - user: a user.
+    ///   - completion: an empty completion block.
+    @discardableResult
+    func unmute(user: User, _ completion: @escaping Client.Completion<EmptyData> = { _ in }) -> URLSessionTask {
+        if user.isCurrent {
+            return .empty
+        }
+        
+        return request(endpoint: .unmuteUser(user), addSideEffect(for: completion, before: { _ in
+            if let currentUser = User.current {
+                var currentUser = currentUser
+                var mutedUsers = currentUser.mutedUsers
+                
+                if let index = mutedUsers.firstIndex(where: { $0.user.id == user.id }) {
+                    mutedUsers.remove(at: index)
+                    currentUser.mutedUsers = mutedUsers
+                    Client.shared.user = currentUser
+                }
+            }
+        }))
+    }
+    
+    // MARK: Flag User
+    
+    /// Flag a user.
+    /// - Parameters:
+    ///   - user: a user.
+    ///   - completion: a completion block with `FlagUserResponse`.
+    @discardableResult
+    func flag(user: User, _ completion: @escaping Client.Completion<FlagUserResponse>) -> URLSessionTask {
+        return flagUnflag(user, endpoint: .flagUser(user), completion)
+    }
+    
+    /// Unflag a user.
+    /// - Parameters:
+    ///   - user: a user.
+    ///   - completion: a completion block with `FlagUserResponse`.
+    @discardableResult
+    func unflag(user: User, _ completion: @escaping Client.Completion<FlagUserResponse>) -> URLSessionTask {
+        return flagUnflag(user, endpoint: .unflagUser(user), completion)
+    }
+    
+    private func flagUnflag(_ user: User,
+                            endpoint: Endpoint,
+                            _ completion: @escaping Client.Completion<FlagUserResponse>) -> URLSessionTask {
+        return request(endpoint: endpoint) { (result: Result<FlagUserResponse, ClientError>) in
+            let result = result.catchError { error in
+                if case .responseError(let clientResponseError) = error,
+                    clientResponseError.message.contains("flag already exists") {
+                    return .success(FlagUserResponse(user: user, created: Date(), updated: Date()))
+                }
+                
+                return .failure(error)
+            }
+            
+            completion(result)
+        }
+    }
+}
--- Sources/Client/Extensions/Result+Extensions.swift
+++ Sources/Client/Extensions/Result+Extensions.swift
@@ -28,13 +28,37 @@ public extension Result {
     }
 }
 
-public extension Result where Failure == ClientError, Success: Decodable {
+public extension Result where Success: Decodable, Failure == ClientError {
     /// Map the result to a client completion result type.
     /// - Parameters:
     ///   - completion: a client completion block.
     func map<T: Decodable>(_ map: (Success) -> T) -> Result<T, ClientError> {
         if let value = try? get() {
             return .success(map(value))
+        }
+        
+        return .failure(error ?? .unexpectedError(nil, error))
+    }
+    
+    func catchError(_ errorHandler: (Failure) -> Result<Success, ClientError>) -> Result<Success, ClientError> {
+        if let error = error {
+            return errorHandler(error)
+        }
+        
+        return self
+    }
+}
+
+public extension Result where Success: Collection, Failure == ClientError {
+    /// Map values from the result to the first value. If array is empty, will map to an error.
+    /// - Parameter notFoundError: a not forund client error.
+    func first(orError notFoundError: ClientError) -> Result<Success.Element, ClientError> {
+        if let values = try? get() {
+            if let first = values.first {
+                return .success(first)
+            }
+            
+            return .failure(notFoundError)
         }
         
         return .failure(error ?? .unexpectedError(nil, error))
