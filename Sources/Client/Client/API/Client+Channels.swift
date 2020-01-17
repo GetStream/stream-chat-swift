ebfc1ced50eb5c39845d115f2c77fa6dc60705d0
Alexey Bukhtin <bukhtin@gmail.com>
2020-01-17 11:05:11 +0100

    Added channels API + tests.

---
+++ Sources/Client/Client/API/Client+Channels.swift
@@ -0,0 +1,78 @@
+//
+//  Client+Channels.swift
+//  StreamChatCore
+//
+//  Created by Alexey Bukhtin on 09/01/2020.
+//  Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+
+// MARK: Channels Requests
+
+public extension Client {
+    
+    /// A message search.
+    /// - Parameters:
+    ///   - filter: a filter for channels, e.g. .key("members", .in(["john"]))
+    ///   - query: a search query.
+    ///   - pagination: a pagination. It works via the standard limit and offset parameters.
+    ///   - completion: a completion block with `[Message]`.
+    @discardableResult
+    func search(filter: Filter = .none,
+                query: String,
+                pagination: Pagination = .channelsPageSize,
+                _ completion: @escaping Client.Completion<[Message]>) -> URLSessionTask {
+        if query.isBlank {
+            completion(.failure(.channelsSearchQueryEmpty))
+            return .empty
+        }
+        
+        if case .none = filter {
+            completion(.failure(.channelsSearchFilterEmpty))
+            return .empty
+        }
+        
+        let query = SearchQuery(filter: filter, query: query, pagination: pagination)
+        
+        return request(endpoint: .search(query)) { (result: Result<SearchResponse, ClientError>) in
+            completion(result.map({ $0.messages.compactMap({ $0["message"] }) }))
+        }
+    }
+    
+    /// Requests channels with a given query.
+    /// - Parameters:
+    ///   - query: a channels query (see `ChannelsQuery`).
+    ///   - completion: a completion block with `[ChannelResponse]`.
+    @discardableResult
+    func channels(query: ChannelsQuery, _ completion: @escaping Client.Completion<[ChannelResponse]>) -> URLSessionTask {
+        return request(endpoint: .channels(query)) { (result: Result<ChannelsResponse, ClientError>) in
+            completion(result.map({ $0.channels }))
+        }
+    }
+    
+    /// Requests channel with a given query.
+    /// - Parameters:
+    ///   - query: a channels query (see `ChannelsQuery`).
+    ///   - completion: a completion block with `ChannelResponse`.
+    @discardableResult
+    func channel(query: ChannelQuery, _ completion: @escaping Client.Completion<ChannelResponse>) -> URLSessionTask {
+        return request(endpoint: .channel(query), completion)
+    }
+
+    /// Get a message by id.
+    /// - Parameters:
+    ///   - messageId: a message id.
+    ///   - completion: a completion block with `MessageResponse`.
+    @discardableResult
+    func message(with messageId: String, _ completion: @escaping Client.Completion<MessageResponse>) -> URLSessionTask {
+        return request(endpoint: .message(messageId), completion)
+    }
+
+    /// Mark all messages as readed.
+    /// - Parameter completion: an empty completion block.
+    @discardableResult
+    func markAllRead(_ completion: @escaping Client.Completion<EmptyData> = { _ in }) -> URLSessionTask {
+        return request(endpoint: .markAllRead, completion)
+    }
+}
--- Sources/Client/Extensions/Result+Extensions.swift
+++ Sources/Client/Extensions/Result+Extensions.swift
@@ -8,13 +8,35 @@
 
 import Foundation
 
-extension Result {
+public extension Result {
     /// Get the error from the result if it failed.
-    public var error: Error? {
+    var error: Failure? {
         if case .failure(let error) = self {
             return error
         }
         
         return nil
+    }
+    
+    /// Returns true if the result was success.
+    var isSuccess: Bool {
+        if case .success = self {
+            return true
+        }
+        
+        return false
+    }
+}
+
+public extension Result where Failure == ClientError, Success: Decodable {
+    /// Map the result to a client completion result type.
+    /// - Parameters:
+    ///   - completion: a client completion block.
+    func map<T: Decodable>(_ map: (Success) -> T) -> Result<T, ClientError> {
+        if let value = try? get() {
+            return .success(map(value))
+        }
+        
+        return .failure(error ?? .unexpectedError(nil, error))
     }
 }
