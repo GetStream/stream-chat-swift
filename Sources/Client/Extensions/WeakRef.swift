9bb2d5208db0e8fbebf0c66ef145a8b02b92207c
Alexey Bukhtin <bukhtin@gmail.com>
2020-02-05 15:25:37 +0100

    Review updates.

--- Sources/Client/Extensions/WeakRef.swift
+++ Sources/Client/Extensions/WeakRef.swift
@@ -17,21 +17,6 @@ final class WeakRef<T: AnyObject> {
 extension Array {
     /// Remove all nil values.
     mutating func flush<T: AnyObject>() where Element == WeakRef<T> {
-        if isEmpty {
-            return
-        }
-        
-        // Find indices with the nil value.
-        // For example values with indices: [0: nil, 1: value1, 2: nil, 3: value2]
-        let nilIndices = enumerated()
-            // to: [0, nil, 2, nil]
-            .map({ $1.value == nil ? $0 : nil })
-            // to: [0, 2]
-            .compactMap({ $0 })
-            // to: [2, 0]
-            .reversed()
-        
-        // Remove each element from the end of the array.
-        nilIndices.forEach({ remove(at: $0) })
+        self = filter { $0.value != nil }
     }
 }
