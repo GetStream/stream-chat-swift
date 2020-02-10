95738056c1eb01c9c0f21bb2c3b43e0617fd82c6
Alexey Bukhtin <bukhtin@gmail.com>
2020-02-10 10:31:21 +0100

    Added rx client API.

---
+++ Sources/Core/Client/Client+RxDevices.swift
@@ -0,0 +1,51 @@
+//
+//  Client+RxDevices.swift
+//  StreamChatCore
+//
+//  Created by Alexey Bukhtin on 09/01/2020.
+//  Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+import StreamChatClient
+import RxSwift
+
+// MARK: Devices
+
+public extension Reactive where Base == Client {
+    
+    /// Add a device for Push Notifications.
+    /// - Parameter deviceToken: a device token.
+    /// - Returns: an observable completion.
+    func addDevice(deviceToken: Data) -> Observable<Void> {
+        connectedRequest(request({ [unowned base] completion in
+            base.addDevice(deviceToken: deviceToken, completion)
+        })).void()
+    }
+    
+    /// Add a device for Push Notifications.
+    /// - Parameter deviceId: a Push Notifications device identifier.
+    /// - Returns: an observable completion.
+    func addDevice(deviceId: String) -> Observable<Void> {
+        connectedRequest(request({ [unowned base] completion in
+            base.addDevice(deviceId: deviceId, completion)
+        })).void()
+    }
+    
+    /// Request a list if devices.
+    /// - Returns: an observable list of devices.
+    func devices() -> Observable<[Device]> {
+        connectedRequest(request({ [unowned base] completion in
+            base.devices(completion)
+        }))
+    }
+    
+    /// Remove a device.
+    /// - Parameter deviceId: a Push Notifications device identifier.
+    /// - Returns: an observable empty data.
+    func removeDevice(deviceId: String) -> Observable<Void> {
+        connectedRequest(request({ [unowned base] completion in
+            base.removeDevice(deviceId: deviceId, completion)
+        })).void()
+    }
+}
