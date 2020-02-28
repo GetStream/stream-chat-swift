07bcd116a62f8bcd58525f32e6e01d72e324037d
Alexey Bukhtin <bukhtin@gmail.com>
2020-02-28 16:16:13 +0100

    Fixed unread count for notification message new.

--- Sources/Core/Presenter/Messages/ChannelPresenter+Events.swift
+++ Sources/Core/Presenter/Messages/ChannelPresenter+Events.swift
@@ -61,7 +61,7 @@ extension ChannelPresenter {
                 return .footerUpdated
             }
             
-        case .messageNew(let message, let messageNewChannel, _, _):
+        case .messageNew(let message, let messageNewChannel, _, _, _):
             guard shouldMessageEventBeHandled(message) else {
                 return .none
             }
