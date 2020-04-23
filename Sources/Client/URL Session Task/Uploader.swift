66a30cda4b7c0c4af114394167bb6653a2ac1fe9
Alexey Bukhtin <bukhtin@gmail.com>
2020-04-23 19:21:37 +0200

    Added custom Uploader protocol.

---
+++ Sources/Client/URL Session Task/Uploader.swift
@@ -0,0 +1,43 @@
+//
+//  Uploader.swift
+//  StreamChatClient
+//
+//  Created by Alexey Bukhtin on 23/04/2020.
+//  Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+
+/// A custom uploader protocol.
+public protocol Uploader {
+    
+    /// Uploads an image with a given data from the camera.
+    /// - Parameters:
+    ///   - data: an image data.
+    ///   - fileName: a default file name.
+    ///   - mimeType: a mime type.
+    ///   - channel: a channel.
+    ///   - progress: a progress block of the uploading.
+    ///   - completion: a completion block.
+    func uploadImage(data: Data,
+                     fileName: String,
+                     mimeType: String,
+                     channel: Channel,
+                     progress: @escaping Client.Progress,
+                     completion: @escaping Client.Completion<URL>) -> Cancellable
+    
+    /// Uploads a file with a given local file.
+    /// - Parameters:
+    ///   - data: a file data.
+    ///   - fileName: a default file name.
+    ///   - mimeType: a mime type.
+    ///   - channel: a channel.
+    ///   - progress: a progress block of the uploading.
+    ///   - completion: a completion block.
+    func uploadFile(data: Data,
+                    fileName: String,
+                    mimeType: String,
+                    channel: Channel,
+                    progress: @escaping Client.Progress,
+                    completion: @escaping Client.Completion<URL>) -> Cancellable
+}
