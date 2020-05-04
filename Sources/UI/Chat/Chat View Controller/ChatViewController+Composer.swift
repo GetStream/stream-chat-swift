b12a14e0064365d4bd28d3a7a731d496c79099ca
Bahadir Oncel <bahadir@getstream.io>
2020-05-04 14:12:55 +0200

    Fix minor typo
    Upload from a camera -> Upload from camera

--- Sources/UI/Chat/Chat View Controller/ChatViewController+Composer.swift
+++ Sources/UI/Chat/Chat View Controller/ChatViewController+Composer.swift
@@ -369,7 +369,7 @@ extension ChatViewController {
                     UIImagePickerController.isSourceTypeAvailable(.camera) {
                     addButtonToAddFileView(container,
                                            icon: UIImage.Icons.camera,
-                                           title: "Upload from a camera",
+                                           title: "Upload from camera",
                                            sourceType: .photo(.camera)) { [weak self] in
                                             self?.showImagePicker(composerAddFileViewSourceType: $0)
                     }
