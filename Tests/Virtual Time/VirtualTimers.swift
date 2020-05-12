925137e88acee9a2d5787fc274ccb8df5ee72d1d
Vojta Stavik <stavik@outlook.com>
2020-05-12 18:08:20 +0200

    Create `Timer` implementation which uses `VirtualTimer`

---
+++ Tests/Virtual Time/VirtualTimers.swift
@@ -0,0 +1,30 @@
+//
+//  VirtualTimers.swift
+//  StreamChatClientTests
+//
+//  Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+@testable import StreamChatClient
+
+struct VirtualTimeTimer: StreamChatClient.Timer {
+    
+    static var time: VirtualTime!
+    
+    static func schedule(timeInterval: TimeInterval, queue: DispatchQueue, onFire: @escaping () -> Void) {
+        _ = Self.time.scheduleTimer(interval: timeInterval,
+                                    repeating: false,
+                                    callback: { _ in onFire() })
+    }
+    
+    static func scheduleRepeating(timeInterval: TimeInterval,
+                                  queue: DispatchQueue,
+                                  onFire: @escaping () -> Void) -> TimerControl {
+        Self.time.scheduleTimer(interval: timeInterval,
+                                repeating: true,
+                                callback: { _ in onFire() })
+    }
+}
+
+extension VirtualTime.TimerControl: TimerControl { }
