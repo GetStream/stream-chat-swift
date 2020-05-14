0ec3e52fe607d74803015533fd3f9e71224ec2ff
Vojta Stavik <stavik@outlook.com>
2020-05-14 17:08:21 +0200

    Use `testSessionId` in `Client+DeviceTests`

--- Tests/Client/Unit Tests/Client+DevicesTests.swift
+++ Tests/Client/Unit Tests/Client+DevicesTests.swift
@@ -24,16 +24,25 @@ class Client_DevicesTests: XCTestCase {
         sessionConfig.protocolClasses?.insert(RequestRecorderURLProtocol.self, at: 0)
         sessionConfig.protocolClasses?.insert(MockNetworkURLProtocol.self, at: 1)
 
+        let testSessionId = UUID().uuidString
+        sessionConfig.httpAdditionalHeaders = [RequestRecorderURLProtocol.testSessionHeaderKey: testSessionId]
+        
         let clientConfig = Client.Config(apiKey: "test_api_key")
         // We can create a new `Client` instance because we don't use `Client.shared` in tests.
         client = Client(config: clientConfig, defaultURLSessionConfiguration: sessionConfig)
 
         testUser = User(id: "test_user_\(UUID())")
         client.set(user: testUser, token: "test_token")
+        
+        // Prepare the test environment
+        RequestRecorderURLProtocol.reset()
+        RequestRecorderURLProtocol.currentSessionId = testSessionId
+        
+        MockNetworkURLProtocol.reset()
     }
 
-    override func tearDown() {
-        // Reset all recorded requests and prepare it for another run
+    override class func tearDown() {
+        // Make sure everything is cleaned up
         RequestRecorderURLProtocol.reset()
         MockNetworkURLProtocol.reset()
         super.tearDown()
