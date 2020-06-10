89573f63177e544d1f232d2b7b0ba39898d3ec4e
Vojta Stavik <stavik@outlook.com>
2020-06-10 12:03:17 +0200

    Add `TypingStartCleanupMiddleware`
    This is an example of more complex middleware

---
+++ StreamChatClient_v3/WebSocketClient/EventMiddlewares/TypingStartCleanupMiddleware_tests.swift
@@ -0,0 +1,64 @@
+//
+// TypingStartCleanupMiddleware_tests.swift
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+@testable import StreamChatClient_v3
+import XCTest
+
+class TypingStartCleanupMiddlewareTests: XCTestCase {
+  var middleware: TypingStartCleanupMiddleware<DefaultDataTypes>!
+  var currentUser: User!
+
+  var time: VirtualTime!
+  var typingStartTimeout: TimeInterval { type(of: middleware).incomingTypingStartEventTimeout }
+
+  override func setUp() {
+    super.setUp()
+
+    currentUser = User(id: "Luke")
+    middleware = TypingStartCleanupMiddleware(excludedUsers: [currentUser])
+
+    time = VirtualTime()
+    VirtualTimeTimer.time = time
+    middleware.timer = VirtualTimeTimer.self
+  }
+
+  func test_stopTypingEvent_notSentForExcludedUsers() {
+    var result: [EquatableEvent?] = []
+    let typingStartEvent = TypingStart<DefaultDataTypes>(user: currentUser)
+    // Handle a new TypingStart event for the current user and collect resulting events
+    middleware.handle(event: typingStartEvent) {
+      result.append($0.map(EquatableEvent.init))
+    }
+
+    // Simulate time passed for the `typingStartTimeout` period
+    time.run(numberOfSeconds: typingStartTimeout + 1)
+
+    XCTAssertEqual(result, [typingStartEvent].asEquatable())
+  }
+
+  func test_stopTypingEvent_sentAfterTimeout() {
+    // Simulate some user started typing
+    let otherUser = User(id: UUID().uuidString)
+
+    var result: [EquatableEvent?] = []
+    let typingStartEvent = TypingStart<DefaultDataTypes>(user: otherUser)
+    // Handle a new TypingStart event for the current user and collect resulting events
+    middleware.handle(event: typingStartEvent) {
+      result.append($0.map(EquatableEvent.init))
+    }
+
+    // Wait for some timeout shorter than `typingStartTimeout` and assert only `TypingStart` event is sent
+    time.run(numberOfSeconds: typingStartTimeout - 1)
+    XCTAssertEqual(result, [typingStartEvent.asEquatable])
+
+    // Wait for more time and expect a `typingStop` event.
+    time.run(numberOfSeconds: 2)
+    XCTAssertEqual(result, [typingStartEvent.asEquatable, TypingStop<DefaultDataTypes>(user: otherUser).asEquatable])
+
+    // Wait much longer and assert no more `typingStop` events.
+    time.run(numberOfSeconds: 5 + typingStartTimeout)
+    XCTAssertEqual(result, [typingStartEvent.asEquatable, TypingStop<DefaultDataTypes>(user: otherUser).asEquatable])
+  }
+}
---
+++ StreamChatClient_v3/WebSocketClient/Events/TypingEvents.swift
@@ -0,0 +1,18 @@
+//
+// TypingEvents.swift
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+
+// TODO: These are just placeholder events!
+
+struct TypingStart<ExtraData: ExtraDataTypes>: Event {
+  static var eventRawType: String { "typing.start" }
+  let user: UserModel<ExtraData.User>
+}
+
+struct TypingStop<ExtraData: ExtraDataTypes>: Event {
+  static var eventRawType: String { "typing.stop" }
+  let user: UserModel<ExtraData.User>
+}
