1b7da257bf8420ce40dae5a55ede8f7fd9469667
Vojta Stavik <stavik@outlook.com>
2020-06-16 15:55:46 +0200

    Implement saving and loading of `MemberModel`

---
+++ StreamChatClient_v3/Database/DTOs/MemberModelDTO.swift
@@ -0,0 +1,102 @@
+//
+// MemberModelDTO.swift
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import CoreData
+
+@objc(MemberDTO)
+class MemberDTO: NSManagedObject {
+    static let entityName: String = "MemberDTO"
+    
+    // Because we need to have a unique identifier of a member in the db, we use the combination of the related
+    // user' id and the channel the member belongs to.
+    @NSManaged fileprivate var id: String
+    
+    @NSManaged var channelRoleRaw: String
+    @NSManaged var memberCreatedDate: Date
+    @NSManaged var memberUpdatedDate: Date
+    
+    //  @NSManaged var invitedAcceptedDate: Date?
+    //  @NSManaged var invitedRejectedDate: Date?
+    //  @NSManaged var isInvited: Bool
+    
+    // MARK: - Relationships
+    
+    @NSManaged var user: UserDTO
+    
+    private static func createId(userId: String, channeldId: ChannelId) -> String {
+        channeldId.id + userId
+    }
+}
+
+extension MemberDTO {
+    static func load(id: String, channelId: ChannelId, context: NSManagedObjectContext) -> MemberDTO? {
+        let memberId = MemberDTO.createId(userId: id, channeldId: channelId)
+        let request = NSFetchRequest<MemberDTO>(entityName: MemberDTO.entityName)
+        request.predicate = NSPredicate(format: "id == %@", memberId)
+        return try? context.fetch(request).first
+    }
+    
+    /// If a User with the given id exists in the context, fetches and returns it. Otherwise create a new
+    /// `UserDTO` with the given id.
+    ///
+    /// - Parameters:
+    ///   - id: The id of the user to fetch
+    ///   - context: The context used to fetch/create `UserDTO`
+    ///
+    static func loadOrCreate(id: String, channelId: ChannelId, context: NSManagedObjectContext) -> MemberDTO {
+        if let existing = Self.load(id: id, channelId: channelId, context: context) {
+            return existing
+        }
+        
+        let new = NSEntityDescription.insertNewObject(forEntityName: Self.entityName, into: context) as! MemberDTO
+        new.id = Self.createId(userId: id, channeldId: channelId)
+        return new
+    }
+}
+
+extension NSManagedObjectContext {
+    func saveMember<ExtraUserData: Codable & Hashable>(
+        payload: MemberEndpointPayload<ExtraUserData>,
+        channelId: ChannelId
+    ) -> MemberDTO {
+        let dto = MemberDTO.loadOrCreate(id: payload.user.id, channelId: channelId, context: self)
+        
+        // Save user-part of member first
+        dto.user = saveUser(payload: payload.user)
+        
+        // Save member specific data
+        dto.channelRoleRaw = payload.roleRawValue
+        dto.memberCreatedDate = payload.created
+        dto.memberUpdatedDate = payload.updated
+        
+        return dto
+    }
+    
+    func loadMember<ExtraData: UserExtraData>(id: String, channelId: ChannelId) -> MemberModel<ExtraData>? {
+        guard let dto = MemberDTO.load(id: id, channelId: channelId, context: self) else { return nil }
+        return MemberModel.create(fromDTO: dto)
+    }
+}
+
+extension MemberModel {
+    static func create(fromDTO dto: MemberDTO) -> MemberModel {
+        let extraData = dto.user.extraData.flatMap { try? JSONDecoder.default.decode(ExtraData.self, from: $0) }
+        
+        return MemberModel(id: dto.user.id,
+                           isOnline: dto.user.isOnline,
+                           isBanned: dto.user.isBanned,
+                           userRole: UserRole(rawValue: dto.user.userRoleRaw)!,
+                           userCreatedDate: dto.user.userCreatedDate,
+                           userUpdatedDate: dto.user.userUpdatedDate,
+                           lastActiveDate: dto.user.lastActivityDate,
+                           extraData: extraData,
+                           channelRole: ChannelRole(rawValue: dto.channelRoleRaw)!,
+                           memberCreatedDate: dto.memberCreatedDate,
+                           memberUpdatedDate: dto.memberUpdatedDate,
+                           isInvited: false,
+                           inviteAcceptedDate: nil,
+                           inviteRejectedDate: nil)
+    }
+}
---
+++ StreamChatClient_v3/Database/DTOs/MemberModelDTO_Tests.swift
@@ -0,0 +1,70 @@
+//
+// MemberModelDTO_Tests.swift
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+@testable import StreamChatClient_v3
+import XCTest
+
+class MemberModelDTO_Tests: XCTestCase {
+    var database: DatabaseContainer!
+    
+    override func setUp() {
+        super.setUp()
+        database = try! DatabaseContainer(kind: .inMemory)
+    }
+    
+    func test_memberPayload_isStoredAndLoadedFromDB() {
+        let userId = UUID().uuidString
+        let channelId = ChannelId(type: .init(rawValue: "messsaging"), id: UUID().uuidString)
+        
+        let userPayload: UserEndpointPayload<NameAndAvatarUserData> = .init(id: userId,
+                                                                            created: .init(timeIntervalSince1970: 1000),
+                                                                            updated: .init(timeIntervalSince1970: 2000),
+                                                                            lastActiveDate: .init(timeIntervalSince1970: 3000),
+                                                                            isOnline: true,
+                                                                            isInvisible: true,
+                                                                            isBanned: true,
+                                                                            roleRawValue: "admin",
+                                                                            extraData: .init(name: "Luke",
+                                                                                             avatarURL: URL(string: UUID()
+                                                                                                 .uuidString)),
+                                                                            devices: [],
+                                                                            mutedUsers: [],
+                                                                            unreadChannelsCount: nil,
+                                                                            unreadMessagesCount: nil,
+                                                                            teams: [])
+        
+        let payload: MemberEndpointPayload<NameAndAvatarUserData> = .init(roleRawValue: "moderator",
+                                                                          created: .init(timeIntervalSince1970: 4000),
+                                                                          updated: .init(timeIntervalSince1970: 5000),
+                                                                          user: userPayload)
+        
+        // Asynchronously save the payload to the db
+        database.write { session in
+            session.saveMember(payload: payload, channelId: channelId)
+        }
+        
+        // Load the member from the db and check it's the same member
+        var loadedMember: MemberModel<NameAndAvatarUserData>? {
+            database.viewContext.loadMember(id: userId, channelId: channelId)
+        }
+        
+        AssertAsync {
+            Assert.willBeEqual(payload.user.id, loadedMember?.id)
+            Assert.willBeEqual(payload.user.isOnline, loadedMember?.isOnline)
+            Assert.willBeEqual(payload.user.isBanned, loadedMember?.isBanned)
+            Assert.willBeEqual(payload.user.roleRawValue, loadedMember?.userRole.rawValue)
+            Assert.willBeEqual(payload.user.created, loadedMember?.userCreatedDate)
+            Assert.willBeEqual(payload.user.updated, loadedMember?.userUpdatedDate)
+            Assert.willBeEqual(payload.user.lastActiveDate, loadedMember?.lastActiveDate)
+            Assert.willBeEqual(payload.user.extraData, loadedMember?.extraData)
+            Assert.willBeEqual(payload.roleRawValue, loadedMember?.channelRole.rawValue)
+            Assert.willBeEqual(payload.created, loadedMember?.memberCreatedDate)
+            Assert.willBeEqual(payload.updated, loadedMember?.memberUpdatedDate)
+//      Assert.willBeEqual(payload.user.isInvited, loadedMember?.isInvited)
+//      Assert.willBeEqual(payload.user.inviteAcceptedDate, loadedMember?.inviteAcceptedDate)
+//      Assert.willBeEqual(payload.user.inviteRejectedDate, loadedMember?.inviteRejectedDate)
+        }
+    }
+}
