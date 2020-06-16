6288ba6e4a5b83d2df3764acf6bb74a3df4a999c
Vojta Stavik <stavik@outlook.com>
2020-06-16 16:19:56 +0200

    Remove dummy implementation of `ChannelEventsHandler`
    Saving the channel data must happen in the middleware so we're sure the data is saved before the event is published.

--- StreamChatClient_v3/Workers/Background/ChannelEventsHandler.swift
+++ StreamChatClient_v3/Workers/Background/ChannelEventsHandler.swift
@@ -8,11 +8,13 @@ import CoreData
 /// Handles some of the incoming ChannelEvents and propagates related changes to the database.
 class ChannelEventsHandler<ExtraData: ExtraDataTypes>: EventHandlerWorker<ExtraData> {
     override func handleNewEvent(event: Event) {
-        if let event = event as? AddedToChannel<ExtraData> {
-            database.write { session in
-                session.saveChannel(event.channel)
-            }
-        }
+        // TODO: Saving the data must happen in the middleware so we're sure the data is saved before the event is published
+//    if let event = event as? AddedToChannel<ExtraData> {
+        
+//      database.write { session in
+//        session.saveChannel(payload: event.channelPayload)
+//      }
+//    }
         
         // TODO: more events
     }
