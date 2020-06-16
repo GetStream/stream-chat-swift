a96493f93840ec18aa647fcb7b6651dc024a0c30
Vojta Stavik <stavik@outlook.com>
2020-06-16 15:43:27 +0200

    Make it possible to serialize Channel endpoint payload

---
+++ StreamChatClient_v3/APIClient/Endpoints/ChannelEndpoints.swift
@@ -0,0 +1,305 @@
+//
+// ChannelEndpoints.swift
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+
+extension Endpoint {
+    static func channels<ExtraData: ExtraDataTypes>(query: ChannelListQuery)
+        -> Endpoint<ChannelListEndpointPayload<ExtraData>> {
+        .init(path: "channels",
+              method: .get,
+              queryItems: [],
+              jsonQueryItems: ["payload": query],
+              body: nil)
+    }
+}
+
+struct ChannelListEndpointPayload<ExtraData: ExtraDataTypes>: Decodable {
+    /// A list of channels response (see `ChannelQuery`).
+    let channels: [ChannelEndpointPayload<ExtraData>]
+}
+
+struct ChannelEndpointPayload<ExtraData: ExtraDataTypes>: Decodable {
+    let channel: ChannelDetailPayload<ExtraData>
+    
+    let watcherCount: Int
+    
+    let members: [MemberEndpointPayload<ExtraData.User>]
+    
+    // TODO:
+    /*
+     messages
+     message reads
+     
+     */
+    private enum CodingKeys: String, CodingKey {
+        case channel
+//    case messages
+//    case messageReads = "read"
+        case members
+//    case watchers
+        case watcherCount = "watcher_count"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        channel = try container.decode(ChannelDetailPayload<ExtraData>.self, forKey: .channel)
+        watcherCount = try container.decode(Int.self, forKey: .watcherCount)
+        members = try container.decode([MemberEndpointPayload<ExtraData.User>].self, forKey: .members)
+    }
+    
+    // MARK: - For testing
+    
+    init(channel: ChannelDetailPayload<ExtraData>, watcherCount: Int, members: [MemberEndpointPayload<ExtraData.User>]) {
+        self.channel = channel
+        self.watcherCount = watcherCount
+        self.members = members
+    }
+}
+
+struct ChannelDetailPayload<ExtraData: ExtraDataTypes>: Decodable {
+    let cid: String
+    
+    let extraData: ExtraData.Channel?
+    
+    /// A channel type.
+    public let typeRawValue: String
+    /// A channel id.
+    public let id: String
+    
+    /// The last message date.
+    public let lastMessageDate: Date?
+    /// A channel created date.
+    public let created: Date
+    /// A channel deleted date.
+    public let deleted: Date?
+    
+    /// A channel updated date.
+    public let updated: Date
+    
+    /// A creator of the channel.
+    public let createdBy: UserEndpointPayload<ExtraData.User>?
+    /// A config.
+    public let config: ChannelConfig
+    /// Checks if the channel is frozen.
+    public let isFrozen: Bool
+    
+    let memberCount: Int
+    
+    /// A list of users to invite in the channel.
+    let invitedMembers: [MemberEndpointPayload<ExtraData.User>] = [] // TODO?
+    
+    /// The team the channel belongs to. You need to enable multi-tenancy if you want to use this, else it'll be nil.
+    /// Refer to [docs](https://getstream.io/chat/docs/multi_tenant_chat/?language=swift) for more info.
+    public let team: String
+    
+    /// Coding keys for the decoding.
+    public enum DecodingKeys: String, CodingKey {
+        /// An channel id.
+        case id
+        /// A combination of channel id and type.
+        case cid
+        /// A type.
+        case typeRawValue = "type"
+        /// A channel name.
+        case name
+        /// An image URL.
+        case imageURL = "image"
+        /// A last message date.
+        case lastMessageDate = "last_message_at"
+        /// A user created by.
+        case createdBy = "created_by"
+        /// A created date.
+        case created = "created_at"
+        /// A created date.
+        case updated = "updated_at"
+        
+        /// A deleted date.
+        case deleted = "deleted_at"
+        /// A channel config.
+        case config
+        /// A frozen flag.
+        case frozen
+        /// Members.
+        case members
+        /// The team the channel belongs to.
+        case team
+        
+        case memberCount = "member_count"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: DecodingKeys.self)
+        typeRawValue = try container.decode(String.self, forKey: .typeRawValue)
+        id = try container.decode(String.self, forKey: .id)
+        cid = try container.decode(String.self, forKey: .cid)
+        let config = try container.decode(ChannelConfig.self, forKey: .config)
+        self.config = config
+        created = try container.decodeIfPresent(Date.self, forKey: .created) ?? config.created
+        deleted = try container.decodeIfPresent(Date.self, forKey: .deleted)
+        createdBy = try container.decodeIfPresent(UserEndpointPayload<ExtraData.User>.self, forKey: .createdBy)
+        lastMessageDate = try container.decodeIfPresent(Date.self, forKey: .lastMessageDate)
+        isFrozen = try container.decode(Bool.self, forKey: .frozen)
+        team = try container.decodeIfPresent(String.self, forKey: .team) ?? ""
+        extraData = try? ExtraData.Channel(from: decoder)
+        memberCount = try container.decode(Int.self, forKey: .memberCount)
+        updated = try container.decode(Date.self, forKey: .updated)
+    }
+    
+    // MARK: - For testing
+    
+    internal init(
+        id: String,
+        cid: String,
+        extraData: ExtraData.Channel?,
+        typeRawValue: String,
+        lastMessageDate: Date?,
+        created: Date,
+        deleted: Date?,
+        updated: Date,
+        createdBy: UserEndpointPayload<ExtraData.User>?,
+        config: ChannelConfig,
+        isFrozen: Bool,
+        memberCount: Int,
+        team: String
+    ) {
+        self.cid = cid
+        self.extraData = extraData
+        self.typeRawValue = typeRawValue
+        self.id = id
+        self.lastMessageDate = lastMessageDate
+        self.created = created
+        self.deleted = deleted
+        self.updated = updated
+        self.createdBy = createdBy
+        self.config = config
+        self.isFrozen = isFrozen
+        self.memberCount = memberCount
+        self.team = team
+    }
+}
+
+/// A channel config.
+public struct ChannelConfig: Codable {
+    // swiftlint:disable:next nesting
+    private enum CodingKeys: String, CodingKey {
+        case reactionsEnabled = "reactions"
+        case typingEventsEnabled = "typing_events"
+        case readEventsEnabled = "read_events"
+        case connectEventsEnabled = "connect_events"
+        case uploadsEnabled = "uploads"
+        case repliesEnabled = "replies"
+        case searchEnabled = "search"
+        case mutesEnabled = "mutes"
+        case urlEnrichmentEnabled = "url_enrichment"
+        case messageRetention = "message_retention"
+        case maxMessageLength = "max_message_length"
+        case commands
+        case created = "created_at"
+        case updated = "updated_at"
+    }
+    
+    /// If users are allowed to add reactions to messages. Enabled by default.
+    public let reactionsEnabled: Bool
+    /// Controls if typing indicators are shown. Enabled by default.
+    public let typingEventsEnabled: Bool
+    /// Controls whether the chat shows how far you’ve read. Enabled by default.
+    public let readEventsEnabled: Bool
+    /// Determines if events are fired for connecting and disconnecting to a chat. Enabled by default.
+    public let connectEventsEnabled: Bool
+    /// Enables uploads.
+    public let uploadsEnabled: Bool
+    /// Enables message threads and replies. Enabled by default.
+    public let repliesEnabled: Bool
+    /// Controls if messages should be searchable (this is a premium feature). Disabled by default.
+    public let searchEnabled: Bool
+    /// Determines if users are able to mute other users. Enabled by default.
+    public let mutesEnabled: Bool
+    /// Determines if URL enrichment enabled to show they as attachments. Enabled by default.
+    public let urlEnrichmentEnabled: Bool
+    /// A number of days or infinite. Infinite by default.
+    public let messageRetention: String
+    /// The max message length. 5000 by default.
+    public let maxMessageLength: Int
+    /// An array of commands, e.g. /giphy.
+    public let commands: [Command]?
+    /// A channel created date.
+    public let created: Date
+    /// A channel updated date.
+    public let updated: Date
+    
+    /// Determines if users are able to flag messages. Enabled by default.
+    public var flagsEnabled: Bool { commands?.map { $0.name }.contains("flag") ?? false }
+    
+    // TODO: Do we need custom decoding here?
+    
+//    public init(from decoder: Decoder) throws {
+//      let container = try decoder.container(keyedBy: CodingKeys.self)
+//      reactionsEnabled = try container.decode(Bool.self, forKey: .reactionsEnabled)
+//      typingEventsEnabled = try container.decode(Bool.self, forKey: .typingEventsEnabled)
+//      readEventsEnabled = try container.decode(Bool.self, forKey: .readEventsEnabled)
+//      connectEventsEnabled = try container.decode(Bool.self, forKey: .connectEventsEnabled)
+//      uploadsEnabled = try container.decodeIfPresent(Bool.self, forKey: .uploadsEnabled) ?? false
+//      self.repliesEnabled = try container.decode(Bool.self, forKey: .repliesEnabled)
+//      self.searchEnabled = try container.decode(Bool.self, forKey: .searchEnabled)
+//      self.mutesEnabled = try container.decode(Bool.self, forKey: .mutesEnabled)
+//      self.urlEnrichmentEnabled = try container.decode(Bool.self, forKey: .urlEnrichmentEnabled)
+//      self.messageRetention = try container.decode(String.self, forKey: .messageRetention)
+//      self.maxMessageLength = try container.decode(Int.self, forKey: .maxMessageLength)
+//      self.commands = try container.decodeIfPresent([Command].self, forKey: .commands) ?? []
+//      self.created = try container.decode(Date.self, forKey: .created)
+//      self.updated = try container.decode(Date.self, forKey: .updated)
+//    }
+    
+    internal init(
+        reactionsEnabled: Bool = false,
+        typingEventsEnabled: Bool = false,
+        readEventsEnabled: Bool = false,
+        connectEventsEnabled: Bool = false,
+        uploadsEnabled: Bool = false,
+        repliesEnabled: Bool = false,
+        searchEnabled: Bool = false,
+        mutesEnabled: Bool = false,
+        urlEnrichmentEnabled: Bool = false,
+        messageRetention: String = "",
+        maxMessageLength: Int = 0,
+        commands: [Command] = [],
+        created: Date = .init(),
+        updated: Date = .init()
+    ) {
+        self.reactionsEnabled = reactionsEnabled
+        self.typingEventsEnabled = typingEventsEnabled
+        self.readEventsEnabled = readEventsEnabled
+        self.connectEventsEnabled = connectEventsEnabled
+        self.uploadsEnabled = uploadsEnabled
+        self.repliesEnabled = repliesEnabled
+        self.searchEnabled = searchEnabled
+        self.mutesEnabled = mutesEnabled
+        self.urlEnrichmentEnabled = urlEnrichmentEnabled
+        self.messageRetention = messageRetention
+        self.maxMessageLength = maxMessageLength
+        self.commands = commands
+        self.created = created
+        self.updated = updated
+    }
+}
+
+/// A command in a message, e.g. /giphy.
+public struct Command: Codable, Hashable {
+    /// A command name.
+    public let name: String
+    /// A description.
+    public let description: String
+    public let set: String
+    /// Args for the command.
+    public let args: String
+    
+    public init(name: String = "", description: String = "", set: String = "", args: String = "") {
+        self.name = name
+        self.description = description
+        self.set = set
+        self.args = args
+    }
+}
---
+++ StreamChatClient_v3/APIClient/Endpoints/ChannelEndpoints_tests.swift
@@ -0,0 +1,65 @@
+//
+// ChannelEndpoints_tests.swift
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+@testable import StreamChatClient_v3
+import XCTest
+
+class ChannelListPayload_Tests: XCTestCase {
+    let channelJSON: Data = {
+        let url = Bundle(for: ChannelListPayload_Tests.self).url(forResource: "ChannelsQuery", withExtension: "json")!
+        return try! Data(contentsOf: url)
+    }()
+    
+    func test_channelQueryJSON_isSerialized_withDefaultExtraData() throws {
+        let payload = try JSONDecoder.default.decode(ChannelListEndpointPayload<DefaultDataTypes>.self, from: channelJSON)
+        XCTAssertEqual(payload.channels.count, 20)
+    }
+}
+
+class ChannelEndpointPayloadTests: XCTestCase {
+    let channelJSON: Data = {
+        let url = Bundle(for: ChannelListPayload_Tests.self).url(forResource: "Channel", withExtension: "json")!
+        return try! Data(contentsOf: url)
+    }()
+    
+    func test_channelJSON_isSerialized_withDefaultExtraData() throws {
+        let payload = try JSONDecoder.default.decode(ChannelEndpointPayload<DefaultDataTypes>.self, from: channelJSON)
+        
+        XCTAssertEqual(payload.watcherCount, 7)
+        XCTAssertEqual(payload.members.count, 4)
+        
+        let channel = payload.channel
+        XCTAssertEqual(channel.id, "general")
+        XCTAssertEqual(channel.cid, "messaging:general")
+        XCTAssertEqual(channel.created, "2019-05-10T14:03:49.505006Z".toDate())
+        XCTAssertNotNil(channel.createdBy)
+        XCTAssertEqual(channel.typeRawValue, "messaging")
+        XCTAssertEqual(channel.isFrozen, true)
+        XCTAssertEqual(channel.memberCount, 4)
+        XCTAssertEqual(channel.updated, "2019-05-10T14:03:49.505006Z".toDate())
+        
+        XCTAssertEqual(channel.extraData?.name, "The water cooler")
+        XCTAssertEqual(channel.extraData?.imageURL?.absoluteString,
+                       "https://images.unsplash.com/photo-1512138664757-360e0aad5132?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2851&q=80")
+        
+        let config = channel.config
+        XCTAssertEqual(config.reactionsEnabled, true)
+        XCTAssertEqual(config.typingEventsEnabled, true)
+        XCTAssertEqual(config.readEventsEnabled, true)
+        XCTAssertEqual(config.connectEventsEnabled, true)
+        XCTAssertEqual(config.uploadsEnabled, true)
+        XCTAssertEqual(config.repliesEnabled, true)
+        XCTAssertEqual(config.searchEnabled, true)
+        XCTAssertEqual(config.mutesEnabled, true)
+        XCTAssertEqual(config.urlEnrichmentEnabled, true)
+        XCTAssertEqual(config.flagsEnabled, false)
+        XCTAssertEqual(config.messageRetention, "infinite")
+        XCTAssertEqual(config.maxMessageLength, 5000)
+        XCTAssertEqual(config.commands,
+                       [.init(name: "giphy", description: "Post a random gif to the channel", set: "fun_set", args: "[text]")])
+        XCTAssertEqual(config.created, "2019-03-21T15:49:15.40182Z".toDate())
+        XCTAssertEqual(config.updated, "2020-03-17T18:54:09.460881Z".toDate())
+    }
+}
