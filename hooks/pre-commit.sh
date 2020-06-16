bf0d1bb2bdf66d63f0fd768a1f00e81079930c8d
Alexey Bukhtin <bukhtin@gmail.com>
2020-06-16 13:59:00 +0200

    Removed SwifttFormatter pre-commit hook.

--- hooks/pre-commit.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-git diff --diff-filter=d --staged --name-only -- StreamChatClient_v3 -- TestResources_v3 | grep -e '\.swift$' | while read file; do
-  swiftformat "${file}";
-  git add "$file";
-done
