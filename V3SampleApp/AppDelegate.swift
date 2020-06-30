216844b6d7b7b3ea59afd8750f1c457baa5f7ae5
Vojta Stavik <stavik@outlook.com>
2020-06-30 11:12:28 +0200

    Make it possible to build v3 sample app for iOS11

--- V3SampleApp/AppDelegate.swift
+++ V3SampleApp/AppDelegate.swift
@@ -20,6 +20,8 @@ let chatClient: ChatClient = {
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         LogConfig.formatters = [PrefixLogFormatter(prefixes: [.info: "𝒊", .debug: "🛠", .warning: "⚠️", .error: "🚨"])]
@@ -31,18 +33,26 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         LogConfig.level = .info
         
         chatClient.webSocketClient.connect()
+
+        if #available(iOS 11, *) {
+            self.window = UIWindow()
+            window?.rootViewController = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController()
+            window?.makeKeyAndVisible()
+        }
         
         return true
     }
 
     // MARK: UISceneSession Lifecycle
 
+    @available(iOS 13.0, *)
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
+    @available(iOS 13.0, *)
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
