19ce263cda90079610085d7d6353d38bd609768c
Vojta Stavik <stavik@outlook.com>
2020-07-08 14:00:24 +0200

    Make `HealthCheck` event conform to `EventWithPayload`
    We need the information about the current user from it

--- StreamChatClient_v3/WebSocketClient/Events/ConnectionEvents.swift
+++ StreamChatClient_v3/WebSocketClient/Events/ConnectionEvents.swift
@@ -8,9 +8,10 @@ public protocol ConnectionEvent: Event {
     var connectionId: String { get }
 }
 
-public struct HealthCheck: ConnectionEvent {
+public struct HealthCheck: ConnectionEvent, EventWithPayload {
     public static var eventRawType: String { "health.check" }
     public let connectionId: String
+    var payload: Any
     
     init?<ExtraData: ExtraDataTypes>(from eventResponse: EventPayload<ExtraData>) throws {
         guard eventResponse.eventType == Self.eventRawType else { return nil }
@@ -18,5 +19,15 @@ public struct HealthCheck: ConnectionEvent {
             throw ClientError.EventDecoding(missingValue: "connectionId", eventType: "HealthCheck")
         }
         self.connectionId = connectionId
+        payload = eventResponse as Any
+    }
+    
+    init(connectionId: String) {
+        self.connectionId = connectionId
+        payload = EventPayload<DefaultDataTypes>.init(eventType: Self.eventRawType,
+                                                      connectionId: connectionId,
+                                                      channel: nil,
+                                                      currentUser: nil,
+                                                      cid: nil)
     }
 }
