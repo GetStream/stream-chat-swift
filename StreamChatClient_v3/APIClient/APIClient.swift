755f534677950fbef5a03908b9c2cef0f8a6f4a4
Vojta Stavik <stavik@outlook.com>
2020-07-08 13:56:10 +0200

    Simplify `APIClient` to use only request encoder + decoder

--- StreamChatClient_v3/APIClient/APIClient.swift
+++ StreamChatClient_v3/APIClient/APIClient.swift
@@ -6,56 +6,27 @@ import Foundation
 
 /// An object allowing making request to Stream Chat servers.
 class APIClient {
-    /// An object encapsulating all dependencies of `APIClient`.
-    struct Environment {
-        var requestEncoderBuilder: (_ baseURL: URL, _ apiKey: APIKey) -> RequestEncoder = DefaultRequestEncoder.init
-        var requestDecoderBuilder: () -> RequestDecoder = DefaultRequestDecoder.init
-    }
-    
-    /// The base URL for all requests.
-    let baseURL: URL
-    
-    /// The app specific API key.
-    let apiKey: APIKey
-    
     /// The URL session used for all requests.
-    let session: URLSession
+    var session: URLSession
     
     /// `APIClient` uses this object to encode `Endpoint` objects into `URLRequest`s.
-    private(set) lazy var encoder: RequestEncoder = {
-        var encoder = self.environment.requestEncoderBuilder(baseURL, apiKey)
-        encoder.connectionIdProviderDelegate = self
-        return encoder
-    }()
+    let encoder: RequestEncoder
     
     /// `APIClient` uses this object to decode the results of network requests.
-    private(set) lazy var decoder: RequestDecoder = self.environment.requestDecoderBuilder()
-    
-    private let environment: Environment
-    
-    /// The current connection id
-    @Atomic private var connectionId: String?
-    
-    /// An array of requests waiting for the connection id
-    @Atomic private var connectionIdWaiters: [(String?) -> Void] = []
+    let decoder: RequestDecoder
     
     /// Creates a new `APIClient`.
     ///
     /// - Parameters:
-    ///   - apiKey: The app specific API key.
-    ///   - baseURL: The base URL used for all outgoing requests.
     ///   - sessionConfiguration: The session configuration `APIClient` uses to create its `URLSession`.
-    ///   - environment: An object specifying `APIClient` dependencies.
-    init(apiKey: APIKey, baseURL: URL, sessionConfiguration: URLSessionConfiguration, environment: Environment = .init()) {
-        self.apiKey = apiKey
-        self.baseURL = baseURL
+    ///   - requestEncoder: `APIClient` uses this object to encode `Endpoint` objects into `URLRequest`s.
+    ///   - requestDecoder: `APIClient` uses this object to decode the results of network requests.
+    init(sessionConfiguration: URLSessionConfiguration,
+         requestEncoder: RequestEncoder,
+         requestDecoder: RequestDecoder) {
+        encoder = requestEncoder
+        decoder = requestDecoder
         session = URLSession(configuration: sessionConfiguration)
-        self.environment = environment
-    }
-    
-    deinit {
-        connectionIdWaiters.forEach { $0(nil) }
-        connectionIdWaiters.removeAll()
     }
     
     /// Performs a network request.
@@ -85,31 +56,6 @@ class APIClient {
             }
             
             task.resume()
-        }
-    }
-}
-
-/// `APIClient` listens for `WebSocketClient` connection updates so it can forward the current connection id to
-/// its `RequestEncoder`.
-extension APIClient: ConnectionStateDelegate {
-    func webSocketClient(_ client: WebSocketClient, didUpdateConectionState state: ConnectionState) {
-        if case let .connected(connectionId) = state {
-            self.connectionId = connectionId
-            connectionIdWaiters.forEach { $0(connectionId) }
-            connectionIdWaiters.removeAll()
-        } else {
-            connectionId = nil
-        }
-    }
-}
-
-/// `APIClient` provides connection id to the `RequestEncoder` it uses.
-extension APIClient: ConnectionIdProviderDelegate {
-    func provideConnectionId(completion: @escaping (String?) -> Void) {
-        if let connectionId = connectionId {
-            completion(connectionId)
-        } else {
-            connectionIdWaiters.append(completion)
         }
     }
 }
