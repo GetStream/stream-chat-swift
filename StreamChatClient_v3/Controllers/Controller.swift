fbe0e0cf2bee118ac71fc1d62f0b368202b7ce38
Vojta Stavik <stavik@outlook.com>
2020-07-17 17:37:08 +0200

    Add `state` to `Controller`
    This allows us to find out, whether `startUpdating` has been called already.

--- StreamChatClient_v3/Controllers/Controller.swift
+++ StreamChatClient_v3/Controllers/Controller.swift
@@ -6,6 +6,18 @@ import Foundation
 
 /// The base class for all controllers. Not meant to be used directly.
 public class Controller {
+    /// Describes the possible states of Controller
+    public enum State: Equatable {
+        /// The controller is idle. Call `startUpdating` to start listening for changes.
+        case idle
+        
+        /// The controller is active and is listening to changes.
+        case active
+    }
+    
+    /// The current state of the Controller.
+    public internal(set) var state: State = .idle
+    
     /// The queue which is used to perform callback calls. The default value is `.main`.
     public var callbackQueue: DispatchQueue = .main
 }
