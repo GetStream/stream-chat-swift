05c15f198a7022e98ac8707a81ef13141ef60901
Vojta Stavik <stavik@outlook.com>
2020-07-24 09:50:37 +0200

    Introduce `QueueAwareDelegate`
    It's a delegate superclass making it simple to verify the delegate was called on the correct queue.

---
+++ TestResources_v3/QueueAwareDelegate.swift
@@ -0,0 +1,37 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import XCTest
+
+/// A delegate superclass which can verify a certain method was called on the given queue.
+///
+/// Example usage:
+/// ```
+/// class MyDelegate: QueueAwareDelegate {
+///   func controllerWillStartFetchingRemoteData(_ controller: Controller) {
+///       validateQueue()
+///   }
+/// }
+/// ```
+class QueueAwareDelegate {
+    // If set, checks the delegate was called on the correct queue
+    var expectedQueueId: UUID?
+    
+    let file: StaticString
+    let line: UInt
+    
+    init(file: StaticString = #file, line: UInt = #line) {
+        self.file = file
+        self.line = line
+    }
+    
+    func validateQueue(function: StaticString = #function) {
+        if let queueId = expectedQueueId {
+            XCTAssertTrue(DispatchQueue.isTestQueue(withId: queueId),
+                          "Delegate method \(function) called on an incorrect queue",
+                          file: file,
+                          line: line)
+        }
+    }
+}
