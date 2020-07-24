14484a8be1a610c916471994ce109bc6df926258
Vojta Stavik <stavik@outlook.com>
2020-07-24 09:50:44 +0200

    Remove `NSObject` superclass from `Controller`

--- StreamChatClient_v3/Controllers/Controller.swift
+++ StreamChatClient_v3/Controllers/Controller.swift
@@ -5,7 +5,7 @@
 import Foundation
 
 /// The base class for all controllers. Not meant to be used directly.
-public class Controller: NSObject { // TODO: remove NSObject
+public class Controller {
     /// The queue which is used to perform callback calls. The default value is `.main`.
     public var callbackQueue: DispatchQueue = .main
 }
