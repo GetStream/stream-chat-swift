e1a773221176991d28b903817268376c08f133ce
Agustín Prats <aprats@gmail.com>
2020-07-24 11:56:36 +0200

    backport from v3 to v2 MockNetworkURLProtocol and RequestRecorderURLProtocol files

--- Tests/Client/RequestRecorderURLProtocol.swift
+++ Tests/Client/RequestRecorderURLProtocol.swift
@@ -1,8 +1,5 @@
 //
-//  RequestRecorderURLProtocol.swift
-//  StreamChatClientTests
-//
-//  Copyright © 2020 Stream.io Inc. All rights reserved.
+// Copyright © 2020 Stream.io Inc. All rights reserved.
 //
 
 import Foundation
@@ -11,12 +8,24 @@ import XCTest
 /// This URLProtocol subclass allows to intercept the network communication
 /// and provides the latest network request made.
 class RequestRecorderURLProtocol: URLProtocol {
-
-    static let testSessionHeaderKey = "test_session_id"
+    static let testSessionHeaderKey = "RequestRecorderURLProtocol_test_session_id"
+    
+    /// Starts a new recording session. Adds a unique identifier to the configuration headers and listens only
+    /// for the request with this id.
+    static func startTestSession(with configuration: inout URLSessionConfiguration) {
+        reset()
+        let newSessionId = UUID().uuidString
+        currentSessionId = newSessionId
+        
+        configuration.protocolClasses?.insert(RequestRecorderURLProtocol.self, at: 0)
+        var existingHeaders = configuration.httpAdditionalHeaders ?? [:]
+        existingHeaders[RequestRecorderURLProtocol.testSessionHeaderKey] = newSessionId
+        configuration.httpAdditionalHeaders = existingHeaders
+    }
     
     private static var latestRequestExpectation: XCTestExpectation?
     private static var latestRequest: URLRequest?
-
+    
     /// Returns the latest network request this URLProtocol recorded.
     ///
     /// If no request has been made since the last time this function was invoked, it synchronously
@@ -28,15 +37,16 @@ class RequestRecorderURLProtocol: URLProtocol {
             // Delete the used request
             latestRequest = nil
         }
-
+        
         guard latestRequest == nil else { return latestRequest }
-
+        
         latestRequestExpectation = .init(description: "Wait for incoming request.")
         _ = XCTWaiter.wait(for: [latestRequestExpectation!], timeout: timeout)
         return latestRequest
     }
-
-    /// If set, records only requests with `testSessionHeaderKey` header value set to this value. If `nil`, records all requests.
+    
+    /// If set, records only requests with `testSessionHeaderKey` header value set to this value. If `nil`,
+    /// no requests are recorded.
     static var currentSessionId: String?
     
     /// Cleans up existing waiters and recorded requests. We have to explictly reset the state because URLProtocols
@@ -46,36 +56,32 @@ class RequestRecorderURLProtocol: URLProtocol {
         latestRequest = nil
         latestRequestExpectation = nil
     }
-
+    
     override class func canInit(with request: URLRequest) -> Bool {
-        guard let sessionId = currentSessionId else {
-            // No sessionId set, record all requests
-            record(request: request)
-            return false
-        }
+        guard let sessionId = currentSessionId else { return false }
         
         if sessionId == request.value(forHTTPHeaderField: testSessionHeaderKey) {
             record(request: request)
         }
         return false
     }
-
+    
     override class func canonicalRequest(for request: URLRequest) -> URLRequest {
         // Overriding this function is required by the superclass.
-        return request
+        request
     }
-
+    
     private static func record(request: URLRequest) {
         latestRequest = request
         latestRequestExpectation?.fulfill()
     }
     
     // MARK: Instance methods
-
+    
     override func startLoading() {
         // Required by the superclass.
     }
-
+    
     override func stopLoading() {
         // Required by the superclass.
     }
