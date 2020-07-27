16bb94dc44c1aa02e905fb9b19bcf73882fb9486
Agustín Prats <aprats@gmail.com>
2020-07-27 11:20:39 +0200

    Init Client Device tests URL mocking as in v3
    MockNetworkURLProtocol v2 implementation has been replaced with v3
    so this commit applies the same initialization in tests as it's done
    in v3

--- Tests/Client/Unit Tests/ClientTestCase.swift
+++ Tests/Client/Unit Tests/ClientTestCase.swift
@@ -17,22 +17,25 @@ class ClientTestCase: XCTestCase {
     
     var client: Client!
     var testSessionId: String!
+    var sessionConfiguration: URLSessionConfiguration!
     var testUser: User!
     let testToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiYnJva2VuLXdhdGVyZmFsbC01In0.d1xKTlD_D0G-VsBoDBNbaLjO-2XWNA8rlTm4ru4sMHg"
     
     override func setUp() {
         super.setUp()
-        let sessionConfig = URLSessionConfiguration.default
-        sessionConfig.protocolClasses?.insert(RequestRecorderURLProtocol.self, at: 0)
-        sessionConfig.protocolClasses?.insert(MockNetworkURLProtocol.self, at: 1)
+        // Prepare URL Session
+        sessionConfiguration = URLSessionConfiguration.default
+        RequestRecorderURLProtocol.startTestSession(with: &sessionConfiguration)
+        MockNetworkURLProtocol.startTestSession(with: &sessionConfiguration)
         
+        // Some random value to ensure the headers are respected
         testSessionId = UUID().uuidString
-        sessionConfig.httpAdditionalHeaders = [RequestRecorderURLProtocol.testSessionHeaderKey: testSessionId!]
+        sessionConfiguration.httpAdditionalHeaders = [RequestRecorderURLProtocol.testSessionHeaderKey: testSessionId!]
         
         let clientConfig = Client.Config(apiKey: "test_api_key")
         // We can create a new `Client` instance because we don't use `Client.shared` in tests.
         client = Client(config: clientConfig,
-                        defaultURLSessionConfiguration: sessionConfig,
+                        defaultURLSessionConfiguration: sessionConfiguration,
                         defaultWebSocketProviderType: WebSocketProviderMock.self)
         
         testUser = User(id: "broken-waterfall-5")
