ab442e495593aafcabda2afb17753a83114418be
Vojta Stavik <stavik@outlook.com>
2020-07-29 16:33:09 +0200

    Add `isStressTest` flag to `TestRunnerEnvironment`
    Because we want to run each test multiple times when running stress test suite.

--- TestResources_v3/TestRunnerEnvironment.swift
+++ TestResources_v3/TestRunnerEnvironment.swift
@@ -10,4 +10,10 @@ struct TestRunnerEnvironment {
     static var isCI: Bool {
         ProcessInfo.processInfo.environment["CI"] == "TRUE"
     }
+    
+    /// `true` if the current tests are stress test. We get this information by checking for the custom `STRESS_TESTS` environment
+    /// variable passed in to the process.
+    static var isStressTest: Bool {
+        ProcessInfo.processInfo.environment["STRESS_TESTS"] == "TRUE"
+    }
 }
