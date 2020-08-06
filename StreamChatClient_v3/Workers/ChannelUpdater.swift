e28de029afaea7880e561162b4bd1336867eda21
Pavel Lopatine <mantergo@gmail.com>
2020-08-06 17:17:57 +0300

    Add usage of `error` `Result` property

--- StreamChatClient_v3/Workers/ChannelUpdater.swift
+++ StreamChatClient_v3/Workers/ChannelUpdater.swift
@@ -33,10 +33,7 @@ class ChannelUpdater<ExtraData: ExtraDataTypes>: Worker {
     ///   - completion: Called when the API call is finished. Called with `Error` if the remote update fails.
     func muteChannel(cid: ChannelId, mute: Bool, completion: ((Error?) -> Void)? = nil) {
         apiClient.request(endpoint: .muteChannel(cid: cid, mute: mute)) {
-            switch $0 {
-            case .success: completion?(nil)
-            case let .failure(error): completion?(error)
-            }
+            completion?($0.error)
         }
     }
 
@@ -46,10 +43,7 @@ class ChannelUpdater<ExtraData: ExtraDataTypes>: Worker {
     ///   - completion: Called when the API call is finished. Called with `Error` if the remote update fails.
     func deleteChannel(cid: ChannelId, completion: ((Error?) -> Void)? = nil) {
         apiClient.request(endpoint: .deleteChannel(cid: cid)) {
-            switch $0 {
-            case .success: completion?(nil)
-            case let .failure(error): completion?(error)
-            }
+            completion?($0.error)
         }
     }
 
@@ -61,10 +55,7 @@ class ChannelUpdater<ExtraData: ExtraDataTypes>: Worker {
     ///   - completion: Called when the API call is finished. Called with `Error` if the remote update fails.
     func hideChannel(cid: ChannelId, userId: UserId, clearHistory: Bool, completion: ((Error?) -> Void)? = nil) {
         apiClient.request(endpoint: .hideChannel(cid: cid, userId: userId, clearHistory: clearHistory)) {
-            switch $0 {
-            case .success: completion?(nil)
-            case let .failure(error): completion?(error)
-            }
+            completion?($0.error)
         }
     }
 
@@ -75,10 +66,7 @@ class ChannelUpdater<ExtraData: ExtraDataTypes>: Worker {
     ///   - completion: Called when the API call is finished. Called with `Error` if the remote update fails.
     func showChannel(cid: ChannelId, userId: UserId, completion: ((Error?) -> Void)? = nil) {
         apiClient.request(endpoint: .showChannel(cid: cid, userId: userId)) {
-            switch $0 {
-            case .success: completion?(nil)
-            case let .failure(error): completion?(error)
-            }
+            completion?($0.error)
         }
     }
 }
