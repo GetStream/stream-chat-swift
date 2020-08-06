5c04c2850fadee8385750246f21d3a1d9c303dbc
Pavel Evsaev <pavel.evsaev@gmail.com>
2020-08-06 14:33:12 +0300

    Add tests for `channel` and `channels` endpoints

--- StreamChatClient_v3/APIClient/Endpoints/ChannelEndpoints_Tests.swift
+++ StreamChatClient_v3/APIClient/Endpoints/ChannelEndpoints_Tests.swift
@@ -5,4 +5,57 @@
 @testable import StreamChatClient_v3
 import XCTest
 
-final class ChannelEndpoints_Tests: XCTestCase {}
+final class ChannelEndpoints_Tests: XCTestCase {
+    func test_channels_buildsCorrectly() {
+        let filter = Filter.in("member", ["Luke"])
+
+        let testCases: [(ChannelListQuery, Bool)] = [
+            (.init(filter: filter, options: .state), true),
+            (.init(filter: filter, options: .presence), true),
+            (.init(filter: filter, options: .watch), true),
+            (.init(filter: filter, options: .all), true),
+            (.init(filter: filter, options: []), false)
+        ]
+
+        for (query, requiresConnectionId) in testCases {
+            let expectedEndpoint = Endpoint<ChannelListPayload<DefaultDataTypes>>(path: "channels",
+                                                                                  method: .get,
+                                                                                  queryItems: nil,
+                                                                                  requiresConnectionId: requiresConnectionId,
+                                                                                  body: ["payload": query])
+
+            // Build endpoint
+            let endpoint: Endpoint<ChannelListPayload<DefaultDataTypes>> = .channels(query: query)
+
+            // Assert endpoint is built correctly
+            XCTAssertEqual(AnyEndpoint(expectedEndpoint), AnyEndpoint(endpoint))
+        }
+    }
+
+    func test_channel_buildsCorrectly() {
+        let channelID = ChannelId(type: .livestream, id: "qwerty")
+
+        let testCases: [(ChannelQuery<DefaultDataTypes>, Bool)] = [
+            (.init(channelId: channelID, options: .state), true),
+            (.init(channelId: channelID, options: .presence), true),
+            (.init(channelId: channelID, options: .watch), true),
+            (.init(channelId: channelID, options: .all), true),
+            (.init(channelId: channelID, options: []), false)
+        ]
+
+        for (query, requiresConnectionId) in testCases {
+            let expectedEndpoint =
+                Endpoint<ChannelPayload<DefaultDataTypes>>(path: "channels/\(query.cid.type.rawValue)/\(query.cid.id)/query",
+                                                           method: .post,
+                                                           queryItems: nil,
+                                                           requiresConnectionId: requiresConnectionId,
+                                                           body: query)
+
+            // Build endpoint
+            let endpoint: Endpoint<ChannelPayload<DefaultDataTypes>> = .channel(query: query)
+
+            // Assert endpoint is built correctly
+            XCTAssertEqual(AnyEndpoint(expectedEndpoint), AnyEndpoint(endpoint))
+        }
+    }
+}
