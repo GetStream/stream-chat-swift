ff61af473b57d2773994fe2d6583cf444c3fba09
Pavel Evsaev <pavel.evsaev@gmail.com>
2020-08-07 15:06:10 +0300

    Add tests to `ChannelUpdater`

--- StreamChatClient_v3/Workers/ChannelUpdater_Tests.swift
+++ StreamChatClient_v3/Workers/ChannelUpdater_Tests.swift
@@ -70,4 +70,184 @@ class ChannelUpdater_Tests: StressTestCase {
         // Assert the completion is called with the error
         AssertAsync.willBeEqual(completionCalledError as? TestError, error)
     }
+
+    // MARK: - Mute channel
+
+    func test_muteChannel_makesCorrectAPICall() {
+        let channelID = ChannelId.unique
+        let mute = true
+
+        // Simulate `muteChannel(cid:, mute:, completion:)` call
+        queryUpdater.muteChannel(cid: channelID, mute: mute)
+
+        // Assert correct endpoint is called
+        let referenceEndpoint: Endpoint<EmptyResponse> = .muteChannel(cid: channelID, mute: mute)
+        XCTAssertEqual(apiClient.request_endpoint, AnyEndpoint(referenceEndpoint))
+    }
+
+    func test_muteChannel_successfulResponse_isPropagatedToCompletion() {
+        // Simulate `muteChannel(cid:, mute:, completion:)` call
+        var completionCalled = false
+        queryUpdater.muteChannel(cid: .unique, mute: true) { error in
+            XCTAssertNil(error)
+            completionCalled = true
+        }
+
+        // Assert completion is not called yet
+        XCTAssertFalse(completionCalled)
+
+        // Simulate API response with success
+        apiClient.test_simulateResponse(Result<EmptyResponse, Error>.success(.init()))
+
+        // Assert completion is called
+        AssertAsync.willBeTrue(completionCalled)
+    }
+
+    func test_muteChannel_errorResponse_isPropagatedToCompletion() {
+        // Simulate `muteChannel(cid:, mute:, completion:)` call
+        var completionCalledError: Error?
+        queryUpdater.muteChannel(cid: .unique, mute: true) { completionCalledError = $0 }
+
+        // Simulate API response with failure
+        let error = TestError()
+        apiClient.test_simulateResponse(Result<EmptyResponse, Error>.failure(error))
+
+        // Assert the completion is called with the error
+        AssertAsync.willBeEqual(completionCalledError as? TestError, error)
+    }
+
+    // MARK: - Delete channel
+
+    func test_deleteChannel_makesCorrectAPICall() {
+        let channelID = ChannelId.unique
+
+        // Simulate `deleteChannel(cid:, completion:)` call
+        queryUpdater.deleteChannel(cid: channelID)
+
+        // Assert correct endpoint is called
+        let referenceEndpoint: Endpoint<EmptyResponse> = .deleteChannel(cid: channelID)
+        XCTAssertEqual(apiClient.request_endpoint, AnyEndpoint(referenceEndpoint))
+    }
+
+    func test_deleteChannel_successfulResponse_isPropagatedToCompletion() {
+        // Simulate `deleteChannel(cid:, completion:)` call
+        var completionCalled = false
+        queryUpdater.deleteChannel(cid: .unique) { error in
+            XCTAssertNil(error)
+            completionCalled = true
+        }
+
+        // Assert completion is not called yet
+        XCTAssertFalse(completionCalled)
+
+        // Simulate API response with success
+        apiClient.test_simulateResponse(Result<EmptyResponse, Error>.success(.init()))
+
+        // Assert completion is called
+        AssertAsync.willBeTrue(completionCalled)
+    }
+
+    func test_deleteChannel_errorResponse_isPropagatedToCompletion() {
+        // Simulate `deleteChannel(cid:, completion:)` call
+        var completionCalledError: Error?
+        queryUpdater.deleteChannel(cid: .unique) { completionCalledError = $0 }
+
+        // Simulate API response with failure
+        let error = TestError()
+        apiClient.test_simulateResponse(Result<EmptyResponse, Error>.failure(error))
+
+        // Assert the completion is called with the error
+        AssertAsync.willBeEqual(completionCalledError as? TestError, error)
+    }
+
+    // MARK: - Hide channel
+
+    func test_hideChannel_makesCorrectAPICall() {
+        let channelID = ChannelId.unique
+        let userID = UserId.unique
+        let clearHistory = true
+
+        // Simulate `hideChannel(cid:, userId:, clearHistory:, completion:)` call
+        queryUpdater.hideChannel(cid: channelID, userId: userID, clearHistory: clearHistory)
+
+        // Assert correct endpoint is called
+        let referenceEndpoint: Endpoint<EmptyResponse> = .hideChannel(cid: channelID, userId: userID, clearHistory: clearHistory)
+        XCTAssertEqual(apiClient.request_endpoint, AnyEndpoint(referenceEndpoint))
+    }
+
+    func test_hideChannel_successfulResponse_isPropagatedToCompletion() {
+        // Simulate `hideChannel(cid:, userId:, clearHistory:, completion:)` call
+        var completionCalled = false
+        queryUpdater.hideChannel(cid: .unique, userId: .unique, clearHistory: true) { error in
+            XCTAssertNil(error)
+            completionCalled = true
+        }
+
+        // Assert completion is not called yet
+        XCTAssertFalse(completionCalled)
+
+        // Simulate API response with success
+        apiClient.test_simulateResponse(Result<EmptyResponse, Error>.success(.init()))
+
+        // Assert completion is called
+        AssertAsync.willBeTrue(completionCalled)
+    }
+
+    func test_hideChannel_errorResponse_isPropagatedToCompletion() {
+        // Simulate `hideChannel(cid:, userId:, clearHistory:, completion:)` call
+        var completionCalledError: Error?
+        queryUpdater.hideChannel(cid: .unique, userId: .unique, clearHistory: true) { completionCalledError = $0 }
+
+        // Simulate API response with failure
+        let error = TestError()
+        apiClient.test_simulateResponse(Result<EmptyResponse, Error>.failure(error))
+
+        // Assert the completion is called with the error
+        AssertAsync.willBeEqual(completionCalledError as? TestError, error)
+    }
+
+    // MARK: - Show channel
+
+    func test_showChannel_makesCorrectAPICall() {
+        let channelID = ChannelId.unique
+        let userID = UserId.unique
+
+        // Simulate `showChannel(cid:, userId:)` call
+        queryUpdater.showChannel(cid: channelID, userId: userID)
+
+        // Assert correct endpoint is called
+        let referenceEndpoint: Endpoint<EmptyResponse> = .showChannel(cid: channelID, userId: userID)
+        XCTAssertEqual(apiClient.request_endpoint, AnyEndpoint(referenceEndpoint))
+    }
+
+    func test_showChannel_successfulResponse_isPropagatedToCompletion() {
+        // Simulate `showChannel(cid:, userId:)` call
+        var completionCalled = false
+        queryUpdater.showChannel(cid: .unique, userId: .unique) { error in
+            XCTAssertNil(error)
+            completionCalled = true
+        }
+
+        // Assert completion is not called yet
+        XCTAssertFalse(completionCalled)
+
+        // Simulate API response with success
+        apiClient.test_simulateResponse(Result<EmptyResponse, Error>.success(.init()))
+
+        // Assert completion is called
+        AssertAsync.willBeTrue(completionCalled)
+    }
+
+    func test_showChannel_errorResponse_isPropagatedToCompletion() {
+        // Simulate `showChannel(cid:, userId:)` call
+        var completionCalledError: Error?
+        queryUpdater.showChannel(cid: .unique, userId: .unique) { completionCalledError = $0 }
+
+        // Simulate API response with failure
+        let error = TestError()
+        apiClient.test_simulateResponse(Result<EmptyResponse, Error>.failure(error))
+
+        // Assert the completion is called with the error
+        AssertAsync.willBeEqual(completionCalledError as? TestError, error)
+    }
 }
