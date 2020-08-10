c4326e98de87ddb4e3ccefeead744b25bba7b25a
Pavel Lopatine <mantergo@gmail.com>
2020-08-10 16:42:33 +0300

    Move channel payloads related coding keys to separate enum `ChannelCodingKeys`

---
+++ StreamChatClient_v3/APIClient/Endpoints/Payloads/ChannelCodingKeys.swift
@@ -0,0 +1,38 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+
+/// Coding keys channel related payloads.
+public enum ChannelCodingKeys: String, CodingKey {
+    /// A combination of channel id and type.
+    case cid
+    /// A type.
+    case typeRawValue = "type"
+    /// A channel name.
+    case name
+    /// An image URL.
+    case imageURL = "image"
+    /// A last message date.
+    case lastMessageAt = "last_message_at"
+    /// A user created by.
+    case createdBy = "created_by"
+    /// A created date.
+    case createdAt = "created_at"
+    /// A created date.
+    case updatedAt = "updated_at"
+    /// A deleted date.
+    case deletedAt = "deleted_at"
+    /// A channel config.
+    case config
+    /// A frozen flag.
+    case frozen
+    /// Members.
+    case members
+    /// Invites.
+    case invites
+    /// The team the channel belongs to.
+    case team
+    case memberCount = "member_count"
+}
