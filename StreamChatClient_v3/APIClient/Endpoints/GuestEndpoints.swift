57cee6282fa296e2e6a002cb9c9ca12da8355153
Pavel Evsaev <pavel.evsaev@gmail.com>
2020-08-11 16:27:39 +0300

    Introduce endpoint to obtain token for guest-user

---
+++ StreamChatClient_v3/APIClient/Endpoints/GuestEndpoints.swift
@@ -0,0 +1,20 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+
+extension Endpoint {
+    /// Builds the endpoint to obtain an access-token for a guest user.
+    /// - Parameters:
+    ///   - userId: The user's identifier
+    ///   - extraData: The user's extra data
+    /// - Returns: The endpoint that expects **GuestUserTokenPayload<ExtraData>** payload in a response
+    static func guestUserToken<ExtraData: UserExtraData>(userId: UserId, extraData: ExtraData) -> Endpoint<GuestUserTokenPayload<ExtraData>> {
+        .init(path: "guest",
+              method: .post,
+              queryItems: nil,
+              requiresConnectionId: false,
+              body: ["user": GuestUserTokenRequestPayload(userId: userId, extraData: extraData)])
+    }
+}
