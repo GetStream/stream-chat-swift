53008bb05d217709eff4931db0c9a33bcdf5c175
Pavel Evsaev <pavel.evsaev@gmail.com>
2020-08-13 18:23:10 +0300

    Update member events to be non-generic and have only generic `init`

--- Sources_v3/WebSocketClient/Events/MemberEvents.swift
+++ Sources_v3/WebSocketClient/Events/MemberEvents.swift
@@ -4,39 +4,39 @@
 
 import Foundation
 
-public struct MemberAddedEvent<ExtraData: ExtraDataTypes>: EventWithMemberPayload, EventWithChannelId {
+public struct MemberAddedEvent: EventWithMemberPayload, EventWithChannelId {
     public let userId: UserId
     public let cid: ChannelId
     
     let payload: Any
     
-    init(from response: EventPayload<ExtraData>) throws {
+    init<ExtraData: ExtraDataTypes>(from response: EventPayload<ExtraData>) throws {
         userId = try response.value(at: \.memberContainer?.member?.user.id)
         cid = try response.value(at: \.cid)
         payload = response
     }
 }
 
-public struct MemberUpdatedEvent<ExtraData: ExtraDataTypes>: EventWithMemberPayload, EventWithChannelId {
+public struct MemberUpdatedEvent: EventWithMemberPayload, EventWithChannelId {
     public let userId: UserId
     public let cid: ChannelId
     
     let payload: Any
     
-    init(from response: EventPayload<ExtraData>) throws {
+    init<ExtraData: ExtraDataTypes>(from response: EventPayload<ExtraData>) throws {
         userId = try response.value(at: \.memberContainer?.member?.user.id)
         cid = try response.value(at: \.cid)
         payload = response
     }
 }
 
-public struct MemberRemovedEvent<ExtraData: ExtraDataTypes>: EventWithUserPayload, EventWithChannelId {
+public struct MemberRemovedEvent: EventWithUserPayload, EventWithChannelId {
     public let userId: UserId
     public let cid: ChannelId
     
     let payload: Any
     
-    init(from response: EventPayload<ExtraData>) throws {
+    init<ExtraData: ExtraDataTypes>(from response: EventPayload<ExtraData>) throws {
         userId = try response.value(at: \.user?.id)
         cid = try response.value(at: \.cid)
         payload = response
