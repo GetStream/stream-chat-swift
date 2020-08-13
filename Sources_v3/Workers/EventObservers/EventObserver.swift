51ebee5b0f3ecde3003e218ef8318c93f3b6d85d
Pavel Evsaev <pavel.evsaev@gmail.com>
2020-08-13 18:24:43 +0300

    Introduce `EventObserver`

---
+++ Sources_v3/Workers/EventObservers/EventObserver.swift
@@ -0,0 +1,29 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+
+/// The type is designed to observe events conforming to `Event` protocol delivered via `NotificationCenter`
+class EventObserver {
+    private let stopObserving: () -> Void
+
+    init<EventType>(
+        notificationCenter: NotificationCenter,
+        tranform: @escaping (Event) -> EventType?,
+        callback: @escaping (EventType) -> Void
+    ) {
+        let observer = notificationCenter.addObserver(forName: .NewEventReceived, object: nil, queue: nil) {
+            guard let event = $0.event.flatMap(tranform) else { return }
+            callback(event)
+        }
+
+        stopObserving = { [weak notificationCenter] in
+            notificationCenter?.removeObserver(observer)
+        }
+    }
+
+    deinit {
+        stopObserving()
+    }
+}
