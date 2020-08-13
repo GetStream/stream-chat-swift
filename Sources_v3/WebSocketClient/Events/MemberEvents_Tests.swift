522f405e7ae9f733ec028697273425c8973fc9a4
Pavel Evsaev <pavel.evsaev@gmail.com>
2020-08-13 18:23:10 +0300

    Update member events to be non-generic and have only generic `init`

--- Sources_v3/WebSocketClient/Events/MemberEvents_Tests.swift
+++ Sources_v3/WebSocketClient/Events/MemberEvents_Tests.swift
@@ -10,21 +10,21 @@ class MemberEvents_Tests: XCTestCase {
     
     func test_added() throws {
         let json = XCTestCase.mockData(fromFile: "MemberAdded")
-        let event = try eventDecoder.decode(from: json) as? MemberAddedEvent<DefaultDataTypes>
+        let event = try eventDecoder.decode(from: json) as? MemberAddedEvent
         XCTAssertEqual(event?.userId, "steep-moon-9")
         XCTAssertEqual(event?.cid, ChannelId(type: .messaging, id: "new_channel_9125"))
     }
     
     func test_updated() throws {
         let json = XCTestCase.mockData(fromFile: "MemberUpdated")
-        let event = try eventDecoder.decode(from: json) as? MemberUpdatedEvent<DefaultDataTypes>
+        let event = try eventDecoder.decode(from: json) as? MemberUpdatedEvent
         XCTAssertEqual(event?.userId, "steep-moon-9")
         XCTAssertEqual(event?.cid, ChannelId(type: .messaging, id: "new_channel_9125"))
     }
     
     func test_removed() throws {
         let json = XCTestCase.mockData(fromFile: "MemberRemoved")
-        let event = try eventDecoder.decode(from: json) as? MemberRemovedEvent<DefaultDataTypes>
+        let event = try eventDecoder.decode(from: json) as? MemberRemovedEvent
         XCTAssertEqual(event?.userId, "steep-moon-9")
         XCTAssertEqual(event?.cid, ChannelId(type: .messaging, id: "new_channel_9125"))
     }
