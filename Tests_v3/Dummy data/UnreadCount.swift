1815731352894deeb730891faffa7145b9fc7c86
Pavel Evsaev <pavel.evsaev@gmail.com>
2020-08-21 17:34:14 +0300

    Introduce dummy `UnreadCount`

---
+++ Tests_v3/Dummy data/UnreadCount.swift
@@ -0,0 +1,12 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+@testable import StreamChatClient
+
+extension UnreadCount {
+    static var dummy: Self {
+        .init(channels: 10, messages: 323)
+    }
+}
