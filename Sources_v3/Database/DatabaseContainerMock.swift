ed995d8b2c839bfa7bab7aedbe58de61cf8fa3b0
Pavel Evsaev <pavel.evsaev@gmail.com>
2020-08-24 16:23:46 +0300

    Move all `TestDatabaseContainer` features to `DatabaseContainerMock`

--- Sources_v3/Database/DatabaseContainerMock.swift
+++ Sources_v3/Database/DatabaseContainerMock.swift
@@ -6,7 +6,10 @@ import CoreData
 @testable import StreamChatClient
 import XCTest
 
+/// A testable subclass of DatabaseContainer allowing response simulation.
 class DatabaseContainerMock: DatabaseContainer {
+    /// If set, the `write` completion block is called with this value.
+    @Atomic var write_errorResponse: Error?
     @Atomic var init_kind: DatabaseContainer.Kind
     @Atomic var flush_called = false
     
@@ -22,5 +25,39 @@ class DatabaseContainerMock: DatabaseContainer {
     override func removeAllData(force: Bool, completion: ((Error?) -> Void)? = nil) {
         flush_called = true
         super.removeAllData(force: force, completion: completion)
+    }
+
+    override func write(_ actions: @escaping (DatabaseSession) throws -> Void, completion: @escaping (Error?) -> Void) {
+        if let error = write_errorResponse {
+            super.write(actions, completion: { _ in })
+            completion(error)
+        } else {
+            super.write(actions, completion: completion)
+        }
+    }
+}
+
+extension DatabaseContainerMock {
+    /// Writes changes to the DB synchronously. Only for test purposes!
+    func writeSynchronously(_ actions: @escaping (DatabaseSession) throws -> Void) throws {
+        _ = try await { completion in
+            self.write(actions, completion: completion)
+        }
+    }
+    
+    /// Synchrnously creates a new CurrentUserDTO in the DB with the given id.
+    func createCurrentUser(id: UserId = .unique) throws {
+        try writeSynchronously { session in
+            try session.saveCurrentUser(payload: .dummy(userId: id,
+                                                        role: .admin,
+                                                        extraData: NameAndImageExtraData(name: nil, imageURL: nil)))
+        }
+    }
+    
+    /// Synchrnously creates a new ChannelDTO in the DB with the given cid.
+    func createChannel(cid: ChannelId = .unique) throws {
+        try writeSynchronously { session in
+            try session.saveChannel(payload: XCTestCase().dummyPayload(with: cid))
+        }
     }
 }
