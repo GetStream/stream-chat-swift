01ad09fd472ba9c0905bd8da098efe2073d1ea50
Vojta Stavik <stavik@outlook.com>
2020-08-25 15:29:32 +0200

    Introduce `EphemeralValuesContainer`
    A protocol marking an DTO object containing ephemeral values, i.e. user online state, or unread counts. These values need to be reset every time the database is initialized.

---
+++ Sources_v3/Database/DTOs/EphemeralValuesContainer.swift
@@ -0,0 +1,12 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+
+/// A protocol marking an DTO object containing ephemeral values, i.e. user online state, or unread counts. These values
+/// need to be reset every time the database is initialized.
+protocol EphemeralValuesContainer {
+    /// Resets the ephemeral values of the container to their default state.
+    func resetEphemeralValues()
+}
