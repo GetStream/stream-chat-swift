859158506062d881dcf783129a1b4de698c57c10
Pavel Evsaev <pavel.evsaev@gmail.com>
2020-08-27 13:04:08 +0300

    Introduce `EventNotificationCenter`

---
+++ Sources_v3/Workers/EventNotificationCenter.swift
@@ -0,0 +1,26 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+
+/// The type is designed to pre-process some incoming `Event` via middlewares before being published
+class EventNotificationCenter: NotificationCenter {
+    private(set) var middlewares: [EventMiddleware] = []
+    
+    init(middlewares: [EventMiddleware] = []) {
+        super.init()
+        middlewares.forEach(add)
+    }
+
+    func add(middleware: EventMiddleware) {
+        middlewares.append(middleware)
+    }
+    
+    func process(_ event: Event) {
+        middlewares.process(event: event) { [weak self] in
+            guard let self = self, let eventToPublish = $0 else { return }
+            self.post(Notification(newEventReceived: eventToPublish, sender: self))
+        }
+    }
+}
