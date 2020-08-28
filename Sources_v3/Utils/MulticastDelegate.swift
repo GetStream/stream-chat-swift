b6e06f7122190a88fd0234aa62d6b16669ca026d
Vojta Stavik <stavik@outlook.com>
2020-08-28 15:30:47 +0200

    Introduce `MulticastDelegate` helper
    This makes writing wrappers around controller possible, while still making the underlying controllers and their functionality available to users.

---
+++ Sources_v3/Utils/MulticastDelegate.swift
@@ -0,0 +1,27 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+
+/// A wrapper object that allows attaching additional delegates and call the callbacks on all of them.
+///
+/// - Warning: ⚠️ Because `MulticastDelegate` keeps strong references to the delegates, it's strongly recommended to use
+/// an additional wrapper which will make sure the delegate instance is referenced only weakly.
+struct MulticastDelegate<WrappedDelegate> {
+    /// Invokes the delegate callback on all delegates.
+    func invoke(_ action: (WrappedDelegate) -> Void) {
+        if let main = mainDelegate {
+            action(main)
+        }
+        additionalDelegates.forEach { action($0) }
+    }
+    
+    /// The is usually the delegate instance you want to expose to your users as _the_ delegate.
+    // swiftlint:disable:next weak_delegate
+    var mainDelegate: WrappedDelegate?
+    
+    /// Aditional delegates that receive the same callback as the main one. These delegates receive callbacks also when
+    /// the main delegate is not set.
+    var additionalDelegates: [WrappedDelegate] = []
+}
---
+++ Sources_v3/Utils/MulticastDelegateTests.swift
@@ -0,0 +1,69 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+@testable import StreamChatClient
+import XCTest
+
+class MulticastDelegate_Tests: XCTestCase {
+    // swiftlint:disable:next weak_delegate
+    fileprivate var multicastDelegate: MulticastDelegate<TestDelegate>!
+    
+    override func setUp() {
+        super.setUp()
+        
+        multicastDelegate = .init()
+    }
+
+    func test_mainDelegate_isCalled() {
+        let mainDelegate = TestDelegate()
+        assert(mainDelegate.called == false)
+        
+        multicastDelegate.mainDelegate = mainDelegate
+        multicastDelegate.invoke {
+            $0.called = true
+        }
+        
+        XCTAssertTrue(mainDelegate.called)
+    }
+
+    func test_mainDelegate_isRessetable() {
+        let mainDelegate = TestDelegate()
+        
+        multicastDelegate.mainDelegate = mainDelegate
+        XCTAssert(multicastDelegate.mainDelegate === mainDelegate)
+        
+        multicastDelegate.mainDelegate = nil
+        XCTAssertNil(multicastDelegate.mainDelegate)
+    }
+    
+    func test_additionalDelegate_isCalled_whenNoMainDelegateIsSet() {
+        let additionalDelegate = TestDelegate()
+        
+        multicastDelegate.additionalDelegates.append(additionalDelegate)
+        multicastDelegate.invoke {
+            $0.called = true
+        }
+        
+        XCTAssertTrue(additionalDelegate.called)
+    }
+
+    func test_allDelegates_areCalled() {
+        let mainDelegate = TestDelegate()
+        let additionalDelegate = TestDelegate()
+        
+        multicastDelegate.mainDelegate = mainDelegate
+        multicastDelegate.additionalDelegates.append(additionalDelegate)
+
+        multicastDelegate.invoke {
+            $0.called = true
+        }
+        
+        XCTAssertTrue(mainDelegate.called)
+        XCTAssertTrue(additionalDelegate.called)
+    }
+}
+
+private class TestDelegate {
+    var called = false
+}
