35bd68da20a3b60539c39e237e276da0fd755336
Vojta Stavik <stavik@outlook.com>
2020-08-31 17:30:48 +0200

    Expose `ChannelListController` output using publishers

---
+++ Sources_v3/Controllers/ChannelListController+Combine.swift
@@ -0,0 +1,71 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import Combine
+import UIKit
+
+@available(iOS 13, *)
+extension ChannelListControllerGeneric {
+    /// A publisher emitting a new value every time the state of the controller changes.
+    public var statePublisher: AnyPublisher<Controller.State, Never> {
+        basePublishers.state.keepAlive(self)
+    }
+    
+    /// A publisher emitting a new value every time the list of the channels mathicn the query changes.
+    public var channelsChangesPublisher: AnyPublisher<[ListChange<ChannelModel<ExtraData>>], Never> {
+        basePublishers.channelsChanges.keepAlive(self)
+    }
+
+    /// An internal backing object for all publicly available Combine publishers. We use it to simplify the way we expose
+    /// publishers. Instead of creating custom `Publisher` types, we use `CurrentValueSubject` and `PassthroughSubject` internally,
+    /// and expose the published values by mapping them to a read-only `AnyPublisher` type.
+    class BasePublishers {
+        /// The wrapper controller
+        unowned let controller: ChannelListControllerGeneric
+        
+        /// A backing subject for `remoteActivityPublisher`.
+        let state: CurrentValueSubject<Controller.State, Never>
+        
+        /// A backing subject for `channelChangesPublisher`.
+        let channelsChanges: PassthroughSubject<[ListChange<ChannelModel<ExtraData>>], Never> = .init()
+                
+        init(controller: ChannelListControllerGeneric<ExtraData>) {
+            self.controller = controller
+            state = .init(controller.state)
+            
+            controller.multicastDelegate.additionalDelegates.append(AnyChannelListControllerDelegate(self))
+            
+            if controller.state == .inactive {
+                // Start updating and load the current data
+                controller.startUpdating()
+            }
+        }
+    }
+}
+
+@available(iOS 13, *)
+extension ChannelListControllerGeneric.BasePublishers: ChannelListControllerDelegateGeneric {
+    func controller(_ controller: Controller, didChangeState state: Controller.State) {
+        self.state.send(state)
+    }
+    
+    func controller(
+        _ controller: ChannelListControllerGeneric<ExtraData>,
+        didChangeChannels changes: [ListChange<ChannelModel<ExtraData>>]
+    ) {
+        channelsChanges.send(changes)
+    }
+}
+
+@available(iOS 13.0, *)
+extension Publisher {
+    /// A helper function which attaches the provided object to the publisher chain and keeps it alive as long
+    /// as the publisher chain is alive.
+    func keepAlive(_ object: AnyObject) -> AnyPublisher<Output, Failure> {
+        map {
+            _ = object
+            return $0
+        }.eraseToAnyPublisher()
+    }
+}
