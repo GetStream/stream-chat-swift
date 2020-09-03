7928f31fcc4ec73093d8838fc84e09f141de84d3
Vojta Stavik <stavik@outlook.com>
2020-09-03 14:17:19 +0200

    Add custom URLSessionConfig `equatable` implementation for iOS11 + 12
    Because on < iOS13 the configuration class gets copied and we can't simply compare it's the same instance, we need to provide custom implementation for comparing. The default `Equatable` implementation of `URLSessionConfiguration` simply compares the pointers.

--- Sources_v3/APIClient/APIClient_Tests.swift
+++ Sources_v3/APIClient/APIClient_Tests.swift
@@ -51,7 +51,7 @@ class APIClient_Tests: StressTestCase {
     func test_isInitializedWithCorrectValues() {
         XCTAssert(apiClient.encoder as AnyObject === encoder)
         XCTAssert(apiClient.decoder as AnyObject === decoder)
-        XCTAssertEqual(apiClient.session.configuration, sessionConfiguration)
+        XCTAssertTrue(apiClient.session.configuration.isTestEqual(to: sessionConfiguration))
     }
     
     func test_requestEncoderIsCalledWithEndpoint() {
@@ -269,5 +269,40 @@ private class TestWebSocketClient: WebSocketClient {
             eventNotificationCenter: EventNotificationCenter(),
             reconnectionStrategy: DefaultReconnectionStrategy()
         )
+    }
+}
+
+extension URLSessionConfiguration {
+    // Because on < iOS13 the configuration class gets copied and we can't simply compare it's the same instance, we need to
+    // provide custom implementation for comparing. The default `Equatable` implementation of `URLSessionConfiguration`
+    // simply compares the pointers.
+    @available(iOS, deprecated: 12.0, message: "Remove this workaround when dropping iOS 12 support.")
+    func isTestEqual(to otherConfiguration: URLSessionConfiguration) -> Bool {
+        identifier == otherConfiguration.identifier
+            && requestCachePolicy == otherConfiguration.requestCachePolicy
+            && timeoutIntervalForRequest == otherConfiguration.timeoutIntervalForRequest
+            && timeoutIntervalForResource == otherConfiguration.timeoutIntervalForResource
+            && networkServiceType == otherConfiguration.networkServiceType
+            && allowsCellularAccess == otherConfiguration.allowsCellularAccess
+            && httpShouldUsePipelining == otherConfiguration.httpShouldUsePipelining
+            && httpShouldSetCookies == otherConfiguration.httpShouldSetCookies
+            && httpCookieAcceptPolicy == otherConfiguration.httpCookieAcceptPolicy
+            && httpAdditionalHeaders as? [String: String] == otherConfiguration.httpAdditionalHeaders as? [String: String]
+            && httpMaximumConnectionsPerHost == otherConfiguration.httpMaximumConnectionsPerHost
+            && httpCookieStorage == otherConfiguration.httpCookieStorage
+            && urlCredentialStorage == otherConfiguration.urlCredentialStorage
+            && urlCache == otherConfiguration.urlCache
+            && shouldUseExtendedBackgroundIdleMode == otherConfiguration.shouldUseExtendedBackgroundIdleMode
+//            && protocolClasses == otherConfiguration.protocolClasses
+            && multipathServiceType == otherConfiguration.multipathServiceType
+            && waitsForConnectivity == otherConfiguration.waitsForConnectivity
+            && isDiscretionary == otherConfiguration.isDiscretionary
+            && sharedContainerIdentifier == otherConfiguration.sharedContainerIdentifier
+            && sessionSendsLaunchEvents == otherConfiguration.sessionSendsLaunchEvents
+//            && connectionProxyDictionary == otherConfiguration.connectionProxyDictionary
+            && waitsForConnectivity == otherConfiguration.waitsForConnectivity
+            && isDiscretionary == otherConfiguration.isDiscretionary
+            && sharedContainerIdentifier == otherConfiguration.sharedContainerIdentifier
+            && sessionSendsLaunchEvents == otherConfiguration.sessionSendsLaunchEvents
     }
 }
