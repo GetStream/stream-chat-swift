1fa407f1cde7e9a3d6947c553f9d7cd66060fdb9
Vojta Stavik <stavik@outlook.com>
2020-09-03 16:20:15 +0200

    Don't show deleted channels in `ChannelListController`

--- Sources_v3/Database/DTOs/ChannelDTO.swift
+++ Sources_v3/Database/DTOs/ChannelDTO.swift
@@ -123,7 +123,10 @@ extension ChannelDTO {
         let sortDescriptors = query.sort.compactMap { $0.key.sortDescriptor(isAscending: $0.isAscending) }
         request.sortDescriptors = sortDescriptors.isEmpty ? [ChannelListSortingKey.defaultSortDescriptor] : sortDescriptors
         
-        request.predicate = NSPredicate(format: "ANY queries.filterHash == %@", query.filter.filterHash)
+        let matchingQuery = NSPredicate(format: "ANY queries.filterHash == %@", query.filter.filterHash)
+        let notDeleted = NSPredicate(format: "deletedAt == nil")
+        
+        request.predicate = NSCompoundPredicate(type: .and, subpredicates: [matchingQuery, notDeleted])
         return request
     }
 
