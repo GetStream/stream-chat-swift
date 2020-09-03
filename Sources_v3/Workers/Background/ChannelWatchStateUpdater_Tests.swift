be04f19f24e01299152f6003d19a134c80b10ddf
Pavel Lopatine <mantergo@gmail.com>
2020-09-03 17:21:16 +0300

    Introduce `ChannelWatchStateUpdater`

---
+++ Sources_v3/Workers/Background/ChannelWatchStateUpdater_Tests.swift
@@ -0,0 +1,96 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+@testable import StreamChatClient
+import XCTest
+
+class ChannelWatchStateUpdater_Tests: StressTestCase {
+    typealias ExtraData = DefaultDataTypes
+    
+    var database: DatabaseContainerMock!
+    var webSocketClient: WebSocketClientMock!
+    var apiClient: APIClientMock!
+    
+    var channelWatchStateUpdater: ChannelWatchStateUpdater<ExtraData>?
+    
+    override func setUp() {
+        super.setUp()
+        
+        database = try! DatabaseContainerMock(kind: .inMemory)
+        webSocketClient = WebSocketClientMock()
+        apiClient = APIClientMock()
+        
+        channelWatchStateUpdater = ChannelWatchStateUpdater(
+            database: database,
+            webSocketClient: webSocketClient,
+            apiClient: apiClient
+        )
+    }
+    
+    override func tearDown() {
+        apiClient.cleanUp()
+        
+        AssertAsync {
+            Assert.canBeReleased(&channelWatchStateUpdater)
+            Assert.canBeReleased(&database)
+            Assert.canBeReleased(&webSocketClient)
+            Assert.canBeReleased(&apiClient)
+        }
+        
+        super.tearDown()
+    }
+    
+    func test_WebSocketConnectedProcessor_ignores_other_events() throws {
+        let cid1: ChannelId = .unique
+        let cid2: ChannelId = .unique
+        
+        try database.createChannel(cid: cid1, withMessages: false)
+        try database.createChannel(cid: cid2, withMessages: false)
+        
+        simulateWebSocket(state: .connecting)
+        simulateWebSocket(state: .disconnected(error: .none))
+        simulateWebSocket(state: .waitingForConnectionId)
+        
+        // Assert APIClient is not called for other events
+        XCTAssertNil(apiClient.request_endpoint)
+    }
+    
+    func test_apiClient_is_not_called_on_empty_channels() {
+        // Simulate WebSocket successfully connected
+        simulateWebSocket(state: .connected(connectionId: .unique))
+        
+        // Assert APIClient is not called for empty channels
+        XCTAssertNil(apiClient.request_endpoint)
+    }
+    
+    func test_apiClient_called_on_websocket_connected() throws {
+        let cid1: ChannelId = .unique
+        let cid2: ChannelId = .unique
+        
+        try database.createChannel(cid: cid1, withMessages: false)
+        try database.createChannel(cid: cid2, withMessages: false)
+        
+        XCTAssertNil(apiClient.request_endpoint)
+        
+        // Simulate WebSocket successfully connected
+        simulateWebSocket(state: .connected(connectionId: .unique))
+        
+        let query: ChannelListQuery = .init(
+            filter: .in("cid", [cid1, cid2].map(\.rawValue)),
+            pagination: [.limit(1)],
+            options: [.watch]
+        )
+        
+        let endpoint: Endpoint<ChannelListPayload<ExtraData>> = .channels(query: query)
+        
+        // Assert APIClient is called with the correct endpoint
+        XCTAssertEqual(apiClient.request_endpoint, AnyEndpoint(endpoint))
+    }
+    
+    func simulateWebSocket(state: WebSocketConnectionState) {
+        let event = ConnectionStatusUpdated(webSocketConnectionState: state)
+        let notification = Notification(newEventReceived: event, sender: self)
+        webSocketClient.eventNotificationCenter.post(notification)
+    }
+}
