b4ddfa58691265ad25212a7fb32959626920b45e
Pavel Lopatine <mantergo@gmail.com>
2020-09-04 17:29:05 +0300

    Add `Combine` wrapper to `ChannelController`

---
+++ Sources_v3/Utils/Publisher+Extensions.swift
@@ -0,0 +1,17 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import Combine
+
+@available(iOS 13.0, *)
+extension Publisher {
+    /// A helper function which attaches the provided object to the publisher chain and keeps it alive as long
+    /// as the publisher chain is alive.
+    func keepAlive(_ object: AnyObject) -> AnyPublisher<Output, Failure> {
+        map {
+            _ = object
+            return $0
+        }.eraseToAnyPublisher()
+    }
+}
