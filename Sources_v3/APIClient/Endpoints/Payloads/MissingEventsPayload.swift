0a4278a101be606e9639eb48f8cced6651e6d05d
Pavel Evsaev <pavel.evsaev@gmail.com>
2020-09-07 16:54:21 +0300

    Introduce `MissingEventsPayload` for incoming json

---
+++ Sources_v3/APIClient/Endpoints/Payloads/MissingEventsPayload.swift
@@ -0,0 +1,14 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+
+/// The type describes the incoming JSON from `/sync` endpoint
+struct MissingEventsPayload<ExtraData: ExtraDataTypes>: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case eventPayloads = "events"
+    }
+    
+    let eventPayloads: [EventPayload<ExtraData>]
+}
