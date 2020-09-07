e48a1100dda4622f571370fa421294997ae5184d
Pavel Evsaev <pavel.evsaev@gmail.com>
2020-09-07 16:56:40 +0300

    Introduce `MissingEventsPublisher`

---
+++ Sources_v3/Workers/Background/MissingEventsPublisher.swift
@@ -0,0 +1,102 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import CoreData
+import Foundation
+
+/// The type is designed to obtain missing events that happened in watched channels while user
+/// was not connected to the web-socket.
+///
+/// The object listenes for `ConnectionStatusUpdated` events
+/// and remembers the `CurrentUserDTO.lastReceivedEventDate` when status becomes `connecting`.
+///
+/// When the status becomes `connected` the `/sync` endpoint is called
+/// with `lastReceivedEventDate` and `cids` of watched channels.
+///
+/// We remember `lastReceivedEventDate` when state becomes `connecting` to catch the last event date
+/// before the `HealthCheck` override the `lastReceivedEventDate` with the recent date.
+///
+class MissingEventsPublisher<ExtraData: ExtraDataTypes>: Worker {
+    // MARK: - Properties
+    
+    private var connectionObserver: EventObserver?
+    @Atomic private var lastSyncedAt: Date?
+    
+    // MARK: - Init
+
+    override init(database: DatabaseContainer, webSocketClient: WebSocketClient, apiClient: APIClient) {
+        super.init(database: database, webSocketClient: webSocketClient, apiClient: apiClient)
+        startObserving()
+    }
+    
+    // MARK: - Private
+    
+    private func startObserving() {
+        connectionObserver = EventObserver(
+            notificationCenter: webSocketClient.eventNotificationCenter,
+            tranform: { $0 as? ConnectionStatusUpdated },
+            callback: { [unowned self] in
+                switch $0.webSocketConnectionState {
+                case .connecting:
+                    self.obtainLastSyncDate()
+                case .connected:
+                    self.fetchAndReplayMissingEvents()
+                default:
+                    break
+                }
+            }
+        )
+    }
+    
+    private func obtainLastSyncDate() {
+        lastSyncedAt = database.backgroundReadOnlyContext.currentUser()?.lastReceivedEventDate
+    }
+    
+    private func fetchAndReplayMissingEvents() {
+        guard let lastSyncedAt = lastSyncedAt else { return }
+        
+        let watchedChannelIDs = allChannels.filter { $0.isWatched }.map(\.cid)
+        
+        let endpoint: Endpoint<MissingEventsPayload<ExtraData>> = .missingEvents(
+            since: lastSyncedAt,
+            cids: watchedChannelIDs
+        )
+        
+        apiClient.request(endpoint: endpoint) { [weak self] in
+            switch $0 {
+            case let .success(payload):
+                self?.webSocketClient.eventNotificationCenter.process(payload.eventPayloads)
+            case let .failure(error):
+                log.error("Internal error: Failed to fetch and reply missing events: \(error)")
+            }
+        }
+    }
+    
+    private var allChannels: [ChannelModel<ExtraData>] {
+        do {
+            return try database.backgroundReadOnlyContext.fetch(ChannelDTO.allChannelsFetchRequest).map(ChannelModel.create)
+        } catch {
+            log.error("Internal error: Failed to fetch [ChannelDTO]: \(error)")
+            return []
+        }
+    }
+}
+
+// MARK: - Extensions
+
+private extension EventNotificationCenter {
+    /// The method is used to convert incoming event payloads into events and calls `process(_:)` for each event
+    /// that was successfully decoded.
+    ///
+    /// - Parameter payloads: The event payloads
+    func process<ExtraData: ExtraDataTypes>(_ payloads: [EventPayload<ExtraData>]) {
+        payloads.forEach {
+            do {
+                process(try $0.event())
+            } catch {
+                log.error("Failed to transform a payload into an event: \($0)")
+            }
+        }
+    }
+}
---
+++ Sources_v3/Workers/Background/MissingEventsPublisher_Tests.swift
@@ -0,0 +1,147 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+@testable import StreamChatClient
+import XCTest
+
+final class MissingEventsReplayer_Tests: StressTestCase {
+    typealias ExtraData = DefaultDataTypes
+    
+    var database: DatabaseContainerMock!
+    var webSocketClient: WebSocketClientMock!
+    var apiClient: APIClientMock!
+    var publisher: MissingEventsPublisher<ExtraData>?
+    
+    // MARK: - Setup
+    
+    override func setUp() {
+        super.setUp()
+        
+        database = try! DatabaseContainerMock(kind: .inMemory)
+        webSocketClient = WebSocketClientMock()
+        apiClient = APIClientMock()
+        
+        publisher = MissingEventsPublisher(
+            database: database,
+            webSocketClient: webSocketClient,
+            apiClient: apiClient
+        )
+    }
+    
+    override func tearDown() {
+        apiClient.cleanUp()
+        
+        AssertAsync {
+            Assert.canBeReleased(&publisher)
+            Assert.canBeReleased(&database)
+            Assert.canBeReleased(&webSocketClient)
+            Assert.canBeReleased(&apiClient)
+        }
+        
+        super.tearDown()
+    }
+    
+    // MARK: - Tests
+    
+    func test_endpointIsNotCalled_ifThereIsNoCurrentUser() throws {
+        // Simulate `.connecting` connection state of a web-socket
+        webSocketClient.simulateConnectionStatus(.connecting)
+        
+        // Simulate `.connected` connection state of a web-socket
+        webSocketClient.simulateConnectionStatus(.connected(connectionId: .unique))
+        
+        // Assert endpoint is not called as `lastSyncAt` is unknown
+        XCTAssertNil(apiClient.request_endpoint)
+    }
+    
+    func test_endpointIsCalled_whenStatusBecomesConnected() throws {
+        let cid: ChannelId = .unique
+        let lastReceivedEventDate: Date = .unique
+        
+        // Create current user in the database
+        try database.createCurrentUser()
+        
+        // Create channel in the database
+        try database.createChannel(cid: cid)
+
+        // Set `lastReceivedEventDate`
+        try database.writeSynchronously { session in
+            let currentUser = try XCTUnwrap(session.currentUser())
+            currentUser.lastReceivedEventDate = lastReceivedEventDate
+        }
+        
+        // Simulate `.connecting` connection state of a web-socket
+        webSocketClient.simulateConnectionStatus(.connecting)
+        
+        // Simulate `.connected` connection state of a web-socket
+        webSocketClient.simulateConnectionStatus(.connected(connectionId: .unique))
+        
+        // Assert endpoint is called with correct values
+        let endpoint: Endpoint<MissingEventsPayload<ExtraData>> = .missingEvents(
+            since: lastReceivedEventDate,
+            cids: [cid]
+        )
+        XCTAssertEqual(try XCTUnwrap(apiClient.request_endpoint), AnyEndpoint(endpoint))
+    }
+    
+    func test_noEventsArePublished_ifErrorResponseComes() throws {
+        let cid: ChannelId = .unique
+
+        // Create current user in the database
+        try database.createCurrentUser()
+        
+        // Create channel in the database
+        try database.createChannel(cid: cid)
+        
+        // Set `lastReceivedEventDate`
+        try database.writeSynchronously { session in
+            let currentUser = try XCTUnwrap(session.currentUser())
+            currentUser.lastReceivedEventDate = Date()
+        }
+        
+        // Simulate `.connecting` connection state of a web-socket
+        webSocketClient.simulateConnectionStatus(.connecting)
+        
+        // Simulate `.connected` connection state of a web-socket
+        webSocketClient.simulateConnectionStatus(.connected(connectionId: .unique))
+        
+        // Create event logger to check published events
+        let eventLogger = EventLogger(webSocketClient.init_eventNotificationCenter)
+        
+        // Simulate error response
+        apiClient.test_simulateResponse(Result<MissingEventsPayload<ExtraData>, Error>.failure(TestError()))
+        
+        // Assert no events are published
+        AssertAsync.willBeTrue(eventLogger.equatableEvents.isEmpty)
+    }
+    
+    func test_eventsFromPayloadArePublished_ifSuccessfulResponseComes() throws {
+        let json = XCTestCase.mockData(fromFile: "MissingEventsPayload")
+        let payload = try JSONDecoder.default.decode(MissingEventsPayload<ExtraData>.self, from: json)
+        let events = payload.eventPayloads.compactMap { try? $0.event() }
+        
+        // Create current user in the database
+        try database.createCurrentUser()
+        
+        // Create channel in the database
+        try database.createChannel(cid: (events.first as! EventWithChannelId).cid)
+        
+        try database.writeSynchronously { session in
+            let currentUser = try XCTUnwrap(session.currentUser())
+            currentUser.lastReceivedEventDate = .unique
+        }
+        
+        webSocketClient.simulateConnectionStatus(.connecting)
+        webSocketClient.simulateConnectionStatus(.connected(connectionId: .unique))
+        
+        // Create event logger to check published events
+        let eventLogger = EventLogger(webSocketClient.init_eventNotificationCenter)
+        
+        // Simulate successful response
+        apiClient.test_simulateResponse(Result<MissingEventsPayload<ExtraData>, Error>.success(payload))
+        
+        // Assert events from payload are published
+        AssertAsync.willBeEqual(eventLogger.equatableEvents, events.map(\.asEquatable))
+    }
+}
