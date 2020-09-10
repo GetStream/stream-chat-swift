8a93bb21e4b43fb7616b1d8301249348ebde7f04
Bahadir Oncel <bahadir@getstream.io>
2020-09-10 16:42:25 +0200

    Add ChannelReadUpdaterMiddleware and tests

---
+++ Sources_v3/WebSocketClient/EventMiddlewares/ChannelReadUpdaterMiddleware.swift
@@ -0,0 +1,51 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+
+/// A middleware which updates a channel's read events as websocket events arrive.
+struct ChannelReadUpdaterMiddleware<ExtraData: ExtraDataTypes>: EventMiddleware {
+    let database: DatabaseContainer
+    
+    func handle(event: Event, completion: @escaping (Event?) -> Void) {
+        if let event = event as? MessageReadEvent<ExtraData> {
+            updateReadEvent(for: event.cid, userId: event.userId, lastReadAt: event.readAt) { completion(event) }
+            return
+        }
+        
+        if let event = event as? NotificationMarkReadEvent<ExtraData> {
+            updateReadEvent(for: event.cid, userId: event.userId, lastReadAt: event.readAt) { completion(event) }
+            return
+        }
+        
+        if let event = event as? NotificationMarkAllReadEvent<ExtraData> {
+            database.write({ session in
+                session.loadChannelReads(for: event.userId).forEach { read in
+                    read.lastReadAt = event.readAt
+                    read.unreadMessageCount = 0
+                }
+            }, completion: { error in
+                if let error = error {
+                    log.error("Failed to update channel reads for userId \(event.userId), error: \(error)")
+                }
+                completion(event)
+            })
+            return
+        }
+    }
+    
+    private func updateReadEvent(for cid: ChannelId, userId: UserId, lastReadAt: Date, completion: @escaping () -> Void) {
+        database.write({ session in
+            if let read = session.loadChannelRead(cid: cid, userId: userId) {
+                read.lastReadAt = lastReadAt
+                read.unreadMessageCount = 0
+            }
+        }, completion: { error in
+            if let error = error {
+                log.error("Failed to update channel read for cid \(cid) and userId \(userId), error: \(error)")
+            }
+            completion()
+        })
+    }
+}
