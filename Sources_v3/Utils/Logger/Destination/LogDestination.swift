f251924efae997b07ad9b59ca4215a076c8ed8f9
Pavel Lopatine <mantergo@gmail.com>
2020-09-17 16:41:30 +0300

    Revise log leves in `Logger`

--- Sources_v3/Utils/Logger/Destination/LogDestination.swift
+++ Sources_v3/Utils/Logger/Destination/LogDestination.swift
@@ -5,9 +5,16 @@
 import Foundation
 
 /// Log level for any messages to be logged.
-/// Please check [this Apple Logging Article](https://developer.apple.com/documentation/os/logging/choosing_the_log_level_for_a_message) to understand different logging levels.
+/// Please check [this Apple Logging Article](https://developer.apple.com/documentation/os/logging/generating_log_messages_from_your_code) to understand different logging levels.
 public enum LogLevel: Int {
-    case info = 0, debug, warning, error
+    /// Use this log level if you want to see everything that is logged.
+    case debug = 0
+    /// Use this log level if you want to see what is happening during the app execution.
+    case info
+    /// Use this log level if you want to see if something is not 100% right.
+    case warning
+    /// Use this log level if you want to see only errors.
+    case error
 }
 
 /// Encapsulates the components of a log message.
