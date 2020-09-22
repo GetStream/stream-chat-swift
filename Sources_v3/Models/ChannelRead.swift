5953a12a70f28c70e960b6ebb08ce6d690442312
Vojta Stavik <stavik@outlook.com>
2020-09-22 12:14:41 +0200

    Add documentation to `ChannelRead` public API

--- Sources_v3/Models/ChannelRead.swift
+++ Sources_v3/Models/ChannelRead.swift
@@ -4,9 +4,22 @@
 
 import Foundation
 
+/// A type representing a user's last read action on a channel.
+///
+/// - Note: `ChatChannelRead` is a typealias of `_ChatChannelRead` with default extra data. If you're using custom extra data,
+/// create your own typealias of `ChatChannelRead`.
+///
+/// Learn more about using custom extra data in our [cheat sheet](https://github.com/GetStream/stream-chat-swift/wiki/StreamChat-SDK-Cheat-Sheet#working-with-extra-data).
+///
 public typealias ChatChannelRead = _ChatChannelRead<DefaultExtraData>
 
-/// A type represnting a user's last read action on a channel.
+/// A type representing a user's last read action on a channel.
+///
+/// - Note: `_ChatChannelRead` type is not meant to be used directly. If you're using default extra data, use `ChatChannelRead`
+/// typealias instead. If you're using custom extra data, create your own typealias of `ChatChannelRead`.
+///
+/// Learn more about using custom extra data in our [cheat sheet](https://github.com/GetStream/stream-chat-swift/wiki/StreamChat-SDK-Cheat-Sheet#working-with-extra-data).
+///
 public struct _ChatChannelRead<ExtraData: ExtraDataTypes> {
     /// The last time the user has read the channel.
     public let lastReadAt: Date
