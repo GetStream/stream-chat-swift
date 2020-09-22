84c0005eaf63217ee5ef19ac5f21dd44def7ab44
Vojta Stavik <stavik@outlook.com>
2020-09-22 12:04:09 +0200

    Add Singapore+Sydney datacenters + documentation

--- Sources_v3/Config/BaseURL.swift
+++ Sources_v3/Config/BaseURL.swift
@@ -4,29 +4,36 @@
 
 import Foundation
 
-/// A base URL for the `Client`.
+/// A struct representing base URL for `ChatClient`.
 public struct BaseURL: CustomStringConvertible {
+    /// The base url for StreamChat data center located in the US East Cost.
     public static let usEast = BaseURL(urlString: "https://chat-proxy-us-east.stream-io-api.com/")
+    
+    /// The base url for StreamChat data center located in Dublin.
     public static let dublin = BaseURL(urlString: "https://chat-proxy-dublin.stream-io-api.com/")
     
-    static let placeholderURL = URL(string: "https://getstream.io")!
+    /// The base url for StreamChat data center located in Singapore.
+    public static let singapore = BaseURL(urlString: "https://chat-proxy-singapore.stream-io-api.com/")
     
-    public let restAPIBaseURL: URL
-    public let webSocketBaseURL: URL
+    /// The base url for StreamChat data center located in Sydney.
+    public static let sydney = BaseURL(urlString: "https://chat-proxy-sydney.stream-io-api.com/")
+    
+    let restAPIBaseURL: URL
+    let webSocketBaseURL: URL
     
     public var description: String { restAPIBaseURL.absoluteString }
     
     /// Create a base URL from an URL string.
     ///
     /// - Parameter urlString: a Stream Chat server location url string.
-    init(urlString: String) {
+    public init(urlString: String) {
         self.init(url: URL(string: urlString)!)
     }
     
     /// Init with a custom server URL.
     ///
     /// - Parameter url: an URL
-    init(url: URL) {
+    public init(url: URL) {
         var urlString = url.absoluteString
         
         // Remove a scheme prefix.
