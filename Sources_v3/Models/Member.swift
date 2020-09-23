100ca623d5fe0d5e1e9682ea44e45c87a3554f6d
Vojta Stavik <stavik@outlook.com>
2020-09-23 11:39:33 +0200

    Improve documentation for `ChatChannelMember`

--- Sources_v3/Models/Member.swift
+++ Sources_v3/Models/Member.swift
@@ -4,27 +4,42 @@
 
 import Foundation
 
+/// A type representing a chat channel member. `ChatChannelMember` is an immutable snapshot of a chat channel member entity
+/// at the given time.
+///
+/// - Note: `ChatChannelMember` is a typealias of `_ChatChannelMember` with default extra data. If you're using custom extra data,
+/// create your own typealias of `ChatChannelMember`.
+///
+/// Learn more about using custom extra data in our [cheat sheet](https://github.com/GetStream/stream-chat-swift/wiki/StreamChat-SDK-Cheat-Sheet#working-with-extra-data).
+///
 public typealias ChatChannelMember = _ChatChannelMember<NameAndImageExtraData>
 
+/// A type representing a chat channel member. `_ChatChannelMember` is an immutable snapshot of a channel entity at the given time.
+///
+/// - Note: `_ChatChannelMember` type is not meant to be used directly. If you're using default extra data, use `ChatChannelMember`
+/// typealias instead. If you're using custom extra data, create your own typealias of `_ChatChannelMember`.
+///
+/// Learn more about using custom extra data in our [cheat sheet](https://github.com/GetStream/stream-chat-swift/wiki/StreamChat-SDK-Cheat-Sheet#working-with-extra-data).
+///
 public class _ChatChannelMember<ExtraData: UserExtraData>: _ChatUser<ExtraData> {
-    // MARK: - Public
-    
     /// The role of the user within the channel.
     public let memberRole: MemberRole
     
-    /// A created date.
+    /// The date the user was added to the channel.
     public let memberCreatedAt: Date
     
-    /// A updated date.
+    /// The date the membership was updated for the last time.
     public let memberUpdatedAt: Date
     
-    /// Checks if he was invited.
+    /// Returns `true` if the member has been invited to the channel.
     public let isInvited: Bool
     
-    /// A date when an invited was accepted.
+    /// If the member accepted a channel invitation, this field contains date of when the invitation was accepted,
+    /// otherwise it's `nil`.
     public let inviteAcceptedAt: Date?
     
-    /// A date when an invited was rejected.
+    /// If the member rejected a channel invitation, this field contains date of when the invitation was rejected,
+    /// otherwise it's `nil`.
     public let inviteRejectedAt: Date?
     
     public init(
@@ -63,10 +78,17 @@ public class _ChatChannelMember<ExtraData: UserExtraData>: _ChatUser<ExtraData> 
     }
 }
 
-/// The role of the meber in the channel
+/// An enum describing possible roles of a member in a channel.
 public enum MemberRole: String, Codable, Hashable {
+    /// This is the default role assigned to any member.
     case member
+    
+    /// Allows the member to perform moderation, e.g. ban users, add/remove users, etc.
     case moderator
+    
+    /// This role allows the member to perform more advanced actions. This role should be granted only to staff users.
     case admin
+
+    /// This rele allows the member to perform destructive actions on the channel.
     case owner
 }
