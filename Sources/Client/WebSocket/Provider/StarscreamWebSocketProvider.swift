76f36782e001ea42a4e841fe6652c4d442856482
Pavel Evsaev <pavel.evsaev@gmail.com>
2020-09-24 13:57:42 +0300

    Update `StarscreamWebSocketProvider` in v2 to meet `Starscream` API changes

--- Sources/Client/WebSocket/Provider/StarscreamWebSocketProvider.swift
+++ Sources/Client/WebSocket/Provider/StarscreamWebSocketProvider.swift
@@ -20,7 +20,7 @@ final class StarscreamWebSocketProvider: WebSocketProvider {
         }
     }
     
-    var isConnected: Bool { webSocket.isConnected }
+    private(set) var isConnected = false
     var callbackQueue: DispatchQueue { webSocket.callbackQueue }
     weak var delegate: WebSocketProviderDelegate?
     
@@ -35,7 +35,7 @@ final class StarscreamWebSocketProvider: WebSocketProvider {
     }
     
     func disconnect() {
-        webSocket.disconnect(forceTimeout: 0)
+        webSocket.disconnect()
     }
     
     func sendPing() {
@@ -45,26 +45,42 @@ final class StarscreamWebSocketProvider: WebSocketProvider {
 
 extension StarscreamWebSocketProvider: Starscream.WebSocketDelegate {
     
-    func websocketDidConnect(socket: Starscream.WebSocketClient) {
+    func didReceive(event: Starscream.WebSocketEvent, client: Starscream.WebSocket) {
+        switch event {
+        case .connected:
+            didConnect()
+        case let .text(text):
+            delegate?.websocketDidReceiveMessage(text)
+        case let .error(error):
+            didDisconnect(error: error.flatMap {
+                .init(reason: $0.localizedDescription,
+                      code: Int(($0 as? WSError)?.code ?? 0),
+                      providerType: StarscreamWebSocketProvider.self,
+                      providerError: $0)
+            })
+        case let .disconnected(reason, code):
+            didDisconnect(error: .init(reason: reason,
+                                       code: Int(code),
+                                       providerType: StarscreamWebSocketProvider.self,
+                                       providerError: nil))
+        case .cancelled:
+            didDisconnect(error: .init(reason: "Cancelled",
+                                       code: -1,
+                                       providerType: StarscreamWebSocketProvider.self,
+                                       providerError: nil))
+        default:
+            break
+        }
+    }
+    
+    private func didConnect() {
+        isConnected = true
         delegate?.websocketDidConnect()
     }
     
-    func websocketDidDisconnect(socket: Starscream.WebSocketClient, error: Error?) {
-        var webSocketProviderError: WebSocketProviderError?
-        
-        if let error = error {
-            webSocketProviderError = .init(reason: error.localizedDescription,
-                                           code: (error as? WSError)?.code ?? 0,
-                                           providerType: StarscreamWebSocketProvider.self,
-                                           providerError: error)
-        }
-        
-        delegate?.websocketDidDisconnect(error: webSocketProviderError)
+    private func didDisconnect(error: WebSocketProviderError?) {
+        isConnected = false
+        delegate?.websocketDidDisconnect(error: error)
     }
     
-    func websocketDidReceiveMessage(socket: Starscream.WebSocketClient, text: String) {
-        delegate?.websocketDidReceiveMessage(text)
-    }
-    
-    func websocketDidReceiveData(socket: Starscream.WebSocketClient, data: Data) {}
 }
