a2c6c0411af7d06921a151741552ffb11ac7b920
Vojta Stavik <stavik@outlook.com>
2020-09-24 12:26:45 +0200

    Improve documentation of `CurrentUserController`

--- Sources_v3/Controllers/CurrentUserController/CurrentUserController.swift
+++ Sources_v3/Controllers/CurrentUserController/CurrentUserController.swift
@@ -6,19 +6,39 @@ import CoreData
 import Foundation
 
 public extension _ChatClient {
-    /// Creates a new `CurrentUserControllerGeneric`
+    /// Creates a new `CurrentUserController` instance.
+    ///
     /// - Returns: A new instance of `ChannelController`.
+    ///
     func currentUserController() -> _CurrentChatUserController<ExtraData> {
         .init(client: self)
     }
 }
 
-/// A convenience typealias for `CurrentUserControllerGeneric` with `DefaultExtraData`
+/// `CurrentChatUserController` is a controller class which allows observing and mutating the currently logged-in
+/// user of `ChatClient`. You can also use it to explicitly connect/disconnect the `ChatClient`.
+///
+/// Learn more about `CurrentChatUserController` and its usage in our [cheat sheet](https://github.com/GetStream/stream-chat-swift/wiki/StreamChat-SDK-Cheat-Sheet#user).
+///
+/// - Note: `CurrentChatUserController` is a typealias of `_CurrentChatUserController` with default extra data. If you're using
+/// custom extra data, create your own typealias of `_CurrentChatUserController`.
+///
+/// Learn more about using custom extra data in our [cheat sheet](https://github.com/GetStream/stream-chat-swift/wiki/StreamChat-SDK-Cheat-Sheet#working-with-extra-data).
+///
 public typealias CurrentChatUserController = _CurrentChatUserController<DefaultExtraData>
 
-/// `CurrentUserControllerGeneric` allows to observer current user updates
+/// `CurrentChatUserController` is a controller class which allows observing and mutating the currently logged-in
+/// user of `ChatClient`. You can also use it to explicitly connect/disconnect the `ChatClient`.
+///
+/// Learn more about `CurrentChatUserController` and its usage in our [cheat sheet](https://github.com/GetStream/stream-chat-swift/wiki/StreamChat-SDK-Cheat-Sheet#user).
+///
+/// - Note: `_CurrentChatUserController` type is not meant to be used directly. If you're using default extra data, use
+/// `CurrentChatUserController` typealias instead. If you're using custom extra data, create your own typealias
+/// of `_CurrentChatUserController`.
+///
+/// Learn more about using custom extra data in our [cheat sheet](https://github.com/GetStream/stream-chat-swift/wiki/StreamChat-SDK-Cheat-Sheet#working-with-extra-data).
+///
 public class _CurrentChatUserController<ExtraData: ExtraDataTypes>: Controller, DelegateCallable, DataStoreProvider {
-    /// The queue which is used to perform callback calls. The default value is `.main`.
     public var callbackQueue: DispatchQueue = .main
     
     /// The `ChatClient` instance this controller belongs to.
@@ -46,6 +66,10 @@ public class _CurrentChatUserController<ExtraData: ExtraDataTypes>: Controller, 
         }
     
     /// The current connection status of the client.
+    ///
+    /// To observe changes of the connection status, set your class as a delegate of this controller or use the provided
+    /// `Combine` publishers.
+    ///
     public var connectionStatus: ConnectionStatus {
         .init(webSocketConnectionState: client.webSocketClient.connectionState)
     }
@@ -63,21 +87,26 @@ public class _CurrentChatUserController<ExtraData: ExtraDataTypes>: Controller, 
     // swiftlint:disable:next weak_delegate
     var multicastDelegate: MulticastDelegate<AnyCurrentUserControllerDelegate<ExtraData>> = .init()
     
-    /// The currently logged-in user.
+    /// The currently logged-in user. `nil` if the connection hasn't been fully established yet, or the connection
+    /// wasn't successful.
     public var currentUser: _CurrentChatUser<ExtraData.User>? {
         currentUserObserver.item
     }
 
     /// The unread messages and channels count for the current user.
-    /// Returns `noUnread` if `currentUser` doesn't exist.
+    ///
+    /// Returns `noUnread` if `currentUser` doesn't exist yet.
+    ///
     public var unreadCount: UnreadCount {
         currentUser?.unreadCount ?? .noUnread
     }
 
     /// Creates a new `CurrentUserControllerGeneric`.
+    ///
     /// - Parameters:
     ///   - client: The `Client` instance this controller belongs to.
     ///   - environment: The source of internal dependencies
+    ///
     init(client: _ChatClient<ExtraData>, environment: Environment = .init()) {
         self.client = client
         self.environment = environment
@@ -128,11 +157,13 @@ public class _CurrentChatUserController<ExtraData: ExtraDataTypes>: Controller, 
 // MARK: - Set current user
 
 public extension _CurrentChatUserController {
-    /// Connects a client the controller owns to the chat servers.
-    /// When the connection is established, `Client` starts receiving chat updates.
+    /// Connects the chat client the controller represents to the chat servers.
+    ///
+    /// When the connection is established, `ChatClient` starts receiving chat updates, and `currentUser` variable is available.
     ///
     /// - Parameter completion: Called when the connection is established. If the connection fails, the completion is
     /// called with an error.
+    ///
     func connect(completion: ((Error?) -> Void)? = nil) {
         guard client.connectionId == nil else {
             log.warning("The client is already connected. Skipping the `connect` call.")
@@ -152,7 +183,8 @@ public extension _CurrentChatUserController {
         client.webSocketClient.connect()
     }
     
-    /// Disconnects a client the controller owns from the chat servers. No further updates from the servers are received.
+    /// Disconnects the chat client the controller represents from the chat servers. No further updates from the servers
+    /// are received.
     func disconnect() {
         // Disconnect the web socket
         client.webSocketClient.disconnect(source: .userInitiated)
@@ -165,14 +197,14 @@ public extension _CurrentChatUserController {
         client.connectionIdWaiters.removeAll()
     }
     
-    /// Sets a new anonymous as a current user.
+    /// Sets a new anonymous user as the current user.
     ///
     /// Anonymous users have limited set of permissions. A typical use case for anonymous users are livestream channels,
     /// where they are allowed to read the conversation.
     ///
-    /// - Parameters:
-    ///   - completion: Called when the new anonymous user is set. If setting up the new user fails, the completion
+    /// - Parameter completion: Called when the new anonymous user is set. If setting up the new user fails, the completion
     /// is called with an error.
+    ///
     func setAnonymousUser(completion: ((Error?) -> Void)? = nil) {
         disconnect()
         prepareEnvironmentForNewUser(userId: .anonymous, role: .anonymous, extraData: nil) { error in
@@ -185,10 +217,9 @@ public extension _CurrentChatUserController {
         }
     }
     
-    /// Sets a new **guest** user as a current user.
+    /// Sets a new **guest** user as the current user.
     ///
-    /// Guest sessions do not require any server-side authentication.
-    /// Guest users have a limited set of permissions.
+    /// Guest sessions do not require any server-side authentication. Guest users have a limited set of permissions.
     ///
     /// - Parameters:
     ///   - userId: The new guest-user identifier.
@@ -215,20 +246,21 @@ public extension _CurrentChatUserController {
         }
     }
     
-    /// Sets a new current user.
-    ///
-    /// - Important: Setting a new user disconnects all the existing controllers. You should create new controllers
-    /// if you want to keep receiving updates about the newly set user.
+    /// Sets a new current user of the `ChatClient`.
     ///
     /// - Parameters:
     ///   - userId: The id of the new current user.
+    ///
     ///   - userExtraData: You can optionally provide additional data to be set for the user. This is an equivalent of
     ///   setting the current user detail data manually using `CurrentUserController`.
+    ///
     ///   - token: You can provide a token which is used for user authentication. If the `token` is not explicitly provided,
     ///   the client uses `ChatClientConfig.tokenProvider` to obtain a token. If you haven't specified the token provider,
     ///   providing a token explicitly is required. In case both the `token` and `ChatClientConfig.tokenProvider` is specified,
     ///   the `token` value is used.
+    ///
     ///   - completion: Called when the new user is successfully set.
+    ///
     func setUser(
         userId: UserId,
         userExtraData: ExtraData.User? = nil,
@@ -317,15 +349,16 @@ private extension _CurrentChatUserController {
 
 // MARK: - Delegates
 
-/// `CurrentUserController` uses this protocol to communicate changes to its delegate.
+/// `CurrentChatUserController` uses this protocol to communicate changes to its delegate.
 ///
 /// This protocol can be used only when no custom extra data are specified.
-/// If you're using custom extra data types, please use `CurrentUserControllerDelegateGeneric` instead.
+/// If you're using custom extra data types, please use `_CurrentChatUserControllerDelegate` instead.
+///
 public protocol CurrentChatUserControllerDelegate: AnyObject {
     /// The controller observed a change in the `UnreadCount`.
     func currentUserController(_ controller: CurrentChatUserController, didChangeCurrentUserUnreadCount: UnreadCount)
     
-    /// The controller observed a change in the `CurrentUser` entity.
+    /// The controller observed a change in the `CurrentChatUser` entity.
     func currentUserController(_ controller: CurrentChatUserController, didChangeCurrentUser: EntityChange<CurrentChatUser>)
     
     /// The controller observed a change in connection status.
@@ -340,10 +373,11 @@ public extension CurrentChatUserControllerDelegate {
     func currentUserController(_ controller: CurrentChatUserController, didUpdateConnectionStatus status: ConnectionStatus) {}
 }
 
-/// `CurrentUserControllerGeneric` uses this protocol to communicate changes to its delegate.
+/// `CurrentChatUserController` uses this protocol to communicate changes to its delegate.
 ///
 /// If you're **not** using custom extra data types, you can use a convenience version of this protocol
-/// named `CurrentUserControllerDelegate`, which hides the generic types, and make the usage easier.
+/// named `CurrentChatUserControllerDelegate`, which hides the generic types, and make the usage easier.
+///
 public protocol _CurrentChatUserControllerDelegate: AnyObject {
     associatedtype ExtraData: ExtraDataTypes
     
