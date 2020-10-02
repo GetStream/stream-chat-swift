0ebaf8b896233f63b6fb8dc5eac3fc0d0aabea30
Pavel Evsaev <pavel.evsaev@gmail.com>
2020-10-02 15:56:38 +0300

    Introduce dummy `MutedUser`

---
+++ Tests_v3/Dummy data/MutedUser.swift
@@ -0,0 +1,31 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+@testable import StreamChatClient
+
+extension MutedUser {
+    /// Returns a muted user with the given `userId` and `extraData`
+    static func dummy(
+        userId: UserId,
+        extraData: ExtraData = .defaultValue
+    ) -> Self {
+        .init(
+            mutedUser: .init(
+                id: userId,
+                role: .user,
+                createdAt: .unique,
+                updatedAt: .unique,
+                lastActiveAt: .unique,
+                isOnline: true,
+                isInvisible: true,
+                isBanned: true,
+                teams: [],
+                extraData: extraData
+            ),
+            created: .unique,
+            updated: .unique
+        )
+    }
+}
