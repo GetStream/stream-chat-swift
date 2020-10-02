6d672bbe84574bcdcde9c1a0ec6b355bb737aafd
Pavel Evsaev <pavel.evsaev@gmail.com>
2020-10-02 15:55:38 +0300

    Introduce dummy `NameAndImageExtraData`

---
+++ Tests_v3/Dummy data/ExtraData.swift
@@ -0,0 +1,13 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+@testable import StreamChatClient
+
+extension NameAndImageExtraData {
+    /// Returns a dummy data with unique `name` and `imageURL`
+    static var dummy: Self {
+        .init(name: .unique, imageURL: .unique())
+    }
+}
