6c6287dfe798b6fab6b7a6963c68959aa97619b7
Pavel Evsaev <pavel.evsaev@gmail.com>
2020-10-05 14:27:46 +0300

    Implement `SwiftUI` wrapper for `ChatUserController`

---
+++ Sources_v3/Controllers/UserController/UserController+SwiftUI.swift
@@ -0,0 +1,48 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+@available(iOS 13, *)
+extension _ChatUserController {
+    /// A wrapper object that exposes the controller variables in the form of `ObservableObject` to be used in SwiftUI.
+    public var observableObject: ObservableObject { .init(controller: self) }
+    
+    /// A wrapper object for `ChatUserController` type which makes it possible to use the controller comfortably in SwiftUI.
+    public class ObservableObject: SwiftUI.ObservableObject {
+        /// The underlying controller. You can still access it and call methods on it.
+        public let controller: _ChatUserController
+        
+        /// The user matching the `userId`.
+        @Published public private(set) var user: _ChatUser<ExtraData.User>?
+        
+        /// The current state of the controller.
+        @Published public private(set) var state: DataController.State
+        
+        /// Creates a new `ObservableObject` wrapper with the provided controller instance.
+        init(controller: _ChatUserController<ExtraData>) {
+            self.controller = controller
+            state = controller.state
+            
+            controller.multicastDelegate.additionalDelegates.append(AnyChatUserControllerDelegate(self))
+            
+            user = controller.user
+        }
+    }
+}
+
+@available(iOS 13, *)
+extension _ChatUserController.ObservableObject: _ChatUserControllerDelegate {
+    public func userController(
+        _ controller: _ChatUserController<ExtraData>,
+        didUpdateUser change: EntityChange<_ChatUser<ExtraData.User>>
+    ) {
+        user = change.item
+    }
+    
+    public func controller(_ controller: DataController, didChangeState state: DataController.State) {
+        self.state = state
+    }
+}
--- Sources_v3/Controllers/UserController/UserController.swift
+++ Sources_v3/Controllers/UserController/UserController.swift
@@ -72,6 +72,12 @@ public class _ChatUserController<ExtraData: ExtraDataTypes>: DataController, Del
         }
     
     private let environment: Environment
+    
+    /// An internal backing object for all publicly available Combine publishers. We use it to simplify the way we expose
+    /// publishers. Instead of creating custom `Publisher` types, we use `CurrentValueSubject` and `PassthroughSubject` internally,
+    /// and expose the published values by mapping them to a read-only `AnyPublisher` type.
+    @available(iOS 13, *)
+    lazy var basePublishers: BasePublishers = .init(controller: self)
 
     /// Creates a new `_ChatUserController`
     /// - Parameters:
