372fca242ef2bedda8efeae4897a22f08a8d40ac
Pavel Lopatine <mantergo@gmail.com>
2020-10-05 17:48:22 +0300

    Add `SwiftUI` wrapper for `UserListController`

---
+++ Sources_v3/Controllers/UserListController/UserListController+SwiftUI.swift
@@ -0,0 +1,49 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+@available(iOS 13, *)
+extension _ChatUserListController {
+    /// A wrapper object that exposes the controller variables in the form of `ObservableObject` to be used in SwiftUI.
+    public var observableObject: ObservableObject { .init(controller: self) }
+    
+    /// A wrapper object for `UserListController` type which makes it possible to use the controller comfortably in SwiftUI.
+    public class ObservableObject: SwiftUI.ObservableObject {
+        /// The underlying controller. You can still access it and call methods on it.
+        public let controller: _ChatUserListController
+        
+        /// The users matching the query.
+        @Published public private(set) var users: [_ChatUser<ExtraData.User>] = []
+        
+        /// The current state of the Controller.
+        @Published public private(set) var state: DataController.State
+        
+        /// Creates a new `ObservableObject` wrapper with the provided controller instance.
+        init(controller: _ChatUserListController<ExtraData>) {
+            self.controller = controller
+            state = controller.state
+            
+            controller.multicastDelegate.additionalDelegates.append(AnyUserListControllerDelegate(self))
+            users = controller.users
+        }
+    }
+}
+
+@available(iOS 13, *)
+extension _ChatUserListController.ObservableObject: _ChatUserListControllerDelegate {
+    public func controller(
+        _ controller: _ChatUserListController<ExtraData>,
+        didChangeUsers changes: [ListChange<_ChatUser<ExtraData.User>>]
+    ) {
+        // We don't care about detailed changes. We just need to update the `users` property and keep SwiftUI
+        // deal with the rest.
+        users = controller.users
+    }
+    
+    public func controller(_ controller: DataController, didChangeState state: DataController.State) {
+        self.state = state
+    }
+}
