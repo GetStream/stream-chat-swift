edb293054ba74c7c24ae9dcf4bac1810419d1d66
Vojta Stavik <stavik@outlook.com>
2020-10-05 15:02:36 +0200

    Add helper for safely calling sync on main queue

---
+++ Sources_v3/Utils/MainQueue+Synchronous.swift
@@ -0,0 +1,22 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+
+extension DispatchQueue {
+    /// Synchronously performs the provided action on the main thread.
+    ///
+    /// Performing this action is safe because the function checks the current thread, and if it's currently in the main
+    /// one, it performs the action safely without dead-locking the thread.
+    ///
+    static func performSynchronouslyOnMainQueue(_ action: () throws -> Void) rethrows {
+        if Thread.current.isMainThread {
+            try action()
+        } else {
+            try DispatchQueue.main.sync {
+                try action()
+            }
+        }
+    }
+}
