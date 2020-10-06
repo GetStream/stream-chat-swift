19096352e4162557fe8ef4a8fb360dc007b38023
Pavel Evsaev <pavel.evsaev@gmail.com>
2020-10-06 17:44:15 +0300

    Introduce `ChannelMemberBanRequestPayload`

---
+++ Sources_v3/APIClient/Endpoints/Requests/ChannelMemberBanRequestPayload_Tests.swift
@@ -0,0 +1,35 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+@testable import StreamChatClient
+import XCTest
+
+final class ChannelMemberBanRequestPayload_Tests: XCTestCase {
+    func test_payload_isBuiltAndEncodedCorrectly() throws {
+        let userId: UserId = .unique
+        let cid: ChannelId = .unique
+        let timeoutInMinutes = 15
+        let reason: String = .unique
+        
+        // Build the payload.
+        let payload = ChannelMemberBanRequestPayload(
+            userId: userId,
+            cid: cid,
+            timeoutInMinutes: timeoutInMinutes,
+            reason: reason
+        )
+        
+        // Encode the payload.
+        let json = try JSONEncoder.default.encode(payload)
+        
+        // Assert encoding is correct.
+        AssertJSONEqual(json, [
+            "target_user_id": userId,
+            "type": cid.type.rawValue,
+            "id": cid.id,
+            "timeout": timeoutInMinutes,
+            "reason": reason
+        ])
+    }
+}
