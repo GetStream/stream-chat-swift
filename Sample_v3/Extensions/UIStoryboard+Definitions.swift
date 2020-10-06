c3ac3b011ee29654b9a7084bf7786707d79bef04
Pavel Lopatine <mantergo@gmail.com>
2020-10-06 13:18:22 +0300

    Expose `UserListController` in `UIKit+Combine` sample

--- Sample_v3/Extensions/UIStoryboard+Definitions.swift
+++ Sample_v3/Extensions/UIStoryboard+Definitions.swift
@@ -8,4 +8,5 @@ extension UIStoryboard {
     static let main = UIStoryboard(name: "Main", bundle: nil)
     static let settings = UIStoryboard(name: "Settings", bundle: nil)
     static let simpleChat = UIStoryboard(name: "SimpleChat", bundle: nil)
+    static let combineSimpleChat = UIStoryboard(name: "CombineSimpleChat", bundle: nil)
 }
