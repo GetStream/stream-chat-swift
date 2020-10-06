a1e2825b71b389c1afa916e8016b8ba3211afafe
Pavel Evsaev <pavel.evsaev@gmail.com>
2020-10-06 16:33:58 +0300

    Introduce `ChannelMemberListPayload`

---
+++ Sources_v3/APIClient/Endpoints/Payloads/ChannelMemberListPayload_Tests.swift
@@ -0,0 +1,14 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+@testable import StreamChatClient
+import XCTest
+
+final class ChannelMemberListPayload_Tests: XCTestCase {
+    func test_queryJSON_isDeserialized_withDefaultExtraData() throws {
+        let json = XCTestCase.mockData(fromFile: "ChannelMembersQuery", extension: "json")
+        let payload = try JSONDecoder.default.decode(ChannelMemberListPayload<DefaultExtraData.User>.self, from: json)
+        XCTAssertEqual(payload.members.count, 1)
+    }
+}
