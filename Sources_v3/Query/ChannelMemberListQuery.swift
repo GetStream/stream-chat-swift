f731f5d0ecf3aa1a413ddb8aa4931b03ce4cdd0c
Pavel Evsaev <pavel.evsaev@gmail.com>
2020-10-06 16:29:25 +0300

    Introduce `ChannelMemberListQuery`

---
+++ Sources_v3/Query/ChannelMemberListQuery.swift
@@ -0,0 +1,73 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+
+/// A query type used for fetching channel members from the backend.
+public struct ChannelMemberListQuery: Encodable {
+    private enum CodingKeys: String, CodingKey {
+        case filter = "filter_conditions"
+        case sort
+        case channelId = "id"
+        case channelType = "type"
+    }
+    
+    /// A channel identifier the members should be fetched for.
+    public let cid: ChannelId
+    /// A filter for the query (see `Filter`).
+    public let filter: Filter
+    /// A sorting for the query (see `Sorting`).
+    public let sort: [Sorting<ChannelMemberListSortingKey>]
+    /// A pagination.
+    public var pagination: Pagination
+    
+    /// Creates new `ChannelMemberListQuery` instance.
+    /// - Parameters:
+    ///   - cid: The channel identifier.
+    ///   - filter: The members filter.
+    ///   - sort: The sorting for members list.
+    ///   - pagination: The pagination.
+    public init(
+        cid: ChannelId,
+        filter: Filter,
+        sort: [Sorting<ChannelMemberListSortingKey>] = [],
+        pagination: Pagination = [.channelMembersPageSize]
+    ) {
+        self.cid = cid
+        self.filter = filter
+        self.sort = sort
+        self.pagination = pagination
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        try container.encode(cid.id, forKey: .channelId)
+        try container.encode(cid.type, forKey: .channelType)
+        try container.encode(filter, forKey: .filter)
+        try pagination.encode(to: encoder)
+        if !sort.isEmpty { try container.encode(sort, forKey: .sort) }
+    }
+}
+
+extension ChannelMemberListQuery {
+    var hash: String {
+        [
+            cid.rawValue,
+            filter.filterHash,
+            sort.map(\.description).joined()
+        ].joined(separator: "-")
+    }
+}
+
+extension ChannelMemberListQuery {
+    /// Builds `ChannelMemberListQuery` for a single member in a specific channel
+    /// - Parameters:
+    ///   - userId: The user identifier.
+    ///   - cid: The channel identifier.
+    /// - Returns: `ChannelMemberListQuery` for a specific user in a specific channel
+    static func channelMember(userId: UserId, cid: ChannelId) -> Self {
+        .init(cid: cid, filter: .equal("id", to: userId))
+    }
+}
---
+++ Sources_v3/Query/ChannelMemberListQuery_Tests.swift
@@ -0,0 +1,101 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+@testable import StreamChatClient
+import XCTest
+
+final class ChannelMemberListQuery_Tests: XCTestCase {
+    func test_query_isEncodedCorrectly() throws {
+        // Create the query.
+        let query = ChannelMemberListQuery(
+            cid: .unique,
+            filter: .contains("name", "a"),
+            sort: [.init(key: .createdAt, isAscending: true)],
+            pagination: [.offset(3)]
+        )
+
+        // Encode the query.
+        let json = try JSONEncoder.default.encode(query)
+
+        // Assert query is encoded correctly.
+        AssertJSONEqual(json, [
+            "id": query.cid.id,
+            "type": query.cid.type.rawValue,
+            "filter_conditions": ["name": ["$contains": "a"]],
+            "sort": [["key": "created_at", "direction": 1]] as NSArray,
+            "offset": 3
+        ])
+    }
+    
+    func test_hash_isCalculatedCorrectly() {
+        // Create the query.
+        let query = ChannelMemberListQuery(
+            cid: .unique,
+            filter: .contains("name", "a"),
+            sort: [.init(key: .createdAt, isAscending: true)]
+        )
+        
+        let expectedHash = [
+            query.cid.rawValue,
+            query.filter.filterHash,
+            query.sort.map(\.description).joined()
+        ].joined(separator: "-")
+        
+        // Assert hash is calculated correctly.
+        XCTAssertEqual(query.hash, expectedHash)
+    }
+    
+    func test_emptySorting_isNotEncoded() throws {
+        // Create the query without any sort options.
+        let query = ChannelMemberListQuery(
+            cid: .unique,
+            filter: .contains("name", "a"),
+            pagination: [.offset(3)]
+        )
+
+        // Encode the query.
+        let json = try JSONEncoder.default.encode(query)
+
+        // Assert encoding does not contain `sort` key.
+        AssertJSONEqual(json, [
+            "id": query.cid.id,
+            "type": query.cid.type.rawValue,
+            "filter_conditions": ["name": ["$contains": "a"]],
+            "offset": 3
+        ])
+    }
+    
+    func test_defaultPageSizeIsUsed_ifNotSpecified() throws {
+        // Create the query with default params.
+        let query = ChannelMemberListQuery(
+            cid: .unique,
+            filter: .contains("name", "a")
+        )
+
+        // Encode the query.
+        let json = try JSONEncoder.default.encode(query)
+
+        // Assert encoding does not contain `sort` key AND has default page size.
+        AssertJSONEqual(json, [
+            "id": query.cid.id,
+            "type": query.cid.type.rawValue,
+            "filter_conditions": ["name": ["$contains": "a"]],
+            "limit": PaginationOption.channelMembersPageSize.limit!
+        ])
+    }
+    
+    func test_singleMemberQuery_worksCorrectly() throws {
+        let userId: UserId = .unique
+        let cid: ChannelId = .unique
+
+        let actual = ChannelMemberListQuery.channelMember(userId: userId, cid: cid)
+        let actualJSON = try JSONEncoder.default.encode(actual)
+
+        let expected = ChannelMemberListQuery(cid: cid, filter: .equal("id", to: userId))
+        let expectedJSON = try JSONEncoder.default.encode(expected)
+    
+        // Assert queries match
+        AssertJSONEqual(actualJSON, expectedJSON)
+    }
+}
