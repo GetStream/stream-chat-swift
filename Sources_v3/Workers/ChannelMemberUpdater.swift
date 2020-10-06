1f6ce4a313b454d260c88bfc8c545c9fe9cf9b44
Pavel Evsaev <pavel.evsaev@gmail.com>
2020-10-06 17:47:15 +0300

    Introduce `ChannelMemberUpdater`

---
+++ Sources_v3/Workers/ChannelMemberUpdater.swift
@@ -0,0 +1,41 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+
+/// Makes channel member related calls to the backend.
+class ChannelMemberUpdater: Worker {
+    /// Bans the user in the channel for a specific # of minutes.
+    /// - Parameters:
+    ///   - userId: The user identifier to ban.
+    ///   - cid: The channel identifier to ban in.
+    ///   - timeoutInMinutes: The # of minutes the user should be banned for.
+    ///   - reason: The ban reason.
+    ///   - completion: Called when the API call is finished. Called with `Error` if the remote update fails.
+    func banMember(
+        _ userId: UserId,
+        in cid: ChannelId,
+        for timeoutInMinutes: Int? = nil,
+        reason: String? = nil,
+        completion: ((Error?) -> Void)? = nil
+    ) {
+        apiClient.request(endpoint: .banMember(userId, cid: cid, timeoutInMinutes: timeoutInMinutes, reason: reason)) {
+            completion?($0.error)
+        }
+    }
+    
+    /// Unbans the user in the channel.
+    /// - Parameters:
+    ///   - userId: The user identifier to unban.
+    ///   - cid: The channel identifier to unban in.
+    ///   - completion: Called when the API call is finished. Called with `Error` if the remote update fails.
+    func unbanMember(
+        _ userId: UserId,
+        in cid: ChannelId, completion: ((Error?) -> Void)? = nil
+    ) {
+        apiClient.request(endpoint: .unbanMember(userId, cid: cid)) {
+            completion?($0.error)
+        }
+    }
+}
