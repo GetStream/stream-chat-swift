69d4061ac90d0c5f5c2aff935d9e2e3321863c4d
Pavel Evsaev <pavel.evsaev@gmail.com>
2020-10-06 17:44:15 +0300

    Introduce `ChannelMemberBanRequestPayload`

---
+++ Sources_v3/APIClient/Endpoints/Requests/ChannelMemberBanRequestPayload.swift
@@ -0,0 +1,35 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+
+/// The type describes the outgoing JSON to `moderation/ban` endpoint
+struct ChannelMemberBanRequestPayload: Encodable {
+    private enum CodingKeys: String, CodingKey {
+        case userId = "target_user_id"
+        case channelType = "type"
+        case channelId = "id"
+        case timeoutInMinutes = "timeout"
+        case reason
+    }
+    
+    let userId: String
+    let channelType: ChannelType
+    let channelId: String
+    let timeoutInMinutes: Int?
+    let reason: String?
+    
+    init(
+        userId: UserId,
+        cid: ChannelId,
+        timeoutInMinutes: Int? = nil,
+        reason: String? = nil
+    ) {
+        self.userId = userId
+        channelType = cid.type
+        channelId = cid.id
+        self.timeoutInMinutes = timeoutInMinutes
+        self.reason = reason
+    }
+}
