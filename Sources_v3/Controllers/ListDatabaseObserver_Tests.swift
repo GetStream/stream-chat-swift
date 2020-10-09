cc985467a6707399d7bbfa1d6b31f70134e6e0a1
Pavel Evsaev <pavel.evsaev@gmail.com>
2020-10-09 15:15:52 +0300

    Introduce `item` and `fieldChange` for `ListChange`

--- Sources_v3/Controllers/ListDatabaseObserver_Tests.swift
+++ Sources_v3/Controllers/ListDatabaseObserver_Tests.swift
@@ -32,6 +32,52 @@ final class ListChange_Tests: XCTestCase {
             XCTAssertEqual(change.description, description)
         }
     }
+    
+    func test_item() {
+        let insertedItem: String = .unique
+        let updatedItem: String = .unique
+        let removedItem: String = .unique
+        let movedItem: String = .unique
+
+        XCTAssertEqual(ListChange.insert(insertedItem, index: [0, 0]).item, insertedItem)
+        XCTAssertEqual(ListChange.update(updatedItem, index: [0, 0]).item, updatedItem)
+        XCTAssertEqual(ListChange.remove(removedItem, index: [0, 0]).item, removedItem)
+        XCTAssertEqual(ListChange.move(movedItem, fromIndex: [0, 0], toIndex: [0, 1]).item, movedItem)
+    }
+    
+    func test_fieldChange() {
+        let insertedItem: MemberPayload<DefaultExtraData.User> = .dummy()
+        let insertedAt = IndexPath(row: 1, section: 1)
+
+        let updatedItem: MemberPayload<DefaultExtraData.User> = .dummy()
+        let updatedAt = IndexPath(row: 2, section: 3)
+
+        let removedItem: MemberPayload<DefaultExtraData.User> = .dummy()
+        let removedAt = IndexPath(row: 3, section: 4)
+
+        let movedItem: MemberPayload<DefaultExtraData.User> = .dummy()
+        let movedFrom = IndexPath(row: 5, section: 6)
+        let movedTo = IndexPath(row: 7, section: 8)
+
+        let path = \MemberPayload<DefaultExtraData.User>.user.id
+
+        XCTAssertEqual(
+            ListChange.insert(insertedItem, index: insertedAt).fieldChange(path),
+            .insert(insertedItem.user.id, index: insertedAt)
+        )
+        XCTAssertEqual(
+            ListChange.update(updatedItem, index: updatedAt).fieldChange(path),
+            .update(updatedItem.user.id, index: updatedAt)
+        )
+        XCTAssertEqual(
+            ListChange.remove(removedItem, index: removedAt).fieldChange(path),
+            .remove(removedItem.user.id, index: removedAt)
+        )
+        XCTAssertEqual(
+            ListChange.move(movedItem, fromIndex: movedFrom, toIndex: movedTo).fieldChange(path),
+            .move(movedItem.user.id, fromIndex: movedFrom, toIndex: movedTo)
+        )
+    }
 }
 
 class ListDatabaseObserver_Tests: XCTestCase {
