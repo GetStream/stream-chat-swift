c4c314ff3308ecbb6501ec720a482f4280f90e9d
Bahadir Oncel <bahadir@getstream.io>
2020-10-09 16:01:54 +0300

    Introduce CompareJSONEqual
    Comparing Encodable's with String(describing:) solution was failing with encodable dicts with more than 1 key, now we can wrap them as AnyEncodable and compare as we like

--- Tests_v3/Custom Assertions/AssertJSONEqual.swift
+++ Tests_v3/Custom Assertions/AssertJSONEqual.swift
@@ -10,6 +10,24 @@ func error(domain: String, code: Int = -1, message: @autoclosure () -> String) -
     NSError(domain: domain, code: code, userInfo: ["message:": message()])
 }
 
+/// Compares the given 2 JSON Serializations are equal, by creating JSON objects from Data and comparing dictionaries.
+/// Recursively calls itself for nested dictionaries.
+/// - Parameters:
+///   - expression1: JSON object 1, as Data. From string, you can do `Data(jsonString.utf8)`
+///   - expression2: JSON object 2.
+func CompareJSONEqual(
+    _ expression1: @autoclosure () throws -> Data,
+    _ expression2: @autoclosure () throws -> [String: Any]
+) throws {
+    guard var json = try JSONSerialization.jsonObject(with: expression1()) as? [String: Any] else {
+        throw error(domain: "CompareJSONEqual", message: "The first expression is not a valid json object!")
+    }
+    
+    preprocessBoolValues(&json)
+    
+    try CompareJSONEqual(json, try expression2())
+}
+
 /// Asserts the given 2 JSON Serializations are equal, by creating JSON objects from Data and comparing dictionaries.
 /// Recursively calls itself for nested dictionaries.
 /// - Parameters:
@@ -24,17 +42,29 @@ func AssertJSONEqual(
     line: UInt = #line
 ) {
     do {
-        guard var json = try JSONSerialization.jsonObject(with: expression1()) as? [String: Any] else {
-            throw error(domain: "AssertJSONEqual", message: "The first expression is not a valid json object!")
-        }
-        
-        preprocessBoolValues(&json)
-        
-        AssertJSONEqual(json, try expression2(), file: file, line: line)
-        
+        try CompareJSONEqual(expression1(), expression2())
     } catch {
         XCTFail("Error: \(error)", file: file, line: line)
     }
+}
+
+/// Compares the given 2 JSON Serializations are equal, by creating JSON objects from Data and comparing dictionaries.
+/// Recursively calls itself for nested dictionaries.
+/// - Parameters:
+///   - expression1: JSON object 1, as Data. From string, you can do `Data(jsonString.utf8)`
+///   - expression2: JSON object 2, as Data.
+func CompareJSONEqual(
+    _ expression1: @autoclosure () throws -> Data,
+    _ expression2: @autoclosure () throws -> Data
+) throws {
+    guard let json1 = try JSONSerialization.jsonObject(with: expression1()) as? [String: Any] else {
+        throw error(domain: "CompareJSONEqual", message: "First expression is not a valid json object!")
+    }
+    guard let json2 = try JSONSerialization.jsonObject(with: expression2()) as? [String: Any] else {
+        throw error(domain: "CompareJSONEqual", message: "Second expression is not a valid json object!")
+    }
+    
+    try CompareJSONEqual(json1, json2)
 }
 
 /// Asserts the given 2 JSON Serializations are equal, by creating JSON objects from Data and comparing dictionaries.
@@ -51,17 +81,48 @@ func AssertJSONEqual(
     line: UInt = #line
 ) {
     do {
-        guard let json1 = try JSONSerialization.jsonObject(with: expression1()) as? [String: Any] else {
-            throw error(domain: "AssertJSONEqual", message: "First expression is not a valid json object!")
-        }
-        guard let json2 = try JSONSerialization.jsonObject(with: expression2()) as? [String: Any] else {
-            throw error(domain: "AssertJSONEqual", message: "Second expression is not a valid json object!")
-        }
-        
-        AssertJSONEqual(json1, json2, file: file, line: line)
-        
+        try CompareJSONEqual(expression1(), expression2())
     } catch {
         XCTFail("Error: \(error)", file: file, line: line)
+    }
+}
+
+/// Compares the given 2 JSON Serializations are equal, by creating JSON objects from Data and comparing dictionaries.
+/// Recursively calls itself for nested dictionaries.
+/// - Parameters:
+///   - expression1: JSON object 1
+///   - expression2: JSON object 2
+func CompareJSONEqual(
+    _ expression1: @autoclosure () throws -> [String: Any],
+    _ expression2: @autoclosure () throws -> [String: Any]
+) throws {
+    do {
+        let json1 = try expression1()
+        let json2 = try expression2()
+        
+        guard json1.keys == json2.keys else {
+            throw error(domain: "CompareJSONEqual", message: "JSON keys do not match")
+        }
+        
+        try json1.forEach { key, value in
+            guard let value2 = json2[key] else {
+                throw error(domain: "CompareJSONEqual", message: "Expression 2 does not have value for \(key)")
+            }
+            if let nestedDict1 = value as? [String: Any] {
+                if let nestedDict2 = value2 as? [String: Any] {
+                    try CompareJSONEqual(
+                        JSONSerialization.data(withJSONObject: nestedDict1),
+                        JSONSerialization.data(withJSONObject: nestedDict2)
+                    )
+                } else {
+                    throw error(domain: "CompareJSONEqual", message: "Values for key \(key) do not match")
+                }
+            } else if String(describing: value) != String(describing: value2) {
+                throw error(domain: "CompareJSONEqual", message: "Values for key \(key) do not match")
+            }
+        }
+    } catch {
+        throw error
     }
 }
 
@@ -79,32 +140,7 @@ func AssertJSONEqual(
     line: UInt = #line
 ) {
     do {
-        let json1 = try expression1()
-        let json2 = try expression2()
-        
-        guard json1.keys == json2.keys else {
-            throw error(domain: "AssertJSONEqual", message: "JSON keys do not match")
-        }
-        
-        try json1.forEach { key, value in
-            guard let value2 = json2[key] else {
-                throw error(domain: "AssertJSONEqual", message: "Expression 2 does not have value for \(key)")
-            }
-            if let nestedDict1 = value as? [String: Any] {
-                if let nestedDict2 = value2 as? [String: Any] {
-                    try AssertJSONEqual(
-                        JSONSerialization.data(withJSONObject: nestedDict1),
-                        JSONSerialization.data(withJSONObject: nestedDict2),
-                        file: file,
-                        line: line
-                    )
-                } else {
-                    throw error(domain: "AssertJSONEqual", message: "Values for key \(key) do not match")
-                }
-            } else if String(describing: value) != String(describing: value2) {
-                throw error(domain: "AssertJSONEqual", message: "Values for key \(key) do not match")
-            }
-        }
+        try CompareJSONEqual(expression1(), expression2())
     } catch {
         XCTFail("Error: \(error)", file: file, line: line)
     }
