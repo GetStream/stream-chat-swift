adb1a1ed2911ec7e72cac295406023540c15b465
Pavel Evsaev <pavel.evsaev@gmail.com>
2020-10-09 11:06:49 +0300

    Introduce mock `ListDatabaseObserver`

---
+++ Sources_v3/Controllers/ListDatabaseObserver_Mock.swift
@@ -0,0 +1,19 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import CoreData
+@testable import StreamChatClient
+import XCTest
+
+final class ListDatabaseObserverMock<Item, DTO: NSManagedObject>: ListDatabaseObserver<Item, DTO> {
+    var synchronizeError: Error?
+    
+    override func startObserving() throws {
+        if let error = synchronizeError {
+            throw error
+        } else {
+            try super.startObserving()
+        }
+    }
+}
