946e5ce4dcd2b275edcd8b5fa6b7a914dc91808c
Bahadir Oncel <bahadir@getstream.io>
2020-10-09 16:09:48 +0300

    Introduce DevicePayloads and tests

---
+++ Sources_v3/APIClient/Endpoints/Payloads/DevicePayloads.swift
@@ -0,0 +1,27 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+
+struct DevicePayload: Decodable, Equatable {
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case createdAt = "created_at"
+    }
+    
+    /// Device identifier.
+    let id: DeviceId
+    /// Date the device was created for the user.
+    let createdAt: Date?
+    
+    init(id: DeviceId, createdAt: Date? = .init()) {
+        self.id = id
+        self.createdAt = createdAt
+    }
+}
+
+struct DeviceListPayload: Decodable {
+    /// List of devices belonging to user.
+    let devices: [DevicePayload]
+}
