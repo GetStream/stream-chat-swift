46162d304556c4fa9f68daa37ad29f8755a30d41
Pavel Evsaev <pavel.evsaev@gmail.com>
2020-10-14 13:13:00 +0300

    Implement `SwiftUI` wrapper for `ChatChannelMemberListController`

---
+++ Sources_v3/Controllers/MemberListController/MemberListController+SwiftUI.swift
@@ -0,0 +1,49 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+@available(iOS 13, *)
+extension _ChatChannelMemberListController {
+    /// A wrapper object that exposes the controller variables in the form of `ObservableObject` to be used in SwiftUI.
+    public var observableObject: ObservableObject { .init(controller: self) }
+    
+    /// A wrapper object for `_ChatChannelMemberListController` type which makes it possible to use the controller
+    /// comfortably in SwiftUI.
+    public class ObservableObject: SwiftUI.ObservableObject {
+        /// The underlying controller. You can still access it and call methods on it.
+        public let controller: _ChatChannelMemberListController
+        
+        /// The channel members.
+        @Published public private(set) var members: [_ChatChannelMember<ExtraData.User>] = []
+        
+        /// The current state of the controller.
+        @Published public private(set) var state: DataController.State
+        
+        /// Creates a new `ObservableObject` wrapper with the provided controller instance.
+        init(controller: _ChatChannelMemberListController<ExtraData>) {
+            self.controller = controller
+            state = controller.state
+            
+            controller.multicastDelegate.additionalDelegates.append(AnyChatChannelMemberListControllerDelegate(self))
+            
+            members = controller.members
+        }
+    }
+}
+
+@available(iOS 13, *)
+extension _ChatChannelMemberListController.ObservableObject: _ChatChannelMemberListControllerDelegate {
+    public func memberListController(
+        _ controller: _ChatChannelMemberListController<ExtraData>,
+        didChangeMembers changes: [ListChange<_ChatChannelMember<ExtraData.User>>]
+    ) {
+        members = controller.members
+    }
+    
+    public func controller(_ controller: DataController, didChangeState state: DataController.State) {
+        self.state = state
+    }
+}
---
+++ Sources_v3/Controllers/MemberListController/MemberListController+SwiftUI_Tests.swift
@@ -0,0 +1,79 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+@testable import StreamChatClient
+import XCTest
+
+@available(iOS 13, *)
+final class MemberListController_SwiftUI_Tests: iOS13TestCase {
+    var query: ChannelMemberListQuery<DefaultExtraData.User>!
+    var memberListController: ChatChannelMemberListControllerMock!
+    
+    // MARK: - Setup
+    
+    override func setUp() {
+        super.setUp()
+        
+        query = .init(cid: .unique)
+        memberListController = .init(query: query, client: .mock)
+    }
+    
+    override func tearDown() {
+        query = nil
+        AssertAsync.canBeReleased(&memberListController)
+        
+        super.tearDown()
+    }
+    
+    // MARK: - Tests
+    
+    func test_controllerInitialValuesAreLoaded() {
+        // Simulate state and member list.
+        memberListController.state_simulated = .localDataFetched
+        memberListController.members_simulated = [.dummy, .dummy, .dummy]
+        
+        // Get an observable object.
+        let observableObject = memberListController.observableObject
+        
+        // Assert simulated values are forwarded to observable object.
+        XCTAssertEqual(observableObject.state, memberListController.state)
+        XCTAssertEqual(observableObject.members, memberListController.members)
+    }
+    
+    func test_observableObject_reactsToDelegateMemberChangesCallback() {
+        // Get an observable object.
+        let observableObject = memberListController.observableObject
+        
+        // Simulate member change.
+        let newMember: ChatChannelMember = .dummy
+        memberListController.members_simulated = [newMember]
+        memberListController.delegateCallback {
+            $0.memberListController(
+                self.memberListController,
+                didChangeMembers: [.insert(newMember, index: [0, 0])]
+            )
+        }
+        
+        // Simulate the changes are forwarded to observable object.
+        AssertAsync.willBeEqual(observableObject.members, [newMember])
+    }
+    
+    func test_observableObject_reactsToDelegateStateChangesCallback() {
+        // Get an observable object.
+        let observableObject = memberListController.observableObject
+        
+        // Simulate state change.
+        let newState: DataController.State = .remoteDataFetchFailed(ClientError(with: TestError()))
+        memberListController.state_simulated = newState
+        memberListController.delegateCallback {
+            $0.controller(
+                self.memberListController,
+                didChangeState: newState
+            )
+        }
+        
+        // Simulate the updated state is forwarded to observable object.
+        AssertAsync.willBeEqual(observableObject.state, newState)
+    }
+}
