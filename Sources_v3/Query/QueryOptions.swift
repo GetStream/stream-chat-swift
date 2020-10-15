48f366da0d42e5ec2c9ad6d6a246c38888801d53
Pavel Lopatine <mantergo@gmail.com>
2020-10-15 12:38:53 +0300

    Make `QueryOptions` internal

--- Sources_v3/Query/QueryOptions.swift
+++ Sources_v3/Query/QueryOptions.swift
@@ -5,32 +5,32 @@
 import Foundation
 
 /// Query options.
-public struct QueryOptions: OptionSet, Encodable {
+struct QueryOptions: OptionSet, Encodable {
     private enum CodingKeys: String, CodingKey {
         case state
         case watch
         case presence
     }
     
-    public let rawValue: Int
+    let rawValue: Int
     
     /// A query will return a channel state, e.g. messages.
-    public static let state = QueryOptions(rawValue: 1 << 0)
+    static let state = QueryOptions(rawValue: 1 << 0)
     
     /// Listen for a channel changes in real time, e.g. a new message event.
-    public static let watch = QueryOptions(rawValue: 1 << 1)
+    static let watch = QueryOptions(rawValue: 1 << 1)
     
     /// Get updates when the user goes offline/online.
-    public static let presence = QueryOptions(rawValue: 1 << 2)
+    static let presence = QueryOptions(rawValue: 1 << 2)
     
     /// Includes all query options: state, watch and presence.
-    public static let all: QueryOptions = [.state, .watch, .presence]
+    static let all: QueryOptions = [.state, .watch, .presence]
     
-    public init(rawValue: Int) {
+    init(rawValue: Int) {
         self.rawValue = rawValue
     }
     
-    public func encode(to encoder: Encoder) throws {
+    func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         
         if contains(.state) {
