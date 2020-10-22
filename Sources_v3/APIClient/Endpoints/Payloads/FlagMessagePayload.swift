b1553efad8c2aa1201b1eb17c28925f2576316b7
Pavel Evsaev <pavel.evsaev@gmail.com>
2020-10-22 15:08:11 +0300

    Introduce `FlagMessagePayload` type

---
+++ Sources_v3/APIClient/Endpoints/Payloads/FlagMessagePayload.swift
@@ -0,0 +1,38 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+
+/// The type describes the incoming JSON from `moderation/(un)flag` message endpoint.
+struct FlagMessagePayload<ExtraData: UserExtraData>: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case flag
+        case currentUser = "user"
+        case flaggedMessageId = "target_message_id"
+    }
+    
+    /// The payload of the current user who performed flag/unflag action.
+    let currentUser: CurrentUserPayload<ExtraData>
+    /// The `id` of the message which was flagged or unflagged.
+    let flaggedMessageId: MessageId
+    
+    init(from decoder: Decoder) throws {
+        let nestedContainer = try decoder
+            .container(keyedBy: CodingKeys.self)
+            .nestedContainer(keyedBy: CodingKeys.self, forKey: .flag)
+        
+        self.init(
+            currentUser: try nestedContainer.decode(CurrentUserPayload<ExtraData>.self, forKey: .currentUser),
+            flaggedMessageId: try nestedContainer.decode(MessageId.self, forKey: .flaggedMessageId)
+        )
+    }
+    
+    init(
+        currentUser: CurrentUserPayload<ExtraData>,
+        flaggedMessageId: MessageId
+    ) {
+        self.currentUser = currentUser
+        self.flaggedMessageId = flaggedMessageId
+    }
+}
