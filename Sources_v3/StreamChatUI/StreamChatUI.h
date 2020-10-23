4914d43de1f8059eb1a78f61ce3fbbe89171e89d
Bahadir Oncel <bahadir@getstream.io>
2020-10-23 17:35:12 +0200

    Add StreamChatUI target
    and also StreamChatUITests target

---
+++ Sources_v3/StreamChatUI/StreamChatUI.h
@@ -0,0 +1,19 @@
+//
+//  StreamChatUI.h
+//  StreamChatUI
+//
+//  Created by Bahadir Oncel on 23/10/2020.
+//  Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StreamChatUI.
+FOUNDATION_EXPORT double StreamChatUIVersionNumber;
+
+//! Project version string for StreamChatUI.
+FOUNDATION_EXPORT const unsigned char StreamChatUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StreamChatUI/PublicHeader.h>
+
+
