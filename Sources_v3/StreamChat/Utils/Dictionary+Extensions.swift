5e70bf00ff2444a40cc9c8fdabc5a0299f4213c1
Pavel Evsaev <pavel.evsaev@gmail.com>
2020-10-26 18:06:52 +0300

    Introduce `mapKeys` helper for `Dictionary` type

---
+++ Sources_v3/StreamChat/Utils/Dictionary+Extensions.swift
@@ -0,0 +1,11 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+
+extension Dictionary {
+    func mapKeys<TransformedKey: Hashable>(_ transform: (Key) -> TransformedKey) -> [TransformedKey: Value] {
+        .init(uniqueKeysWithValues: map { (transform($0.key), $0.value) })
+    }
+}
