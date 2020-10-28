9061bc41164a218a17a632adb123d2a28f6e93e2
Pavel Evsaev <pavel.evsaev@gmail.com>
2020-10-28 16:25:59 +0300

    Introduce `MessageReactionsMiddleware` type

---
+++ Sources_v3/StreamChat/WebSocketClient/EventMiddlewares/MessageReactionsMiddleware.swift
@@ -0,0 +1,46 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+
+/// The middleware listens for `EventWithReactionPayload` events and updates `MessageReactionDTO` accordingly.
+struct MessageReactionsMiddleware<ExtraData: ExtraDataTypes>: EventMiddleware {
+    let database: DatabaseContainer
+    
+    func handle(event: Event, completion: @escaping (Event?) -> Void) {
+        guard
+            let reactionEvent = event as? EventWithReactionPayload,
+            let payload = reactionEvent.payload as? EventPayload<ExtraData>,
+            let reaction = payload.reaction
+        else {
+            completion(event)
+            return
+        }
+        
+        database.write({ session in
+            if reactionEvent is ReactionNewEvent {
+                try session.saveReaction(payload: reaction)
+            } else if reactionEvent is ReactionUpdatedEvent {
+                try session.saveReaction(payload: reaction)
+            } else if reactionEvent is ReactionDeletedEvent {
+                guard
+                    let dto = session.reaction(
+                        messageId: reaction.messageId,
+                        userId: reaction.user.id,
+                        type: reaction.type
+                    )
+                else { return }
+                
+                session.delete(reaction: dto)
+            } else {
+                throw ClientError.Unexpected("Middleware has tried to handle unsupported event type.")
+            }
+        }, completion: { error in
+            if let error = error {
+                log.error("Failed to update message reaction in the database, error: \(error)")
+            }
+            completion(event)
+        })
+    }
+}
