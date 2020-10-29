2d09de445937183ec5d788c4cfecf3be9d799e33
Pavel Lopatine <mantergo@gmail.com>
2020-10-29 21:10:12 +0100

    Introduce `ChatMesssageAttachment` with underlying types

---
+++ Sources_v3/StreamChat/Models/Attachment_Tests.swift
@@ -0,0 +1,66 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+@testable import StreamChat
+import XCTest
+
+class Attachment_Tests: XCTestCase {
+    func test_type_encodedAndDecodedCorrectly() throws {
+        let types: [AttachmentType] = [.youtube, .imgur, .giphy, .custom("sticker")]
+        
+        // Different test for < iOS 13 because of decoding bug.
+        if #available(iOS 13, *) {
+            // Encode objects
+            let encoded = try types.map { try JSONEncoder.default.encode($0) }
+            
+            // Decode objects
+            let decoded = try encoded.map { try JSONDecoder().decode(AttachmentType.self, from: $0) }
+            
+            // Assert objects encoded and decoded correctly
+            XCTAssertEqual(types, decoded)
+        } else {
+            // Encoded strings
+            let encoded = types.map(JSONEncoder.default.encodedString)
+            
+            // Assert objects encoded correctly
+            XCTAssertEqual(types.map(\.rawValue), encoded)
+        }
+    }
+    
+    func test_action_encodedAndDecodedCorrectly() throws {
+        let action: AttachmentAction = .init(
+            name: .unique,
+            value: .unique,
+            style: .default,
+            type: .button,
+            text: .unique
+        )
+        
+        // Encode object
+        let encoded = try JSONEncoder.default.encode(action)
+        
+        // Decode object
+        let decoded = try JSONDecoder().decode(AttachmentAction.self, from: encoded)
+        
+        // Assert object encoded and decoded correctly
+        XCTAssertEqual(action, decoded)
+    }
+    
+    func test_file_encodedAndDecodedCorrectly() throws {
+        let file: AttachmentFile = .init(
+            type: .gif,
+            size: 1024,
+            mimeType: "image/gif"
+        )
+        
+        // Encode object
+        let encoded = try JSONEncoder.default.encode(file)
+        
+        // Decode object
+        let decoded = try JSONDecoder().decode(AttachmentFile.self, from: encoded)
+        
+        // Assert object encoded and decoded correctly
+        XCTAssertEqual(file, decoded)
+    }
+}
