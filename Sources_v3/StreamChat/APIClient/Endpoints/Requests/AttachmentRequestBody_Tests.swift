31f1b6bc2697773e0c2c54580e1ace027d7d1dcb
Pavel Lopatine <mantergo@gmail.com>
2020-10-29 12:22:22 +0100

    Introduce `AttachmentRequestBody`

---
+++ Sources_v3/StreamChat/APIClient/Endpoints/Requests/AttachmentRequestBody_Tests.swift
@@ -0,0 +1,38 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+@testable import StreamChat
+import XCTest
+
+class AttachmentRequestBody_Tests: XCTestCase {
+    func test_body_isBuiltAndEncodedCorrectly() throws {
+        let type: AttachmentType = .giphy
+        let title: String = .unique
+        let url: URL? = URL(string: "https://getstream.io")
+        let file: AttachmentFile = .init(type: .gif, size: 1024, mimeType: "gif")
+        
+        // Build the body.
+        let body = AttachmentRequestBody<DefaultExtraData.Attachment>(
+            type: type,
+            title: title,
+            url: url,
+            imageURL: url,
+            file: file,
+            extraData: .defaultValue
+        )
+    
+        // Encode the body.
+        let json = try JSONEncoder.default.encode(body)
+        
+        // Assert encoding is correct.
+        AssertJSONEqual(json, [
+            "type": type.rawValue!,
+            "title": title,
+            "asset_url": url!,
+            "image_url": url!,
+            "mime_type": file.mimeType!,
+            "file_size": file.size
+        ])
+    }
+}
