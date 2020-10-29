9bf0dc83d9654d480a72ac8066251a4539b84791
Bahadir Oncel <bahadir@getstream.io>
2020-10-29 11:21:55 +0100

    Add Xcode12 carthage script and use it
    ...for carthage_bootstrap lane. Reading here: https://github.com/Carthage/Carthage/blob/master/Documentation/Xcode12Workaround.md

--- carthage.sh
+++ carthage.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env bash
+# carthage.sh
+# Usage example: ./carthage.sh build --platform iOS
 
 set -euo pipefail
 
@@ -15,4 +16,4 @@ echo 'EXCLUDED_ARCHS__EFFECTIVE_PLATFORM_SUFFIX_simulator__NATIVE_ARCH_64_BIT_x8
 echo 'EXCLUDED_ARCHS = $(inherited) $(EXCLUDED_ARCHS__EFFECTIVE_PLATFORM_SUFFIX_$(EFFECTIVE_PLATFORM_SUFFIX)__NATIVE_ARCH_64_BIT_$(NATIVE_ARCH_64_BIT)__XCODE_$(XCODE_VERSION_MAJOR))' >> $xcconfig
 
 export XCODE_XCCONFIG_FILE="$xcconfig"
-carthage bootstrap
+carthage "$@"
