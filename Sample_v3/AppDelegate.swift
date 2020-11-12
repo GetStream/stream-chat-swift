b0e5d4ed82df9dd8140b5946980a91708ac598d4
Pavel Evsaev <pavel.evsaev@gmail.com>
2020-11-12 14:46:12 +0300

    Make use of `StreamChatUI` in `SampleApp`

--- Sample_v3/AppDelegate.swift
+++ Sample_v3/AppDelegate.swift
@@ -3,6 +3,7 @@
 //
 
 @testable import StreamChat
+import StreamChatUI
 import UIKit
 
 @UIApplicationMain
@@ -25,6 +26,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         LogConfig.level = .debug
         
         LogStore.registerShared()
+        
+        setupAppearance()
+
         return true
     }
 
@@ -46,5 +50,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    private func setupAppearance() {
+        ChatChannelListCollectionView.appearance().backgroundColor = .white
+        AvatarView.appearance().backgroundColor = .gray
     }
 }
