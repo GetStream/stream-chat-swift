45661578aea8614902e7abac7ab80e1712e75d4f
Pavel Evsaev <pavel.evsaev@gmail.com>
2020-11-16 16:45:09 +0300

    Update `ChatChannelListCollectionViewCell` to be self-sizing

--- Sources_v3/StreamChatUI/ChatChannelList/ChatChannelListCollectionViewCell.swift
+++ Sources_v3/StreamChatUI/ChatChannelList/ChatChannelListCollectionViewCell.swift
@@ -5,4 +5,36 @@
 import Foundation
 import UIKit
 
-open class ChatChannelListCollectionViewCell: UICollectionViewCell {}
+open class ChatChannelListCollectionViewCell<ExtraData: UIExtraDataTypes>: UICollectionViewCell {
+    
+    // MARK: - Properties
+    
+    var uiConfig: UIConfig<ExtraData> = .default
+    
+    public private(set) lazy var channelView: ChatChannelView<ExtraData> = {
+        let view = uiConfig.channelList.channelView.init(uiConfig: uiConfig)
+        contentView.embed(view)
+        return view
+    }()
+    
+    // MARK: - Layout
+    
+    override open func preferredLayoutAttributesFitting(
+        _ layoutAttributes: UICollectionViewLayoutAttributes
+    ) -> UICollectionViewLayoutAttributes {
+        let preferredAttributes = super.preferredLayoutAttributesFitting(layoutAttributes)
+        
+        let targetSize = CGSize(
+            width: layoutAttributes.frame.width,
+            height: UIView.layoutFittingCompressedSize.height
+        )
+        
+        preferredAttributes.frame.size = contentView.systemLayoutSizeFitting(
+            targetSize,
+            withHorizontalFittingPriority: .required,
+            verticalFittingPriority: .fittingSizeLevel
+        )
+        
+        return preferredAttributes
+    }
+}
