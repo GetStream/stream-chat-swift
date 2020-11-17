174d3e4be0177b71658665d8740f6c0cefb1ea5e
Bahadir Oncel <bahadir@getstream.io>
2020-11-17 14:27:24 +0100

    Introduce and use Nuke for image loading
    We'll be using it for gif loading in the future, too

--- Sources_v3/StreamChatUI/Utils/UIImageView+Extensions.swift
+++ Sources_v3/StreamChatUI/Utils/UIImageView+Extensions.swift
@@ -2,29 +2,12 @@
 // Copyright © 2020 Stream.io Inc. All rights reserved.
 //
 
+import Nuke
 import UIKit
 
 extension UIImageView {
-    private enum AssociatedKeys {
-        static var loadImageTask: UInt8 = 0
-    }
-    
-    private var loadImageTask: URLSessionTask? {
-        get { objc_getAssociatedObject(self, &AssociatedKeys.loadImageTask) as? URLSessionTask }
-        set {
-            loadImageTask?.cancel()
-            objc_setAssociatedObject(self, &AssociatedKeys.loadImageTask, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
-            loadImageTask?.resume()
-        }
-    }
-    
-    func setImage(from url: URL, session: URLSession = .shared, placeholder: UIImage? = nil) {
-        image = placeholder
-        loadImageTask = session.dataTask(with: url) { [weak self] data, _, _ in
-            DispatchQueue.main.async {
-                self?.image = data.flatMap(UIImage.init)
-                self?.loadImageTask = nil
-            }
-        }
+    func setImage(from url: URL, placeholder: UIImage? = nil) {
+        let options = ImageLoadingOptions(placeholder: placeholder)
+        Nuke.loadImage(with: url, options: options, into: self)
     }
 }
