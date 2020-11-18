19fcf56490dfd0b25f3b5d41ed3dbe41c9944f05
Pavel Evsaev <pavel.evsaev@gmail.com>
2020-11-18 18:20:31 +0300

    Introduce `ChatRouter` type

---
+++ Sources_v3/StreamChatUI/Utils/ChatRouter.swift
@@ -0,0 +1,17 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import UIKit
+
+open class ChatRouter<Controller: UIViewController> {
+    public weak var rootViewController: Controller?
+    
+    public var navigationController: UINavigationController? {
+        rootViewController?.navigationController
+    }
+    
+    public required init(rootViewController: Controller) {
+        self.rootViewController = rootViewController
+    }
+}
