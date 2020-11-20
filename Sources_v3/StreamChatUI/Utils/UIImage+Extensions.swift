41939209e97d2a01db4db77bce2da0bc9f1dfbae
Pavel Lopatin <mantergo@gmail.com>
2020-11-20 02:20:46 +0100

    Add helper extensions

---
+++ Sources_v3/StreamChatUI/Utils/UIImage+Extensions.swift
@@ -0,0 +1,11 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage {
+    convenience init?(named name: String, in bundle: Bundle) {
+        self.init(named: name, in: bundle, compatibleWith: nil)
+    }
+}
