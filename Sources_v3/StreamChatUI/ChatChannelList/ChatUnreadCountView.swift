11379625be8fb5bd8f739e7b3cdb7f4cd2075cba
Pavel Lopatin <mantergo@gmail.com>
2020-11-20 02:32:23 +0100

    Introduce `ChatUnreadCountBubbleView`

---
+++ Sources_v3/StreamChatUI/ChatChannelList/ChatUnreadCountView.swift
@@ -0,0 +1,83 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import StreamChat
+import UIKit
+
+open class ChatUnreadCountView: UIView {
+    // MARK: - Properties
+    
+    public var inset: CGFloat = 3
+    
+    override open var intrinsicContentSize: CGSize {
+        let height: CGFloat = max(unreadCountLabel.font.pointSize + inset * 2, frame.height)
+        let width = max(unreadCountLabel.intrinsicContentSize.width + inset * 2, height)
+        return .init(width: width, height: height)
+    }
+    
+    public var unreadCount: ChannelUnreadCount = .noUnread {
+        didSet {
+            updateContent()
+        }
+    }
+    
+    // MARK: - Subviews
+    
+    private lazy var unreadCountLabel = UILabel().withoutAutoresizingMaskConstraints
+    
+    // MARK: - Init
+    
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    public required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        setupLayout()
+        setupAppearance()
+        updateContent()
+    }
+    
+    // MARK: - Layout
+    
+    override open func invalidateIntrinsicContentSize() {
+        super.invalidateIntrinsicContentSize()
+        
+        unreadCountLabel.invalidateIntrinsicContentSize()
+    }
+    
+    override open func layoutSubviews() {
+        super.layoutSubviews()
+        
+        invalidateIntrinsicContentSize()
+                
+        layer.cornerRadius = intrinsicContentSize.height / 2
+    }
+    
+    // MARK: - Public
+    
+    open func setupAppearance() {
+        layer.masksToBounds = true
+        backgroundColor = .systemRed
+        unreadCountLabel.textColor = .white
+        unreadCountLabel.font = UIFont.preferredFont(forTextStyle: .caption1).bold()
+        unreadCountLabel.adjustsFontForContentSizeCategory = true
+        unreadCountLabel.textAlignment = .center
+    }
+    
+    open func setupLayout() {
+        embed(unreadCountLabel, insets: .init(top: inset, leading: inset, bottom: inset, trailing: inset))
+        setContentCompressionResistancePriority(.defaultHigh, for: .horizontal)
+    }
+    
+    open func updateContent() {
+        isHidden = unreadCount.mentionedMessages == 0 && unreadCount.messages == 0
+        unreadCountLabel.text = String(unreadCount.messages)
+    }
+}
