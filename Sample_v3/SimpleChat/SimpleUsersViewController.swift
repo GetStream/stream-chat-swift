e4bf74bedee6c0b2883f2642c558488358beb929
Denis Bogomolov <denis@manwithbear.com>
2020-11-23 04:13:20 +0100

    CleanUp in SimpleUsersViewController

--- Sample_v3/SimpleChat/SimpleUsersViewController.swift
+++ Sample_v3/SimpleChat/SimpleUsersViewController.swift
@@ -5,88 +5,48 @@
 import StreamChat
 import UIKit
 
-///
-/// # SimpleUsersViewController
-///
-/// A `UITableViewController` subclass that displays and manages the list of users.  It uses the `ChatUserListController`  class to make calls to the Stream Chat API
+/// A `UITableViewController` subclass that displays and manages the list of users.
+/// It uses the `ChatUserListController` class to make calls to the Stream Chat API
 /// and listens to events by conforming to `ChatUserListControllerDelegate`.
-///
-class SimpleUsersViewController: UITableViewController, ChatUserListControllerDelegate {
-    // MARK: - Properties
-    
-    ///
-    /// # userListController
-    ///
-    ///  The property below holds the `ChatUserListController` object.  It is used to make calls to the Stream Chat API and to listen to the events related to the users list.
-    ///  After it is set, `userListController.delegate` needs to receive a reference to a `ChatUserListControllerDelegate`, which, in this case, is `self`. After the
-    ///  delegate is set,`userListController.synchronize()` must be called to start listening to events related to the users list. Additionally,
+class SimpleUsersViewController: UITableViewController {
+    var didSelectUser: ((UserId) -> Void)?
+
+    ///  `ChatUserListController` is used to make calls to the Stream Chat API and to listen to the events related to the users list.
     ///  `userListController.client` holds a reference to the `ChatClient` which created this instance. It can be used to create other controllers.
     ///
     var userListController: ChatUserListController! {
         didSet {
+            /// Provide `ChatUserListControllerDelegate` that will receive user list updates
             userListController.delegate = self
+            /// it's good practice to synchronize local storage with backend on start
             userListController.synchronize()
         }
     }
-    
-    // MARK: - Actions
-    
-    ///
-    /// # openDirectMessagesChat
-    ///
-    /// Closure that will be triggered on `didSelectRowAt`.
-    /// After user selection it will dismiss current controller and show direct message chat with the selected user.
-    ///
-    var didSelectUser: ((UserId) -> Void)?
-    
-    ///
-    /// # handleLongPress
-    ///
-    /// The method below handles long press on user cells by displaying a `UIAlertController` with actions that can be taken on the `userController`. (`mute` and `unmute`)
-    @objc func handleLongPress(_ gestureRecognizer: UILongPressGestureRecognizer) {
-        guard
-            let indexPath = tableView.indexPathForRow(at: gestureRecognizer.location(in: tableView)),
-            gestureRecognizer.state == .began
-        else {
-            return
-        }
 
-        let userId = userListController.users[indexPath.row].id
-        let userController = userListController.client.userController(userId: userId)
-
-        let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
-        let actions = [
-            UIAlertAction(title: "Mute", style: .default) { _ in
-                userController.mute()
-            },
-            UIAlertAction(title: "Unmute", style: .default) { _ in
-                userController.unmute()
-            },
-            UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-        ]
-        actions.forEach(actionSheet.addAction)
-
-        present(actionSheet, animated: true)
+    // MARK: - User operations
+    func muteUser(with id: UserId) {
+        let userController = userListController.client.userController(userId: id)
+        userController.mute()
     }
-    
-    // MARK: - ChatUserControllerDelegate
 
-    ///
-    /// The methods below are part of the `ChatUserListControllerDelegate` protocol and will be called when events happen in the user list. In order for these updates to
-    /// happen, `userListController.delegate` must be equal `self` and `userListController.synchronize()` must be called.
-    ///
-    
-    ///
-    /// # didChangeUsers
-    ///
-    /// The method below receives the `changes` that happen in the list of users and updates the `UITableView` accordingly.
-    ///
+    func unmuteUser(with id: UserId) {
+        let userController = userListController.client.userController(userId: id)
+        userController.unmute()
+    }
+}
+
+// MARK: - ChatUserListController
+extension SimpleUsersViewController: ChatUserListControllerDelegate {
+    /// The methods below are part of the `ChatUserListControllerDelegate` protocol and will be called when events
+    /// happen in the user list. In order for these updates to happen, `userListController.delegate` must be equal `self`
+
+    /// Receives the `changes` that happen in the list of users and updates the `UITableView` accordingly.
     func controller(
         _ controller: ChatUserListController,
         didChangeUsers changes: [ListChange<ChatUser>]
     ) {
         tableView.beginUpdates()
-        
+
         for change in changes {
             switch change {
             case let .insert(_, index: index):
@@ -99,32 +59,50 @@ class SimpleUsersViewController: UITableViewController, ChatUserListControllerDe
                 tableView.deleteRows(at: [index], with: .automatic)
             }
         }
-        
+
         tableView.endUpdates()
     }
-    
-    // MARK: - UITableViewDataSource
+}
 
-    ///
-    /// The methods below are part of the `UITableViewDataSource` protocol and will be called when the `UITableView` needs information which will be given by the
-    /// `userListController` object.
-    ///
-    
-    ///
-    /// # numberOfRowsInSection
-    ///
-    /// The method below returns the current loaded users count `userListController.users.count`. It will increase as more users are loaded or decrease as
-    /// users are deleted.
-    ///
+// MARK: - Actions
+extension SimpleUsersViewController {
+    /// On long press on user cells display a `UIAlertController` with actions that can be taken
+    /// on the `userController`. (`mute` and `unmute`)
+    @objc func handleLongPress(_ gestureRecognizer: UILongPressGestureRecognizer) {
+        guard
+            let indexPath = tableView.indexPathForRow(at: gestureRecognizer.location(in: tableView)),
+            gestureRecognizer.state == .began
+        else {
+            return
+        }
+
+        let userId = userListController.users[indexPath.row].id
+
+        let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let actions = [
+            UIAlertAction(title: "Mute", style: .default) { [weak self] _ in
+                self?.muteUser(with: userId)
+            },
+            UIAlertAction(title: "Unmute", style: .default) { [weak self] _ in
+                self?.unmuteUser(with: userId)
+            },
+            UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        ]
+        actions.forEach(actionSheet.addAction)
+
+        present(actionSheet, animated: true)
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension SimpleUsersViewController {
+    /// The method below returns the current loaded users count `userListController.users.count`.
+    /// It will increase as more users are loaded or decrease as users are deleted.
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         userListController.users.count
     }
 
-    ///
-    /// # cellForRowAt
-    ///
     /// The method below returns a cell configured based on the user in position `indexPath.row` of `userListController.users`.
-    ///
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let user = userListController.users[indexPath.row]
         
@@ -150,39 +128,31 @@ class SimpleUsersViewController: UITableViewController, ChatUserListControllerDe
         
         return cell!
     }
-    
-    // MARK: - UITableViewDelegate
-    
-    ///
-    /// # willDisplay
-    ///
-    /// The method below handles the case when the last cell in the users list is displayed by calling `userListController.loadNextUsers()` to fetch more
-    /// users.
-    ///
+}
+
+// MARK: - UITableViewDelegate
+extension SimpleUsersViewController {
     override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        /// when user scrolls to last cell in table, load more users
         if indexPath.section == tableView.numberOfSections - 1,
             indexPath.row == tableView.numberOfRows(inSection: indexPath.section) - 1 {
             userListController.loadNextUsers()
         }
     }
-    
-    ///
-    /// # didSelectRowAt
-    ///
-    /// The method below handles the user selection.
-    ///
+
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         didSelectUser?(userListController.users[indexPath.row].id)
     }
-    
-    // MARK: - UI Code
-    
-    private lazy var longPressRecognizer = UILongPressGestureRecognizer(
-        target: self,
-        action: #selector(handleLongPress)
-    )
-    
+}
+
+// MARK: - UI Code
+extension SimpleUsersViewController {
     override func viewDidLoad() {
+        super.viewDidLoad()
+        let longPressRecognizer = UILongPressGestureRecognizer(
+            target: self,
+            action: #selector(handleLongPress)
+        )
         tableView.addGestureRecognizer(longPressRecognizer)
         navigationItem.rightBarButtonItem = UIBarButtonItem(
             barButtonSystemItem: .done,
