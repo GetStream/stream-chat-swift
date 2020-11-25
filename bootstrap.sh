b3bfc72827adc0f2c97fbbb4a44de453b0856d1d
Vojta Stavik <stavik@outlook.com>
2020-11-25 09:25:25 +0100

    Use Mint to install Swift-based dependencies

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,27 +1,40 @@
 #!/usr/bin/env bash
 # Usage: ./bootstrap.sh
-# This script will install swiftformat, link git hooks and install required ruby gems.
+# This script will install Mint and bootstrap its dependencies, link git hooks and install required ruby gems.
 # You should have homebrew installed.
 # If you get `zsh: permission denied: ./bootstrap.sh` error, please run `chmod +x bootstrap.sh` first
 
-# Install swiftformat 0.45.6
-brew unlink swiftformat
-brew tap-new getstream/ios
-brew extract --version 0.45.6 swiftformat getstream/ios
-brew reinstall getstream/ios/swiftformat@0.45.6
-brew untap getstream/ios
+# Check if Homebrew is installed
+if [[ $(command -v brew) == "" ]]; then
+    echo "Homebrew not installed. Please install."
+    exit 1
+fi
+
+echo
+echo -e "👉 Install Mint if needed"
+# List installed Mint versions, if fails, install Mint
+brew list mint || brew install mint
 
 # Set bash to Strict Mode (http://redsymbol.net/articles/unofficial-bash-strict-mode/)
 set -euo pipefail
 
-# Symlink hooks folder to .git/hooks folder
-ln -sf ../../hooks/pre-commit.sh .git/hooks/pre-commit
-chmod +x .git/hooks/pre-commit
-echo "Symlink for pre-commit created successfully"
+echo
+echo -e "👉 Bootstrap Mint dependencies"
+mint bootstrap
 
-# git-format-staged
-chmod +x hooks/git-format-staged
-echo "chmod +x for git-format-staged set successfully"
+# Symlink hooks folder to .git/hooks folder
+echo
+echo -e "👉 Create symlink for pre-commit hooks"
+ln -sf ./hooks/pre-commit.sh .git/hooks/pre-commit
+chmod +x .git/hooks/pre-commit
+chmod +x ./hooks/git-format-staged
+
+# Get SwiftLint version from Mintfile to be used by danger-swiftlint too
+SWIFTLINT_VERSION=$(cat Mintfile | grep SwiftLint | awk -F@ '{print $2}')
 
 # Install gems
+echo
+echo -e "👉 Install bundle dependencies"
+echo "SwiftLint version: " $SWIFTLINT_VERSION
+bundle config SWIFTLINT_VERSION "$SWIFTLINT_VERSION"
 bundle install
