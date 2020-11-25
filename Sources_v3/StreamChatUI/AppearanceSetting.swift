53d66618e26e53963fdb3c5c33e24c6621dcb89f
Vojta Stavik <stavik@outlook.com>
2020-11-25 10:55:39 +0100

    Update AppearanceSetting and remove static rules

--- Sources_v3/StreamChatUI/AppearanceSetting.swift
+++ Sources_v3/StreamChatUI/AppearanceSetting.swift
@@ -8,33 +8,38 @@ import UIKit
 /// Used to define the default appearance for views.
 public protocol AppearanceSetting: AnyObject {
     /// Objects must implement this method and apply the default configuration on the provided object.
-    static func initialAppearanceSetup(_ view: Self)
+    func defaultAppearance()
 }
 
-extension AppearanceSetting {
-    /// An object describing the default appearance of the view. Be aware that the appearance object is generic-type-specific,
-    /// in other words: `MyView<A>.defaultAppearance != MyView<V>.defaultAppearance`.
-    public static var defaultAppearance: Appearance<Self> {
-        get {
-            let key = String(describing: Self.self)
-            if let existing = _AppearanceStorage.shared.appearances[key] as? Appearance<Self> {
-                return existing
-            } else {
-                let appearance = Appearance<Self>()
-                appearance.rules.append { Self.initialAppearanceSetup($0) }
-                _AppearanceStorage.shared.appearances[key] = appearance
-                return appearance
-            }
-        }
+public extension AppearanceSetting {
+    func defaultAppearance() { /* default empty implementation */ }
+}
 
-        set {
-            let key = String(describing: Self.self)
-            _AppearanceStorage.shared.appearances[key] = newValue
-        }
-    }
-    
+public extension AppearanceSetting {
+    /// Applies the default appearance specified by the type, including the custom rules set using the `defaultAppearance` API.
     func applyDefaultAppearance() {
-        Self.defaultAppearance.rules.forEach { $0(self) }
+        defaultAppearance()
+        let appearance: Appearance<Self> = Self.defaultAppearance
+        appearance.rules.forEach { $0(self) }
+    }
+}
+
+public extension AppearanceSetting {
+    static var defaultAppearance: Appearance<Self> {
+        let key = String(describing: self)
+        return fetchDefaultAppearance(key)
+    }
+}
+
+/// An object describing the default appearance of the view. Be aware that the appearance object is generic-type-specific,
+/// in other words: `MyView<A>.defaultAppearance != MyView<V>.defaultAppearance`.
+private func fetchDefaultAppearance<T: AppearanceSetting>(_ key: String) -> Appearance<T> {
+    if let existing = _AppearanceStorage.shared.appearances[key] as? Appearance<T> {
+        return existing
+    } else {
+        let appearance = Appearance<T>()
+        _AppearanceStorage.shared.appearances[key] = appearance
+        return appearance
     }
 }
 
@@ -48,5 +53,9 @@ public class Appearance<Root: AnyObject> {
 
     public func addRule(_ rule: @escaping (Root) -> Void) {
         rules.append(rule)
+    }
+    
+    public func callAsFunction(_ rule: @escaping (Root) -> Void) {
+        addRule(rule)
     }
 }
