7bceee51135f7da9b464cec1ef4d3f69008a0dad
Vojta Stavik <stavik@outlook.com>
2020-11-25 09:26:31 +0100

    Use versions of SwiftLint and SwiftFormat from Mint

--- hooks/pre-commit.sh
+++ hooks/pre-commit.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-./hooks/git-format-staged --formatter 'swiftformat --config .swiftformat stdin' 'Sources_v3/*.swift'
-./hooks/git-format-staged --formatter 'swiftformat --config .swiftformat stdin' 'Tests_v3/*.swift'
-./hooks/git-format-staged --formatter 'swiftformat --config .swiftformat stdin' 'Sample_v3/*.swift'
+./hooks/git-format-staged --formatter 'mint run swiftformat --config .swiftformat stdin' 'Sources_v3/*.swift'
+./hooks/git-format-staged --formatter 'mint run swiftformat --config .swiftformat stdin' 'Tests_v3/*.swift'
+./hooks/git-format-staged --formatter 'mint run swiftformat --config .swiftformat stdin' 'Sample_v3/*.swift'
