4c98ff10e8bb90bf9a11db7a0113b0c8d9751cec
Vojta Stavik <stavik@outlook.com>
2020-11-25 10:56:15 +0100

    Add base views for existing elements

---
+++ Sources_v3/StreamChatUI/Common Views/BaseViews.swift
@@ -0,0 +1,108 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// Just a protocol to formalize the methods required
+public protocol Customizable {
+    /// Main point of customization for appearance.
+    ////Calling super implementation is not necessary nor encouraged.
+    func setUpAppearance()
+    
+    /// Main point of customization for appearance.
+    /// Calling super implementation is not necessary nor encouraged.
+    func setUpLayout()
+    
+    /// Main point of updating views with the latest data.
+    /// Calling super implementation is not necessary nor encouraged.
+    func updateContent()
+}
+
+/// Base class for overridable views StreamChatUI provides.
+/// All conformers will have StreamChatUI appearance settings by default.
+open class View: UIView, AppearanceSetting, Customizable {
+    override open func didMoveToSuperview() {
+        super.didMoveToSuperview()
+        guard superview != nil else { return }
+        
+        (self as! Self).applyDefaultAppearance()
+        setUpAppearance()
+        setUpLayout()
+        updateContent()
+    }
+    
+    open func setUpAppearance() { /* default empty implementation */ }
+    open func setUpLayout() { /* default empty implementation */ }
+    open func updateContent() { /* default empty implementation */ }
+}
+
+/// Base class for overridable views StreamChatUI provides.
+/// All conformers will have StreamChatUI appearance settings by default.
+open class Control: UIControl, AppearanceSetting, Customizable {
+    override open func didMoveToSuperview() {
+        super.didMoveToSuperview()
+        guard superview != nil else { return }
+        
+        (self as! Self).applyDefaultAppearance()
+        setUpAppearance()
+        setUpLayout()
+        updateContent()
+    }
+    
+    open func setUpAppearance() { /* default empty implementation */ }
+    open func setUpLayout() { /* default empty implementation */ }
+    open func updateContent() { /* default empty implementation */ }
+}
+
+/// Base class for overridable views StreamChatUI provides.
+/// All conformers will have StreamChatUI appearance settings by default.
+open class Button: UIButton, AppearanceSetting, Customizable {
+    override open func didMoveToSuperview() {
+        super.didMoveToSuperview()
+        guard superview != nil else { return }
+        
+        (self as! Self).applyDefaultAppearance()
+        setUpAppearance()
+        setUpLayout()
+        updateContent()
+    }
+    
+    open func setUpAppearance() { /* default empty implementation */ }
+    open func setUpLayout() { /* default empty implementation */ }
+    open func updateContent() { /* default empty implementation */ }
+}
+
+/// Base class for overridable views StreamChatUI provides.
+/// All conformers will have StreamChatUI appearance settings by default.
+open class NavigationBar: UINavigationBar, AppearanceSetting, Customizable {
+    override open func didMoveToSuperview() {
+        super.didMoveToSuperview()
+        guard superview != nil else { return }
+        
+        (self as! Self).applyDefaultAppearance()
+        setUpAppearance()
+        setUpLayout()
+        updateContent()
+    }
+    
+    open func setUpAppearance() { /* default empty implementation */ }
+    open func setUpLayout() { /* default empty implementation */ }
+    open func updateContent() { /* default empty implementation */ }
+}
+
+open class ViewController: UIViewController, AppearanceSetting, Customizable {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        
+        (self as! Self).applyDefaultAppearance()
+        setUpAppearance()
+        setUpLayout()
+        updateContent()
+    }
+    
+    open func setUpAppearance() { /* default empty implementation */ }
+    open func setUpLayout() { /* default empty implementation */ }
+    open func updateContent() { /* default empty implementation */ }
+}
