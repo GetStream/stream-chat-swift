e31831659cbc456ea814a87fd5ada06b2cd37c48
Vojta Stavik <stavik@outlook.com>
2020-11-26 16:30:25 +0100

    Add intro screens to Demo app

---
+++ DemoApp/AdvancedOptionsViewController.swift
@@ -0,0 +1,43 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainButton: UIButton {
+    override func tintColorDidChange() {
+        backgroundColor = tintColor
+        setTitleColor(.white, for: .normal)
+    }
+    
+    override func updateConstraints() {
+        super.updateConstraints()
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        clipsToBounds = true
+        layer.cornerRadius = bounds.height / 2.0
+    }
+}
+
+class AdvancedOptionsViewController: UIViewController {
+    @IBOutlet var mainStackView: UIStackView! {
+        didSet {
+            mainStackView.preservesSuperviewLayoutMargins = true
+            mainStackView.isLayoutMarginsRelativeArrangement = true
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "Advanced Options"
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = false
+    }
+}
