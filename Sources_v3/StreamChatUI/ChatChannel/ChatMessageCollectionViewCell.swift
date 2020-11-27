1a7ba4057368edc2b2e33108f4efea0c3aecdc04
Pavel Evsaev <pavel.evsaev@gmail.com>
2020-11-27 16:37:23 +0300

    Introduce `СhatMessageCollectionViewCell` types

--- Sources_v3/StreamChatUI/ChatChannel/ChatMessageCollectionViewCell.swift
+++ Sources_v3/StreamChatUI/ChatChannel/ChatMessageCollectionViewCell.swift
@@ -2,11 +2,87 @@
 // Copyright © 2020 Stream.io Inc. All rights reserved.
 //
 
-import Foundation
+import StreamChat
 import UIKit
 
-protocol ChatMessageCollectionViewCellDelegate: class {}
+class СhatMessageCollectionViewCell<ExtraData: UIExtraDataTypes>: UICollectionViewCell, UIConfigProvider {
+    var message: _ChatMessageGroupPart<ExtraData>? {
+        didSet { updateContent() }
+    }
+    
+    // MARK: - Subviews
 
-open class ChatMessageCollectionViewCell: UICollectionViewCell {
-    weak var delegate: ChatMessageCollectionViewCellDelegate?
+    public private(set) lazy var messageView = uiConfig.messageList.messageContentView.init().withoutAutoresizingMaskConstraints
+    
+    // MARK: - Lifecycle
+
+    override func didMoveToSuperview() {
+        super.didMoveToSuperview()
+
+        guard superview != nil else { return }
+
+        setUpLayout()
+        updateContent()
+    }
+
+    func setUpLayout() {
+        contentView.addSubview(messageView)
+
+        NSLayoutConstraint.activate([
+            messageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            messageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            messageView.widthAnchor.constraint(lessThanOrEqualTo: contentView.widthAnchor, multiplier: 0.75)
+        ])
+    }
+
+    func updateContent() {
+        messageView.message = message
+    }
+
+    // MARK: - Overrides
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+
+        message = nil
+    }
+    
+    override func preferredLayoutAttributesFitting(
+        _ layoutAttributes: UICollectionViewLayoutAttributes
+    ) -> UICollectionViewLayoutAttributes {
+        let preferredAttributes = super.preferredLayoutAttributesFitting(layoutAttributes)
+        
+        let targetSize = CGSize(
+            width: layoutAttributes.frame.width,
+            height: UIView.layoutFittingCompressedSize.height
+        )
+        
+        preferredAttributes.frame.size = contentView.systemLayoutSizeFitting(
+            targetSize,
+            withHorizontalFittingPriority: .required,
+            verticalFittingPriority: .fittingSizeLevel
+        )
+        
+        return preferredAttributes
+    }
+}
+
+class СhatIncomingMessageCollectionViewCell<ExtraData: UIExtraDataTypes>: СhatMessageCollectionViewCell<ExtraData> {
+    static var reuseId: String { String(describing: self) }
+
+    override func setUpLayout() {
+        super.setUpLayout()
+
+        messageView.leadingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leadingAnchor).isActive = true
+    }
+}
+
+class СhatOutgoingMessageCollectionViewCell<ExtraData: UIExtraDataTypes>: СhatMessageCollectionViewCell<ExtraData> {
+    static var reuseId: String { String(describing: self) }
+
+    override func setUpLayout() {
+        super.setUpLayout()
+
+        messageView.trailingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.trailingAnchor).isActive = true
+    }
 }
