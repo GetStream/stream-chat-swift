2fb273733911acbb13d48c35ed430f0c3bf76cbf
Pavel Evsaev <pavel.evsaev@gmail.com>
2020-11-27 16:33:38 +0300

    Introduce `ChatMessageMetadataView` type

---
+++ Sources_v3/StreamChatUI/ChatChannel/ChatMessageMetadataView.swift
@@ -0,0 +1,35 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import StreamChat
+import UIKit
+
+open class ChatMessageMetadataView<ExtraData: UIExtraDataTypes>: View {
+    public var message: _ChatMessageGroupPart<ExtraData>? {
+        didSet { updateContent() }
+    }
+    
+    // MARK: - Subviews
+    
+    public private(set) lazy var timestampLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .footnote)
+        label.adjustsFontForContentSizeCategory = true
+        return label
+    }()
+    
+    // MARK: - Overrides
+
+    override public func defaultAppearance() {
+        timestampLabel.textColor = .messageTimestamp
+    }
+
+    override open func setUpLayout() {
+        embed(timestampLabel)
+    }
+
+    override open func updateContent() {
+        timestampLabel.text = message?.createdAt.getFormattedDate(format: "hh:mm a")
+    }
+}
