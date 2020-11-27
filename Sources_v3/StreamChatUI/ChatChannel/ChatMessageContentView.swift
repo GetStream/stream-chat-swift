5ba3208c276f20764835ab02ed06bc0b0176d453
Pavel Evsaev <pavel.evsaev@gmail.com>
2020-11-27 16:35:43 +0300

    Introduce `ChatMessageContentView` type

---
+++ Sources_v3/StreamChatUI/ChatChannel/ChatMessageContentView.swift
@@ -0,0 +1,147 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import StreamChat
+import UIKit
+
+open class ChatMessageContentView<ExtraData: UIExtraDataTypes>: View, UIConfigProvider {
+    public var message: _ChatMessageGroupPart<ExtraData>? {
+        didSet { updateContent() }
+    }
+
+    // MARK: - Subviews
+
+    public private(set) lazy var messageBubbleView = uiConfig
+        .messageList
+        .messageContentSubviews
+        .bubbleView
+        .init(showRepliedMessage: true)
+        .withoutAutoresizingMaskConstraints
+
+    public private(set) lazy var messageMetadataView = uiConfig
+        .messageList
+        .messageContentSubviews
+        .metadataView
+        .init()
+        .withoutAutoresizingMaskConstraints
+    
+    public private(set) lazy var authorAvatarView = uiConfig
+        .messageList
+        .messageContentSubviews
+        .authorAvatarView
+        .init()
+        .withoutAutoresizingMaskConstraints
+
+    private var incomingMessageGroupPartConstraints: [NSLayoutConstraint] = []
+    private var incomingMessageGroupFooterConstraints: [NSLayoutConstraint] = []
+    private var outgoingMessageGroupPartConstraints: [NSLayoutConstraint] = []
+    private var outgoingMessageGroupFooterConstraints: [NSLayoutConstraint] = []
+
+    // MARK: - Overrides
+
+    override open func setUpLayout() {
+        addSubview(messageBubbleView)
+        addSubview(messageMetadataView)
+        addSubview(authorAvatarView)
+
+        incomingMessageGroupPartConstraints = [
+            messageBubbleView.topAnchor.constraint(equalTo: topAnchor),
+            messageBubbleView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 40),
+            messageBubbleView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            messageBubbleView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ]
+
+        incomingMessageGroupFooterConstraints = [
+            authorAvatarView.widthAnchor.constraint(equalToConstant: 32),
+            authorAvatarView.heightAnchor.constraint(equalToConstant: 32),
+            authorAvatarView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            authorAvatarView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            
+            messageBubbleView.topAnchor.constraint(equalTo: topAnchor),
+            messageBubbleView.leadingAnchor.constraint(equalToSystemSpacingAfter: authorAvatarView.trailingAnchor, multiplier: 1),
+            messageBubbleView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            
+            messageMetadataView.topAnchor.constraint(equalToSystemSpacingBelow: messageBubbleView.bottomAnchor, multiplier: 1),
+            messageMetadataView.leadingAnchor.constraint(equalTo: messageBubbleView.leadingAnchor),
+            messageMetadataView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ]
+
+        outgoingMessageGroupPartConstraints = [
+            messageBubbleView.topAnchor.constraint(equalTo: topAnchor),
+            messageBubbleView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            messageBubbleView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            messageBubbleView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ]
+
+        outgoingMessageGroupFooterConstraints = [
+            messageBubbleView.topAnchor.constraint(equalTo: topAnchor),
+            messageBubbleView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            messageBubbleView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            
+            messageMetadataView.topAnchor.constraint(equalToSystemSpacingBelow: messageBubbleView.bottomAnchor, multiplier: 1),
+            messageMetadataView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            messageMetadataView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ]
+    }
+
+    override open func updateContent() {
+        messageBubbleView.message = message
+        messageMetadataView.message = message
+
+        let placeholder = UIImage(named: "pattern1", in: .streamChatUI)
+        if let imageURL = message?.author.imageURL {
+            authorAvatarView.imageView.setImage(from: imageURL, placeholder: placeholder)
+        } else {
+            authorAvatarView.imageView.image = placeholder
+        }
+
+        authorAvatarView.isHidden = message?.isSentByCurrentUser == true || message?.isLastInGroup == false
+        messageMetadataView.isHidden = message?.isLastInGroup == false
+        activateNecessaryConstraints()
+    }
+
+    // MARK: - Private
+    
+    private func activateNecessaryConstraints() {
+        let constraintsToDeactivate: [NSLayoutConstraint]
+        let constraintsToActivate: [NSLayoutConstraint]
+
+        switch (message?.isSentByCurrentUser, message?.isLastInGroup) {
+        case (true, true):
+            constraintsToDeactivate =
+                outgoingMessageGroupPartConstraints +
+                incomingMessageGroupPartConstraints +
+                incomingMessageGroupFooterConstraints
+            constraintsToActivate = outgoingMessageGroupFooterConstraints
+        case (true, false):
+            constraintsToDeactivate =
+                outgoingMessageGroupFooterConstraints +
+                incomingMessageGroupPartConstraints +
+                incomingMessageGroupFooterConstraints
+            constraintsToActivate = outgoingMessageGroupPartConstraints
+        case (false, true):
+            constraintsToDeactivate =
+                outgoingMessageGroupPartConstraints +
+                outgoingMessageGroupFooterConstraints +
+                incomingMessageGroupPartConstraints
+            constraintsToActivate = incomingMessageGroupFooterConstraints
+        case (false, false):
+            constraintsToDeactivate =
+                outgoingMessageGroupPartConstraints +
+                outgoingMessageGroupFooterConstraints +
+                incomingMessageGroupFooterConstraints
+            constraintsToActivate = incomingMessageGroupPartConstraints
+        default:
+            constraintsToDeactivate =
+                incomingMessageGroupPartConstraints +
+                incomingMessageGroupFooterConstraints +
+                outgoingMessageGroupPartConstraints +
+                outgoingMessageGroupFooterConstraints
+            constraintsToActivate = []
+        }
+        
+        constraintsToDeactivate.forEach { $0.isActive = false }
+        constraintsToActivate.forEach { $0.isActive = true }
+    }
+}
