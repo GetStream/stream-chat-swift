0df37c0e30bb45427e6bc1de39f3ad28282f7aee
Pavel Evsaev <pavel.evsaev@gmail.com>
2020-11-27 16:30:03 +0300

    Extend `UIColor` with stream colors

--- Sources_v3/StreamChatUI/Utils/UIColor+Extensions.swift
+++ Sources_v3/StreamChatUI/Utils/UIColor+Extensions.swift
@@ -6,6 +6,36 @@ import UIKit
 
 extension UIColor {
     convenience init(r: CGFloat, g: CGFloat, b: CGFloat) {
-        self.init(red: r / 255, green: g / 255, blue: b / 255, alpha: 1)
+        self.init(
+            red: r / 255,
+            green: g / 255,
+            blue: b / 255,
+            alpha: 1
+        )
     }
+
+    convenience init(red: Int, green: Int, blue: Int) {
+        self.init(
+            r: CGFloat(red),
+            g: CGFloat(green),
+            b: CGFloat(blue)
+        )
+    }
+
+    convenience init(rgb: Int) {
+        self.init(
+            red: (rgb >> 16) & 0xff,
+            green: (rgb >> 8) & 0xff,
+            blue: rgb & 0xff
+        )
+    }
+}
+
+extension UIColor {
+    static let outgoingMessageBubbleBackground = UIColor(rgb: 0xe5e5e5)
+    static let outgoingMessageBubbleBorder = outgoingMessageBubbleBackground
+    static let incomingMessageBubbleBackground = white
+    static let incomingMessageBubbleBorder = UIColor.black.withAlphaComponent(0.08)
+    static let chatBackground = UIColor(rgb: 0xfcfcfc)
+    static let messageTimestamp = UIColor.lightGray
 }
