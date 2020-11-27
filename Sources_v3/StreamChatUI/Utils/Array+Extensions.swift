2a59c3a46332c79f4d40993cf9464d6ff1293171
Pavel Evsaev <pavel.evsaev@gmail.com>
2020-11-27 16:30:52 +0300

    Extend `Array` with safe subscript

---
+++ Sources_v3/StreamChatUI/Utils/Array+Extensions.swift
@@ -0,0 +1,12 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+
+extension Array {
+    subscript(safe index: Int) -> Element? {
+        guard index >= 0, index < count else { return nil }
+        return self[index]
+    }
+}
