fafd539d7b8e32a872e074551d64a0fda4b91229
Pavel Evsaev <pavel.evsaev@gmail.com>
2020-12-02 13:05:54 +0300

    Remove init from `UIConfig` and nested types

--- Sources_v3/StreamChatUI/UIConfig.swift
+++ Sources_v3/StreamChatUI/UIConfig.swift
@@ -6,22 +6,10 @@ import StreamChat
 import UIKit
 
 public struct UIConfig<ExtraData: UIExtraDataTypes> {
-    public var channelList: ChannelListUI
-    public var messageList: MessageListUI
-    public var currentUser: CurrentUserUI
-    public var navigation: Navigation
-    
-    public init(
-        channelList: ChannelListUI = .init(),
-        messageList: MessageListUI = .init(),
-        currentUser: CurrentUserUI = .init(),
-        navigation: Navigation = .init()
-    ) {
-        self.channelList = channelList
-        self.messageList = messageList
-        self.currentUser = currentUser
-        self.navigation = navigation
-    }
+    public var channelList = ChannelListUI()
+    public var messageList = MessageListUI()
+    public var currentUser = CurrentUserUI()
+    public var navigation = Navigation()
 }
 
 // MARK: - UIConfig + Default
@@ -51,16 +39,8 @@ public extension UIConfig {
 
 public extension UIConfig {
     struct Navigation {
-        public var navigationBar: ChatNavigationBar.Type
-        public var channelListRouter: ChatChannelListRouter<ExtraData>.Type
-        
-        public init(
-            navigationBar: ChatNavigationBar.Type = ChatNavigationBar.self,
-            channelListRouter: ChatChannelListRouter<ExtraData>.Type = ChatChannelListRouter<ExtraData>.self
-        ) {
-            self.navigationBar = navigationBar
-            self.channelListRouter = channelListRouter
-        }
+        public var navigationBar: ChatNavigationBar.Type = ChatNavigationBar.self
+        public var channelListRouter: ChatChannelListRouter<ExtraData>.Type = ChatChannelListRouter<ExtraData>.self
     }
 }
 
@@ -68,44 +48,19 @@ public extension UIConfig {
 
 public extension UIConfig {
     struct ChannelListUI {
-        public var channelCollectionView: ChatChannelListCollectionView.Type
-        public var channelCollectionLayout: UICollectionViewLayout.Type
-        public var channelListItemView: ChatChannelListItemView<ExtraData>.Type
-        public var channelViewCell: ChatChannelListCollectionViewCell<ExtraData>.Type
-        public var newChannelButton: CreateNewChannelButton.Type
-        public var channelListItemSubviews: ChannelListItemSubviews
-
-        public init(
-            channelCollectionView: ChatChannelListCollectionView.Type = ChatChannelListCollectionView.self,
-            channelCollectionLayout: UICollectionViewLayout.Type = ChatChannelListCollectionViewLayout.self,
-            channelListItemView: ChatChannelListItemView<ExtraData>.Type = ChatChannelListItemView<ExtraData>.self,
-            channelViewCell: ChatChannelListCollectionViewCell<ExtraData>.Type = ChatChannelListCollectionViewCell<ExtraData>.self,
-            newChannelButton: CreateNewChannelButton.Type = CreateNewChannelButton.self,
-            channelListItemSubviews: ChannelListItemSubviews = .init()
-        ) {
-            self.channelCollectionView = channelCollectionView
-            self.channelCollectionLayout = channelCollectionLayout
-            self.channelListItemView = channelListItemView
-            self.channelViewCell = channelViewCell
-            self.channelListItemSubviews = channelListItemSubviews
-            self.newChannelButton = newChannelButton
-        }
+        public var channelCollectionView: ChatChannelListCollectionView.Type = ChatChannelListCollectionView.self
+        public var channelCollectionLayout: UICollectionViewLayout.Type = ChatChannelListCollectionViewLayout.self
+        public var channelListItemView: ChatChannelListItemView<ExtraData>.Type = ChatChannelListItemView<ExtraData>.self
+        public var channelViewCell: ChatChannelListCollectionViewCell<ExtraData>.Type =
+            ChatChannelListCollectionViewCell<ExtraData>.self
+        public var newChannelButton: CreateNewChannelButton.Type = CreateNewChannelButton.self
+        public var channelListItemSubviews = ChannelListItemSubviews()
     }
     
     struct ChannelListItemSubviews {
-        public var avatarView: ChatChannelAvatarView<ExtraData>.Type
-        public var unreadCountView: ChatUnreadCountView.Type
-        public var readStatusView: ChatReadStatusCheckmarkView.Type
-        
-        public init(
-            avatarView: ChatChannelAvatarView<ExtraData>.Type = ChatChannelAvatarView.self,
-            unreadCountView: ChatUnreadCountView.Type = ChatUnreadCountView.self,
-            readStatusView: ChatReadStatusCheckmarkView.Type = ChatReadStatusCheckmarkView.self
-        ) {
-            self.avatarView = avatarView
-            self.unreadCountView = unreadCountView
-            self.readStatusView = readStatusView
-        }
+        public var avatarView: ChatChannelAvatarView<ExtraData>.Type = ChatChannelAvatarView.self
+        public var unreadCountView: ChatUnreadCountView.Type = ChatUnreadCountView.self
+        public var readStatusView: ChatReadStatusCheckmarkView.Type = ChatReadStatusCheckmarkView.self
     }
 }
 
@@ -113,16 +68,8 @@ public extension UIConfig {
 
 public extension UIConfig {
     struct CurrentUserUI {
-        public var currentUserViewAvatarView: CurrentChatUserAvatarView<ExtraData>.Type
-        public var avatarView: AvatarView.Type
-        
-        public init(
-            currentUserViewAvatarView: CurrentChatUserAvatarView<ExtraData>.Type = CurrentChatUserAvatarView<ExtraData>.self,
-            avatarView: AvatarView.Type = AvatarView.self
-        ) {
-            self.currentUserViewAvatarView = currentUserViewAvatarView
-            self.avatarView = avatarView
-        }
+        public var currentUserViewAvatarView: CurrentChatUserAvatarView<ExtraData>.Type = CurrentChatUserAvatarView<ExtraData>.self
+        public var avatarView: AvatarView.Type = AvatarView.self
     }
 }
 
@@ -130,43 +77,18 @@ public extension UIConfig {
 
 public extension UIConfig {
     struct MessageListUI {
-        public var collectionView: ChatChannelCollectionView.Type
-        public var collectionLayout: UICollectionViewLayout.Type
-        public var minTimeInvteralBetweenMessagesInGroup: TimeInterval
-        public var messageContentView: ChatMessageContentView<ExtraData>.Type
-        public var messageContentSubviews: MessageContentViewSubviews
-
-        public init(
-            collectionView: ChatChannelCollectionView.Type = ChatChannelCollectionView.self,
-            collectionLayout: UICollectionViewLayout.Type = ChatChannelCollectionViewLayout.self,
-            minTimeInvteralBetweenMessagesInGroup: TimeInterval = 10,
-            messageContentView: ChatMessageContentView<ExtraData>.Type = ChatMessageContentView<ExtraData>.self,
-            messageContentSubviews: MessageContentViewSubviews = .init()
-        ) {
-            self.collectionView = collectionView
-            self.collectionLayout = collectionLayout
-            self.minTimeInvteralBetweenMessagesInGroup = minTimeInvteralBetweenMessagesInGroup
-            self.messageContentView = messageContentView
-            self.messageContentSubviews = messageContentSubviews
-        }
+        public var collectionView: ChatChannelCollectionView.Type = ChatChannelCollectionView.self
+        public var collectionLayout: UICollectionViewLayout.Type = ChatChannelCollectionViewLayout.self
+        public var minTimeInvteralBetweenMessagesInGroup: TimeInterval = 10
+        public var messageContentView: ChatMessageContentView<ExtraData>.Type = ChatMessageContentView<ExtraData>.self
+        public var messageContentSubviews = MessageContentViewSubviews()
     }
 
     struct MessageContentViewSubviews {
-        public var authorAvatarView: AvatarView.Type
-        public var bubbleView: ChatMessageBubbleView<ExtraData>.Type
-        public var metadataView: ChatMessageMetadataView<ExtraData>.Type
-        public var repliedMessageContentView: ChatRepliedMessageContentView<ExtraData>.Type
-
-        public init(
-            authorAvatarView: AvatarView.Type = AvatarView.self,
-            bubbleView: ChatMessageBubbleView<ExtraData>.Type = ChatMessageBubbleView<ExtraData>.self,
-            metadataView: ChatMessageMetadataView<ExtraData>.Type = ChatMessageMetadataView<ExtraData>.self,
-            repliedMessageContentView: ChatRepliedMessageContentView<ExtraData>.Type = ChatRepliedMessageContentView<ExtraData>.self
-        ) {
-            self.authorAvatarView = authorAvatarView
-            self.bubbleView = bubbleView
-            self.metadataView = metadataView
-            self.repliedMessageContentView = repliedMessageContentView
-        }
+        public var authorAvatarView: AvatarView.Type = AvatarView.self
+        public var bubbleView: ChatMessageBubbleView<ExtraData>.Type = ChatMessageBubbleView<ExtraData>.self
+        public var metadataView: ChatMessageMetadataView<ExtraData>.Type = ChatMessageMetadataView<ExtraData>.self
+        public var repliedMessageContentView: ChatRepliedMessageContentView<ExtraData>.Type =
+            ChatRepliedMessageContentView<ExtraData>.self
     }
 }
