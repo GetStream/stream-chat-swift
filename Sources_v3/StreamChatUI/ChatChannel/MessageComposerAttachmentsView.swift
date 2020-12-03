1a760f684dd0074824f19906baf85d47cdc384c8
Pavel Lopatin <mantergo@gmail.com>
2020-12-03 21:25:33 +0100

    Move all logic from subviews to `MessageComposerInputAccessoryViewController`

--- Sources_v3/StreamChatUI/ChatChannel/MessageComposerAttachmentsView.swift
+++ Sources_v3/StreamChatUI/ChatChannel/MessageComposerAttachmentsView.swift
@@ -9,10 +9,15 @@ open class MessageComposerAttachmentsView<ExtraData: UIExtraDataTypes>: UIView,
     UICollectionViewDataSource {
     // MARK: - Properties
 
-    public weak var composer: UIView?
     public let uiConfig: UIConfig<ExtraData>
     
-    var attachments: [UIImage] = []
+    var previews: [UIImage] = [] {
+        didSet {
+            collectionView.reloadData()
+        }
+    }
+    
+    var didTapRemoveItemButton: ((Int) -> Void)?
     
     // MARK: - Subviews
     
@@ -68,37 +73,18 @@ open class MessageComposerAttachmentsView<ExtraData: UIExtraDataTypes>: UIView,
         collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
     }
     
-    // MARK: - Actions
-    
-    public func insertNewItem(with image: UIImage) {
-        attachments.append(image)
-        collectionView.reloadData()
-        if !attachments.isEmpty {
-            isHidden = false
-        }
-    }
-
-    public func remove(at index: Int) {
-        attachments.remove(at: index)
-        collectionView.reloadData()
-        if attachments.isEmpty {
-            isHidden = true
-            composer?.invalidateIntrinsicContentSize()
-        }
-    }
-    
     // MARK: - UICollectionView
     
     public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        attachments.count
+        previews.count
     }
     
     public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
         let imageView = MessageComposerAttachmentsCellView(
-            image: attachments[indexPath.row],
+            image: previews[indexPath.row],
             deleteClosure: { [weak self] in
-                self?.remove(at: indexPath.row)
+                self?.didTapRemoveItemButton?(indexPath.row)
                 collectionView.reloadItems(at: collectionView.indexPathsForVisibleItems)
             }
         )
