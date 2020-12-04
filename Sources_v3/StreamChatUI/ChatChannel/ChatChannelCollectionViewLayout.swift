087880584d6b5b9afd35af2bf7eecda7de59732e
Denis Bogomolov <ManWithBear@users.noreply.github.com>
2020-12-04 22:19:50 +0100

    Prevent user from overscrolling out of visible content (#626)

--- Sources_v3/StreamChatUI/ChatChannel/ChatChannelCollectionViewLayout.swift
+++ Sources_v3/StreamChatUI/ChatChannel/ChatChannelCollectionViewLayout.swift
@@ -5,10 +5,17 @@
 import Foundation
 import UIKit
 
+/// This layout flips items such that item at indexPath 0-0 appears at the bottom of collectionView,
+/// while last item appears first.
+///
+/// Such approach comes with self-sizing cell issue: when you scroll to "estimated" cell bottom anchor, cell size calculated
+/// and you may jump right into middle of cell, because `collectionViewContentSize` have been changed,
+/// while `contentOffset` stay same.
+/// To fight it we lock `collectionViewContentSize`, now newly calculated cell "expands" up into invisible yet area, removing jumps.
 open class ChatChannelCollectionViewLayout: UICollectionViewFlowLayout {
     // MARK: - Init & Deinit
     
-    override public init() {
+    override public required init() {
         super.init()
         commonInit()
     }
@@ -22,8 +29,24 @@ open class ChatChannelCollectionViewLayout: UICollectionViewFlowLayout {
         minimumInteritemSpacing = 0
         minimumLineSpacing = 4
     }
+
+    open var zeroOffset: CGPoint {
+        CGPoint(
+            x: 0,
+            y: collectionViewContentSize.height - realContentSize.height
+        )
+    }
+
+    open var realContentSize: CGSize {
+        super.collectionViewContentSize
+    }
     
     // MARK: - Overrides
+
+    override open var collectionViewContentSize: CGSize {
+        let size = super.collectionViewContentSize
+        return CGSize(width: size.width, height: 100_000)
+    }
     
     override open func prepare() {
         super.prepare()
@@ -32,10 +55,6 @@ open class ChatChannelCollectionViewLayout: UICollectionViewFlowLayout {
             width: collectionView?.bounds.width ?? 0,
             height: 60
         )
-        if collectionView!.contentSize == .zero {
-            let newOffset = CGPoint(x: 0, y: collectionViewContentSize.height)
-            collectionView?.contentOffset = newOffset
-        }
     }
 
     override open func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
@@ -53,6 +72,32 @@ open class ChatChannelCollectionViewLayout: UICollectionViewFlowLayout {
     override open func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
         super.layoutAttributesForItem(at: indexPath).map(flip(_:))
     }
+
+    override open func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint) -> CGPoint {
+        // Content offset may be zero only when view just loaded for first time and needs to be scrolled to bottom
+        if proposedContentOffset == .zero {
+            return CGPoint(
+                x: 0,
+                y: collectionViewContentSize.height - (collectionView?.bounds.height ?? 0)
+            )
+        }
+        if proposedContentOffset.y < zeroOffset.y {
+            return zeroOffset
+        }
+        return super.targetContentOffset(forProposedContentOffset: proposedContentOffset)
+    }
+
+    override open func targetContentOffset(
+        forProposedContentOffset proposedContentOffset: CGPoint,
+        withScrollingVelocity velocity: CGPoint
+    ) -> CGPoint {
+        if proposedContentOffset.y < zeroOffset.y {
+            return zeroOffset
+        }
+        return super.targetContentOffset(forProposedContentOffset: proposedContentOffset, withScrollingVelocity: velocity)
+    }
+
+    // MARK: - Dark Magic
 
     private func flip(_ attribute: UICollectionViewLayoutAttributes) -> UICollectionViewLayoutAttributes {
         let contentSize = collectionViewContentSize
