50d416712e57e4a45c9629360bf786af4083b735
Pavel Lopatin <mantergo@gmail.com>
2020-12-07 21:37:49 +0100

    Improve `ChatChannelMessageComposerView` UI and behavior

--- Sources_v3/StreamChatUI/ChatChannel/ChatChannelMessageComposerView.swift
+++ Sources_v3/StreamChatUI/ChatChannel/ChatChannelMessageComposerView.swift
@@ -4,97 +4,152 @@
 
 import UIKit
 
-open class ChatChannelMessageComposerView<ExtraData: UIExtraDataTypes>: UIInputView {
+open class ChatChannelMessageComposerView<ExtraData: UIExtraDataTypes>: UIInputView,
+    UIConfigProvider,
+    Customizable,
+    AppearanceSetting {
     // MARK: - Properties
     
-    public var buttonHeight: CGFloat = 20
+    public var attachmentsViewHeight: CGFloat = .zero
+    public var stateIconHeight: CGFloat = .zero
     
-    public let uiConfig: UIConfig<ExtraData>
-        
     // MARK: - Subviews
 
     public private(set) lazy var container = ContainerStackView().withoutAutoresizingMaskConstraints
     
     public private(set) lazy var replyView: UIView = .init()
     
-    public private(set) lazy var attachmentsView: MessageComposerAttachmentsView<ExtraData> = .init(uiConfig: uiConfig)
+    public private(set) lazy var attachmentsView: MessageComposerAttachmentsView<ExtraData> = uiConfig
+        .messageComposer
+        .attachmentsView.init()
+        .withoutAutoresizingMaskConstraints
     
-    public private(set) lazy var messageInputView: ChatChannelMessageInputView<ExtraData> = .init(uiConfig: uiConfig)
+    public private(set) lazy var messageInputView: ChatChannelMessageInputView<ExtraData> = uiConfig
+        .messageComposer
+        .messageInputView.init()
+        .withoutAutoresizingMaskConstraints
     
-    public private(set) lazy var sendButton: UIButton = {
-        let button = UIButton().withoutAutoresizingMaskConstraints
-        if #available(iOS 13.0, *) {
-            button.setImage(UIImage(systemName: "arrow.up.circle.fill"), for: .normal)
-        }
-        button.widthAnchor.constraint(equalTo: button.heightAnchor, multiplier: 1).isActive = true
-        return button
-    }()
-        
-    public private(set) lazy var searchButton: UIButton = {
-        let button = UIButton().withoutAutoresizingMaskConstraints
-        if #available(iOS 13.0, *) {
-            button.setImage(UIImage(systemName: "magnifyingglass"), for: .normal)
-        }
-        button.widthAnchor.constraint(equalTo: button.heightAnchor, multiplier: 1).isActive = true
-        return button
+    public private(set) lazy var sendButton: MessageComposerSendButton<ExtraData> = uiConfig
+        .messageComposer
+        .sendButton.init()
+        .withoutAutoresizingMaskConstraints
+    
+    public private(set) lazy var attachmentButton: UIButton = uiConfig
+        .messageComposer
+        .composerButton.init()
+        .withoutAutoresizingMaskConstraints
+    
+    public private(set) lazy var commandsButton: UIButton = uiConfig
+        .messageComposer
+        .composerButton.init()
+        .withoutAutoresizingMaskConstraints
+    
+    public private(set) lazy var shrinkInputButton: UIButton = uiConfig
+        .messageComposer
+        .composerButton.init()
+        .withoutAutoresizingMaskConstraints
+    
+    public private(set) lazy var stateIcon: UIImageView = {
+        let imageView = UIImageView().withoutAutoresizingMaskConstraints
+        imageView.contentMode = .center
+        imageView.widthAnchor.constraint(equalTo: imageView.heightAnchor, multiplier: 1).isActive = true
+        return imageView
     }()
     
-    public private(set) lazy var attachmentButton: UIButton = {
-        let button = UIButton().withoutAutoresizingMaskConstraints
-        if #available(iOS 13.0, *) {
-            button.setImage(UIImage(systemName: "paperclip"), for: .normal)
-        }
-        button.widthAnchor.constraint(equalTo: button.heightAnchor, multiplier: 1).isActive = true
-        return button
-    }()
-
-    public private(set) lazy var boltButton: UIButton = {
-        let button = UIButton().withoutAutoresizingMaskConstraints
-        if #available(iOS 13.0, *) {
-            button.setImage(UIImage(systemName: "bolt.fill"), for: .normal)
-        }
-        button.widthAnchor.constraint(equalTo: button.heightAnchor, multiplier: 1).isActive = true
-        return button
-    }()
+    public private(set) lazy var dismissButton: UIButton = uiConfig
+        .messageComposer
+        .composerButton.init()
+        .withoutAutoresizingMaskConstraints
+    
+    public private(set) lazy var titleLabel: UILabel = UILabel().withoutAutoresizingMaskConstraints
     
     // MARK: - Init
     
-    public required init(
-        uiConfig: UIConfig<ExtraData> = .default
-    ) {
-        self.uiConfig = uiConfig
-        
+    public required init() {
         super.init(frame: .zero, inputViewStyle: .default)
-        
-        commonInit()
     }
     
     public required init?(coder: NSCoder) {
-        uiConfig = .default
-        
         super.init(coder: coder)
-        
-        commonInit()
     }
     
-    public func commonInit() {
-        embed(container)
-
-        setupAppearance()
-        setupLayout()
+    // MARK: - Overrides
+    
+    override open func didMoveToSuperview() {
+        super.didMoveToSuperview()
+        guard superview != nil else { return }
+        
+        setUp()
+        (self as! Self).applyDefaultAppearance()
+        setUpAppearance()
+        setUpLayout()
+        updateContent()
+    }
+    
+    override open var intrinsicContentSize: CGSize {
+        let size = CGSize(
+            width: UIView.noIntrinsicMetric,
+            height: container.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize).height
+        )
+        return size
     }
     
     // MARK: - Public
+
+    open func setUp() {}
     
-    open func setupAppearance() {
+    open func defaultAppearance() {
+        attachmentsViewHeight = 80
+        stateIconHeight = 40
+        
         backgroundColor = uiConfig.colorPalette.messageComposerBackground
+        
+        container.centerStackView.clipsToBounds = true
+        container.centerStackView.layer.cornerRadius = 25
+        container.centerStackView.layer.borderWidth = 1
+        container.centerStackView.layer.borderColor = uiConfig.colorPalette.messageComposerBorder.cgColor
+        
+        layer.shadowColor = UIColor.systemGray.cgColor
+        layer.shadowOpacity = 1
+        layer.shadowOffset = .zero
+        layer.shadowRadius = 0.5
+        
+        let clipIcon = UIImage(named: "clip", in: .streamChatUI)?.tinted(with: uiConfig.colorPalette.messageComposerButton)
+        attachmentButton.setImage(clipIcon, for: .normal)
+        
+        let boltIcon = UIImage(named: "bolt", in: .streamChatUI)?.tinted(with: uiConfig.colorPalette.messageComposerButton)
+        commandsButton.setImage(boltIcon, for: .normal)
+        
+        let shrinkArrowIcon = UIImage(named: "shrinkInputArrow", in: .streamChatUI)
+        shrinkInputButton.setImage(shrinkArrowIcon, for: .normal)
+        
+        let dismissIcon =
+            UIImage(named: "dismissInCircle", in: .streamChatUI)?.tinted(with: uiConfig.colorPalette.messageComposerButton)
+        dismissButton.setImage(dismissIcon, for: .normal)
+        
+        titleLabel.textAlignment = .center
+        titleLabel.font = .preferredFont(forTextStyle: .headline)
+        titleLabel.adjustsFontForContentSizeCategory = true
     }
     
-    open func setupLayout() {
+    open func setUpAppearance() {}
+    
+    open func setUpLayout() {
+        embed(container)
+        
+        preservesSuperviewLayoutMargins = true
+        
         container.preservesSuperviewLayoutMargins = true
         container.isLayoutMarginsRelativeArrangement = true
         
-        container.centerContainerStackView.spacing = UIStackView.spacingUseSystem
+        container.spacing = UIStackView.spacingUseSystem
+        
+        container.topStackView.alignment = .center
+        container.topStackView.addArrangedSubview(stateIcon)
+        container.topStackView.addArrangedSubview(titleLabel)
+        container.topStackView.addArrangedSubview(dismissButton)
+        
+        stateIcon.heightAnchor.constraint(equalToConstant: stateIconHeight).isActive = true
         
         container.centerStackView.isHidden = false
         container.centerStackView.axis = .vertical
@@ -102,7 +157,7 @@ open class ChatChannelMessageComposerView<ExtraData: UIExtraDataTypes>: UIInputV
         container.centerStackView.addArrangedSubview(replyView)
         
         container.centerStackView.addArrangedSubview(attachmentsView)
-        attachmentsView.heightAnchor.constraint(equalToConstant: 80).isActive = true
+        attachmentsView.heightAnchor.constraint(equalToConstant: attachmentsViewHeight).isActive = true
         
         container.centerStackView.addArrangedSubview(messageInputView)
         messageInputView.setContentHuggingPriority(.defaultLow, for: .horizontal)
@@ -110,59 +165,17 @@ open class ChatChannelMessageComposerView<ExtraData: UIExtraDataTypes>: UIInputV
         container.rightStackView.isHidden = false
         container.rightStackView.alignment = .center
         container.rightStackView.spacing = UIStackView.spacingUseSystem
-        searchButton.isHidden = true
-        container.rightStackView.addArrangedSubview(searchButton)
         container.rightStackView.addArrangedSubview(sendButton)
         
         container.leftStackView.isHidden = false
         container.leftStackView.alignment = .center
-        container.leftStackView.spacing = UIStackView.spacingUseSystem
+        container.leftStackView.addArrangedSubview(shrinkInputButton)
         container.leftStackView.addArrangedSubview(attachmentButton)
-        boltButton.isHidden = true
-        container.leftStackView.addArrangedSubview(boltButton)
-        
-        searchButton.widthAnchor.constraint(equalToConstant: buttonHeight).isActive = true
-        sendButton.widthAnchor.constraint(equalToConstant: buttonHeight).isActive = true
-        attachmentButton.widthAnchor.constraint(equalToConstant: buttonHeight).isActive = true
-        boltButton.widthAnchor.constraint(equalToConstant: buttonHeight).isActive = true
+        container.leftStackView.addArrangedSubview(commandsButton)
 
         attachmentsView.isHidden = true
-
-        addObserver(self, forKeyPath: "safeAreaInsets", options: .new, context: nil)
-        messageInputView.textView.addObserver(self, forKeyPath: "contentSize", options: .new, context: nil)
+        shrinkInputButton.isHidden = true
     }
     
-    // MARK: - Overrides
-    
-    override open var intrinsicContentSize: CGSize {
-        let size = CGSize(
-            width: .zero,
-            height: container.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize).height
-        )
-        return size
-    }
-    
-    override open func layoutSubviews() {
-        super.layoutSubviews()
-                
-        container.centerStackView.clipsToBounds = true
-        container.centerStackView.layer.cornerRadius = 20
-        container.centerStackView.layer.borderWidth = 2
-        container.centerStackView.layer.borderColor = uiConfig.colorPalette.messageComposerBorder.cgColor
-    }
-    
-    // There are some issues with new-style KVO so that is something that will need attention later.
-    // swiftlint:disable block_based_kvo
-    override open func observeValue(
-        forKeyPath keyPath: String?,
-        of object: Any?,
-        change: [NSKeyValueChangeKey: Any]?,
-        context: UnsafeMutableRawPointer?
-    ) {
-        if object as AnyObject? === messageInputView.textView, keyPath == "contentSize" {
-            invalidateIntrinsicContentSize()
-        } else if object as AnyObject? === self, keyPath == "safeAreaInsets" {
-            invalidateIntrinsicContentSize()
-        }
-    }
+    open func updateContent() {}
 }
