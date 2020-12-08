afb39e24cb6d305ab65be4ec8bb77a00900679e8
Pavel Evsaev <pavel.evsaev@gmail.com>
2020-12-08 14:37:44 +0300

    Introduce `with(priority:)` helper for `NSLayoutConstraint`

---
+++ Sources_v3/StreamChatUI/Utils/NSLayoutConstraint+Extensions.swift
@@ -0,0 +1,12 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import UIKit
+
+extension NSLayoutConstraint {
+    func with(priority: UILayoutPriority) -> NSLayoutConstraint {
+        self.priority = priority
+        return self
+    }
+}
