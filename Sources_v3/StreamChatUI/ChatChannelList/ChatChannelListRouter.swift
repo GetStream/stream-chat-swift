9725db58d5f5bcaaeed2fbbfbe27f508ce1d74bc
Vojta Stavik <stavik@outlook.com>
2020-12-11 15:49:59 +0100

    Open chat detail from channel list

--- Sources_v3/StreamChatUI/ChatChannelList/ChatChannelListRouter.swift
+++ Sources_v3/StreamChatUI/ChatChannelList/ChatChannelListRouter.swift
@@ -11,7 +11,20 @@ open class ChatChannelListRouter<ExtraData: ExtraDataTypes>: ChatRouter<ChatChan
     }
     
     open func openChat(for channel: _ChatChannel<ExtraData>) {
-        debugPrint(channel)
+        guard let client = rootViewController?.controller.client else {
+            log.error("Can't open chat detail, the root controller is `nil`.")
+            return
+        }
+        
+        let vc = ChatChannelVC<ExtraData>()
+        vc.controller = client.channelController(for: channel.cid)
+        
+        guard let navController = rootViewController?.navigationController else {
+            log.error("Can't push chat detail, no navigation controller available")
+            return
+        }
+        
+        navController.show(vc, sender: self)
     }
     
     open func openCreateNewChannel() {
