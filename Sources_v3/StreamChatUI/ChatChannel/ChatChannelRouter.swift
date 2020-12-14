288acb63792813d6fbae297e0513e647301a7415
Denis Bogomolov <denis@manwithbear.com>
2020-12-14 19:19:27 +0100

    Introduce Thread controller

--- Sources_v3/StreamChatUI/ChatChannel/ChatChannelRouter.swift
+++ Sources_v3/StreamChatUI/ChatChannel/ChatChannelRouter.swift
@@ -6,5 +6,10 @@ import StreamChat
 import UIKit
 
 open class ChatChannelRouter<ExtraData: ExtraDataTypes>: ChatRouter<ChatChannelVC<ExtraData>> {
-    open func showThreadDetail(for message: _ChatMessage<ExtraData>, within channel: _ChatChannelController<ExtraData>) {}
+    open func showThreadDetail(for message: _ChatMessage<ExtraData>, within channel: _ChatChannelController<ExtraData>) {
+        let controller = ChatThreadVC<ExtraData>()
+        controller.channelController = channel
+        controller.controller = channel.client.messageController(cid: channel.cid!, messageId: message.id)
+        navigationController?.show(controller, sender: self)
+    }
 }
