2b8b0439ad9ebda7d354165beb4d60014f39ad0a
Denis Bogomolov <denis@manwithbear.com>
2020-12-17 17:01:36 +0100

    Disable all interactions with message textview except links

---
+++ Sources_v3/StreamChatUI/Common Views/OnlyLinkTappableTextView.swift
@@ -0,0 +1,20 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import UIKit
+
+/// Text View that ignore all user interactions except touches on links
+class OnlyLinkTappableTextView: UITextView {
+    override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        if let range = characterRange(at: point),
+            !range.isEmpty,
+            let position = closestPosition(to: point, within: range),
+            let styles = textStyling(at: position, in: .forward),
+            styles[.link] != nil {
+            return super.hitTest(point, with: event)
+        } else {
+            return nil
+        }
+    }
+}
