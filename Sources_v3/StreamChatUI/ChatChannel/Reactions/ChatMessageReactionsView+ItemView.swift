36ed3d698ff333962d9d3b2f18f86e98fc8da88b
Pavel Evsaev <pavel.evsaev@gmail.com>
2020-12-23 16:40:15 +0300

    Introduce `ChatMessageReactionsView.ItemView`

---
+++ Sources_v3/StreamChatUI/ChatChannel/Reactions/ChatMessageReactionsView+ItemView.swift
@@ -0,0 +1,85 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import StreamChat
+import UIKit
+
+extension ChatMessageReactionsView {
+    open class ItemView: Button, UIConfigProvider {
+        public var content: Content? {
+            didSet { updateContentIfNeeded() }
+        }
+
+        // MARK: - Overrides
+
+        override open var intrinsicContentSize: CGSize {
+            reactionImage?.size ?? .zero
+        }
+
+        override open func setUp() {
+            super.setUp()
+
+            addTarget(self, action: #selector(didTap), for: .touchUpInside)
+        }
+
+        override open func updateContent() {
+            setImage(reactionImage, for: .normal)
+            imageView?.tintColor = reactionImageTint
+        }
+
+        override open func tintColorDidChange() {
+            super.tintColorDidChange()
+
+            updateContentIfNeeded()
+        }
+
+        // MARK: - Actions
+
+        @objc open func didTap() {
+            guard let content = self.content else { return }
+
+            content.onTap(content.reaction.type)
+        }
+    }
+}
+
+// MARK: - Content
+
+extension ChatMessageReactionsView.ItemView {
+    public struct Content {
+        public let useBigIcon: Bool
+        public let reaction: ChatMessageReactionData
+        public var onTap: (MessageReactionType) -> Void
+
+        public init(
+            useBigIcon: Bool,
+            reaction: ChatMessageReactionData,
+            onTap: @escaping (MessageReactionType) -> Void
+        ) {
+            self.useBigIcon = useBigIcon
+            self.reaction = reaction
+            self.onTap = onTap
+        }
+    }
+}
+
+// MARK: - Private
+
+private extension ChatMessageReactionsView.ItemView {
+    var reactionImage: UIImage? {
+        guard let content = content else { return nil }
+
+        return content.useBigIcon ?
+            uiConfig.messageList.messageReactions.bigIconForMessageReaction(content.reaction.type) :
+            uiConfig.messageList.messageReactions.smallIconForMessageReaction(content.reaction.type)
+    }
+
+    var reactionImageTint: UIColor? {
+        guard let content = content else { return nil }
+
+        return content.reaction.isChosenByCurrentUser ?
+            tintColor :
+            uiConfig.colorPalette.inactiveReactionTint
+    }
+}
