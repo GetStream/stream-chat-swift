4d6e203889fab12b99181e1b54f4e4f1e72a73e0
Pavel Evsaev <pavel.evsaev@gmail.com>
2020-12-23 16:54:44 +0300

    Make use of `ChatMessageReactionsBubbleView`

--- Sources_v3/StreamChatUI/MessageActionsPopup/ChatMessagePopupViewController.swift
+++ Sources_v3/StreamChatUI/MessageActionsPopup/ChatMessagePopupViewController.swift
@@ -73,6 +73,8 @@ open class ChatMessagePopupViewController<ExtraData: ExtraDataTypes>: ViewContro
                 ),
                 actionsController.view.trailingAnchor.constraint(equalTo: messageContentView.trailingAnchor),
                 reactionsController?.view.centerXAnchor.constraint(equalTo: messageContentView.leadingAnchor)
+                    .with(priority: .defaultHigh),
+                reactionsController?.reactionsBubble.tailTrailingAnchor.constraint(equalTo: messageContentView.leadingAnchor)
             ])
         } else {
             constraints.append(contentsOf: [
@@ -82,16 +84,17 @@ open class ChatMessagePopupViewController<ExtraData: ExtraDataTypes>: ViewContro
                 ),
                 actionsController.view.leadingAnchor.constraint(equalTo: messageContentView.messageBubbleView.leadingAnchor),
                 reactionsController?.view.centerXAnchor.constraint(equalTo: messageContentView.trailingAnchor)
+                    .with(priority: .defaultHigh),
+                reactionsController?.reactionsBubble.tailLeadingAnchor.constraint(equalTo: messageContentView.trailingAnchor)
             ])
         }
 
-        constraints.last??.priority = .defaultHigh
         NSLayoutConstraint.activate(constraints.compactMap { $0 })
     }
 
     override open func updateContent() {
         messageContentView.message = message
-        messageContentView.messageReactionsView.isHidden = true
+        messageContentView.reactionsBubble.isHidden = true
     }
 
     // MARK: - Actions
