3ad581ce2999b13e3cc1cff65e42d8ec86121562
Pavel Evsaev <pavel.evsaev@gmail.com>
2020-12-24 14:04:41 +0300

    Introduce `AttachmentActionRequestBody`

---
+++ Sources_v3/StreamChat/APIClient/Endpoints/Requests/AttachmentActionRequestBody.swift
@@ -0,0 +1,28 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+
+/// The type describes the outgoing JSON to `message/{id}/action` endpoint
+struct AttachmentActionRequestBody: Encodable {
+    private enum CodingKeys: String, CodingKey {
+        case channelId = "id"
+        case channelType = "type"
+        case messageId = "message_id"
+        case data = "form_data"
+    }
+
+    let cid: ChannelId
+    let messageId: MessageId
+    let action: AttachmentAction
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+
+        try container.encode(cid.id, forKey: .channelId)
+        try container.encode(cid.type, forKey: .channelType)
+        try container.encode(messageId, forKey: .messageId)
+        try container.encode([action.name: action.value], forKey: .data)
+    }
+}
