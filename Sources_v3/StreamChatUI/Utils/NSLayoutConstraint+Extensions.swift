6a267e959d67c2fbc2270b432b520447ca1f5cc7
Pavel Evsaev <pavel.evsaev@gmail.com>
2020-12-24 17:24:00 +0300

    Introduce `almostRequired` helper for `NSLayoutConstraint`

--- Sources_v3/StreamChatUI/Utils/NSLayoutConstraint+Extensions.swift
+++ Sources_v3/StreamChatUI/Utils/NSLayoutConstraint+Extensions.swift
@@ -4,9 +4,29 @@
 
 import UIKit
 
+extension UILayoutPriority {
+    /// The `UILayoutPriority` that is less than required by 1.
+    ///
+    /// Such helper is especially handy when working with `UIStackView`:
+    /// when the arranged subview is hidden, the constraint for `height == 0`
+    /// with `.required` priority is activated.
+    ///
+    /// Having all height constraints inside the `UIStackView` with `almostRequired`
+    /// priority eliminates breaking constraints warnings in console.
+    static let almostRequired: Self = .required - 1
+}
+
 extension NSLayoutConstraint {
+    /// Changes the priority of `self` to the provided one.
+    /// - Parameter priority: The priority to be applied.
+    /// - Returns: `self` with updated `priority`.
     func with(priority: UILayoutPriority) -> NSLayoutConstraint {
         self.priority = priority
         return self
+    }
+
+    /// Returns updated `self` with `priority == .almostRequired`
+    var almostRequired: NSLayoutConstraint {
+        with(priority: .almostRequired)
     }
 }
