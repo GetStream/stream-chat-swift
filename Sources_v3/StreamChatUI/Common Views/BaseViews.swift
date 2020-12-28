9bcc0083b3751699dabcd23f204377590cca985e
Pavel Lopatin <mantergo@gmail.com>
2020-12-28 15:43:14 +0100

    Introduce base `CollectionViewCell`

--- Sources_v3/StreamChatUI/Common Views/BaseViews.swift
+++ Sources_v3/StreamChatUI/Common Views/BaseViews.swift
@@ -65,6 +65,27 @@ open class View: UIView, AppearanceSetting, Customizable {
 
 /// Base class for overridable views StreamChatUI provides.
 /// All conformers will have StreamChatUI appearance settings by default.
+open class CollectionViewCell: UICollectionViewCell, AppearanceSetting, Customizable {
+    override open func didMoveToSuperview() {
+        super.didMoveToSuperview()
+        guard superview != nil else { return }
+        
+        setUp()
+        (self as! Self).applyDefaultAppearance()
+        setUpAppearance()
+        setUpLayout()
+        updateContent()
+    }
+    
+    public func defaultAppearance() { /* default empty implementation */ }
+    open func setUp() { /* default empty implementation */ }
+    open func setUpAppearance() { /* default empty implementation */ }
+    open func setUpLayout() { /* default empty implementation */ }
+    open func updateContent() { /* default empty implementation */ }
+}
+
+/// Base class for overridable views StreamChatUI provides.
+/// All conformers will have StreamChatUI appearance settings by default.
 open class Control: UIControl, AppearanceSetting, Customizable {
     override open func didMoveToSuperview() {
         super.didMoveToSuperview()
