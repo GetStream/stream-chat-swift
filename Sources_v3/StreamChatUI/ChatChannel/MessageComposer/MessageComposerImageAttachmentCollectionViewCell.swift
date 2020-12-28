d064b0e8f38236bf789d9480ecbf39d132a48ce7
Pavel Lopatin <mantergo@gmail.com>
2020-12-28 15:51:09 +0100

    Introduce `MessageComposerImageAttachmentCollectionViewCell`

---
+++ Sources_v3/StreamChatUI/ChatChannel/MessageComposer/MessageComposerImageAttachmentCollectionViewCell.swift
@@ -0,0 +1,59 @@
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import StreamChat
+import UIKit
+
+open class MessageComposerImageAttachmentCollectionViewCell<ExtraData: ExtraDataTypes>: CollectionViewCell,
+    UIConfigProvider {
+    // MARK: - Properties
+    
+    class var reuseId: String { String(describing: self) }
+    
+    public var discardButtonHandler: (() -> Void)?
+    
+    // MARK: - Subviews
+    
+    public private(set) lazy var imageView: UIImageView = UIImageView()
+        .withoutAutoresizingMaskConstraints
+    
+    public private(set) lazy var discardButton: UIButton = UIButton()
+        .withoutAutoresizingMaskConstraints
+        
+    // MARK: - Lifecycle
+    
+    override open func setUp() {
+        super.setUp()
+        
+        discardButton.addTarget(self, action: #selector(discard), for: .touchUpInside)
+    }
+    
+    override public func defaultAppearance() {
+        discardButton.setImage(UIImage(named: "discardAttachment", in: .streamChatUI), for: .normal)
+        
+        layer.masksToBounds = true
+        layer.cornerRadius = 15
+        
+        imageView.contentMode = .scaleAspectFill
+    }
+        
+    override open func setUpLayout() {
+        contentView.embed(imageView)
+        
+        contentView.addSubview(discardButton)
+        
+        NSLayoutConstraint.activate([
+            discardButton.topAnchor.constraint(equalTo: contentView.layoutMarginsGuide.topAnchor),
+            discardButton.trailingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.trailingAnchor),
+            discardButton.leadingAnchor.constraint(
+                greaterThanOrEqualToSystemSpacingAfter: contentView.layoutMarginsGuide.leadingAnchor,
+                multiplier: 2
+            )
+        ])
+    }
+    
+    @objc func discard() {
+        discardButtonHandler?()
+    }
+}
