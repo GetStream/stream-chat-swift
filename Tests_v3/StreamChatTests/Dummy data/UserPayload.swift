f671da67e99eb9aa722d263d82f136a8e451f151
Nuno Vieira <nuno.fcvieira93@gmail.com>
2021-01-13 17:49:08 +0000

    Make possible to change `name` and `imageURL` for dummy `UserPayload`

--- Tests_v3/StreamChatTests/Dummy data/UserPayload.swift
+++ Tests_v3/StreamChatTests/Dummy data/UserPayload.swift
@@ -1,5 +1,5 @@
 //
-// Copyright © 2020 Stream.io Inc. All rights reserved.
+// Copyright © 2021 Stream.io Inc. All rights reserved.
 //
 
 import Foundation
@@ -9,12 +9,14 @@ extension UserPayload where ExtraData == DefaultExtraData.User {
     /// Returns a dummy user payload with the given `id` and `extraData`
     static func dummy(
         userId: UserId,
+        name: String = .unique,
+        imageUrl: URL? = .unique(),
         extraData: DefaultExtraData.User = .defaultValue
     ) -> UserPayload {
         .init(
             id: userId,
-            name: .unique,
-            imageURL: .unique(),
+            name: name,
+            imageURL: imageUrl,
             role: .admin,
             createdAt: .unique,
             updatedAt: .unique,
