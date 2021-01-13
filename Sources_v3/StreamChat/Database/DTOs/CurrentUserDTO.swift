ae9942bfe25f73d9631034222ae5f73539680b06
Nuno Vieira <nuno.fcvieira93@gmail.com>
2021-01-13 17:45:31 +0000

    Remove unused `asRequestBody` from `CurrentUserDTO`

--- Sources_v3/StreamChat/Database/DTOs/CurrentUserDTO.swift
+++ Sources_v3/StreamChat/Database/DTOs/CurrentUserDTO.swift
@@ -1,5 +1,5 @@
 //
-// Copyright © 2020 Stream.io Inc. All rights reserved.
+// Copyright © 2021 Stream.io Inc. All rights reserved.
 //
 
 import CoreData
@@ -115,13 +115,6 @@ extension NSManagedObjectContext: CurrentUserDatabaseSession {
 extension CurrentUserDTO {
     /// Snapshots the current state of `CurrentUserDTO` and returns an immutable model object from it.
     func asModel<ExtraData: UserExtraData>() -> _CurrentChatUser<ExtraData> { .create(fromDTO: self) }
-    
-    /// Snapshots the current state of `CurrentUserDTO` and returns its representation for used in API calls.
-    func asRequestBody<ExtraData: UserExtraData>() -> CurrentUserRequestBody<ExtraData> {
-        // swiftlint:disable:previous unavailable_function
-        fatalError("Not implemented yet")
-        // TODO: CIS-235
-    }
 }
 
 extension _CurrentChatUser {
