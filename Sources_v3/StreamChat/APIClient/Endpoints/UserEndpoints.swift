05b10d29f06eb476930a187fad57e927a5412445
Nuno Vieira <nuno.fcvieira93@gmail.com>
2021-01-13 17:55:15 +0000

    Add `updateUser` endpoint

--- Sources_v3/StreamChat/APIClient/Endpoints/UserEndpoints.swift
+++ Sources_v3/StreamChat/APIClient/Endpoints/UserEndpoints.swift
@@ -1,5 +1,5 @@
 //
-// Copyright © 2020 Stream.io Inc. All rights reserved.
+// Copyright © 2021 Stream.io Inc. All rights reserved.
 //
 
 import Foundation
@@ -13,6 +13,26 @@ extension Endpoint {
             queryItems: nil,
             requiresConnectionId: query.options.contains(oneOf: [.presence, .state, .watch]),
             body: ["payload": query]
+        )
+    }
+    
+    static func updateUser<ExtraData: UserExtraData>(
+        id: UserId,
+        payload: UserUpdateRequestBody<ExtraData>
+    ) -> Endpoint<UserUpdateResponse<ExtraData>> {
+        let users: [String: AnyEncodable] = [
+            "id": AnyEncodable(id),
+            "set": AnyEncodable(payload)
+        ]
+        let body: [String: AnyEncodable] = [
+            "users": AnyEncodable([users])
+        ]
+        return Endpoint<UserUpdateResponse<ExtraData>>(
+            path: "users",
+            method: .patch,
+            queryItems: nil,
+            requiresConnectionId: false,
+            body: body
         )
     }
 }
