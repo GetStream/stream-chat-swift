e3d8d56e65d29035678f53e5a4e787e9e2ac6f74
Pavel Evsaev <pavel.evsaev@gmail.com>
2021-01-18 20:23:10 +0300

    Update web-socket-client to connect with user-id only

--- Sources_v3/StreamChat/APIClient/Endpoints/WebSocketConnectEndpoint_Tests.swift
+++ Sources_v3/StreamChat/APIClient/Endpoints/WebSocketConnectEndpoint_Tests.swift
@@ -1,5 +1,5 @@
 //
-// Copyright © 2020 Stream.io Inc. All rights reserved.
+// Copyright © 2021 Stream.io Inc. All rights reserved.
 //
 
 @testable import StreamChat
@@ -8,35 +8,17 @@ import XCTest
 final class WebSocketConnectEndpoint_Tests: XCTestCase {
     func test_webSocketConnect_buildsCorrectly() {
         let userId: UserId = .unique
-        let userRole: UserRole = .admin
-        let name = String.unique
-        let imageURL = URL.unique()
-        let extraData = DefaultExtraData.User.defaultValue
         
         let expectedEndpoint = Endpoint<EmptyResponse>(
             path: "connect",
             method: .get,
             queryItems: nil,
             requiresConnectionId: false,
-            body: [
-                "json": WebSocketConnectPayload(
-                    userId: userId,
-                    name: name,
-                    imageURL: imageURL,
-                    userRole: userRole,
-                    extraData: extraData
-                )
-            ]
+            body: ["json": WebSocketConnectPayload(userId: userId)]
         )
         
         // Build endpoint
-        let endpoint: Endpoint<EmptyResponse> = .webSocketConnect(
-            userId: userId,
-            name: name,
-            imageURL: imageURL,
-            role: userRole,
-            extraData: extraData
-        )
+        let endpoint: Endpoint<EmptyResponse> = .webSocketConnect(userId: userId)
         
         // Assert endpoint is built correctly
         XCTAssertEqual(AnyEndpoint(expectedEndpoint), AnyEndpoint(endpoint))
