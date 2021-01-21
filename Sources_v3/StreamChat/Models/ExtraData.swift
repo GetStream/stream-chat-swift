d4b00b5ece8a1630e1543306f396d66ad0e542a6
Pavel Evsaev <pavel.evsaev@gmail.com>
2021-01-21 13:34:13 +0300

    Update `NoExtraData` to be `ExtraDataTypes`

--- Sources_v3/StreamChat/Models/ExtraData.swift
+++ Sources_v3/StreamChat/Models/ExtraData.swift
@@ -11,8 +11,14 @@ public protocol ExtraData: Codable & Hashable {
 }
 
 /// A type representing no extra data for the given model object.
-public struct NoExtraData: Codable, Hashable, UserExtraData, ChannelExtraData, MessageExtraData, MessageReactionExtraData,
-    AttachmentExtraData {
+public struct NoExtraData: Codable,
+                           Hashable,
+                           UserExtraData,
+                           ChannelExtraData,
+                           MessageExtraData,
+                           MessageReactionExtraData,
+                           AttachmentExtraData,
+                           ExtraDataTypes {
     /// Returns a concrete `NoExtraData` instance.
     public static var defaultValue: Self { .init() }
 }
