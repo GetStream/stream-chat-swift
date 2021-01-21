42954af8bb8594ee053720d158c95949ef7a2d45
Pavel Evsaev <pavel.evsaev@gmail.com>
2021-01-21 13:44:58 +0300

    Remove `GuestUserTokenPayload` test for `NoExtraData`

--- Sources_v3/StreamChat/APIClient/Endpoints/Payloads/GuestUserTokenPayload_Tests.swift
+++ Sources_v3/StreamChat/APIClient/Endpoints/Payloads/GuestUserTokenPayload_Tests.swift
@@ -7,7 +7,6 @@ import XCTest
 
 final class GuestUserTokenPayload_Tests: XCTestCase {
     let guestUserDefaultExtraDataJSON = XCTestCase.mockData(fromFile: "GuestUser+DefaultExtraData")
-    let guestUserNoExtraDataJSON = XCTestCase.mockData(fromFile: "GuestUser+NoExtraData")
     let guestUserCustomExtraDataJSON = XCTestCase.mockData(fromFile: "GuestUser+CustomExtraData")
     let guestUserInvalidTokenJSON = XCTestCase.mockData(fromFile: "GuestUser+InvalidToken")
 
@@ -32,23 +31,6 @@ final class GuestUserTokenPayload_Tests: XCTestCase {
             payload.user.imageURL,
             URL(string: "https://getstream.io/random_svg/?id=broken-waterfall-5&amp;name=Broken+waterfall")!
         )
-        XCTAssertEqual(payload.user.role, .guest)
-        XCTAssertTrue(payload.user.isOnline)
-    }
-    
-    func test_guestUserNoExtraData_isSerialized() throws {
-        let payload = try JSONDecoder.default.decode(GuestUserTokenPayload<NoExtraData>.self, from: guestUserNoExtraDataJSON)
-
-        XCTAssertEqual(
-            payload.token,
-            // swiftlint:disable:next line_length
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYnJva2VuLXdhdGVyZmFsbC01In0.QPeAmdig1KbLwYInW8hwi0XML3kO1M6HH76k4IU0sDg"
-        )
-        XCTAssertNotNil(payload.user)
-        XCTAssertEqual(payload.user.id, "broken-waterfall-5")
-        XCTAssertFalse(payload.user.isBanned)
-        XCTAssertEqual(payload.user.createdAt, "2019-12-12T15:33:46.488935Z".toDate())
-        XCTAssertEqual(payload.user.updatedAt, "2020-06-10T14:11:29.946106Z".toDate())
         XCTAssertEqual(payload.user.role, .guest)
         XCTAssertTrue(payload.user.isOnline)
     }
