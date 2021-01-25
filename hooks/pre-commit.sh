8577a07aee63201022ffb6b9375a8c4f03bc838b
Pavel Evsaev <pavel.evsaev@gmail.com>
2021-01-25 20:54:35 +0300

    Adjust hook-script for new file structure

--- hooks/pre-commit.sh
+++ hooks/pre-commit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-./hooks/git-format-staged --formatter 'mint run swiftformat --config .swiftformat stdin' 'Sources_v3/*.swift'
-./hooks/git-format-staged --formatter 'mint run swiftformat --config .swiftformat stdin' 'Tests_v3/*.swift'
-./hooks/git-format-staged --formatter 'mint run swiftformat --config .swiftformat stdin' 'Sample_v3/*.swift'
+./hooks/git-format-staged --formatter 'mint run swiftformat --config .swiftformat stdin' 'Sources/*.swift'
+./hooks/git-format-staged --formatter 'mint run swiftformat --config .swiftformat stdin' 'Tests/*.swift'
+./hooks/git-format-staged --formatter 'mint run swiftformat --config .swiftformat stdin' 'Sample/*.swift'
 ./hooks/git-format-staged --formatter 'mint run swiftformat --config .swiftformat stdin' 'DemoApp/*.swift'
