69f8a2677a97fe708362625a52796467d452b259
Dominik Bucher <d.bucher@centrum.cz>
2021-01-29 11:24:44 +0100

    Separate UIConfig into extensions in separate files

---
+++ Sources/StreamChatUI/UIConfig+Fonts.swift
@@ -0,0 +1,20 @@
+//
+// Copyright © 2021 Stream.io Inc. All rights reserved.
+//
+
+import UIKit.UIFont
+
+public extension _UIConfig {
+    struct Font {
+        public var caption1 = UIFont.preferredFont(forTextStyle: .caption1)
+        public var footnoteBold = UIFont.preferredFont(forTextStyle: .footnote).bold
+        public var subheadline = UIFont.preferredFont(forTextStyle: .subheadline)
+        public var subheadlineBold = UIFont.preferredFont(forTextStyle: .subheadline).bold
+        public var body = UIFont.preferredFont(forTextStyle: .body)
+        public var bodyBold = UIFont.preferredFont(forTextStyle: .body).bold
+        public var bodyItalic = UIFont.preferredFont(forTextStyle: .body).italic
+        public var headline = UIFont.preferredFont(forTextStyle: .headline)
+        public var headlineBold = UIFont.preferredFont(forTextStyle: .headline).bold
+        public var title = UIFont.preferredFont(forTextStyle: .title1)
+    }
+}
