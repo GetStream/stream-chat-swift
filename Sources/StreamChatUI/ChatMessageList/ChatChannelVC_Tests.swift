e0ce262813d03be859d049a8b229e14f459c57e6
Nuno Vieira <nuno.fcvieira93@gmail.com>
2021-02-02 19:24:11 +0000

    Fix compilation ChatChannelVC_Tests

--- Sources/StreamChatUI/ChatMessageList/ChatChannelVC_Tests.swift
+++ Sources/StreamChatUI/ChatMessageList/ChatChannelVC_Tests.swift
@@ -1,46 +1,52 @@
 //
-// Copyright © 2020 Stream.io Inc. All rights reserved.
+// Copyright © 2021 Stream.io Inc. All rights reserved.
 //
 
+//
+// Copyright © 2020 Stream.io Inc. All rights reserved.
+//
 import StreamChat
 import StreamChatTestTools
 @testable import StreamChatUI
 import XCTest
 
 // ====== Example test only ====
-
 class ChatChannelVC_Tests: XCTestCase {
-    var vc: ChatChannelVC<DefaultUIExtraData>!
-    var channelController: ChatChannelController_Mock<DefaultUIExtraData>!
+    var vc: ChatChannelVC!
+    var channelController: ChatChannelController_Mock<NoExtraData>!
+    var userSuggestionSearchController: ChatUserSearchController_Mock<NoExtraData>!
     
     override func setUp() {
         super.setUp()
         channelController = .mock()
+        userSuggestionSearchController = .mock()
         vc = ChatChannelVC()
-        vc.controller = channelController
-        
+        vc.channelController = channelController
+        vc.userSuggestionSearchController = userSuggestionSearchController
         // Load default data
         let cid = ChannelId(type: .messaging, id: "test")
-        let message: _ChatMessage<DefaultUIExtraData> = .mock(
+        let message: _ChatMessage = _ChatMessage<NoExtraData>.mock(
             id: UUID().uuidString,
             text: "This is a test message",
-            author: .init(id: "luke", name: "Luke Skywalker", imageURL: nil)
+            author: _ChatUser.mock(
+                id: "ID",
+                extraData: NoExtraData.defaultValue
+            )
         )
-        let channel: _ChatChannel<DefaultUIExtraData> = .mock(cid: cid, extraData: .init(name: "Family chat", imageURL: nil))
         
+        let channel = _ChatChannel<NoExtraData>.mock(cid: cid, name: "Family Chat")
         channelController.simulateInitial(channel: channel, messages: [message], state: .remoteDataFetched)
     }
     
     override func tearDown() {
         vc = nil
         channelController = nil
-
         super.tearDown()
     }
     
     func test_allMessagesAreLoaded() {
         // load view
         vc.loadViewIfNeeded()
-        XCTAssertEqual(vc.collectionView.numberOfItems(inSection: 0), 1)
+        XCTAssertEqual(vc.numberOfMessagesInChatMessageListVC(.init()), 1)
     }
 }
