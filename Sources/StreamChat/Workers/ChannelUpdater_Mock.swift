4bb606b30232d6cf851c6b4562ab25f170e022f1
Pavel Lopatin <mantergo@gmail.com>
2021-02-04 21:46:20 +0300

    Add possibility to send message with `[AttachmentEnvelope]` to `ChannelUpdater`

--- Sources/StreamChat/Workers/ChannelUpdater_Mock.swift
+++ Sources/StreamChat/Workers/ChannelUpdater_Mock.swift
@@ -40,7 +40,8 @@ class ChannelUpdaterMock<ExtraData: ExtraDataTypes>: ChannelUpdater<ExtraData> {
     @Atomic var createNewMessage_text: String?
     @Atomic var createNewMessage_command: String?
     @Atomic var createNewMessage_arguments: String?
-    @Atomic var createNewMessage_attachments: [ChatMessageAttachmentSeed]?
+    @Atomic var createNewMessage_attachments: [AttachmentEnvelope]?
+    @Atomic var createNewMessage_attachmentSeeds: [ChatMessageAttachmentSeed]?
     @Atomic var createNewMessage_quotedMessageId: MessageId?
     @Atomic var createNewMessage_extraData: ExtraData.Message?
     @Atomic var createNewMessage_completion: ((Result<MessageId, Error>) -> Void)?
@@ -84,6 +85,7 @@ class ChannelUpdaterMock<ExtraData: ExtraDataTypes>: ChannelUpdater<ExtraData> {
         createNewMessage_command = nil
         createNewMessage_arguments = nil
         createNewMessage_attachments = nil
+        createNewMessage_attachmentSeeds = nil
         createNewMessage_extraData = nil
         createNewMessage_completion = nil
         
@@ -133,7 +135,8 @@ class ChannelUpdaterMock<ExtraData: ExtraDataTypes>: ChannelUpdater<ExtraData> {
         text: String,
         command: String?,
         arguments: String?,
-        attachments: [ChatMessageAttachmentSeed],
+        attachments: [AttachmentEnvelope],
+        attachmentSeeds: [ChatMessageAttachmentSeed],
         quotedMessageId: MessageId?,
         extraData: ExtraData.Message,
         completion: ((Result<MessageId, Error>) -> Void)? = nil
@@ -143,6 +146,7 @@ class ChannelUpdaterMock<ExtraData: ExtraDataTypes>: ChannelUpdater<ExtraData> {
         createNewMessage_command = command
         createNewMessage_arguments = arguments
         createNewMessage_attachments = attachments
+        createNewMessage_attachmentSeeds = attachmentSeeds
         createNewMessage_quotedMessageId = quotedMessageId
         createNewMessage_extraData = extraData
         createNewMessage_completion = completion
