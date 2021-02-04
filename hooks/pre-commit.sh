39858cf93c57b218b4b2e842106f395ef6d31dda
Vojta Stavik <stavik@outlook.com>
2021-02-04 23:21:49 +0100

    Add test project for SPM integration

--- hooks/pre-commit.sh
+++ hooks/pre-commit.sh
@@ -3,6 +3,7 @@
 ./hooks/git-format-staged --formatter 'mint run swiftformat --config .swiftformat stdin' 'Tests/*.swift'
 ./hooks/git-format-staged --formatter 'mint run swiftformat --config .swiftformat stdin' 'Sample/*.swift'
 ./hooks/git-format-staged --formatter 'mint run swiftformat --config .swiftformat stdin' 'DemoApp/*.swift'
+./hooks/git-format-staged --formatter 'mint run swiftformat --config .swiftformat stdin' 'Integration/*.swift'
 
 # Regenerage Package.swift files if needed
 ./hooks/regenerage-spm-package-if-needed.sh
