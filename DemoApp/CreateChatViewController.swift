5349848f0b1b540e12e3aa1da733ee5284111d59
Vojta Stavik <stavik@outlook.com>
2021-02-08 15:05:31 +0100

    Expose custom attachment sending in `ChatMessageComposerVC`

--- DemoApp/CreateChatViewController.swift
+++ DemoApp/CreateChatViewController.swift
@@ -15,7 +15,7 @@ class CreateChatViewController: UIViewController {
     
     // Composer subclass intended to be only used in this VC
     class DemoComposerVC: _ChatMessageComposerVC<NoExtraData> {
-        override func createNewMessage(text: String, quotedMessageId: MessageId? = nil) {
+        override func createNewMessage(text: String, quotedMessageId: MessageId? = nil, attachments: [AttachmentEnvelope] = []) {
             guard let navController = parent?.parent as? UINavigationController,
                 let controller = controller else { return }
             // Create the Channel on backend
