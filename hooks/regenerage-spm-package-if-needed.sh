1ab9015a046d137912bffeb1514fb0ec4a9b511c
Vojta Stavik <stavik@outlook.com>
2021-02-09 17:20:56 +0100

    Speed up pre-commit hook by using compiled script

--- hooks/regenerage-spm-package-if-needed.sh
+++ hooks/regenerage-spm-package-if-needed.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
+# Regenerate list of files in Package.swift
+arch -x86_64 ./generateSPMFileLists
 
-# Was there a new file added or deleted?
-if [ ! -z "$(git diff --staged | grep  -e 'deleted\|new file mode')" ];
-then
-    # Regenerate list of files in Package.swift
-    ./GenerateSPMFileLists.swift
-
-    # Add changes to the current commit 
-    git add Package.swift
-fi
+# Add changes to the current commit 
+git add Package.swift
