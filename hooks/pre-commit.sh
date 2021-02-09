868c9f5c9f9f01f175524d25c3588c788f5f4c43
Bahadir Oncel <bahadir@getstream.io>
2021-02-09 16:38:21 +0100

    Add SwiftFormat for DocsSnippets files
    We've added a new config file just for snippets, we'll change the rules used as we see fit. These files don't need the same config as our source files.

--- hooks/pre-commit.sh
+++ hooks/pre-commit.sh
@@ -4,6 +4,7 @@
 ./hooks/git-format-staged --formatter 'mint run swiftformat --config .swiftformat stdin' 'Sample/*.swift'
 ./hooks/git-format-staged --formatter 'mint run swiftformat --config .swiftformat stdin' 'DemoApp/*.swift'
 ./hooks/git-format-staged --formatter 'mint run swiftformat --config .swiftformat stdin' 'Integration/*.swift'
+./hooks/git-format-staged --formatter 'mint run swiftformat --config .swiftformat-snippets stdin' 'DocsSnippets/*.swift'
 
 # Regenerage Package.swift files if needed
 ./hooks/regenerage-spm-package-if-needed.sh
