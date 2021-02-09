2b2a4e0e7890b81ae384a3ce0720072c7617ba65
Dominik Bucher <d.bucher@centrum.cz>
2021-02-09 16:16:31 +0100

    Audit ChatChannelAvatarView

--- Sources/StreamChatUI/ChatMessageList/ChatChannelVC.swift
+++ Sources/StreamChatUI/ChatMessageList/ChatChannelVC.swift
@@ -39,7 +39,8 @@ open class _ChatChannelVC<ExtraData: ExtraDataTypes>: _ChatVC<ExtraData> {
         let avatar = _ChatChannelAvatarView<ExtraData>()
         avatar.translatesAutoresizingMaskIntoConstraints = false
         avatar.heightAnchor.pin(equalToConstant: 32).isActive = true
-        avatar.channelAndUserId = (channel, channelController.client.currentUserId)
+        avatar.widthAnchor.pin(equalToConstant: 32).isActive = true
+        avatar.content = (channel, channelController.client.currentUserId)
         navigationItem.rightBarButtonItem = UIBarButtonItem(customView: avatar)
         navigationItem.largeTitleDisplayMode = .never
     }
