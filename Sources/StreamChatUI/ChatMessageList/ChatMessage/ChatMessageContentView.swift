96b45013c54b6d4c72ec28e6d067d92544f37a0f
Bahadir Oncel <bahadir@getstream.io>
2021-02-16 14:13:45 +0100

    Introduce ChatMessageTextContentView
    for text-only cells

--- Sources/StreamChatUI/ChatMessageList/ChatMessage/ChatMessageContentView.swift
+++ Sources/StreamChatUI/ChatMessageList/ChatMessage/ChatMessageContentView.swift
@@ -5,6 +5,246 @@
 import StreamChat
 import UIKit
 
+public typealias ChatMessageTextContentView = _ChatMessageTextContentView<NoExtraData>
+
+open class _ChatMessageTextContentView<ExtraData: ExtraDataTypes>: View, UIConfigProvider {
+    public var message: _ChatMessageGroupPart<ExtraData>? {
+        didSet { updateContentIfNeeded() }
+    }
+    
+    public private(set) lazy var messageBubbleView = uiConfig
+        .messageList
+        .messageContentSubviews
+        .bubbleView.init()
+        .withoutAutoresizingMaskConstraints
+    
+    public private(set) lazy var textView: UITextView = {
+        let textView = OnlyLinkTappableTextView()
+        textView.isEditable = false
+        textView.dataDetectorTypes = .link
+        textView.isScrollEnabled = false
+        textView.backgroundColor = .clear
+        textView.font = uiConfig.font.body
+        textView.adjustsFontForContentSizeCategory = true
+        textView.textContainerInset = .zero
+        textView.textContainer.lineFragmentPadding = 0
+        return textView.withoutAutoresizingMaskConstraints
+    }()
+    
+    public private(set) lazy var messageMetadataView = uiConfig
+        .messageList
+        .messageContentSubviews
+        .metadataView
+        .init()
+        .withoutAutoresizingMaskConstraints
+    
+    public private(set) lazy var authorAvatarView = uiConfig
+        .messageList
+        .messageContentSubviews
+        .authorAvatarView
+        .init()
+        .withoutAutoresizingMaskConstraints
+    
+    public private(set) lazy var reactionsBubble = uiConfig
+        .messageList
+        .messageReactions
+        .reactionsBubbleView
+        .init()
+        .withoutAutoresizingMaskConstraints
+    
+    public private(set) lazy var errorIndicator = uiConfig
+        .messageList
+        .messageContentSubviews
+        .errorIndicator
+        .init()
+        .withoutAutoresizingMaskConstraints
+    
+    public var onErrorIndicatorTap: (_ChatMessageGroupPart<ExtraData>?) -> Void = { _ in }
+    
+    private var incomingMessageConstraints: [NSLayoutConstraint] = []
+    private var outgoingMessageConstraints: [NSLayoutConstraint] = []
+    private var bubbleToReactionsConstraint: NSLayoutConstraint?
+    private var bubbleToMetadataConstraint: NSLayoutConstraint?
+    private var bubbleToErrorIndicatorConstraint: NSLayoutConstraint?
+    
+    override open func setUp() {
+        super.setUp()
+        
+        reactionsBubble.isUserInteractionEnabled = false
+        errorIndicator.addTarget(self, action: #selector(didTapOnErrorIndicator), for: .touchUpInside)
+    }
+    
+    override open func setUpLayout() {
+        addSubview(messageBubbleView)
+        addSubview(messageMetadataView)
+        addSubview(authorAvatarView)
+        addSubview(reactionsBubble)
+        addSubview(errorIndicator)
+        addSubview(textView)
+        
+        errorIndicator.setContentCompressionResistancePriority(.required, for: .horizontal)
+        errorIndicator.setContentCompressionResistancePriority(.required, for: .vertical)
+        
+        NSLayoutConstraint.activate([
+            authorAvatarView.widthAnchor.pin(equalToConstant: 32),
+            authorAvatarView.heightAnchor.pin(equalToConstant: 32),
+            authorAvatarView.leadingAnchor.pin(equalTo: leadingAnchor),
+            authorAvatarView.bottomAnchor.pin(equalTo: bottomAnchor),
+            
+            reactionsBubble.topAnchor.pin(equalTo: topAnchor),
+            
+            messageBubbleView.trailingAnchor.pin(equalTo: trailingAnchor).almostRequired,
+            messageBubbleView.topAnchor.pin(equalTo: topAnchor).with(priority: .defaultHigh),
+            messageBubbleView.bottomAnchor.pin(equalTo: bottomAnchor).with(priority: .defaultHigh),
+            
+            messageMetadataView.heightAnchor.pin(equalToConstant: 16),
+            messageMetadataView.bottomAnchor.pin(equalTo: bottomAnchor),
+            
+            errorIndicator.bottomAnchor.pin(equalTo: messageBubbleView.bottomAnchor),
+            errorIndicator.trailingAnchor.pin(equalTo: trailingAnchor),
+            
+            textView.leadingAnchor.pin(equalTo: messageBubbleView.layoutMarginsGuide.leadingAnchor),
+            textView.trailingAnchor.pin(equalTo: messageBubbleView.layoutMarginsGuide.trailingAnchor),
+            textView.topAnchor.pin(equalTo: messageBubbleView.layoutMarginsGuide.topAnchor),
+            textView.bottomAnchor.pin(equalTo: messageBubbleView.layoutMarginsGuide.bottomAnchor)
+        ])
+        
+        // this one is ugly: reactions view is part of message content, but is not part of it frame horizontally.
+        // In same time we want to prevent reactions view to slip out of screen / cell.
+        // We maybe should rethink layout of content view and make reactions part of frame horizontally as well.
+        // This will solve superview access hack
+        if let superview = self.superview {
+            reactionsBubble.trailingAnchor.pin(lessThanOrEqualTo: superview.trailingAnchor).isActive = true
+            reactionsBubble.leadingAnchor.pin(greaterThanOrEqualTo: superview.leadingAnchor).isActive = true
+        }
+        
+        incomingMessageConstraints = [
+            reactionsBubble.centerXAnchor.pin(equalTo: messageBubbleView.trailingAnchor, constant: 8),
+            reactionsBubble.tailLeadingAnchor.pin(equalTo: messageBubbleView.trailingAnchor, constant: -5),
+            
+            messageMetadataView.leadingAnchor.pin(equalTo: messageBubbleView.leadingAnchor).with(priority: .defaultHigh),
+            messageBubbleView.leadingAnchor.pin(
+                equalToSystemSpacingAfter: authorAvatarView.trailingAnchor,
+                multiplier: 1
+            )
+        ]
+        
+        outgoingMessageConstraints = [
+            reactionsBubble.centerXAnchor.pin(equalTo: messageBubbleView.leadingAnchor, constant: -8),
+            reactionsBubble.tailTrailingAnchor.pin(equalTo: messageBubbleView.leadingAnchor, constant: 5),
+            
+            messageMetadataView.trailingAnchor.pin(equalTo: messageBubbleView.trailingAnchor).with(priority: .defaultHigh),
+            messageBubbleView.leadingAnchor.pin(equalTo: leadingAnchor)
+        ]
+        
+        bubbleToReactionsConstraint = messageBubbleView.topAnchor.pin(
+            equalTo: reactionsBubble.centerYAnchor
+        )
+        bubbleToMetadataConstraint = messageMetadataView.topAnchor.pin(
+            equalToSystemSpacingBelow: messageBubbleView.bottomAnchor,
+            multiplier: 1
+        )
+        bubbleToErrorIndicatorConstraint = messageBubbleView.trailingAnchor.pin(
+            equalTo: errorIndicator.centerXAnchor
+        )
+    }
+    
+    override open func updateContent() {
+        // When message cell is about to be reused, it sets `nil` for message value.
+        // That means we need to remove all dynamic constraints to prevent layout warnings.
+        guard let message = self.message else {
+            NSLayoutConstraint.deactivate(outgoingMessageConstraints)
+            NSLayoutConstraint.deactivate(incomingMessageConstraints)
+            bubbleToReactionsConstraint?.isActive = false
+            bubbleToErrorIndicatorConstraint?.isActive = false
+            bubbleToMetadataConstraint?.isActive = false
+            return
+        }
+        
+        var toActivate: [NSLayoutConstraint] = []
+        var toDeactivate: [NSLayoutConstraint] = []
+        
+        let isOutgoing = message.isSentByCurrentUser
+        
+        messageBubbleView.message = message
+        messageMetadataView.message = message
+        
+        let userReactionIDs = Set(message.currentUserReactions.map(\.type))
+        
+        reactionsBubble.content = .init(
+            style: isOutgoing ? .smallOutgoing : .smallIncoming,
+            reactions: message.message.reactionScores.keys
+                .sorted { $0.rawValue < $1.rawValue }
+                .map { .init(type: $0, isChosenByCurrentUser: userReactionIDs.contains($0)) },
+            didTapOnReaction: { _ in }
+        )
+        
+        let placeholder = uiConfig.images.userAvatarPlaceholder1
+        if let imageURL = message.author.imageURL {
+            authorAvatarView.imageView.loadImage(from: imageURL, placeholder: placeholder)
+        } else {
+            authorAvatarView.imageView.image = placeholder
+        }
+        
+        if isOutgoing {
+            toActivate.append(contentsOf: outgoingMessageConstraints)
+            toDeactivate.append(contentsOf: incomingMessageConstraints)
+        } else {
+            toActivate.append(contentsOf: incomingMessageConstraints)
+            toDeactivate.append(contentsOf: outgoingMessageConstraints)
+        }
+        
+        if message.deletedAt == nil, !message.reactionScores.isEmpty {
+            toActivate.append(bubbleToReactionsConstraint!)
+        } else {
+            toDeactivate.append(bubbleToReactionsConstraint!)
+        }
+        
+        if message.isLastInGroup {
+            toActivate.append(bubbleToMetadataConstraint!)
+        } else {
+            toDeactivate.append(bubbleToMetadataConstraint!)
+        }
+        
+        if message.lastActionFailed {
+            toActivate.append(bubbleToErrorIndicatorConstraint!)
+        } else {
+            toDeactivate.append(bubbleToErrorIndicatorConstraint!)
+        }
+        
+        NSLayoutConstraint.deactivate(toDeactivate)
+        NSLayoutConstraint.activate(toActivate)
+        
+        authorAvatarView.isVisible = !isOutgoing && message.isLastInGroup
+        messageMetadataView.isVisible = bubbleToMetadataConstraint?.isActive ?? false
+        reactionsBubble.isVisible = bubbleToReactionsConstraint?.isActive ?? false
+        errorIndicator.isVisible = message.lastActionFailed
+        
+        let layoutOptions = message.layoutOptions
+        
+        let font: UIFont = uiConfig.font.body
+        textView.attributedText = .init(string: message.textContent, attributes: [
+            .foregroundColor: message.deletedAt == nil ? uiConfig.colorPalette.text : uiConfig.colorPalette.subtitleText,
+            .font: message.deletedAt == nil ? font : font.italic
+        ])
+        textView.isVisible = layoutOptions.contains(.text)
+        
+        if message.type == .ephemeral {
+            messageBubbleView.backgroundColor = uiConfig.colorPalette.popoverBackground
+        } else if layoutOptions.contains(.linkPreview) {
+            messageBubbleView.backgroundColor = uiConfig.colorPalette.highlightedAccentBackground1
+        } else {
+            messageBubbleView.backgroundColor = message.isSentByCurrentUser == true ?
+                uiConfig.colorPalette.background2 :
+                uiConfig.colorPalette.popoverBackground
+        }
+    }
+    
+    @objc open func didTapOnErrorIndicator() {
+        onErrorIndicatorTap(message)
+    }
+}
+
 public typealias ChatMessageContentView = _ChatMessageContentView<NoExtraData>
 
 open class _ChatMessageContentView<ExtraData: ExtraDataTypes>: View, UIConfigProvider {
