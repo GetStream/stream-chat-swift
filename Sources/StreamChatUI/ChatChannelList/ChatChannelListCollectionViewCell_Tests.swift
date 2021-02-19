8b5fcd8b2786180009a40389de05bfee4a410c6d
Pavel Lopatin <mantergo@gmail.com>
2021-02-19 15:45:01 +0300

    Audit `ChatChannelListCollectionViewCell`

---
+++ Sources/StreamChatUI/ChatChannelList/ChatChannelListCollectionViewCell_Tests.swift
@@ -0,0 +1,23 @@
+//
+// Copyright © 2021 Stream.io Inc. All rights reserved.
+//
+
+import StreamChat
+import StreamChatTestTools
+@testable import StreamChatUI
+import XCTest
+
+class ChatChannelListCollectionViewCell_Tests: XCTestCase {
+    // defaultAppearance() is called multiple times so backgroundColor is changed by
+    // ChatChannelListItemView and snapshot test is not possible.
+    func test_isHighlighted() {
+        let view = ChatChannelListCollectionViewCell().withoutAutoresizingMaskConstraints
+        view.executeLifecycleMethods()
+        
+        XCTAssertEqual(view.uiConfig.colorPalette.background, view.channelView.backgroundColor)
+        
+        view.isHighlighted = true
+        
+        XCTAssertEqual(view.uiConfig.colorPalette.highlightedBackground, view.channelView.backgroundColor)
+    }
+}
