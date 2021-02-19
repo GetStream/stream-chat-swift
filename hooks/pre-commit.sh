d4e6730b6a94d4987a18590185bb5485e2a2a3af
Vojta Stavik <stavik@outlook.com>
2021-02-19 12:14:51 +0100

    Don't run formatter on generated files

--- hooks/pre-commit.sh
+++ hooks/pre-commit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-./hooks/git-format-staged --formatter 'mint run swiftformat --config .swiftformat stdin' 'Sources/*.swift'
+./hooks/git-format-staged --formatter 'mint run swiftformat --config .swiftformat stdin' 'Sources/*.swift | grep -v "Generated'
 ./hooks/git-format-staged --formatter 'mint run swiftformat --config .swiftformat stdin' 'Tests/*.swift'
 ./hooks/git-format-staged --formatter 'mint run swiftformat --config .swiftformat stdin' 'Sample/*.swift'
 ./hooks/git-format-staged --formatter 'mint run swiftformat --config .swiftformat stdin' 'DemoApp/*.swift'
