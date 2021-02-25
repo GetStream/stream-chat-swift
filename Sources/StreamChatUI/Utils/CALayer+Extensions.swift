77c0cc4d6d959950e7f0d718c19107746772c7cf
Nuno Vieira <nuno.fcvieira93@gmail.com>
2021-02-25 10:56:17 +0000

    Change drop shadow color to avoid shadow opacity
    This fixes a snapshot issue between Intel vs M1 that when shadow opacity is below 1.0, there is rendering differences between the two chips.

--- Sources/StreamChatUI/Utils/CALayer+Extensions.swift
+++ Sources/StreamChatUI/Utils/CALayer+Extensions.swift
@@ -5,11 +5,14 @@
 import UIKit
 
 extension CALayer {
-    func addShadow(color: UIColor) {
+    func addShadow(color: UIColor, radius: CGFloat = 8) {
         masksToBounds = false
         shadowColor = color.cgColor
         shadowOffset = .zero
-        shadowRadius = 8
-        shadowOpacity = 0.85
+        shadowRadius = radius
+        
+        // The shadow opacity must be 1 for now. Changing this value will make Intel vs M1 snapshots to not be the same.
+        // So for now, we should try to fake transparency in the shadow color, for example, using a lighter color.
+        shadowOpacity = 1.0
     }
 }
