f0ee96d9d82d2b374dac76e6702cf2a663062c5e
Vojta Stavik <stavik@outlook.com>
2021-02-25 09:44:09 +0100

    Add CocoaPods integration test project

---
+++ Integration/CocoaPods/CocoaPods/SceneDelegate.swift
@@ -0,0 +1,11 @@
+//
+// Copyright © 2021 Stream.io Inc. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {}
+}
---
+++ Integration/CocoaPods/CocoaPods/ViewController.swift
@@ -0,0 +1,12 @@
+//
+// Copyright © 2021 Stream.io Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
