56826878aa38a268d0e5e49e14f10d39d08e68f3
Nuno Vieira <nuno.fcvieira93@gmail.com>
2021-03-02 16:33:09 +0000

    Extract UIConfig+CurrentUser to a seperate file

---
+++ Sources/StreamChatUI/UIConfig+CurrentUser.swift
@@ -0,0 +1,13 @@
+//
+//  Copyright © 2021 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+
+public extension _UIConfig {
+    struct CurrentUser {
+        public var currentUserViewAvatarView: _CurrentChatUserAvatarView<ExtraData>.Type = _CurrentChatUserAvatarView<ExtraData>
+            .self
+        public var avatarView: ChatAvatarView.Type = ChatAvatarView.self
+    }
+}
