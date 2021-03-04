e5e64cecad003b4b588708ec88c6af0d22117de3
Vojta Stavik <stavik@outlook.com>
2021-03-04 16:41:35 +0100

    Implement `ChannelTruncatedEventMiddleware`

---
+++ Sources/StreamChat/WebSocketClient/EventMiddlewares/ChannelTruncatedEventMiddleware.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+/// The middleware listens for `ChannelTruncatedEventMiddleware` events and updates `ChannelDTO` accordingly.
+struct ChannelTruncatedEventMiddleware<ExtraData: ExtraDataTypes>: EventMiddleware {
+    let database: DatabaseContainer
+
+    func handle(event: Event, completion: @escaping (Event?) -> Void) {
+        guard
+            let truncatedEvent = event as? ChannelTruncatedEvent,
+            let payload = truncatedEvent.payload as? EventPayload<ExtraData>
+        else {
+            completion(event)
+            return
+        }
+
+        database.write { session in
+            if let channelDTO = session.channel(cid: truncatedEvent.cid) {
+                channelDTO.truncatedAt = payload.createdAt
+            } else {
+                throw ClientError.ChannelDoesNotExist(cid: truncatedEvent.cid)
+            }
+        } completion: { error in
+            if let error = error {
+                log.error("Failed to update message reaction in the database, error: \(error)")
+            }
+            completion(event)
+        }
+    }
+}
