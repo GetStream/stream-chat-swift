d30e2b6fa1a0d9c6186027131e7c12ac8600e399
Nuno Vieira <nuno.fcvieira93@gmail.com>
2021-03-04 18:59:20 +0000

    Add common avatar views to UIConfig

--- Sources/StreamChatUI/UIConfig.swift
+++ Sources/StreamChatUI/UIConfig.swift
@@ -8,6 +8,15 @@ import UIKit
 public typealias UIConfig = _UIConfig<NoExtraData>
 
 public struct _UIConfig<ExtraData: ExtraDataTypes> {
+    /// A view used as an online activity indicator (online/offline).
+    public var onlineIndicatorView: UIView.Type = _ChatOnlineIndicatorView<ExtraData>.self
+
+    /// A view that displays the avatar image. By default a circular image.
+    public var avatarView: ChatAvatarView.Type = ChatAvatarView.self
+
+    /// An avatar view with an online indicator.
+    public var presenceAvatarView: _ChatPresenceAvatarView<ExtraData>.Type = _ChatPresenceAvatarView<ExtraData>.self
+
     public var channelList = ChannelList()
     public var messageList = MessageListUI()
     public var messageComposer = MessageComposer()
