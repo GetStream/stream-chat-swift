289a91dd77ca8d4c9ef25ec50ea9fefc15d14d53
Dominik Bucher <d.bucher@centrum.cz>
2021-03-05 17:43:32 +0100

    Fix Swift file formatter pre-commit hook (#866)

--- hooks/pre-commit.sh
+++ hooks/pre-commit.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-./hooks/git-format-staged --formatter 'mint run swiftformat --config .swiftformat stdin' 'Sources/*.swift | grep -v "Generated'
+
+./hooks/git-format-staged --formatter 'mint run swiftformat --config .swiftformat stdin' 'Sources/*.swift' '!*Generated*'
 ./hooks/git-format-staged --formatter 'mint run swiftformat --config .swiftformat stdin' 'Tests/*.swift'
 ./hooks/git-format-staged --formatter 'mint run swiftformat --config .swiftformat stdin' 'Sample/*.swift'
 ./hooks/git-format-staged --formatter 'mint run swiftformat --config .swiftformat stdin' 'DemoApp/*.swift'
