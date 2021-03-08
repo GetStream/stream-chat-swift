d71322ba74af1431f7b37c1ff58cef7b8f733bc4
Jakub Olejnik <jakub.olejnik@ackee.cz>
2021-03-08 12:32:05 +0100

    Remove initial layout hack in message list

--- Sources/StreamChatUI/ChatMessageList/ChatMessageListVC.swift
+++ Sources/StreamChatUI/ChatMessageList/ChatMessageListVC.swift
@@ -82,7 +82,6 @@ open class _ChatMessageListVC<ExtraData: ExtraDataTypes>: _ViewController,
         collection.keyboardDismissMode = .onDrag
         collection.dataSource = self
         collection.delegate = self
-        collection.isHidden = true
 
         return collection
     }()
@@ -92,24 +91,9 @@ open class _ChatMessageListVC<ExtraData: ExtraDataTypes>: _ViewController,
     /// Consider to call `setNeedsScrollToMostRecentMessage(animated:)` instead
     public private(set) var needsToScrollToMostRecentMessageAnimated = false
 
-    /// When controller loaded first time, message layout is in estimated state.
-    /// We force layout reload on first appear, before showing message list.
-    /// This way we able to hide ugly jump
-    public private(set) var hideInitialLayout = true
-
     open var minTimeInvteralBetweenMessagesInGroup: TimeInterval = 10
 
     // MARK: - Life Cycle
-
-    override open func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-
-        if hideInitialLayout {
-            collectionView.reloadData()
-            collectionView.isHidden = false
-            hideInitialLayout = false
-        }
-    }
 
     override open func setUp() {
         super.setUp()
