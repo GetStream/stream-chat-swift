8570af41a790e46e7dbbded3a249eb09ed768beb
Igor Rosocha <igor955@gmail.com>
2021-03-11 09:47:30 +0100

    Implement ChatChannelUnreadCountView+SwiftUI snapshot tests

---
+++ Sources/StreamChatUI/ChatChannelList/ChatChannelUnreadCountView+SwiftUI_Tests.swift
@@ -0,0 +1,39 @@
+//
+// Copyright © 2021 Stream.io Inc. All rights reserved.
+//
+
+@testable import StreamChat
+@testable import StreamChatUI
+import SwiftUI
+import XCTest
+
+@available(iOS 13.0, *)
+class ChatChannelUnreadCountView_SwiftUI_Tests: XCTestCase {
+    func test_injectedSwiftUIView() {
+        struct CustomUnreadCountView: ChatChannelUnreadCountView.SwiftUIView {
+            public typealias ExtraData = NoExtraData
+
+            @ObservedObject var dataSource: ChatChannelUnreadCountView.ObservedObject<Self>
+
+            public init(dataSource: ChatChannelUnreadCountView.ObservedObject<Self>) {
+                self.dataSource = dataSource
+            }
+
+            public var body: some View {
+                Text(String(dataSource.content.messages))
+                    .fontWeight(.bold)
+                    .padding()
+                    .background(
+                        Circle()
+                            .fill(Color.red)
+                    )
+            }
+        }
+
+        let view = ChatChannelUnreadCountView.SwiftUIWrapper<CustomUnreadCountView>()
+        view.content = .mock(messages: 20)
+        view.translatesAutoresizingMaskIntoConstraints = false
+
+        AssertSnapshot(view)
+    }
+}
