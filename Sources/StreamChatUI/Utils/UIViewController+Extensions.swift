6ae811957f5e2de9cff30d95d4ede4130508bb70
Igor Rosocha <igor955@gmail.com>
2021-03-14 14:47:14 +0100

    Make sure navigationItem properties of wrapped ChatChannelListVC are propagated correctly

--- Sources/StreamChatUI/Utils/UIViewController+Extensions.swift
+++ Sources/StreamChatUI/Utils/UIViewController+Extensions.swift
@@ -19,4 +19,82 @@ extension UIViewController {
         view.removeFromSuperview()
         removeFromParent()
     }
+
+    var isUIHostingController: Bool {
+        String(describing: self).contains("UIHostingController")
+    }
+
+    /// Helper method to correctly setup navigation of parent.
+    /// Used when view is wrapped using `UIHostingController` and used in SwiftUI
+    /// so all properties of `navigationItem` are propagated correctly.
+    @available(iOS 13.0, *)
+    func setupParentNavigation(parent: UIViewController) {
+        let parentNavItem = parent.navigationItem
+
+        if parentNavItem.backBarButtonItem == nil {
+            parentNavItem.backBarButtonItem = navigationItem.backBarButtonItem
+        }
+
+        if #available(iOS 14.0, *) {
+            if parent.navigationItem.backButtonDisplayMode == .default {
+                parent.navigationItem.backButtonDisplayMode = navigationItem.backButtonDisplayMode
+            }
+        }
+
+        if parentNavItem.backButtonTitle == nil {
+            parentNavItem.backButtonTitle = navigationItem.backButtonTitle
+        }
+
+        if parentNavItem.compactAppearance == nil {
+            parentNavItem.compactAppearance = navigationItem.compactAppearance
+        }
+
+        if parentNavItem.hidesSearchBarWhenScrolling {
+            parent.navigationItem.hidesSearchBarWhenScrolling = navigationItem.hidesSearchBarWhenScrolling
+        }
+
+        if parent.navigationItem.largeTitleDisplayMode == .automatic {
+            parentNavItem.largeTitleDisplayMode = navigationItem.largeTitleDisplayMode
+        }
+
+        if parent.navigationItem.hidesBackButton == false {
+            parent.navigationItem.hidesBackButton = navigationItem.hidesBackButton
+        }
+
+        if parentNavItem.leftBarButtonItems == nil || parentNavItem.leftBarButtonItems?.isEmpty == true {
+            parentNavItem.leftBarButtonItems = navigationItem.leftBarButtonItems
+        }
+
+        if parent.navigationItem.leftItemsSupplementBackButton == false {
+            parent.navigationItem.leftItemsSupplementBackButton = navigationItem.leftItemsSupplementBackButton
+        }
+
+        if parentNavItem.prompt == nil {
+            parentNavItem.prompt = navigationItem.prompt
+        }
+
+        if parentNavItem.rightBarButtonItems == nil || parentNavItem.rightBarButtonItems?.isEmpty == true {
+            parentNavItem.rightBarButtonItems = navigationItem.rightBarButtonItems
+        }
+
+        if parentNavItem.scrollEdgeAppearance == nil {
+            parentNavItem.scrollEdgeAppearance = navigationItem.scrollEdgeAppearance
+        }
+
+        if parentNavItem.searchController == nil {
+            parentNavItem.searchController = navigationItem.searchController
+        }
+
+        if parentNavItem.standardAppearance == nil {
+            parentNavItem.standardAppearance = navigationItem.standardAppearance
+        }
+
+        if parentNavItem.title == nil {
+            parentNavItem.title = title
+        }
+
+        if parentNavItem.titleView == nil {
+            parentNavItem.titleView = navigationItem.titleView
+        }
+    }
 }
