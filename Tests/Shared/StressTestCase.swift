d3dc161ccb57a053274d246c943324a1b556d0a6
Jakub Olejnik <jakub.olejnik@ackee.cz>
2021-03-15 21:40:50 +0100

    Add `StressTestCase.tearDown()` cleanup

--- Tests/Shared/StressTestCase.swift
+++ Tests/Shared/StressTestCase.swift
@@ -4,11 +4,39 @@
 
 import XCTest
 
+/// Base class for stress tests
+///
+/// - runs 100 times if `TestRunnerEnvironment.isStressTest`
+/// - removes all files in `NSTemporaryDirectory()` and `Documents` directory when test suite completes
+/// - by default ends test when test failure occurs
 class StressTestCase: XCTestCase {
     override func setUp() {
         super.setUp()
         
         continueAfterFailure = false
+    }
+    
+    override class func tearDown() {
+        // After running test suite cleanup `NSTemporaryDirectory()` and `Documents` directory
+        let fileManager = FileManager.default
+        let tempDirectoryURL = URL(fileURLWithPath: NSTemporaryDirectory(), isDirectory: true)
+        let documentsURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first
+        let tempFileURLs = (try? fileManager.contentsOfDirectory(
+            at: tempDirectoryURL,
+            includingPropertiesForKeys: nil,
+            options: .skipsHiddenFiles
+        )) ?? []
+        let documentsFileURLs = documentsURL.flatMap { try? FileManager.default.contentsOfDirectory(
+            at: $0,
+            includingPropertiesForKeys: nil,
+            options: .skipsHiddenFiles
+        ) } ?? []
+        
+        for fileURL in tempFileURLs + documentsFileURLs {
+            try? fileManager.removeItem(at: fileURL)
+        }
+        
+        super.tearDown()
     }
     
     override func invokeTest() {
