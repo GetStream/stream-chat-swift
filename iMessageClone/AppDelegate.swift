5a111b0e34f421ffbc7f70623850fac659936daa
Vojta Stavik <stavik@outlook.com>
2021-03-16 09:33:27 +0100

    Move `ChatClient` setup to single place

--- iMessageClone/AppDelegate.swift
+++ iMessageClone/AppDelegate.swift
@@ -14,17 +14,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
-        UIConfig.default.channelList.itemView = iMessageChatChannelListItemView.self
-        UIConfig.default.channelList.cellSeparatorReusableView = iMessageCellSeparatorView.self
-        
-        UIConfig.default.navigation.channelListRouter = iMessageChatChannelListRouter.self
-        UIConfig.default.images.newChat = UIImage(systemName: "square.and.pencil")!
-        UIConfig.default.messageComposer.messageComposerView = iMessageChatMessageComposerView.self
-        UIConfig.default.messageList.messageContentView = iMessageChatMessageContentView.self
-        UIConfig.default.messageList.outgoingMessageCell = iMessageСhatMessageCollectionViewCell.self
-        UIConfig.default.messageList.incomingMessageCell = iMessageСhatMessageCollectionViewCell.self
-        UIConfig.default.messageComposer.messageComposerViewController = iMessageChatComposerViewController.self
-        
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         window?.rootViewController = UINavigationController(
@@ -33,17 +22,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         return true
     }
-}
-
-extension ChatClient {
-    /// The singleton instance of `ChatClient`
-    static let shared: ChatClient = {
-        let config = ChatClientConfig(apiKey: APIKey("q95x9hkbyd6p"))
-        return ChatClient(
-            config: config,
-            tokenProvider: .static(
-                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiY2lsdmlhIn0.jHi2vjKoF02P9lOog0kDVhsIrGFjuWJqZelX5capR30"
-            )
-        )
-    }()
 }
