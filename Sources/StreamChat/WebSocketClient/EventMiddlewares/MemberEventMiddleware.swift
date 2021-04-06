bc93e0119b5a870198348b46bb3556ee2dcca014
Pavel Evsaev <pavel.evsaev@gmail.com>
2021-04-06 17:39:55 +0300

    Update `MemberEventMiddleware` type to work synchronously

--- Sources/StreamChat/WebSocketClient/EventMiddlewares/MemberEventMiddleware.swift
+++ Sources/StreamChat/WebSocketClient/EventMiddlewares/MemberEventMiddleware.swift
@@ -6,44 +6,38 @@ import Foundation
 
 /// The middleware listens for `MemberEvent`s and updates `ChannelDTO`s accordingly.
 struct MemberEventMiddleware<ExtraData: ExtraDataTypes>: EventMiddleware {
-    let database: DatabaseContainer
-    
-    func handle(event: Event, completion: @escaping (Event?) -> Void) {
-        guard let memberEvent = event as? MemberEvent else {
-            completion(event)
-            return
-        }
+    func handle(event: Event, session: DatabaseSession) -> Event? {
+        guard let memberEvent = event as? MemberEvent else { return event }
         
-        database.write { session in
+        do {
             switch memberEvent {
             case is MemberAddedEvent, is MemberUpdatedEvent:
                 guard let eventWithMemberPayload = event as? EventWithMemberPayload,
                       let eventPayload = eventWithMemberPayload.payload as? EventPayload<ExtraData>,
                       let memberPayload = eventPayload.memberContainer?.member
                 else {
-                    return
+                    break
                 }
                 try session.saveMember(payload: memberPayload, channelId: memberEvent.cid)
             case is MemberRemovedEvent:
                 guard let channel = session.channel(cid: memberEvent.cid) else {
                     // No need to throw ChannelNotFound error here
-                    return
+                    break
                 }
                 
                 guard let member = channel.members.first(where: { $0.user.id == memberEvent.userId }) else {
                     // No need to throw MemberNotFound error here
-                    return
+                    break
                 }
                 
                 channel.members.remove(member)
             default:
                 break
             }
-        } completion: { error in
-            if let error = error {
-                log.error("Failed to update channel members in the database, error: \(error)")
-            }
-            completion(event)
+        } catch {
+            log.error("Failed to update channel members in the database, error: \(error)")
         }
+
+        return event
     }
 }
