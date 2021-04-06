76ee784f5973d15c33b73d804df61b0d8755c979
Pavel Evsaev <pavel.evsaev@gmail.com>
2021-04-06 17:41:07 +0300

    Update `UserWatchingEventMiddleware` type to work synchronously

--- Sources/StreamChat/WebSocketClient/EventMiddlewares/UserWatchingEventMiddleware.swift
+++ Sources/StreamChat/WebSocketClient/EventMiddlewares/UserWatchingEventMiddleware.swift
@@ -6,15 +6,10 @@ import Foundation
 
 /// The middleware listens for `UserWatchingEvent`s and updates `ChannelDTO`s accordingly.
 struct UserWatchingEventMiddleware<ExtraData: ExtraDataTypes>: EventMiddleware {
-    let database: DatabaseContainer
-    
-    func handle(event: Event, completion: @escaping (Event?) -> Void) {
-        guard let userWatchingEvent = event as? UserWatchingEvent else {
-            completion(event)
-            return
-        }
+    func handle(event: Event, session: DatabaseSession) -> Event? {
+        guard let userWatchingEvent = event as? UserWatchingEvent else { return event }
         
-        database.write { session in
+        do {
             guard let channelDTO = session.channel(cid: userWatchingEvent.cid) else {
                 let currentUserId = session.currentUser()?.user.id
                 if userWatchingEvent.userId == currentUserId {
@@ -28,7 +23,7 @@ struct UserWatchingEventMiddleware<ExtraData: ExtraDataTypes>: EventMiddleware {
                             + "and user \(userWatchingEvent.userId) since Channel doesn't exist locally."
                     )
                 }
-                return
+                return event
             }
             
             channelDTO.watcherCount = Int64(userWatchingEvent.watcherCount)
@@ -42,11 +37,10 @@ struct UserWatchingEventMiddleware<ExtraData: ExtraDataTypes>: EventMiddleware {
             } else {
                 channelDTO.watchers.remove(userDTO)
             }
-        } completion: { error in
-            if let error = error {
-                log.error("Failed to update channel watchers in the database, error: \(error)")
-            }
-            completion(event)
+        } catch {
+            log.error("Failed to update channel watchers in the database, error: \(error)")
         }
+
+        return event
     }
 }
