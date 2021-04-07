b6bc2c9f896925e7d9db6d0af7764e2f834ec22a
Pavel Evsaev <pavel.evsaev@gmail.com>
2021-04-07 14:42:30 +0300

    Evaluate `ChatMessageLayoutOptions` by `MessageListVC`

--- Sources/StreamChatUI/ChatMessageList/RETHINK/MessageListVC.swift
+++ Sources/StreamChatUI/ChatMessageList/RETHINK/MessageListVC.swift
@@ -8,6 +8,8 @@ import UIKit
 
 class MessageListVC<ExtraData: ExtraDataTypes>: _ViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     var channelController: _ChatChannelController<ExtraData>!
+
+    var minTimeIntervalBetweenMessagesInGroup: TimeInterval = 10
     
     public private(set) lazy var collectionView: MessageCollectionView = {
         let collection = MessageCollectionView(frame: .zero, collectionViewLayout: ChatMessageListCollectionViewLayout())
@@ -32,10 +34,64 @@ class MessageListVC<ExtraData: ExtraDataTypes>: _ViewController, UICollectionVie
         
         channelController.synchronize()
     }
+
+    func isMessageLastInGroup(at indexPath: IndexPath) -> Bool {
+        let message = channelController.messages[indexPath.row]
+
+        guard indexPath.row > 0 else { return true }
+
+        let nextMessage = channelController.messages[indexPath.row - 1]
+
+        guard nextMessage.author == message.author else { return true }
+
+        let delay = nextMessage.createdAt.timeIntervalSince(message.createdAt)
+
+        return delay > minTimeIntervalBetweenMessagesInGroup
+    }
     
-    func cellLayoutOptions(for message: _ChatMessage<ExtraData>) -> ChatMessageLayoutOptions {
-        // TODO:
-        [.text]
+    func cellLayoutOptionsForMessage(at indexPath: IndexPath) -> ChatMessageLayoutOptions {
+        let message = channelController.messages[indexPath.row]
+        let isLastInGroup = isMessageLastInGroup(at: indexPath)
+
+        var options: ChatMessageLayoutOptions = []
+
+        if message.isSentByCurrentUser {
+            options.insert(.flipped)
+        }
+        if !isLastInGroup {
+            options.insert(.continuousBubble)
+        }
+        if isLastInGroup {
+            options.insert(.metadata)
+        }
+        if !message.textContent.isEmpty {
+            options.insert(.text)
+        }
+
+        guard message.deletedAt == nil else {
+            return options
+        }
+
+        if isLastInGroup && !message.isSentByCurrentUser {
+            options.insert(.avatar)
+        }
+
+        for attachment in message.attachments {
+            switch attachment.type {
+            case .image:
+                options.insert(.photoPreview)
+            case .giphy:
+                options.insert(.giphy)
+            case .file:
+                options.insert(.attachment)
+            case .link:
+                options.insert(.linkPreview)
+            default:
+                break
+            }
+        }
+
+        return options
     }
     
     func cellReuseIdentifier(for message: _ChatMessage<ExtraData>) -> String {
@@ -50,7 +106,7 @@ class MessageListVC<ExtraData: ExtraDataTypes>: _ViewController, UICollectionVie
         let message = channelController.messages[indexPath.item]
         
         let reuseId = cellReuseIdentifier(for: message)
-        let layoutOptions = cellLayoutOptions(for: message)
+        let layoutOptions = cellLayoutOptionsForMessage(at: indexPath)
         
         let cell = self.collectionView.dequeueReusableCell(
             withReuseIdentifier: reuseId,
