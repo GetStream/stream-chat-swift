141601f73c8a26a2ee1274c39b91de7699dedd2b
Lukáš Hromadník <lukas.hromadnik@ackee.cz>
2021-04-09 09:21:20 +0200

    Integrate other parts

--- Sources/StreamChatUI/ChatMessageList/ChatMessageListRouter.swift
+++ Sources/StreamChatUI/ChatMessageList/ChatMessageListRouter.swift
@@ -43,3 +43,38 @@ open class _ChatMessageListRouter<ExtraData: ExtraDataTypes>: ChatRouter<_ChatMe
         rootViewController.present(navigation, animated: true)
     }
 }
+
+class MessageListRouter<ExtraData: ExtraDataTypes>: ChatRouter<MessageListVC<ExtraData>> {
+    open func showMessageActionsPopUp(
+        messageContentFrame: CGRect,
+        messageData: _ChatMessageGroupPart<ExtraData>,
+        messageActionsController: _ChatMessageActionsVC<ExtraData>,
+        messageReactionsController: _ChatMessageReactionsVC<ExtraData>?
+    ) {
+        let popup = _ChatMessagePopupVC<ExtraData>()
+        popup.message = messageData
+        popup.messageViewFrame = messageContentFrame
+        popup.actionsController = messageActionsController
+        popup.reactionsController = messageReactionsController
+        popup.modalPresentationStyle = .overFullScreen
+        popup.modalTransitionStyle = .crossDissolve
+
+        rootViewController.present(popup, animated: false)
+    }
+    
+    open func showPreview(for attachment: ChatMessageDefaultAttachment) {
+        let preview = ChatMessageAttachmentPreviewVC<ExtraData>()
+        preview.content = attachment.type == .file ? attachment.url : attachment.imageURL
+        
+        let navigation = UINavigationController(rootViewController: preview)
+        rootViewController.present(navigation, animated: true)
+    }
+
+    open func openLink(_ link: ChatMessageDefaultAttachment) {
+        let preview = ChatMessageAttachmentPreviewVC<ExtraData>()
+        preview.content = link.url
+
+        let navigation = UINavigationController(rootViewController: preview)
+        rootViewController.present(navigation, animated: true)
+    }
+}
