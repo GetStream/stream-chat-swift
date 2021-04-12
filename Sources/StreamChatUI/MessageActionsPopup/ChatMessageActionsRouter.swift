3e142bcf4ad24d63bf0e6f04c8e79a770bdc000a
marek.fort <marek.fort@ackee.cz>
2021-04-12 17:55:54 +0200

    Hide ChatMessageActionItem behind protocol

--- Sources/StreamChatUI/MessageActionsPopup/ChatMessageActionsRouter.swift
+++ Sources/StreamChatUI/MessageActionsPopup/ChatMessageActionsRouter.swift
@@ -7,7 +7,11 @@ import UIKit
 
 public typealias ChatMessageActionsRouter = _ChatMessageActionsRouter<NoExtraData>
 
+/// `ChatRouter` instance for routing of chat message actions
 open class _ChatMessageActionsRouter<ExtraData: ExtraDataTypes>: ChatRouter<_ChatMessageActionsVC<ExtraData>> {
+    /// Method for showing alert with confirmation of whether a message should be deleted
+    /// - Parameters:
+    ///     - confirmed: Completion closure with a `Bool` parameter indicating whether the deletion has been confirmed or not
     open func showMessageDeletionConfirmationAlert(confirmed: @escaping (Bool) -> Void) {
         let alert = UIAlertController(
             title: L10n.Message.Actions.Delete.confirmationTitle,
@@ -15,16 +19,20 @@ open class _ChatMessageActionsRouter<ExtraData: ExtraDataTypes>: ChatRouter<_Cha
             preferredStyle: .alert
         )
 
-        alert.addAction(UIAlertAction(
-            title: L10n.Alert.Actions.cancel,
-            style: .cancel,
-            handler: { _ in confirmed(false) }
-        ))
-        alert.addAction(UIAlertAction(
-            title: L10n.Alert.Actions.delete,
-            style: .destructive,
-            handler: { _ in confirmed(true) }
-        ))
+        alert.addAction(
+            UIAlertAction(
+                title: L10n.Alert.Actions.cancel,
+                style: .cancel,
+                handler: { _ in confirmed(false) }
+            )
+        )
+        alert.addAction(
+            UIAlertAction(
+                title: L10n.Alert.Actions.delete,
+                style: .destructive,
+                handler: { _ in confirmed(true) }
+            )
+        )
 
         rootViewController.present(alert, animated: true)
     }
