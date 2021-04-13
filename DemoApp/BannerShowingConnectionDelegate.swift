824727118a44c9db6bcf7a117181fc653df60bdc
Dmitry Gachkovsky <sacethi0@gmail.com>
2021-04-13 12:15:15 +0300

    Add logic to react on connectivity events with banner

---
+++ DemoApp/BannerShowingConnectionDelegate.swift
@@ -0,0 +1,78 @@
+//
+// Copyright © 2021 Stream.io Inc. All rights reserved.
+//
+
+import StreamChat
+import UIKit
+
+final class BannerShowingConnectionDelegate {
+    // MARK: - Private Properties
+    
+    private let view: UIView
+    private let bannerView = BannerView()
+    private let bannerAppearanceDuration: TimeInterval = 0.5
+    
+    // MARK: -
+    
+    init(showUnder view: UIView) {
+        self.view = view
+        setupViews()
+    }
+}
+
+// MARK: - ChatConnectionControllerDelegate
+
+extension BannerShowingConnectionDelegate: ChatConnectionControllerDelegate {
+    public func connectionController(_ controller: ChatConnectionController, didUpdateConnectionStatus status: ConnectionStatus) {
+        switch status {
+        case .disconnected:
+            showBanner()
+        case .connected:
+            hideBanner()
+        case .initialized,
+             .disconnecting,
+             .connecting:
+            break
+        }
+    }
+}
+
+// MARK: - Private Methods
+
+private extension BannerShowingConnectionDelegate {
+    func setupViews() {
+        attachToTopViewIfNeeded()
+        bannerView.alpha = 0
+        bannerView.update(text: "Connecting...")
+    }
+    
+    func attachToTopViewIfNeeded() {
+        guard bannerView.superview != view else { return }
+        
+        view.addSubview(bannerView)
+        
+        bannerView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate(
+            [
+                bannerView.topAnchor.constraint(equalTo: view.bottomAnchor),
+                bannerView.widthAnchor.constraint(equalTo: view.widthAnchor),
+                bannerView.heightAnchor.constraint(equalToConstant: 28)
+            ]
+        )
+    }
+    
+    func showBanner() {
+        attachToTopViewIfNeeded()
+        animateBannerAlpha(to: 1)
+    }
+    
+    func hideBanner() {
+        animateBannerAlpha(to: 0)
+    }
+    
+    func animateBannerAlpha(to value: CGFloat) {
+        UIView.animate(withDuration: bannerAppearanceDuration) {
+            self.bannerView.alpha = value
+        }
+    }
+}
---
+++ DemoApp/DemoAppCoordinator.swift
@@ -0,0 +1,60 @@
+//
+// Copyright © 2021 Stream.io Inc. All rights reserved.
+//
+
+import StreamChat
+import StreamChatUI
+import UIKit
+
+final class DemoAppCoordinator {
+    private var connectionController: ChatConnectionController?
+    private let navigationController: UINavigationController
+    private let connectionDelegate: BannerShowingConnectionDelegate
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+        connectionDelegate = BannerShowingConnectionDelegate(
+            showUnder: navigationController.navigationBar
+        )
+        injectActions()
+    }
+    
+    func presentChat(userCredentials: UserCredentials) {
+        LogConfig.level = .error
+        
+        // Create a token
+        let token = try! Token(rawValue: userCredentials.token)
+        
+        // Create client
+        let config = ChatClientConfig(apiKey: .init(userCredentials.apiKey))
+        let client = ChatClient(config: config, tokenProvider: .static(token))
+        
+        // Config
+        UIConfig.default.navigation.channelListRouter = DemoChatChannelListRouter.self
+        
+        // Channels with the current user
+        let controller = client.channelListController(query: .init(filter: .containMembers(userIds: [userCredentials.id])))
+        let chatList = ChatChannelListVC()
+        chatList.controller = controller
+        
+        connectionController = client.connectionController()
+        connectionController?.delegate = connectionDelegate
+        
+        navigationController.viewControllers = [chatList]
+        navigationController.isNavigationBarHidden = false
+        
+        let window = navigationController.view.window!
+        
+        UIView.transition(with: window, duration: 0.3, options: .transitionFlipFromRight, animations: {
+            window.rootViewController = self.navigationController
+        })
+    }
+    
+    private func injectActions() {
+        if let loginViewController = navigationController.topViewController as? LoginViewController {
+            loginViewController.didRequestChatPresentation = { [weak self] in
+                self?.presentChat(userCredentials: $0)
+            }
+        }
+    }
+}
