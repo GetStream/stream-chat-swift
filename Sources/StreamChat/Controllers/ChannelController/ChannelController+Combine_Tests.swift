7b9e1247d9a0a9e94b813b194b2a7df3e6bb4700
Vojta Stavik <stavik@outlook.com>
2021-04-13 11:13:28 +0200

    Use test tools mocks for `ChatChannel`

--- Sources/StreamChat/Controllers/ChannelController/ChannelController+Combine_Tests.swift
+++ Sources/StreamChat/Controllers/ChannelController/ChannelController+Combine_Tests.swift
@@ -58,7 +58,7 @@ class ChannelController_Combine_Tests: iOS13TestCase {
         weak var controller: ChannelControllerMock? = channelController
         channelController = nil
 
-        let newChannel: ChatChannel = .init(cid: .unique, name: .unique, imageURL: .unique(), extraData: .defaultValue)
+        let newChannel: ChatChannel = .mock(cid: .unique, name: .unique, imageURL: .unique(), extraData: .defaultValue)
         controller?.channel_simulated = newChannel
         controller?.delegateCallback {
             $0.channelController(controller!, didUpdateChannel: .create(newChannel))
