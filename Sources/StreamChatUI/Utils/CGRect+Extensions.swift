e4a7c1e43dfa3704bd6cd3dadf77d85b99fa9d5d
Pavel Evsaev <pavel.evsaev@gmail.com>
2021-04-15 11:25:06 +0300

    Introduce `CGRect.center` helpers

---
+++ Sources/StreamChatUI/Utils/CGRect+Extensions.swift
@@ -0,0 +1,33 @@
+//
+// Copyright © 2021 Stream.io Inc. All rights reserved.
+//
+
+import CoreGraphics
+
+extension CGRect {
+    init(center: CGPoint, size: CGSize) {
+        let origin = CGPoint(
+            x: center.x - size.width / 2,
+            y: center.y - size.height / 2
+        )
+
+        self.init(origin: origin, size: size)
+    }
+
+    var center: CGPoint {
+        .init(
+            x: origin.x + size.width / 2,
+            y: origin.y + size.height / 2
+        )
+    }
+
+    static func circleBounds(center: CGPoint, radius: CGFloat) -> Self {
+        .init(
+            center: center,
+            size: .init(
+                width: radius * 2,
+                height: radius * 2
+            )
+        )
+    }
+}
