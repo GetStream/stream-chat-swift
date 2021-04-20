779ce3c396996affb71a86d81796ef3343a213c0
Nuno Vieira <nuno.fcvieira93@gmail.com>
2021-04-20 12:13:04 +0100

    Change iMessageClone customization with new ContainerStackView

--- iMessageClone/iMessageChatMessageComposerView.swift
+++ iMessageClone/iMessageChatMessageComposerView.swift
@@ -10,31 +10,58 @@ final class iMessageChatMessageComposerView: ChatMessageComposerView {
     override func setUpLayout() {
         super.setUpLayout()
 
-        commandsButton.removeFromSuperview()
-        
+        centerLeftContainer.removeArrangedSubview(commandsButton)
+        centerRightContainer.removeArrangedSubview(sendButton)
+        centerContainer.removeArrangedSubview(centerRightContainer)
+
+        centerContentContainer.axis = .horizontal
+        centerContentContainer.distribution = .natural
+        centerContentContainer.alignment = .fill
+        centerContentContainer.subviews.forEach {
+            centerContentContainer.removeArrangedSubview($0)
+        }
+
+        let newStack = ContainerStackView()
+        newStack.translatesAutoresizingMaskIntoConstraints = false
+        newStack.axis = .vertical
+        newStack.alignment = .fill
+        newStack.distribution = .natural
+        newStack.spacing = 0
+        newStack.addArrangedSubview(messageQuoteView)
+        newStack.addArrangedSubview(imageAttachmentsView)
+        newStack.addArrangedSubview(documentAttachmentsView)
+        newStack.addArrangedSubview(messageInputView)
         messageInputView.container.preservesSuperviewLayoutMargins = false
-        
-        container.rightStackView.isHidden = true
-        
-        messageInputView.textView.setContentHuggingPriority(.required, for: .vertical)
-        
-        sendButton.removeFromSuperview()
-        container.centerStackView.addSubview(sendButton)
+
+        let sendButtonContainer = UIView()
+        sendButtonContainer.addSubview(sendButton)
         NSLayoutConstraint.activate([
-            sendButton.trailingAnchor.constraint(equalTo: container.centerStackView.trailingAnchor, constant: -3.5),
-            sendButton.widthAnchor.constraint(equalToConstant: 27),
-            sendButton.bottomAnchor.constraint(equalTo: container.centerStackView.bottomAnchor, constant: -1.5)
+            sendButtonContainer.widthAnchor.constraint(equalToConstant: 30),
+            sendButton.leadingAnchor.constraint(equalTo: sendButtonContainer.leadingAnchor, constant: 0),
+            sendButton.trailingAnchor.constraint(equalTo: sendButtonContainer.trailingAnchor, constant: 0),
+            sendButton.bottomAnchor.constraint(equalTo: sendButtonContainer.bottomAnchor, constant: 0)
+        ])
+
+        centerContentContainer.addArrangedSubview(newStack)
+        centerContentContainer.addArrangedSubview(sendButtonContainer)
+
+        NSLayoutConstraint.activate([
+            sendButton.widthAnchor.constraint(equalToConstant: 30),
+            sendButton.heightAnchor.constraint(equalToConstant: 35)
         ])
     }
     
     override func setUpAppearance() {
         super.setUpAppearance()
-        container.centerStackView.layer.cornerRadius = 20
+
+        centerContentContainer.layer.cornerRadius = 18
         messageInputView.textView.font = .systemFont(ofSize: 15)
         
         attachmentButton.setImage(
             UIImage(systemName: "camera.fill"),
             for: .normal
         )
+        attachmentButton.widthAnchor.constraint(equalToConstant: 30).isActive = true
+        attachmentButton.heightAnchor.constraint(equalToConstant: 30).isActive = true
     }
 }
