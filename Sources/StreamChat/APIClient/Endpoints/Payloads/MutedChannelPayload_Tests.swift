4eab2e7d7299cfa5932457c21bd4308dff38d276
Pavel Evsaev <pavel.evsaev@gmail.com>
2021-04-22 20:07:03 +0300

    Introduce `MutedChannelPayload` type

---
+++ Sources/StreamChat/APIClient/Endpoints/Payloads/MutedChannelPayload_Tests.swift
@@ -0,0 +1,17 @@
+//
+// Copyright © 2021 Stream.io Inc. All rights reserved.
+//
+
+@testable import StreamChat
+import XCTest
+
+final class MutedChannelPayload_Tests: XCTestCase {
+    func test_payload_isDeserialized() throws {
+        let json = XCTestCase.mockData(fromFile: "MutedChannelPayload")
+        let payload = try JSONDecoder.default.decode(MutedChannelPayload<NoExtraData>.self, from: json)
+        XCTAssertEqual(payload.user.id, "luke_skywalker")
+        XCTAssertEqual(payload.mutedChannel.cid.rawValue, "messaging:B1DFF9C5-E6A6-4BFA-9375-DC5E8C6852FF")
+        XCTAssertEqual(payload.createdAt, "2021-03-22T10:23:52.516225Z".toDate())
+        XCTAssertEqual(payload.updatedAt, "2021-04-22T10:23:52.516225Z".toDate())
+    }
+}
