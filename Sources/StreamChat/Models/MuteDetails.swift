262b944cae8862abbd0f5f41dd5ed5b554c7b448
Pavel Evsaev <pavel.evsaev@gmail.com>
2021-04-22 18:38:46 +0300

    Introduce `MuteDetails` type

---
+++ Sources/StreamChat/Models/MuteDetails.swift
@@ -0,0 +1,13 @@
+//
+// Copyright © 2021 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+
+/// Describes user/channel mute details.
+public struct MuteDetails: Equatable {
+    /// The time when the mute action was taken.
+    public let createdAt: Date
+    /// The time when the mute was updated.
+    public let updatedAt: Date
+}
