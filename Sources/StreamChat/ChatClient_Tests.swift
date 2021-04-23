8d3ac775d30ab69c9a681df9d2df96490e68086c
Vojta Stavik <stavik@outlook.com>
2021-04-23 11:44:10 +0200

    Use `ChannelVisibilityEventMiddleware` in `ChatClient`

--- Sources/StreamChat/ChatClient_Tests.swift
+++ Sources/StreamChat/ChatClient_Tests.swift
@@ -240,6 +240,8 @@ class ChatClient_Tests: StressTestCase {
         XCTAssert(middlewares.contains(where: { $0 is UserChannelBanEventsMiddleware<NoExtraData> }))
         // Assert `UserWatchingEventMiddleware` exists
         XCTAssert(middlewares.contains(where: { $0 is UserWatchingEventMiddleware<NoExtraData> }))
+        // Assert `ChannelVisibilityEventMiddleware` exists
+        XCTAssert(middlewares.contains(where: { $0 is ChannelVisibilityEventMiddleware<NoExtraData> }))
     }
     
     func test_connectionStatus_isExposed() {
