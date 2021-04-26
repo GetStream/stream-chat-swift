fa7b35b01902cd4ee008b4dbf52da52c50558263
Pavel Evsaev <pavel.evsaev@gmail.com>
2021-04-26 22:46:31 +0300

    Introduce `CACornerMask.all` helper

---
+++ Sources/StreamChatUI/Utils/CACornerMask+Extensions.swift
@@ -0,0 +1,14 @@
+//
+// Copyright © 2021 Stream.io Inc. All rights reserved.
+//
+
+import UIKit
+
+extension CACornerMask {
+    static let all: Self = [
+        .layerMinXMinYCorner,
+        .layerMinXMaxYCorner,
+        .layerMaxXMinYCorner,
+        .layerMaxXMaxYCorner
+    ]
+}
