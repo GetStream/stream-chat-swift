d63e92610c609d5d50444c2eabcd39d8873d8f32
Pavel Evsaev <pavel.evsaev@gmail.com>
2021-04-26 22:49:25 +0300

    Introduce `ThreadArrowView` type

---
+++ Sources/StreamChatUI/ChatMessageList/RETHINK/ThreadArrowView.swift
@@ -0,0 +1,63 @@
+//
+// Copyright © 2021 Stream.io Inc. All rights reserved.
+//
+
+import StreamChat
+import UIKit
+
+public typealias ThreadArrowView = _ThreadArrowView<NoExtraData>
+
+open class _ThreadArrowView<ExtraData: ExtraDataTypes>: _View, UIConfigProvider {
+    public enum Direction {
+        case toTrailing
+        case toLeading
+    }
+
+    override public class var layerClass: AnyClass {
+        CAShapeLayer.self
+    }
+
+    public var shape: CAShapeLayer {
+        layer as! CAShapeLayer
+    }
+
+    private var isLeftToRight: Bool {
+        let isLeftToRightWithTrailing = direction == .toTrailing && traitCollection.layoutDirection == .leftToRight
+        let isRightToLeftWithLeading = direction == .toLeading && traitCollection.layoutDirection == .rightToLeft
+        return isLeftToRightWithTrailing || isRightToLeftWithLeading
+    }
+
+    public var direction: Direction = .toTrailing {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+
+    override open func setUpAppearance() {
+        super.setUpAppearance()
+
+        shape.contentsScale = layer.contentsScale
+        shape.strokeColor = uiConfig.colorPalette.border.cgColor
+        shape.fillColor = nil
+        shape.lineWidth = 1
+    }
+
+    override open func draw(_ rect: CGRect) {
+        let corner: CGFloat = 16
+        let height = bounds.height / 2
+        let lineCenter = shape.lineWidth / 2
+
+        let startX = isLeftToRight ? lineCenter : (bounds.width - lineCenter)
+        let endX = isLeftToRight ? corner : (bounds.width - corner)
+
+        let path = CGMutablePath()
+        path.move(to: CGPoint(x: startX, y: -4 * height))
+        path.addLine(to: CGPoint(x: startX, y: height - corner))
+        path.addQuadCurve(
+            to: CGPoint(x: endX, y: height),
+            control: CGPoint(x: startX, y: height)
+        )
+        shape.path = path
+        super.draw(rect)
+    }
+}
