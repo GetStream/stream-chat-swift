528f2a6910dd57270a5079c06bd9e6267c660475
Jakub Skořepa <skorepak@gmail.com>
2021-04-27 12:25:34 +0200

    Split UIConfig into Components and non-generic Appearance

---
+++ Sources/StreamChatUI/Appearance+SwiftUI.swift
@@ -0,0 +1,49 @@
+//
+// Copyright © 2021 Stream.io Inc. All rights reserved.
+//
+
+import StreamChat
+import SwiftUI
+
+@available(iOS 13.0, *)
+extension Appearance {
+    /// Used to initialize `_Components` as `ObservableObject`.
+    public var asObservableObject: ObservableObject { .init(self) }
+
+    @dynamicMemberLookup
+    /// `_Components` represented as `ObservableObject` class for SwiftUI requirements.
+    public class ObservableObject: SwiftUI.ObservableObject {
+        private let wrappedAppearance: Appearance
+
+        public subscript<T>(dynamicMember keyPath: KeyPath<Appearance, T>) -> T {
+            wrappedAppearance[keyPath: keyPath]
+        }
+
+        fileprivate init(_ wrappedAppearance: Appearance) {
+            self.wrappedAppearance = wrappedAppearance
+        }
+    }
+}
+
+@available(iOS 13.0, *)
+/// Modifier for setting `Components` environment object.
+private struct SwiftUIAppearance: ViewModifier {
+    /// Custom `ObservableObject` of `components`
+    private let appearance: Appearance
+
+    public init(_ appearance: Appearance) {
+        self.appearance = appearance
+    }
+
+    public func body(content: Content) -> some View {
+        content.environmentObject(appearance.asObservableObject)
+    }
+}
+
+@available(iOS 13.0, *)
+public extension View {
+    /// Sets up custom `Components`.
+    func setUpStreamChatAppearance(_ appearance: Appearance = .default) -> some View {
+        modifier(SwiftUIAppearance(appearance))
+    }
+}
--- Sources/StreamChatUI/Components.swift
+++ Sources/StreamChatUI/Components.swift
@@ -5,11 +5,11 @@
 import StreamChat
 import UIKit
 
-public typealias UIConfig = _UIConfig<NoExtraData>
+public typealias Components = _Components<NoExtraData>
 
-public struct _UIConfig<ExtraData: ExtraDataTypes> {
+public struct _Components<ExtraData: ExtraDataTypes> {
     /// A view used as an online activity indicator (online/offline).
-    public var onlineIndicatorView: (UIView & MaskProviding).Type = _ChatOnlineIndicatorView<ExtraData>.self
+    public var onlineIndicatorView: (UIView & MaskProviding).Type = ChatOnlineIndicatorView.self
 
     /// A view that displays the avatar image. By default a circular image.
     public var avatarView: ChatAvatarView.Type = ChatAvatarView.self
@@ -24,25 +24,22 @@ public struct _UIConfig<ExtraData: ExtraDataTypes> {
     public var typingIndicatorView: _TypingIndicatorView<ExtraData>.Type = _TypingIndicatorView<ExtraData>.self
     
     /// A `UIView` subclass with animated 3 dots for indicating that user is typing.
-    public var typingAnimationView: _TypingAnimationView<ExtraData>.Type = _TypingAnimationView<ExtraData>.self
+    public var typingAnimationView: TypingAnimationView.Type = TypingAnimationView.self
 
     public var channelList = ChannelList()
     public var messageList = MessageListUI()
     public var messageComposer = MessageComposer()
     public var currentUser = CurrentUser()
     public var navigation = Navigation()
-    public var colorPalette = ColorPalette()
-    public var font = Font()
-    public var images = Images()
 
     public init() {}
 }
 
-// MARK: - UIConfig + Default
+// MARK: - Components + Default
 
 private var defaults: [String: Any] = [:]
 
-public extension _UIConfig {
+public extension _Components {
     static var `default`: Self {
         get {
             let key = String(describing: ExtraData.self)
