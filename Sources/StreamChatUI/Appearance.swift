8bea6ecc7b3fd3b5d26b708e04fc1b705dc1eb03
Jakub Skořepa <skorepak@gmail.com>
2021-04-27 16:08:35 +0200

    Add documentation for the new Protocols

--- Sources/StreamChatUI/Appearance.swift
+++ Sources/StreamChatUI/Appearance.swift
@@ -3,11 +3,21 @@
 //
 
 import StreamChat
-import UIKit
 
 public struct Appearance {
+    /// A color pallete to provide basic set of colors for the Views.
+    ///
+    /// By providing different object or changing individal colors, you can change the look of the views.
     public var colorPalette = ColorPalette()
+    
+    /// A set of fonts to be used in the Views.
+    ///
+    /// By providing different object or changing individal fonts, you can change the look of the views.
     public var fonts = Fonts()
+    
+    /// A set of images to be used.
+    ///
+    /// By providing different object or changing individal images, you can change the look of the views.
     public var images = Images()
     
     public init() {}
