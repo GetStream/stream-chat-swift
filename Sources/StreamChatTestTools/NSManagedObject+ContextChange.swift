f3ff1c03252d4a04dbe1ec3897173a8b19dbb734
Jakub Olejnik <jakub.olejnik@ackee.cz>
2021-04-28 12:51:42 +0200

    Add test helper that changes context of `NSManagedObject`

---
+++ Sources/StreamChatTestTools/NSManagedObject+ContextChange.swift
@@ -0,0 +1,15 @@
+//
+// Copyright © 2021 Stream.io Inc. All rights reserved.
+//
+
+import CoreData
+
+public extension NSManagedObject {
+    /// Shortcut for transfering objects between contexts
+    ///
+    /// Returns implicitly unwrapped optional as it is possible for this operation to fail,
+    /// this extension is **intended to be used in tests** so it should not be an issue
+    func inContext(_ context: NSManagedObjectContext) -> Self! {
+        context.object(with: objectID) as? Self
+    }
+}
