20e9d206076771f4c6bd257e6149f6983def5803
Pavel Evsaev <pavel.evsaev@gmail.com>
2021-04-28 18:12:34 +0300

    Update `SlackClone` with new message content view

---
+++ SlackClone/SlackMessageOptionsResolver.swift
@@ -0,0 +1,36 @@
+//
+// Copyright © 2021 Stream.io Inc. All rights reserved.
+//
+
+import StreamChat
+import StreamChatUI
+import UIKit
+
+final class SlackMessageOptionsResolver: ChatMessageLayoutOptionsResolver {
+    override func optionsForMessage(
+        at indexPath: IndexPath,
+        in channel: ChatChannel,
+        with messages: AnyRandomAccessCollection<ChatMessage>
+    ) -> ChatMessageLayoutOptions {
+        var options = super.optionsForMessage(at: indexPath, in: channel, with: messages)
+        options.remove([.flipped, .bubble, .metadata, .avatar, .avatarSizePadding, .authorName, .threadInfo, .reactions])
+
+        let isFirstInGroup: Bool = {
+            let messageIndex = messages.index(messages.startIndex, offsetBy: indexPath.item)
+            let message = messages[messageIndex]
+            guard messageIndex < messages.index(before: messages.endIndex) else { return true }
+            let previousMessage = messages[messages.index(after: messageIndex)]
+            guard previousMessage.author == message.author else { return true }
+            let delay = previousMessage.createdAt.timeIntervalSince(message.createdAt)
+            return delay > minTimeIntervalBetweenMessagesInGroup
+        }()
+
+        if isFirstInGroup {
+            options.insert([.avatar, .metadata, .authorName])
+        } else {
+            options.insert(.avatarSizePadding)
+        }
+
+        return options
+    }
+}
