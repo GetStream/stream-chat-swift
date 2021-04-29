f5a9e22f4abc894951b46956c6312c912d1dda81
Nuno Vieira <nuno.fcvieira93@gmail.com>
2021-04-29 20:18:58 +0100

    Rename and Move SendButton Component

--- Sources/StreamChatUI/CommonViews/SendButton/ChatSendButton.swift
+++ Sources/StreamChatUI/CommonViews/SendButton/ChatSendButton.swift
@@ -5,21 +5,25 @@
 import StreamChat
 import UIKit
 
-/// Button used for final sending in ComposerView.
-open class ChatMessageSendButton: _Button, AppearanceProvider {
+/// Button used for sending a message, or any type of content.
+open class ChatSendButton: _Button, AppearanceProvider {
     /// Override this variable to enable custom behaviour upon button enabled.
     override open var isEnabled: Bool {
         didSet {
             Animate {
-                self.transform = self.isEnabled ? CGAffineTransform(rotationAngle: -CGFloat.pi / 2.0) : .identity
+                self.transform = self.isEnabled
+                    ? CGAffineTransform(rotationAngle: -CGFloat.pi / 2.0)
+                    : .identity
             }
         }
     }
-    
-    override open func setUpLayout() {
+
+    override open func setUpAppearance() {
+        super.setUpAppearance()
+
         let normalStateImage = appearance.images.messageComposerSendMessage
         setImage(normalStateImage, for: .normal)
-        
+
         let buttonColor: UIColor = appearance.colorPalette.inactiveTint
         let disabledStateImage = appearance.images.messageComposerSendMessage.tinted(with: buttonColor)
         setImage(disabledStateImage, for: .disabled)
