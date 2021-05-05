0c1f49e0d56e25b9d3d8f0c856f4ed15e3c75f13
Nuno Vieira <nuno.fcvieira93@gmail.com>
2021-05-05 12:10:51 +0100

    Change iMessageClone with new Architecture

--- iMessageClone/iMessageChatMessageComposerView.swift
+++ iMessageClone/iMessageChatMessageComposerView.swift
@@ -10,49 +10,16 @@ final class iMessageChatMessageComposerView: ChatMessageComposerView {
     override func setUpLayout() {
         super.setUpLayout()
 
-        centerLeftContainer.removeArrangedSubview(commandsButton)
+        leadingContainer.removeArrangedSubview(commandsButton)
         trailingContainer.removeArrangedSubview(sendButton)
         centerContainer.removeArrangedSubview(trailingContainer)
-
-        centerInputContainer.axis = .horizontal
-        centerInputContainer.distribution = .natural
-        centerInputContainer.alignment = .fill
-        centerInputContainer.removeAllArrangedSubviews()
-
-        let newStack = ContainerStackView()
-        newStack.translatesAutoresizingMaskIntoConstraints = false
-        newStack.axis = .vertical
-        newStack.alignment = .fill
-        newStack.distribution = .natural
-        newStack.spacing = 0
-        newStack.addArrangedSubview(messageQuoteView)
-        newStack.addArrangedSubview(imageAttachmentsView)
-        newStack.addArrangedSubview(documentAttachmentsView)
-        newStack.addArrangedSubview(messageInputView)
-        messageInputView.container.preservesSuperviewLayoutMargins = false
-
-        let sendButtonContainer = UIView()
-        sendButtonContainer.addSubview(sendButton)
-        NSLayoutConstraint.activate([
-            sendButtonContainer.widthAnchor.constraint(equalToConstant: 30),
-            sendButton.leadingAnchor.constraint(equalTo: sendButtonContainer.leadingAnchor, constant: 0),
-            sendButton.trailingAnchor.constraint(equalTo: sendButtonContainer.trailingAnchor, constant: 0),
-            sendButton.bottomAnchor.constraint(equalTo: sendButtonContainer.bottomAnchor, constant: 0)
-        ])
-
-        centerInputContainer.addArrangedSubview(newStack)
-        centerInputContainer.addArrangedSubview(sendButtonContainer)
-
-        NSLayoutConstraint.activate([
-            sendButton.widthAnchor.constraint(equalToConstant: 30),
-            sendButton.heightAnchor.constraint(equalToConstant: 35)
-        ])
+        messageInputView.inputTextContainer.addArrangedSubview(sendButton)
     }
     
     override func setUpAppearance() {
         super.setUpAppearance()
 
-        centerInputContainer.layer.cornerRadius = 18
+        messageInputView.layer.cornerRadius = 16
         messageInputView.inputTextView.font = .systemFont(ofSize: 15)
         
         attachmentButton.setImage(
