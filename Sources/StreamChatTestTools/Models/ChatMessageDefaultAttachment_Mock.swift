a1576f50ed55577ab4f66f9e39792ddad51fdf60
Dominik Bucher <d.bucher@centrum.cz>
2021-05-14 11:38:19 +0200

    Prepare ChatMessageFileAttachmentMock

--- Sources/StreamChatTestTools/Models/ChatMessageDefaultAttachment_Mock.swift
+++ Sources/StreamChatTestTools/Models/ChatMessageDefaultAttachment_Mock.swift
@@ -21,3 +21,33 @@ extension ChatMessageImageAttachment {
         )
     }
 }
+
+extension ChatMessageFileAttachment {
+    public static func mock(
+        id: String = "",
+        title: String = "Sample.pdf",
+        assertURL: URL = URL(string: "http://asset.url")!,
+        file: AttachmentFile = AttachmentFile(type: .pdf, size: 120, mimeType: "pdf"),
+        uploadingState: AttachmentUploadingState = .mock(
+            localFileURL: URL(string: "http://asset.url")!,
+            state: .uploaded
+        )
+    ) -> Self {
+        .init(
+            id: .unique,
+            type: .file,
+            payload: .init(
+                title: title,
+                assetURL: assertURL,
+                file: file
+            ),
+            uploadingState: uploadingState
+        )
+    }
+}
+
+extension AttachmentUploadingState {
+    public static func mock(localFileURL: URL, state: LocalAttachmentState) -> Self {
+        .init(localFileURL: localFileURL, state: state)
+    }
+}
