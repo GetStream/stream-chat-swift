c9f5d974d5ff1ad09f2d167f7e6ec8fdf68f419a
Vojta Stavik <stavik@outlook.com>
2021-05-18 10:09:52 +0200

    Introduce `AttachmentPreviewProvider`
    Co-authored-by: Jakub Skorepa <skorepak@gmail.com>

---
+++ Sources/StreamChatUI/CommonViews/Attachments/AttachmentPreviewProvider.swift
@@ -0,0 +1,40 @@
+//
+// Copyright © 2021 Stream.io Inc. All rights reserved.
+//
+
+import StreamChat
+import UIKit
+
+public protocol AttachmentPreviewProvider {
+    /// The view representing the attachment.
+    func previewView<ExtraData: ExtraDataTypes>(components: _Components<ExtraData>) -> UIView
+    
+    /// The preffered axis to be used for attachment previews in attachments view.
+    static var prefferedAxis: NSLayoutConstraint.Axis { get }
+}
+
+extension ImageAttachmentPayload: AttachmentPreviewProvider {
+    public static var preferredAxis: NSLayoutConstraint.Axis { .horizontal }
+
+    /// The view representing the attachment.
+    public func previewView<ExtraData: ExtraDataTypes>(components: _Components<ExtraData>) -> UIView {
+        let view = components.messageComposer.imageAttachmentView.init()
+        view.content = imagePreviewURL
+        return view
+    }
+}
+
+extension FileAttachmentPayload: AttachmentPreviewProvider {
+    public static var preferredAxis: NSLayoutConstraint.Axis { .vertical }
+
+    /// The view representing the attachment.
+    public func previewView<ExtraData: ExtraDataTypes>(components: _Components<ExtraData>) -> UIView {
+        let view = components.messageComposer.fileAttachmentView.init()
+        view.content = .init(
+            title: title ?? "",
+            size: file.size,
+            iconName: assetURL.pathExtension
+        )
+        return view
+    }
+}
