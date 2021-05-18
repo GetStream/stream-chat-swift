0a10deb3a9c648c14660a77cc026af48098e4340
Vojta Stavik <stavik@outlook.com>
2021-05-18 10:36:23 +0200

    Implement `AttachmentPreviewContainer`
    Co-authored-by: Jakub Skorepa <skorepak@gmail.com>

---
+++ Sources/StreamChatUI/CommonViews/Attachments/AttachmentPreviewContainer.swift
@@ -0,0 +1,48 @@
+//
+// Copyright © 2021 Stream.io Inc. All rights reserved.
+//
+
+import StreamChat
+import UIKit
+
+/// A view that wraps attachment preview and provides default controls (ie: remove button) for it.
+open class AttachmentPreviewContainer: _View, AppearanceProvider {
+    /// A closure handler that is called when the discard button of the attachment is clicked
+    public var discardButtonHandler: (() -> Void)?
+    
+    /// A button to remove the attachment from the collection of attachments.
+    open private(set) lazy var discardButton: UIButton = UIButton()
+        .withoutAutoresizingMaskConstraints
+
+    override open func setUp() {
+        super.setUp()
+        
+        discardButton.addTarget(self, action: #selector(discard), for: .touchUpInside)
+    }
+    
+    override open func setUpLayout() {
+        super.setUpLayout()
+        
+        addSubview(discardButton)
+        
+        discardButton.topAnchor.pin(equalTo: layoutMarginsGuide.topAnchor).isActive = true
+        discardButton.trailingAnchor.pin(equalTo: layoutMarginsGuide.trailingAnchor).isActive = true
+        
+        discardButton.setContentHuggingPriority(.required, for: .horizontal)
+    }
+    
+    override open func setUpAppearance() {
+        super.setUpAppearance()
+        
+        discardButton.setImage(appearance.images.discardAttachment, for: .normal)
+    }
+    
+    open func embed(attachmentView view: UIView) {
+        embed(view)
+        sendSubviewToBack(view)
+    }
+    
+    @objc open func discard() {
+        discardButtonHandler?()
+    }
+}
