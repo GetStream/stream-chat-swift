be5db99a72de4cc10e923d0506914f141a6daa04
Vojta Stavik <stavik@outlook.com>
2021-05-19 17:07:20 +0200

    Improve usage of `ChatChannelListRouter`
    Not tested yet but the public API should be better now.

---
+++ Sources/StreamChatUI/Navigation/ChatChannelListRouter_Mock.swift
@@ -0,0 +1,25 @@
+//
+// Copyright © 2021 Stream.io Inc. All rights reserved.
+//
+
+import StreamChat
+@testable import StreamChatUI
+import XCTest
+
+class ChatChannelListRouter_Mock<ExtraData: ExtraDataTypes>: _ChatChannelListRouter<ExtraData> {
+    var openCurrentUserProfileCalled: Bool = false
+    var openChat_channelId: ChannelId?
+    var openCreateNewChannelCalled: Bool = false
+    
+    override open func showCurrentUserProfile() {
+        openCurrentUserProfileCalled = true
+    }
+
+    override open func showMessageList(for cid: ChannelId) {
+        openChat_channelId = cid
+    }
+        
+    override open func showCreateNewChannelFlow() {
+        openCreateNewChannelCalled = true
+    }
+}
