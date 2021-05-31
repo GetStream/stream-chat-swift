4c31afb81d022d96d23eb328296afba7786f635e
Pavel Evsaev <pavel.evsaev@gmail.com>
2021-05-31 18:24:52 +0300

    Update `ChatMessageBubbleView's` content to hold style

--- Sources/StreamChatUI/ChatMessageList/ChatMessage/ChatMessageBubbleView.swift
+++ Sources/StreamChatUI/ChatMessageList/ChatMessage/ChatMessageBubbleView.swift
@@ -12,14 +12,14 @@ public typealias ChatMessageBubbleView = _ChatMessageBubbleView<NoExtraData>
 open class _ChatMessageBubbleView<ExtraData: ExtraDataTypes>: _View, AppearanceProvider, SwiftUIRepresentable {
     /// A type describing the content of this view.
     public struct Content {
-        /// The message to show the bubble for.
-        public let message: _ChatMessage<ExtraData>
-        /// The layout options the message content view is shown with.
-        public let layoutOptions: ChatMessageLayoutOptions
+        /// The background color of the bubble.
+        public let backgroundColor: UIColor
+        /// The mask saying which corners should be rounded.
+        public let roundedCorners: CACornerMask
 
-        public init(message: _ChatMessage<ExtraData>, layoutOptions: ChatMessageLayoutOptions) {
-            self.message = message
-            self.layoutOptions = layoutOptions
+        public init(backgroundColor: UIColor, roundedCorners: CACornerMask) {
+            self.backgroundColor = backgroundColor
+            self.roundedCorners = roundedCorners
         }
     }
 
@@ -39,30 +39,7 @@ open class _ChatMessageBubbleView<ExtraData: ExtraDataTypes>: _View, AppearanceP
     override open func updateContent() {
         super.updateContent()
         
-        layer.maskedCorners = {
-            guard let layout = content?.layoutOptions else { return .all }
-            
-            if layout.contains(.continuousBubble) {
-                return .all
-            } else if layout.contains(.flipped) {
-                return CACornerMask.all.subtracting(.layerMaxXMaxYCorner)
-            } else {
-                return CACornerMask.all.subtracting(.layerMinXMaxYCorner)
-            }
-        }()
-        
-        backgroundColor = {
-            guard let message = content?.message else { return .clear }
-
-            if message.isSentByCurrentUser {
-                if message.type == .ephemeral {
-                    return appearance.colorPalette.background8
-                } else {
-                    return appearance.colorPalette.background6
-                }
-            } else {
-                return appearance.colorPalette.background8
-            }
-        }()
+        layer.maskedCorners = content?.roundedCorners ?? .all
+        backgroundColor = content?.backgroundColor ?? .clear
     }
 }
--- Sources/StreamChatUI/ChatMessageList/ChatMessage/ChatMessageBubbleView_Tests.swift
+++ Sources/StreamChatUI/ChatMessageList/ChatMessage/ChatMessageBubbleView_Tests.swift
@@ -8,130 +8,37 @@ import StreamChatTestTools
 import XCTest
 
 final class ChatMessageBubbleView_Tests: XCTestCase {
+    private var bubbleContent = ChatMessageBubbleView.Content(
+        backgroundColor: Appearance.default.colorPalette.background2,
+        roundedCorners: CACornerMask.all.subtracting(.layerMaxXMinYCorner)
+    )
+    
     // MARK: - Appearance
 
-    func test_appearance() {
+    func test_appearance_whenNoContentSet() {
         // Create a bubble
         let bubble = ChatMessageBubbleView().withFixedSize
-
+        
+        // Set bubble content
+        bubble.content = nil
+        
         // Assert the bubble is rendered correctly
         AssertSnapshot(bubble, variants: .onlyUserInterfaceStyles)
     }
-
-    func test_appearance_whenEphemeralMessageSentByCurrentUserLastInSequence() {
+    
+    func test_appearance_whenContentIsSet() {
         // Create a bubble
         let bubble = ChatMessageBubbleView().withFixedSize
-
-        // Set content with `.ephemeral` message sent by current user ending the message sequence
-        bubble.content = .init(
-            message: .mock(
-                id: .unique,
-                text: .unique,
-                type: .ephemeral,
-                author: .mock(id: .unique),
-                isSentByCurrentUser: true
-            ),
-            layoutOptions: [.flipped]
-        )
-
+        
+        // Set bubble content
+        bubble.content = bubbleContent
+        
         // Assert the bubble is rendered correctly
         AssertSnapshot(bubble, variants: .onlyUserInterfaceStyles)
-    }
-
-    func test_appearance_whenEphemeralMessageSentByCurrentUserNotLastInSequence() {
-        // Create a bubble
-        let bubble = ChatMessageBubbleView().withFixedSize
-
-        // Set content with `.ephemeral` message sent by current user in the middle of message sequence
-        bubble.content = .init(
-            message: .mock(
-                id: .unique,
-                text: .unique,
-                type: .ephemeral,
-                author: .mock(id: .unique),
-                isSentByCurrentUser: true
-            ),
-            layoutOptions: [.continuousBubble]
-        )
-
-        // Assert the bubble is rendered correctly
-        AssertSnapshot(bubble, variants: .onlyUserInterfaceStyles)
-    }
-
-    func test_appearance_whenMessageSentByCurrentUserLastInSequence() {
-        // Create a bubble
-        let bubble = ChatMessageBubbleView().withFixedSize
-
-        // Set content with message sent by current user ending the message sequence
-        bubble.content = .init(
-            message: .mock(
-                id: .unique,
-                text: .unique,
-                author: .mock(id: .unique),
-                isSentByCurrentUser: true
-            ),
-            layoutOptions: [.flipped]
-        )
-
-        // Assert the bubble is rendered correctly
-        AssertSnapshot(bubble, variants: .onlyUserInterfaceStyles)
-    }
-
-    func test_appearance_whenMessageSentByCurrentUserNotLastInSequence() {
-        // Create a bubble
-        let bubble = ChatMessageBubbleView().withFixedSize
-
-        // Set content with message sent by current user in the middle of message sequence
-        bubble.content = .init(
-            message: .mock(
-                id: .unique,
-                text: .unique,
-                author: .mock(id: .unique),
-                isSentByCurrentUser: true
-            ),
-            layoutOptions: [.continuousBubble]
-        )
-
-        // Assert the bubble is rendered correctly
-        AssertSnapshot(bubble, variants: .onlyUserInterfaceStyles)
-    }
-
-    func test_appearance_whenMessageSentByAnotherUserLastInSequence() {
-        // Create a bubble
-        let bubble = ChatMessageBubbleView().withFixedSize
-
-        // Set content with message sent by another user ending the message sequence
-        bubble.content = .init(
-            message: .mock(
-                id: .unique,
-                text: .unique,
-                author: .mock(id: .unique),
-                isSentByCurrentUser: false
-            ),
-            layoutOptions: []
-        )
-
-        // Assert the bubble is rendered correctly
-        AssertSnapshot(bubble, variants: .onlyUserInterfaceStyles)
-    }
-
-    func test_appearance_whenMessageSentByAnotherUserNotLastInSequence() {
-        // Create a bubble
-        let bubble = ChatMessageBubbleView().withFixedSize
-
-        // Set content with message sent by another user that is in the middle message sequence
-        bubble.content = .init(
-            message: .mock(
-                id: .unique,
-                text: .unique,
-                author: .mock(id: .unique),
-                isSentByCurrentUser: false
-            ),
-            layoutOptions: [.continuousBubble]
-        )
-
-        // Assert the bubble is rendered correctly
-        AssertSnapshot(bubble, variants: .onlyUserInterfaceStyles)
+        
+        // Assert bubble has correct values set
+        XCTAssertEqual(bubble.layer.maskedCorners, bubble.content?.roundedCorners)
+        XCTAssertEqual(bubble.backgroundColor, bubble.content?.backgroundColor)
     }
 
     func test_appearanceCustomization_usingAppearance() {
@@ -160,156 +67,12 @@ final class ChatMessageBubbleView_Tests: XCTestCase {
 
         // Create a custom bubble
         let bubble = TestBubble().withFixedSize
+        
+        // Set bubble content
+        bubble.content = bubbleContent
 
         // Assert the custom bubble is rendered correctly
         AssertSnapshot(bubble, variants: .onlyUserInterfaceStyles)
-    }
-
-    // MARK: - Rounded corners
-
-    func test_bubbleRoundedCorners_whenContentIsNil_returnsAll() {
-        // Create a bubble without a content
-        let bubble = ChatMessageBubbleView()
-        bubble.content = nil
-        
-        // Simulate `updateContent` call
-        bubble.updateContent()
-
-        // Assert all corners are rounded
-        XCTAssertEqual(bubble.layer.maskedCorners, .all)
-    }
-
-    func test_bubbleRoundedCorners_whenContinuousBubble_returnsAll() {
-        // Create a bubble
-        let bubble = ChatMessageBubbleView()
-
-        // Set the content with layout options containing `.continuousBubble`
-        bubble.content = .init(
-            message: .mock(id: .unique, text: .unique, author: .mock(id: .unique)),
-            layoutOptions: [.continuousBubble, .flipped]
-        )
-        
-        // Simulate `updateContent` call
-        bubble.updateContent()
-
-        // Assert all corners are rounded
-        XCTAssertEqual(bubble.layer.maskedCorners, .all)
-    }
-
-    func test_bubbleRoundedCorners_whenNotContinuousBubbleButFlipped_roundsBottomRightCorner() {
-        // Create a bubble
-        let bubble = ChatMessageBubbleView()
-
-        // Set the content with layout options without `.continuousBubble` but with `.flipped` option
-        bubble.content = .init(
-            message: .mock(id: .unique, text: .unique, author: .mock(id: .unique)),
-            layoutOptions: [.flipped]
-        )
-        
-        // Simulate `updateContent` call
-        bubble.updateContent()
-
-        // Assert all corners are rounded but bottom right
-        XCTAssertEqual(bubble.layer.maskedCorners, CACornerMask.all.subtracting(.layerMaxXMaxYCorner))
-    }
-
-    func test_bubbleRoundedCorners_whenNorContinuousBubbleNorFlipped_roundsBottomLeftCorner() {
-        // Create a bubble
-        let bubble = ChatMessageBubbleView()
-
-        // Set the content with layout options without `.continuousBubble` and `.flipped` options
-        bubble.content = .init(
-            message: .mock(id: .unique, text: .unique, author: .mock(id: .unique)),
-            layoutOptions: []
-        )
-        
-        // Simulate `updateContent` call
-        bubble.updateContent()
-
-        // Assert all corners are rounded but bottom left
-        XCTAssertEqual(bubble.layer.maskedCorners, CACornerMask.all.subtracting(.layerMinXMaxYCorner))
-    }
-
-    // MARK: - Background
-
-    func test_bubbleBackgroundColor_whenContentIsNil_returnsClear() {
-        // Create a bubble
-        let bubble = ChatMessageBubbleView()
-        
-        // Reset the content
-        bubble.content = nil
-        
-        // Simulate `updateContent` call
-        bubble.updateContent()
-
-        // Assert `.clear` color is returned
-        XCTAssertEqual(bubble.backgroundColor, .clear)
-    }
-
-    func test_bubbleBackgroundColor_whenEphemeralMessage_returnsPopoverBackground() {
-        // Create a bubble
-        let bubble = ChatMessageBubbleView()
-
-        // Set the content with `.ephemeral` message
-        bubble.content = .init(
-            message: .mock(
-                id: .unique,
-                text: .unique,
-                type: .ephemeral,
-                author: .mock(id: .unique)
-            ),
-            layoutOptions: []
-        )
-        
-        // Simulate `updateContent` call
-        bubble.updateContent()
-
-        // Assert correct color is returned
-        XCTAssertEqual(bubble.backgroundColor, bubble.appearance.colorPalette.popoverBackground)
-    }
-
-    func test_bubbleBackgroundColor_whenMessageIsSentByCurrentUser_returnsBackground6() {
-        // Create a bubble
-        let bubble = ChatMessageBubbleView()
-
-        // Set the content with non-ephemeral message sent by current user
-        bubble.content = .init(
-            message: .mock(
-                id: .unique,
-                text: .unique,
-                author: .mock(id: .unique),
-                isSentByCurrentUser: true
-            ),
-            layoutOptions: []
-        )
-        
-        // Simulate `updateContent` call
-        bubble.updateContent()
-
-        // Assert correct color is returned
-        XCTAssertEqual(bubble.backgroundColor, bubble.appearance.colorPalette.background6)
-    }
-
-    func test_bubbleBackgroundColor_whenMessageIsSentNotByCurrentUser_returnsPopoverBackground() {
-        // Create a bubble
-        let bubble = ChatMessageBubbleView()
-
-        // Set the content with non-ephemeral message sent by another user
-        bubble.content = .init(
-            message: .mock(
-                id: .unique,
-                text: .unique,
-                author: .mock(id: .unique),
-                isSentByCurrentUser: false
-            ),
-            layoutOptions: []
-        )
-        
-        // Simulate `updateContent` call
-        bubble.updateContent()
-
-        // Assert correct color is returned
-        XCTAssertEqual(bubble.backgroundColor, bubble.appearance.colorPalette.popoverBackground)
     }
 }
 
