dac3994ad592db9b8936df80b7979054dc0cc504
Vojta Stavik <stavik@outlook.com>
2021-06-08 14:42:52 +0200

    Add docs snapshot for `AttachmentButton`

---
+++ UISDKdocumentation/AttachmentButton_Documentation_Tests.swift
@@ -0,0 +1,28 @@
+//
+// Copyright © 2021 Stream.io Inc. All rights reserved.
+//
+
+import StreamChat
+import StreamChatTestTools
+@testable import StreamChatUI
+import SwiftUI
+import XCTest
+
+class AttachmentButton_Documentation_Tests: XCTestCase {
+    func test_generateDocsSnapshot() {
+        // Create a view to annotate and create documentation for.
+        let view = AttachmentButton().withoutAutoresizingMaskConstraints
+            
+        NSLayoutConstraint.activate([
+            view.widthAnchor.constraint(equalToConstant: 50),
+            view.heightAnchor.constraint(equalToConstant: 50),
+        ])
+        
+        generateDocs(
+            for: view,
+            annotations: [],
+            name: "AttachmentButton_documentation",
+            variants: .onlyUserInterfaceStyles
+        )
+    }
+}
