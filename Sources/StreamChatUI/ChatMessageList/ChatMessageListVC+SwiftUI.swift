1968a4d3934c603542b91658faae40966faff4f0
Bahadir Oncel <bahadir.onc@gmail.com>
2021-06-10 17:44:46 +0300

    Deprecate old SwiftUI wrapper for ChatChannelListVC
    Since we didn't release the ChatMessageListVC wrapper, it's best to remove it.
    The new typealias and extension function keeps the functionality in non-broken way:
    1. Users can still use `ChatChannelList` as a type
    2. Users can still use `ChatChannelList(controller:)` init

--- Sources/StreamChatUI/ChatMessageList/ChatMessageListVC+SwiftUI.swift
+++ Sources/StreamChatUI/ChatMessageList/ChatMessageListVC+SwiftUI.swift
@@ -5,31 +5,6 @@
 import StreamChat
 import SwiftUI
 
-@available(iOS 13.0, *)
-/// A SwiftUI View that wraps `ChatMessageListVC` and shows list of messages.
-public typealias ChatMessageListView = _ChatMessageListVC<NoExtraData>.View
-
-@available(iOS 13.0, *)
-extension _ChatMessageListVC {
-    /// A SwiftUI View that wraps `ChatMessageListVC` and shows list of messages.
-    public struct View: UIViewControllerRepresentable {
-        /// The `_ChatChannelController` instance that provides channel data.
-        let controller: _ChatChannelController<ExtraData>
-
-        public init(controller: _ChatChannelController<ExtraData>) {
-            self.controller = controller
-        }
-
-        public func makeUIViewController(context: Context) -> _ChatMessageListVC<ExtraData> {
-            let vc = _ChatMessageListVC<ExtraData>()
-            vc.channelController = controller
-            return vc
-        }
-
-        public func updateUIViewController(_ chatChannelListVC: _ChatMessageListVC<ExtraData>, context: Context) {}
-    }
-}
-
 extension _ChatMessageListVC: SwiftUIRepresentable {
     public var content: _ChatChannelController<ExtraData> {
         get {
