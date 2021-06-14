6020a9580f77b75365bf9d112a75dbb9179ab5a4
Bahadir Oncel <bahadir.onc@gmail.com>
2021-06-14 13:28:06 +0200

    Fix sorting MemberList by createdAt causing crash
    The keyPath `createdAt` didn't exist on MemberDTO and it led to a crash

---
+++ Sources/StreamChat/Query/Sorting/ChannelMemberListSortingKey_Tests.swift
@@ -0,0 +1,25 @@
+//
+// Copyright © 2021 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+@testable import StreamChat
+@testable import StreamChatTestTools
+import XCTest
+
+final class ChannelMemberListSortingKey_Tests: XCTestCase {
+    func test_sortDescriptor_keyPaths_areValid() throws {
+        // Put all `ChannelMemberListSortingKey`s in an array
+        // We don't use `CaseIterable` since we only need this for tests
+        let sortingKeys = [ChannelMemberListSortingKey.createdAt]
+        
+        // Iterate over keys...
+        for key in sortingKeys {
+            switch key {
+            case .createdAt:
+                // ... and make sure all keys correspond to a valid KeyPath
+                XCTAssertEqual(key.rawValue, NSExpression(forKeyPath: \MemberDTO.memberCreatedAt).keyPath)
+            }
+        }
+    }
+}
