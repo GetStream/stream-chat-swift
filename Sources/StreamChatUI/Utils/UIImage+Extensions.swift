6371f3d115bd8aca10888fe013840ff621d91f0f
Tommaso Barbugli <tbarbugli@gmail.com>
2021-06-28 22:36:04 +0200

    manually cherry-pick docs from nuno final bit

--- Sources/StreamChatUI/Utils/UIImage+Extensions.swift
+++ Sources/StreamChatUI/Utils/UIImage+Extensions.swift
@@ -25,3 +25,15 @@ extension UIImage {
         return imageColored
     }
 }
+
+extension UIImage {
+    func temporaryLocalFileUrl() throws -> URL? {
+        guard let imageData = jpegData(compressionQuality: 1.0) else { return nil }
+        let imageName = "\(UUID().uuidString).jpg"
+        let documentDirectory = NSTemporaryDirectory()
+        let localPath = documentDirectory.appending(imageName)
+        let photoURL = URL(fileURLWithPath: localPath)
+        try imageData.write(to: photoURL)
+        return photoURL
+    }
+}
