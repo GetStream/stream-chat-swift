deea0948dca13b733fcfd902b40e3f55e7f6e828
Sagar Dagdu <shags032@gmail.com>
2021-07-05 10:31:34 +0530

    Add YouTube clone to fast file and pre-commit hook

--- hooks/pre-commit.sh
+++ hooks/pre-commit.sh
@@ -7,6 +7,7 @@
 ./hooks/git-format-staged --formatter 'mint run swiftformat --config .swiftformat stdin' 'SlackClone/*.swift'
 ./hooks/git-format-staged --formatter 'mint run swiftformat --config .swiftformat stdin' 'iMessageClone/*.swift'
 ./hooks/git-format-staged --formatter 'mint run swiftformat --config .swiftformat stdin' 'MessengerClone/*.swift'
+./hooks/git-format-staged --formatter 'mint run swiftformat --config .swiftformat stdin' 'YouTubeClone/*.swift'
 ./hooks/git-format-staged --formatter 'mint run swiftformat --config .swiftformat stdin' 'Integration/*.swift'
 ./hooks/git-format-staged --formatter 'mint run swiftformat --config .swiftformat-snippets stdin' 'DocsSnippets/*.swift'
 
