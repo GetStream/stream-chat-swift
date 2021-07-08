131da0451908ee04ac01be74636b9c992067767c
Nuno Vieira <nuno.fcvieira93@gmail.com>
2021-07-08 18:23:11 +0100

    Fix youtube clone send button for white theme

--- YouTubeClone/SendButton/YTSendButton.swift
+++ YouTubeClone/SendButton/YTSendButton.swift
@@ -6,10 +6,10 @@ import StreamChatUI
 import UIKit
 
 /// A button which appears in the composer used for sending messages
-final class YTSendButton: _Button {
+final class YTSendButton: _Button, AppearanceProvider {
     override func setUpAppearance() {
         // Customise the appearance to make it look like the YouTube SEND button
         setImage(UIImage(systemName: "paperplane.fill"), for: .normal)
-        tintColor = .white
+        tintColor = .secondaryLabel
     }
 }
