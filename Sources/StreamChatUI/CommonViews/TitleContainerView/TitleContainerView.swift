7c72a20897978b49789118fce55bb724e97049b3
Nuno Vieira <nuno.fcvieira93@gmail.com>
2021-07-13 14:56:46 +0100

    Revert the naming of `TitleContainerView`

--- Sources/StreamChatUI/CommonViews/TitleContainerView/TitleContainerView.swift
+++ Sources/StreamChatUI/CommonViews/TitleContainerView/TitleContainerView.swift
@@ -5,8 +5,8 @@
 import StreamChat
 import UIKit
 
-/// A view that is used as a wrapper for status data in navigationItem's titleView.
-open class HeaderTitleView: _View, AppearanceProvider, SwiftUIRepresentable {
+/// A view that displays a title label and subtitle in a container stack view.
+open class TitleContainerView: _View, AppearanceProvider, SwiftUIRepresentable {
     /// Content of the view that contains title (first line) and subtitle (second nil)
     open var content: (title: String?, subtitle: String?) = (nil, nil) {
         didSet {
