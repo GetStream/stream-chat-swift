94e5611bd454c396b2e09c54b739ed7a00ac84cc
Pavel Evsaev <pavel.evsaev@gmail.com>
2021-07-15 12:16:27 +0300

    Introduce `EventsController`

---
+++ Sources/StreamChat/Controllers/EventsController/EventsController.swift
@@ -0,0 +1,76 @@
+//
+// Copyright © 2021 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+
+public extension _ChatClient {
+    /// Creates a new `EventsController` that can be used for event listening.
+    ///
+    /// - Returns: A new instance of `EventsController`.
+    func eventsController() -> EventsController {
+        .init(notificationCenter: eventNotificationCenter)
+    }
+}
+
+/// `EventsController` uses this protocol to communicate events to the delegate.
+public protocol EventsControllerDelegate: AnyObject {
+    /// The method is invoked when an event is observed.
+    /// - Parameters:
+    ///   - controller: The events controller listening for the events.
+    ///   - event: The observed event.
+    func eventsController(_ controller: EventsController, didReceiveEvent event: Event)
+}
+
+/// `EventsController` is a controller class which allows to observe custom and system events.
+public class EventsController: Controller, DelegateCallable {
+    // An underlaying observer listening for events.
+    private var observer: EventObserver!
+    
+    /// A callback queue on which delegate methods are invoked.
+    public var callbackQueue: DispatchQueue = .main
+    
+    /// An internal backing object for all publicly available Combine publishers. We use it to simplify the way we expose
+    /// publishers. Instead of creating custom `Publisher` types, we use `CurrentValueSubject` and `PassthroughSubject` internally,
+    /// and expose the published values by mapping them to a read-only `AnyPublisher` type.
+    @available(iOS 13, *)
+    lazy var basePublishers = BasePublishers(controller: self)
+    
+    /// A backing object used to deliver updates to main and additional delegates.
+    var multicastDelegate = MulticastDelegate<EventsControllerDelegate>()
+    
+    /// A delegate the controller notifies about the updates.
+    public var delegate: EventsControllerDelegate? {
+        get { (multicastDelegate.mainDelegate as? AnyEventsControllerDelegate)?.delegate }
+        set { multicastDelegate.mainDelegate = newValue.map(AnyEventsControllerDelegate.init) }
+    }
+            
+    /// Create a new instance of `EventsController`.
+    ///
+    /// - Parameter notificationCenter: A notification center that is listened for events.
+    init(notificationCenter: EventNotificationCenter) {
+        observer = .init(
+            notificationCenter: notificationCenter,
+            transform: { $0 },
+            callback: { [unowned self] event in
+                self.delegateCallback {
+                    $0.eventsController(self, didReceiveEvent: event)
+                }
+            }
+        )
+    }
+}
+
+/// An wrapper around `EventsControllerDelegate` which holds a weak reference
+/// to the underlaying delegate.
+class AnyEventsControllerDelegate: EventsControllerDelegate {
+    weak var delegate: EventsControllerDelegate?
+   
+    init(delegate: EventsControllerDelegate) {
+        self.delegate = delegate
+    }
+    
+    func eventsController(_ controller: EventsController, didReceiveEvent event: Event) {
+        delegate?.eventsController(controller, didReceiveEvent: event)
+    }
+}
