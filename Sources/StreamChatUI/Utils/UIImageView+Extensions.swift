ecfceb7f3d3f1bf4e8dc0b8f10441bae1a8b8871
Peyman Khanjan <p.khanjan@gmail.com>
2021-08-18 15:34:51 +0100

    Update Nuke Version

--- Sources/StreamChatUI/Utils/UIImageView+Extensions.swift
+++ Sources/StreamChatUI/Utils/UIImageView+Extensions.swift
@@ -64,11 +64,7 @@ extension UIImageView {
         
         let imageKey = components.imageCDN.cachingKey(forImage: url)
         let urlRequest = components.imageCDN.urlRequest(forImage: url)
-        let request = ImageRequest(
-            urlRequest: urlRequest,
-            processors: preprocessors,
-            options: ImageRequestOptions(filteredURL: imageKey)
-        )
+        let request = ImageRequest(urlRequest: urlRequest, processors: preprocessors, userInfo: [.imageIdKey: imageKey])
         let options = ImageLoadingOptions(placeholder: placeholder)
 
         currentImageLoadingTask = Nuke.loadImage(with: request, options: options, into: self, completion: completion)
