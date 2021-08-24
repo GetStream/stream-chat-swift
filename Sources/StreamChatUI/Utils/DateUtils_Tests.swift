2cdc60af9b86ced88254ddd0b7634ae48fe59750
Nuno Vieira <nuno.fcvieira93@gmail.com>
2021-08-24 14:47:22 +0100

    Fix UI Tests

--- Sources/StreamChatUI/Utils/DateUtils_Tests.swift
+++ Sources/StreamChatUI/Utils/DateUtils_Tests.swift
@@ -8,7 +8,7 @@ import XCTest
 
 class DateUtils_Tests: XCTestCase {
     func test_timeAgoNow() throws {
-        XCTAssertEqual(DateUtils.timeAgo(relativeTo: Date()), "just one second ago")
+        XCTAssertEqual(DateUtils.timeAgo(relativeTo: Date()), "last seen just one second ago")
     }
     
     func test_timeAgoFuture() throws {
@@ -18,31 +18,31 @@ class DateUtils_Tests: XCTestCase {
 
     func test_timeAgo1MinuteAgo() throws {
         let date = Calendar.current.date(byAdding: .second, value: -60, to: Date())!
-        XCTAssertEqual(DateUtils.timeAgo(relativeTo: date), "one minute ago")
+        XCTAssertEqual(DateUtils.timeAgo(relativeTo: date), "last seen one minute ago")
     }
 
     func test_timeAgo59SecondsAgo() throws {
         let date = Calendar.current.date(byAdding: .second, value: -59, to: Date())!
-        XCTAssertEqual(DateUtils.timeAgo(relativeTo: date), "59 seconds ago")
+        XCTAssertEqual(DateUtils.timeAgo(relativeTo: date), "last seen 59 seconds ago")
     }
 
     func test_timeAgo42SecondsAgo() throws {
         let date = Calendar.current.date(byAdding: .second, value: -42, to: Date())!
-        XCTAssertEqual(DateUtils.timeAgo(relativeTo: date), "42 seconds ago")
+        XCTAssertEqual(DateUtils.timeAgo(relativeTo: date), "last seen 42 seconds ago")
     }
     
     func test_timeAgo42MinutesAgo() throws {
         let date = Calendar.current.date(byAdding: .minute, value: -42, to: Date())!
-        XCTAssertEqual(DateUtils.timeAgo(relativeTo: date), "42 minutes ago")
+        XCTAssertEqual(DateUtils.timeAgo(relativeTo: date), "last seen 42 minutes ago")
     }
     
     func test_timeAgo42DaysAgo() throws {
         let date = Calendar.current.date(byAdding: .day, value: -42, to: Date())!
-        XCTAssertEqual(DateUtils.timeAgo(relativeTo: date), "one month ago")
+        XCTAssertEqual(DateUtils.timeAgo(relativeTo: date), "last seen one month ago")
     }
     
     func test_timeAgo42WeeksAgo() throws {
         let date = Calendar.current.date(byAdding: .day, value: -42 * 7, to: Date())!
-        XCTAssertEqual(DateUtils.timeAgo(relativeTo: date), "9 months ago")
+        XCTAssertEqual(DateUtils.timeAgo(relativeTo: date), "last seen 9 months ago")
     }
 }
