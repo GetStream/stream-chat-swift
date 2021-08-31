97a58703b3df536cebd0fcabc216e56787e7c490
Tommaso Barbugli <tbarbugli@gmail.com>
2021-08-31 23:16:57 +0200

    fix test

--- Sources/StreamChat/APIClient/ChatRemoteNotificationHandler.swift
+++ Sources/StreamChat/APIClient/ChatRemoteNotificationHandler.swift
@@ -155,7 +155,7 @@ public class ChatRemoteNotificationHandler {
                 return
             }
             self.syncChannel() {
-                let channel = ChannelDTO.load(cid: cid, context: self.database.viewContext)?.asModel()
+                let channel = ChannelDTO.load(cid: cid, context: self.database.backgroundReadOnlyContext)?.asModel()
                 completion(message, channel)
             }
         }
