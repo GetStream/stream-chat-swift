82af28bd076416f02ea02943e4bb45e3b58e8db8
Illia Kucheriavyi <illia.kucheriavyi@getstream.io>
2021-09-09 08:53:20 +0300

    [CIS-1145] Unit Tests For Filter Decoding (#1417)
    [-] Replaced deprecated throws of NSError
        with correct DecodingErrors
    [-] Improved Warnings in JSONDecoder and Encoder Tests
    [-] Added tests for encoding-decoding
    [-] Moved reusable test logic to FilterTestExtensions_Tests
    [-] Now testcases are shared across encoding and decoding
    [-] Renamed TestScope to FilterTestScope
    [-] Updated Package.swift with new tests ignored

---
+++ Sources/StreamChat/Query/FilterDecoding_Tests.swift
@@ -0,0 +1,62 @@
+//
+// Copyright © 2021 Stream.io Inc. All rights reserved.
+//
+
+@testable import StreamChat
+import XCTest
+
+class FilterDecoding_Tests: XCTestCase {
+    // MARK: - Exception thrown tests
+
+    func testFilterDecodingThrowExceptionOnEmpty() {
+        checkDecodingFilterThrowException(json: "")
+    }
+
+    func testFilterDecodingThrowExceptionOnInvalidJSON() {
+        checkDecodingFilterThrowException(json: "12345")
+    }
+
+    func testFilterDecodingThrowExceptionOnInvalidKeylessJSON() {
+        checkDecodingFilterThrowException(json: #"{"value"}"#)
+    }
+
+    func testFilterRightSideDecodingThrowExceptionWithMoreThanOneKey() {
+        let filterJson = #"{"test_key":{"$eq":"test_value_1","$ne":"test_value_2"}}"#
+        checkDecodingFilterThrowException(json: filterJson)
+    }
+
+    func testFilterRightSideDecodingThrowExceptionWithoutOperationKey() {
+        let filterJson = #"{"test_key":{"eq":"test_value_1"}}"#
+        checkDecodingFilterThrowException(json: filterJson)
+    }
+
+    func testFilterRightSideDecodingThrowExceptionOnNonExistingOperation() {
+        let filterJson = #"{"test_key":{"$myspecialOperation":"test_value_1"}}"#
+        checkDecodingFilterThrowException(json: filterJson)
+    }
+
+    // MARK: - Correct decoded tests
+
+    func testFilterDecoding() {
+        // Given
+        let testCases = FilterCodingTestPair.allCases
+        for pair in testCases {
+            // When
+            let decoded: Filter<FilterTestScope> = try! pair.json.deserializeFilterThrows()
+            // Then
+            XCTAssertEqual(decoded, pair.filter)
+        }
+    }
+
+    // MARK: - Private methods
+
+    private func checkDecodingFilterThrowException(json: String) {
+        do {
+            // When
+            let _: Filter<FilterTestScope> = try json.deserializeFilterThrows()
+        } catch {
+            // Then
+            XCTAssertNotNil(error)
+        }
+    }
+}
