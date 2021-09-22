d06dca1cc2282e96063af52623b8551b16038445
Pavel Evsaev <pavel.evsaev@gmail.com>
2021-09-22 16:58:14 +0300

    Add local notifications support to DemoApp

--- DemoApp/DemoAppCoordinator.swift
+++ DemoApp/DemoAppCoordinator.swift
@@ -27,6 +27,14 @@ final class DemoAppCoordinator: NSObject, UNUserNotificationCenterDelegate {
     
     func userNotificationCenter(
         _ center: UNUserNotificationCenter,
+        willPresent notification: UNNotification,
+        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
+    ) {
+        completionHandler([.banner, .badge, .sound, .list])
+    }
+    
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
         didReceive response: UNNotificationResponse,
         withCompletionHandler completionHandler: @escaping () -> Void
     ) {
