59f5fea29321372724d83393e83bd1daca59f9f6
Pavel Evsaev <pavel.evsaev@gmail.com>
2021-10-07 20:22:59 +0300

    Fix responder chain being cutoff for child controllers

--- Sources/StreamChatUI/Utils/UIViewController+Extensions.swift
+++ Sources/StreamChatUI/Utils/UIViewController+Extensions.swift
@@ -7,6 +7,7 @@ import UIKit
 extension UIViewController {
     func addChildViewController(_ child: UIViewController, targetView superview: UIView) {
         addChild(child)
+        child.view.translatesAutoresizingMaskIntoConstraints = false
         superview.addSubview(child.view)
         child.didMove(toParent: self)
     }
