0d3a62aa4786261980fcb237e5eb97eb9eb161d1
Nuno Vieira <nuno.fcvieira93@gmail.com>
2021-10-19 17:47:19 +0100

    Refactor `MulticastDelegate`

--- Sources/StreamChat/Utils/MulticastDelegate.swift
+++ Sources/StreamChat/Utils/MulticastDelegate.swift
@@ -4,23 +4,27 @@
 
 import Foundation
 
-/// A wrapper object that allows attaching additional delegates and call the callbacks on all of them.
-///
-/// - Warning: ⚠️ Because `MulticastDelegate` keeps strong references to the delegates, it's strongly recommended to use
-/// an additional wrapper which will make sure the delegate instance is referenced only weakly.
-struct MulticastDelegate<WrappedDelegate> {
-    /// Invokes the delegate callback on all delegates.
-    func invoke(_ action: (WrappedDelegate) -> Void) {
-        if let main = mainDelegate {
-            action(main)
-        }
-        additionalDelegates.forEach { action($0) }
+struct MulticastDelegate<T> {
+    private var _delegates = NSHashTable<AnyObject>.weakObjects()
+
+    var delegates: [T] {
+        _delegates.allObjects.map { $0 as! T }
     }
-    
-    /// The is usually the delegate instance you want to expose to your users as _the_ delegate.
-    var mainDelegate: WrappedDelegate?
-    
-    /// Aditional delegates that receive the same callback as the main one. These delegates receive callbacks also when
-    /// the main delegate is not set.
-    var additionalDelegates: [WrappedDelegate] = []
+
+    func invoke(_ action: (T) -> Void) {
+        delegates
+            .forEach { action($0) }
+    }
+
+    mutating func add(_ delegate: T) {
+        _delegates.add(delegate as AnyObject)
+    }
+
+    mutating func remove(_ delegate: T) {
+        _delegates.remove(delegate as AnyObject)
+    }
+
+    mutating func removeAll() {
+        _delegates.removeAllObjects()
+    }
 }
