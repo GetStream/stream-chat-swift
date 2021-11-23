d77aed52e24901cdcfca9bb55a051e561c31d305
martinmitrevski <martinmitrevski.oh@gmail.com>
2021-11-23 18:02:31 +0100

    Fixed failing test

--- Sources/StreamChat/ChatClient_Tests.swift
+++ Sources/StreamChat/ChatClient_Tests.swift
@@ -1343,7 +1343,7 @@ extension ChatClient_Tests {
         let headers = config.httpAdditionalHeaders as? [String: String] ?? [:]
         XCTAssertEqual(
             headers["X-Stream-Client"],
-            "stream-chat-swift-client-v\(SystemEnvironment.version)"
+            "stream-chat-uikit-client-v\(SystemEnvironment.version)"
         )
     }
 }
