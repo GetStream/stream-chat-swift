1bf656e0b4eefaf90146279d43a3d46b72a7a48d
Bahadır Öncel <bahadir@getstream.io>
2021-12-22 15:57:55 +0100

    Add `ChannelListQuery.membersLimit` param (#1721)

--- Sources/StreamChat/Query/ChannelListQuery_Tests.swift
+++ Sources/StreamChat/Query/ChannelListQuery_Tests.swift
@@ -79,3 +79,39 @@ final class ChannelListFilterScope_Tests: XCTestCase {
         }
     }
 }
+
+final class ChannelListQuery_Tests: XCTestCase {
+    func test_channelListQuery_encodedCorrectly() throws {
+        let cid = ChannelId.unique
+        let filter = Filter<ChannelListFilterScope>.equal(.cid, to: cid)
+        let sort = Sorting<ChannelListSortingKey>.init(key: .cid)
+        let pageSize = Int.channelsPageSize
+        let messagesLimit = Int.messagesPageSize
+        let membersLimit = Int.channelMembersPageSize
+        
+        // Create ChannelListQuery
+        var query = ChannelListQuery(
+            filter: filter,
+            sort: [sort],
+            pageSize: pageSize,
+            messagesLimit: messagesLimit,
+            membersLimit: membersLimit
+        )
+        query.options = .watch
+        
+        let expectedData: [String: Any] = [
+            "limit": pageSize,
+            "message_limit": messagesLimit,
+            "member_limit": membersLimit,
+            "sort": [["field": "cid", "direction": -1]],
+            "filter_conditions": ["cid": ["$eq": cid.rawValue]],
+            "watch": true
+        ]
+        
+        let expectedJSON = try JSONSerialization.data(withJSONObject: expectedData, options: [])
+        let encodedJSON = try JSONEncoder.default.encode(query)
+        
+        // Assert ChannelListQuery encoded correctly
+        AssertJSONEqual(expectedJSON, encodedJSON)
+    }
+}
