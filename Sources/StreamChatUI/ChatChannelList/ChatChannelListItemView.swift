5a2642c64f0106191235ba0b21e372b7270bd2b6
Nuno Vieira <nuno.fcvieira93@gmail.com>
2021-12-29 16:11:29 +0000

    Fix Snapshot Tests

--- Sources/StreamChatUI/ChatChannelList/ChatChannelListItemView.swift
+++ Sources/StreamChatUI/ChatChannelList/ChatChannelListItemView.swift
@@ -115,6 +115,7 @@ open class ChatChannelListItemView: _View, ThemeProvider, SwiftUIRepresentable {
 
     override open func setUpAppearance() {
         super.setUpAppearance()
+        backgroundColor = appearance.colorPalette.background
 
         titleLabel.font = appearance.fonts.bodyBold
 
