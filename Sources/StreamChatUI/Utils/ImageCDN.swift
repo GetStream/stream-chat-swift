42c08731e5c8aeb71071ce7a2ea0890333bf8adf
Ajay Ghodadra <ajay.g@simformsolutions.com>
2022-01-28 20:06:22 +0530

    - increase avatar scale size

--- Sources/StreamChatUI/Utils/ImageCDN.swift
+++ Sources/StreamChatUI/Utils/ImageCDN.swift
@@ -83,5 +83,5 @@ public extension CGSize {
     /// Maximum size of avatar used in the UI.
     ///
     /// It's better to use single size of avatar thumbnail to utilise the cache.
-    static var avatarThumbnailSize: CGSize { CGSize(width: 40, height: 40) }
+    static var avatarThumbnailSize: CGSize { CGSize(width: 130, height: 130) }
 }
