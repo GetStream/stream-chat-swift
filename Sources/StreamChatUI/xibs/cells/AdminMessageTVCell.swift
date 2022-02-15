a65c0fddaf4bcd6940537247e7f9e784bfa706a6
Ajay Ghodadra <ajay.g@simformsolutions.com>
2022-02-15 15:54:48 +0530

    - text spacing check

--- Sources/StreamChatUI/xibs/cells/AdminMessageTVCell.swift
+++ Sources/StreamChatUI/xibs/cells/AdminMessageTVCell.swift
@@ -57,25 +57,24 @@ class AdminMessageTVCell: UITableViewCell {
     private func getInitialAddSignerDesc() -> String {
         var descText = ""
         if content?.isSentByCurrentUser ?? false {
-            descText.append("You")
+            descText.append("You ")
         } else {
-            descText.append(content?.author.name ?? "-")
+            descText.append("\(content?.author.name ?? "-") ")
         }
-        descText.append(" created this group")
+        descText.append("created this group ")
         let otherAdmins = content?.extraData.daoAdmins.filter({ ($0["signerUserId"] as? String ?? "") != ChatClient.shared.currentUserId }) ?? [[String: Any]]()
         if otherAdmins.count >= 1 {
-            descText.append(" with ")
+            descText.append("with ")
             descText.append(otherAdmins.first?["signerName"] as? String ?? "")
         }
         if otherAdmins.count >= 2 {
-            descText.append(" and")
-            descText.append(" \(otherAdmins.count - 1)")
+            descText.append("and ")
+            descText.append("\(otherAdmins.count - 1) ")
             if otherAdmins.count - 1 == 1 {
-                descText.append(" other.")
+                descText.append("other.")
             } else {
-                descText.append(" others.")
+                descText.append("others.")
             }
-
         }
         descText.append("\nTry using the menu item to share with others.")
         return descText
