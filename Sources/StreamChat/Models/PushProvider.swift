b049fdda04ccfe705d8b27194c3408ad268c0e51
Nuno Vieira <nuno.fcvieira93@gmail.com>
2022-03-18 16:41:11 +0000

    Add support for multiple push providers

---
+++ Sources/StreamChat/Models/PushProvider.swift
@@ -0,0 +1,20 @@
+//
+// Copyright © 2022 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+
+public struct PushProvider: RawRepresentable, Hashable, ExpressibleByStringLiteral {
+    public static let firebase: Self = "firebase"
+    public static let apn: Self = "apn"
+
+    public let rawValue: String
+
+    public init(rawValue: String) {
+        self.rawValue = rawValue
+    }
+
+    public init(stringLiteral value: String) {
+        self.init(rawValue: value)
+    }
+}
