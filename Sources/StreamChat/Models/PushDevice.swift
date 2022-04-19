cd2f3225a7470367be554f458eacfb480be514a1
Nuno Vieira <nuno.fcvieira93@gmail.com>
2022-04-19 11:04:02 +0100

    Use PushProvider's static properties

--- Sources/StreamChat/Models/PushDevice.swift
+++ Sources/StreamChat/Models/PushDevice.swift
@@ -18,13 +18,13 @@ public struct PushDevice {
     /// - Parameter token: The device token obtained via `didRegisterForRemoteNotificationsWithDeviceToken` function in `AppDelegate`.
     /// - Returns: The Push Device details.
     public static func apn(token: Data) -> Self {
-        .init(deviceId: token.deviceId, provider: "apn")
+        .init(deviceId: token.deviceId, provider: .apn)
     }
 
     /// Creates a Push Device for Firebase Push Notifications.
     /// - Parameter token: The FCM token.
     /// - Returns: The Push Device details.
     public static func firebase(token: String) -> Self {
-        .init(deviceId: DeviceId(token), provider: "firebase")
+        .init(deviceId: DeviceId(token), provider: .firebase)
     }
 }
