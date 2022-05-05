eb1447c375f1347272e324cf593a5fb21f07a4cd
Pavel Evsaev <pavel.evsaev@gmail.com>
2022-05-05 16:55:34 +0400

    Add missing handling for unread counts

--- Sources/StreamChat/WebSocketClient/EventMiddlewares/ChannelReadUpdaterMiddleware.swift
+++ Sources/StreamChat/WebSocketClient/EventMiddlewares/ChannelReadUpdaterMiddleware.swift
@@ -9,16 +9,22 @@ struct ChannelReadUpdaterMiddleware: EventMiddleware {
     func handle(event: Event, session: DatabaseSession) -> Event? {
         switch event {
         case let event as MessageNewEventDTO:
-            increaseUnreadCountIfNeeded(
+            incrementUnreadCountIfNeeded(
                 for: event.cid,
                 message: event.message,
                 session: session
             )
 
         case let event as NotificationMessageNewEventDTO:
-            increaseUnreadCountIfNeeded(
+            incrementUnreadCountIfNeeded(
                 for: event.channel.cid,
                 message: event.message,
+                session: session
+            )
+        
+        case let event as MessageDeletedEventDTO:
+            decrementUnreadCountIfNeeded(
+                event: event,
                 session: session
             )
             
@@ -50,36 +56,136 @@ struct ChannelReadUpdaterMiddleware: EventMiddleware {
         session.markChannelAsRead(cid: cid, userId: userId, at: lastReadAt)
     }
     
-    private func increaseUnreadCountIfNeeded(
+    private func incrementUnreadCountIfNeeded(
         for cid: ChannelId,
         message: MessagePayload,
         session: DatabaseSession
     ) {
-        // Silent messages don't increase unread count
-        if message.isSilent {
-            return
+        guard let currentUser = session.currentUser else {
+            return log.error("Current user is missing", subsystems: .webSocket)
         }
-
-        // Thread replies don't increase unread count
-        if message.parentId != nil && !message.showReplyInChannel {
-            return
+        
+        guard let channelRead = session.loadChannelRead(cid: cid, userId: currentUser.user.id) else {
+            return log.error("Channel read is missing", subsystems: .webSocket)
         }
-
-        // Own messages don't increase unread count
-        guard let currentUserId = session.currentUser?.user.id, currentUserId != message.user.id else {
-            return
-        }
-
-        // Try to get the existing channel read for the current user
-        if let read = session.loadChannelRead(cid: cid, userId: currentUserId) {
-            if message.createdAt > read.lastReadAt ?? Date.distantPast {
-                read.unreadMessageCount += 1
-            }
-        } else {
-            log.info(
-                "Can't increase unread count for \(cid) because the `ChannelReadDTO` for " +
-                    "the current user doesn't exist."
+        
+        if let skipReason = unreadCountUpdateSkippingReason(
+            currentUser: currentUser,
+            channelRead: channelRead,
+            message: message
+        ) {
+            return log.debug(
+                "Message \(message.id) does not increment unread counts for channel \(cid): \(skipReason)",
+                subsystems: .webSocket
             )
+        }
+        
+        log.debug(
+            "Message \(message.id) increments unread counts for channel \(cid)",
+            subsystems: .webSocket
+        )
+        
+        channelRead.unreadMessageCount += 1
+    }
+    
+    private func decrementUnreadCountIfNeeded(
+        event: MessageDeletedEventDTO,
+        session: DatabaseSession
+    ) {
+        guard let currentUser = session.currentUser else {
+            return log.error("Current user is missing", subsystems: .webSocket)
+        }
+        
+        guard let channelRead = session.loadChannelRead(cid: event.cid, userId: currentUser.user.id) else {
+            return log.error("Channel read is missing", subsystems: .webSocket)
+        }
+        
+        if let skipReason = !event.hardDelete
+            ? .messageIsSoftDeleted
+            : unreadCountUpdateSkippingReason(
+                currentUser: currentUser,
+                channelRead: channelRead,
+                message: event.message
+            ) {
+            return log.debug(
+                "Message \(event.message.id) does not decrement unread coutns for \(event.message.user.id): \(skipReason)",
+                subsystems: .webSocket
+            )
+        }
+        
+        log.debug(
+            "Message \(event.message.id) decrements unread counts for channel \(event.cid)",
+            subsystems: .webSocket
+        )
+        
+        channelRead.unreadMessageCount = max(0, channelRead.unreadMessageCount - 1)
+    }
+        
+    private func unreadCountUpdateSkippingReason(
+        currentUser: CurrentUserDTO,
+        channelRead: ChannelReadDTO,
+        message: MessagePayload
+    ) -> UnreadSkippingReason? {
+        if channelRead.channel.mute != nil {
+            return .channelIsMuted
+        }
+        
+        if message.user.id == currentUser.user.id {
+            return .messageIsOwn
+        }
+
+        if currentUser.mutedUsers.contains(where: { message.user.id == $0.id }) {
+            return .authorIsMuted
+        }
+        
+        if message.isSilent {
+            return .messageIsSilent
+        }
+        
+        if message.parentId != nil && !message.showReplyInChannel {
+            return .messageIsThreadReply
+        }
+        
+        if message.type == .system {
+            return .messageIsSystem
+        }
+        
+        if message.createdAt <= (channelRead.lastReadAt ?? Date.distantPast) {
+            return .messageIsSeen
+        }
+        
+        return nil
+    }
+}
+
+private enum UnreadSkippingReason: CustomStringConvertible {
+    case channelIsMuted
+    case authorIsMuted
+    case messageIsOwn
+    case messageIsSilent
+    case messageIsThreadReply
+    case messageIsSystem
+    case messageIsSeen
+    case messageIsSoftDeleted
+
+    var description: String {
+        switch self {
+        case .channelIsMuted:
+            return "Channel is muted"
+        case .messageIsOwn:
+            return "Own messages do not affect unread counts"
+        case .authorIsMuted:
+            return "Message author is muted"
+        case .messageIsSilent:
+            return "Silent messages do not affect unread counts"
+        case .messageIsThreadReply:
+            return "Thread replies do not affect unread counts"
+        case .messageIsSystem:
+            return "System messages do not affect unread counts"
+        case .messageIsSeen:
+            return "Seen messages do not affect unread counts"
+        case .messageIsSoftDeleted:
+            return "Soft-deleted messages do not affect unread counts"
         }
     }
 }
