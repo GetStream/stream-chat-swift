34047721b3900ef6af7482916afa0ac80f4af5d4
Pavel Evsaev <pavel.evsaev@gmail.com>
2022-05-06 19:25:49 +0400

    [CIS-1827] Expose `actions` on image attachment payload (#1979)

--- Tests/StreamChatTests/Models/Attachments/ImageAttachmentPayload_Tests.swift
+++ Tests/StreamChatTests/Models/Attachments/ImageAttachmentPayload_Tests.swift
@@ -7,41 +7,20 @@
 import XCTest
 
 final class ImageAttachmentPayload_Tests: XCTestCase {
-    func test_decodingDefaultValues() throws {
-        // Create attachment field values.
-        let title: String = .unique
-        let imageURL: URL = .unique()
-        let thumbURL: URL = .unique()
-        
-        // Create JSON with the given values.
-        let json = """
-        {
-            "title": "\(title)",
-            "image_url": "\(imageURL.absoluteString)",
-            "thumb_url": "\(thumbURL.absoluteString)"
-        }
-        """.data(using: .utf8)!
-        
-        // Decode attachment from JSON.
-        let payload = try JSONDecoder.stream.decode(ImageAttachmentPayload.self, from: json)
-        
-        // Assert values are decoded correctly.
-        XCTAssertEqual(payload.title, title)
-        XCTAssertEqual(payload.imageURL, imageURL)
-        XCTAssertEqual(payload.imagePreviewURL, thumbURL)
-        XCTAssertNil(payload.extraData)
-    }
-    
-    func test_decodingExtraData() throws {
-        struct ExtraData: Codable {
-            let comment: String
-        }
-        
+    func test_decode() throws {
         // Create attachment field values.
         let title: String = .unique
         let imageURL: URL = .unique()
         let thumbURL: URL = .unique()
         let comment: String = .unique
+        
+        let action = AttachmentAction(
+            name: .unique,
+            value: .unique,
+            style: .default,
+            type: .button,
+            text: .unique
+        )
         
         // Create JSON with the given values.
         let json = """
@@ -49,7 +28,16 @@ final class ImageAttachmentPayload_Tests: XCTestCase {
             "title": "\(title)",
             "image_url": "\(imageURL.absoluteString)",
             "thumb_url": "\(thumbURL.absoluteString)",
-            "comment": "\(comment)"
+            "comment": "\(comment)",
+            "actions": [
+                {
+                    "name": "\(action.name)",
+                    "value": "\(action.value)",
+                    "style": "\(action.style.rawValue)",
+                    "type": "\(action.type.rawValue)",
+                    "text": "\(action.text)"
+                }
+            ]
         }
         """.data(using: .utf8)!
         
@@ -60,9 +48,52 @@ final class ImageAttachmentPayload_Tests: XCTestCase {
         XCTAssertEqual(payload.title, title)
         XCTAssertEqual(payload.imageURL, imageURL)
         XCTAssertEqual(payload.imagePreviewURL, thumbURL)
-        
+        XCTAssertEqual(payload.actions, [action])
+
         // Assert extra data can be decoded.
         let extraData = try XCTUnwrap(payload.extraData(ofType: ExtraData.self))
         XCTAssertEqual(extraData.comment, comment)
     }
+    
+    func test_encode() throws {
+        let extraData = ExtraData(comment: .unique)
+        
+        let payload = ImageAttachmentPayload(
+            title: .unique,
+            imageRemoteURL: .unique(),
+            actions: [
+                .init(
+                    name: .unique,
+                    value: .unique,
+                    style: .default,
+                    type: .button,
+                    text: .unique
+                )
+            ],
+            extraData: [
+                "comment": .string(extraData.comment)
+            ]
+        )
+
+        let serialized = try JSONEncoder.stream.encode(payload)
+        let expected: [String: Any] = [
+            "title": payload.title!,
+            "image_url": payload.imageURL.absoluteString,
+            "actions": NSArray(array: payload.actions.map {
+                [
+                    "name": $0.name,
+                    "value": $0.value,
+                    "style": $0.style.rawValue,
+                    "type": $0.type.rawValue,
+                    "text": $0.text
+                ]
+            }),
+            "comment": extraData.comment
+        ]
+        AssertJSONEqual(serialized, expected)
+    }
+}
+
+private struct ExtraData: Codable {
+    let comment: String
 }
