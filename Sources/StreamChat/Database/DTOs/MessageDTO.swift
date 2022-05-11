04b0f3211946ae4c5249ad2aa18fd78ecc7ba354
Adam Rush <adam@adr-studio.co.uk>
2022-05-11 17:23:25 +0200

    These are code smells reported by SonarCloud worth changing (#1993)
    Co-authored-by: Adam Rush <adam.rush@getstream.io>

--- Sources/StreamChat/Database/DTOs/MessageDTO.swift
+++ Sources/StreamChat/Database/DTOs/MessageDTO.swift
@@ -754,7 +754,6 @@ extension NSManagedObjectContext: MessageDatabaseSession {
         }
 
         message.reactionScores[type.rawValue] = max(reactionScore - Int(reaction.score), 0)
-        message.reactionScores[type.rawValue] = reactionScore
         return reaction
     }
     
