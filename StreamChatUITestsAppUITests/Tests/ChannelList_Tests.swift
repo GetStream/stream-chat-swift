cbe89778aa1c002a177948d8144c5d3b008923da
Boris Bielik <h3sperian@gmail.com>
2022-05-13 10:24:26 +0200

    Automated UI scenarios

--- StreamChatUITestsAppUITests/Tests/ChannelList_Tests.swift
+++ StreamChatUITestsAppUITests/Tests/ChannelList_Tests.swift
@@ -6,6 +6,53 @@ import XCTest
 
 final class ChannelList_Tests: StreamTestCase {
 
+    let message = "message"
+
+    func test_newMessageShownInChannelPreview_whenComingBackFromOffline() {
+        linkToScenario(withId: 79)
+
+        GIVEN("user opens the channel") {
+            userRobot
+                .login()
+                .openChannel()
+        }
+        WHEN("participant sends a new message") {
+            participantRobot
+                .sendMessage(message)
+                .waitForNewMessage(withText: message)
+        }
+        AND("user goes back to channel list") {
+            userRobot.tapOnBackButton()
+        }
+        THEN("user observes a preview of participants message") {
+            userRobot.assertLastMessageInChannelPreview(message)
+        }
+    }
+
+    func test_participantMessageShownInChannelPreview_whenReturningFromOffline() {
+        linkToScenario(withId: 92)
+        
+        GIVEN("user opens the channel") {
+            deviceRobot.setConnectivitySwitchVisibility(to: .on)
+            userRobot
+                .login()
+                .openChannel()
+                .tapOnBackButton()
+        }
+        AND("user becomes offline") {
+            deviceRobot.setConnectivity(to: .off)
+        }
+        WHEN("participant sends a new message") {
+            participantRobot.sendMessage(message).chill(duration: 2)
+        }
+        AND("user becomes online") {
+            deviceRobot.setConnectivity(to: .on)
+        }
+        THEN("list shows a preview of participant's message") {
+            userRobot.assertLastMessageInChannelPreview(message)
+        }
+    }
+
     func test_errorMessageIsNotShownInChannelPreview_whenErrorMessageIsReceived() {
         linkToScenario(withId: 185)
 
@@ -33,3 +80,4 @@ final class ChannelList_Tests: StreamTestCase {
         }
     }
 }
+
