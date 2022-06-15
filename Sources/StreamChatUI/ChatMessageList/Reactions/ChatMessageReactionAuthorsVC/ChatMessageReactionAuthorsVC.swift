5dba1571b7abad0489b7cf7d4cbf44a4ab62d57e
Adam Rush <adam.rush@getstream.io>
2022-06-15 10:15:58 +0100

    CIS-1815 - We should always use the custom cell provided or the default

--- Sources/StreamChatUI/ChatMessageList/Reactions/ChatMessageReactionAuthorsVC/ChatMessageReactionAuthorsVC.swift
+++ Sources/StreamChatUI/ChatMessageList/Reactions/ChatMessageReactionAuthorsVC/ChatMessageReactionAuthorsVC.swift
@@ -129,7 +129,7 @@ open class ChatMessageReactionAuthorsVC: _ViewController,
         _ collectionView: UICollectionView,
         cellForItemAt indexPath: IndexPath
     ) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(with: ChatMessageReactionAuthorViewCell.self, for: indexPath)
+        let cell = collectionView.dequeueReusableCell(with: components.reactionAuthorCell.self, for: indexPath)
 
         guard let reaction = content[safe: indexPath.item],
               let currentUserId = messageController?.client.currentUserId else {
