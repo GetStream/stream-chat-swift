688cfc322d0fccaf1e4e4ea21ab8965bbe486303
Boris Bielik <h3sperian@gmail.com>
2022-06-28 04:59:35 +0200

    [BW] Propagate updates consistently (#2118)
    * [BW] Propagate updates consistently

--- Sources/StreamChat/Controllers/Controller.swift
+++ Sources/StreamChat/Controllers/Controller.swift
@@ -16,20 +16,11 @@ public protocol Controller {
 extension Controller {
     /// A helper function to ensure the callback is performed on the callback queue.
     func callback(_ action: @escaping () -> Void) {
-        if Thread.current.isMainThread {
-            if callbackQueue == .main {
-                // We perform the callback synchronously
-                action()
-            } else {
-                // We dispatch from the main queue, we must perform
-                // the callback must be performed asynchronously
-                callbackQueue.async {
-                    action()
-                }
-            }
+        // We perform the callback synchronously if we're on main & `callbackQueue` is on main, too.
+        if Thread.current.isMainThread && callbackQueue == .main {
+            action()
         } else {
-            // Dispatching from a background queue, the callback can be performed synchronously
-            callbackQueue.sync {
+            callbackQueue.async {
                 action()
             }
         }
