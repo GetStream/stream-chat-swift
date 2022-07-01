789418affad5cad387a4b5eb5c110f5d55332aae
Alexey Alter-Pesotskiy <a.alterpesotskiy@yahoo.com>
2022-07-01 12:16:02 +0100

    [CIS-1910] Remove Docs snippets from source code and CI

--- DocsSnippets/01-UX/01-Client-Setup/03-Enable-Logs.swift
+++
@@ -1,9 +0,0 @@
-// LINK: https://getstream.io/chat/docs/ios-swift/ios_client_setup/?preview=1&language=swift#1.-enable-logs
-
-import StreamChat
-
-func snippet_ux_client_setup_enable_logs() {
-    // > import StreamChat
-
-    LogConfig.level = .debug
-}
--- DocsSnippets/05-Messages/05-Search/00-Search.swift
+++
@@ -1,5 +0,0 @@
-// LINK: https://getstream.io/chat/docs/ios-swift/search/?language=swift&preview=1
-
-func snippet_messages_search() {
-    // coming soon
-}
