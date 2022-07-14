4fcd4de96172e68fe42add100a93e36f12e77845
Nuno Vieira <nuno.fcvieira93@gmail.com>
2022-07-14 18:51:06 +0100

    Provide helpers to extract values from RawJSON

--- Sources/StreamChat/APIClient/Endpoints/Payloads/RawJSON.swift
+++ Sources/StreamChat/APIClient/Endpoints/Payloads/RawJSON.swift
@@ -57,7 +57,137 @@ public indirect enum RawJSON: Codable, Hashable {
     }
 }
 
+// MARK: Value extract helpers
+
 public extension RawJSON {
+    /// Extracts a number value of RawJSON.
+    /// Returns nil if the value is not a number.
+    ///
+    /// Example:
+    /// ```
+    /// let extraData = message.extraData
+    /// let price = extraData["price"]?.numberValue ?? 0
+    /// ```
+    var numberValue: Double? {
+        guard case let .number(value) = self else {
+            return nil
+        }
+        return value
+    }
+
+    /// Extracts a string value of RawJSON.
+    /// Returns nil if the value is not a string.
+    ///
+    /// Example:
+    /// ```
+    /// let extraData = message.extraData
+    /// let email = extraData["email"]?.stringValue ?? ""
+    /// ```
+    var stringValue: String? {
+        guard case let .string(value) = self else {
+            return nil
+        }
+        return value
+    }
+
+    /// Extracts a bool value of RawJSON.
+    /// Returns nil if the value is not a bool.
+    ///
+    /// Example:
+    /// ```
+    /// let extraData = message.extraData
+    /// let isManager = extraData["isManager"]?.boolValue ?? false
+    /// ```
+    var boolValue: Bool? {
+        guard case let .bool(value) = self else {
+            return nil
+        }
+        return value
+    }
+
+    /// Extracts a dictionary value of RawJSON.
+    /// Returns nil if the value is not a dictionary.
+    ///
+    /// Example:
+    /// ```
+    /// let extraData = message.extraData
+    /// let flightPrice = extraData["flight"]?.dictionaryValue?["price"]?.numberValue ?? 0
+    /// ```
+    var dictionaryValue: [String: RawJSON]? {
+        guard case let .dictionary(value) = self else {
+            return nil
+        }
+        return value
+    }
+
+    /// Extracts an array value of RawJSON.
+    /// Returns nil if the value is not an array.
+    ///
+    /// Example:
+    /// ```
+    /// let extraData = message.extraData
+    /// let flights: [RawJSON]? = extraData["flights"]?.arrayValue
+    /// ```
+    var arrayValue: [RawJSON]? {
+        guard case let .array(value) = self else {
+            return nil
+        }
+        return value
+    }
+
+    /// Extracts a number array of RawJSON.
+    /// Returns nil if the value is not an array of numbers.
+    ///
+    /// Example:
+    /// ```
+    /// let extraData = message.extraData
+    /// let ages = extraData["ages"]?.numberArrayValue ?? []
+    /// ```
+    var numberArrayValue: [Double]? {
+        guard let rawArrayValue = arrayValue else {
+            return nil
+        }
+
+        return rawArrayValue.compactMap(\.numberValue)
+    }
+
+    /// Extracts a string array of RawJSON.
+    /// Returns nil if the value is not an array of strings.
+    ///
+    /// Example:
+    /// ```
+    /// let extraData = message.extraData
+    /// let names = extraData["names"]?.stringArrayValue ?? []
+    /// ```
+    var stringArrayValue: [String]? {
+        guard let rawArrayValue = arrayValue else {
+            return nil
+        }
+
+        return rawArrayValue.compactMap(\.stringValue)
+    }
+
+    /// Extracts a bool array of RawJSON.
+    /// Returns nil if the value is not an array of bools.
+    var boolArrayValue: [Bool]? {
+        guard let rawArrayValue = arrayValue else {
+            return nil
+        }
+
+        return rawArrayValue.compactMap(\.boolValue)
+    }
+
+    /// Checks if the RawJSON value is null.
+    var isNil: Bool {
+        switch self {
+        case .nil:
+            return true
+        default:
+            return false
+        }
+    }
+}
+
 // MARK: Deprecations
 
 public extension RawJSON {
@@ -68,4 +198,3 @@ public extension RawJSON {
         return .dictionary(content)
     }
 }
-
