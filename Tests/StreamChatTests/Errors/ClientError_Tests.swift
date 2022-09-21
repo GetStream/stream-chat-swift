ff52de7fb541e8ac239de4db4f2a8b6c6e0abf20
Pol Quintana <pol.quintana1@gmail.com>
2022-09-21 18:44:52 +0400

    Add tests for Ephemeral Errors

--- Tests/StreamChatTests/Errors/ClientError_Tests.swift
+++ Tests/StreamChatTests/Errors/ClientError_Tests.swift
@@ -64,4 +64,49 @@ final class ClientError_Tests: XCTestCase {
         // Assert `isBouncedMessageError` returns false
         XCTAssertFalse(error.isBouncedMessageError)
     }
+
+    func test_rateLimitError_isEphemeralError() {
+        let errorPayload = ErrorPayload(
+            code: 9,
+            message: .unique,
+            statusCode: 429
+        )
+
+        let error = ClientError(with: errorPayload)
+
+        // Assert `isRateLimitError` returns true
+        XCTAssertTrue(error.isRateLimitError)
+        XCTAssertTrue(ClientError.isEphemeral(error: error))
+    }
+
+    func test_temporaryErrors_areEphemeralError() {
+        [
+            NSURLErrorCancelled,
+            NSURLErrorNetworkConnectionLost,
+            NSURLErrorTimedOut,
+            NSURLErrorCannotFindHost,
+            NSURLErrorCannotConnectToHost,
+            NSURLErrorNetworkConnectionLost,
+            NSURLErrorDNSLookupFailed,
+            NSURLErrorNotConnectedToInternet,
+            NSURLErrorBadServerResponse,
+            NSURLErrorUserCancelledAuthentication,
+            NSURLErrorCannotLoadFromNetwork,
+            NSURLErrorDataNotAllowed
+        ].forEach {
+            let error = NSError(domain: NSURLErrorDomain, code: $0)
+            XCTAssertTrue(ClientError.isEphemeral(error: error))
+        }
+    }
+
+    func test_otherNSURLErrors_areNotEphemeralError() {
+        [
+            NSURLErrorUnknown,
+            NSURLErrorUnsupportedURL,
+            NSURLErrorCannotParseResponse
+        ].forEach {
+            let error = NSError(domain: NSURLErrorDomain, code: $0)
+            XCTAssertFalse(ClientError.isEphemeral(error: error))
+        }
+    }
 }
