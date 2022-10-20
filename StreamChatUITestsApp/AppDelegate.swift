5efd1c2da50fe462310699acbe1ec8f95e6e9a3d
Alexey Alter-Pesotskiy <a.alterpesotskiy@yahoo.com>
2022-10-20 11:11:34 +0100

    Test flaky push notification tests

--- StreamChatUITestsApp/AppDelegate.swift
+++ StreamChatUITestsApp/AppDelegate.swift
@@ -21,7 +21,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
 
-        chat.logOut()
         disableAnimations()
         setupUI()
         registerForPushNotifications()
