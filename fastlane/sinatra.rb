d31918854bfe9695dab6e095793adcddeb60ebf4
Alexey Alter-Pesotskiy <a.alterpesotskiy@mail.ru>
2022-11-09 13:27:05 +0000

    [CIS-2245] Implement Fastlane Stream Actions (#2367)

--- fastlane/sinatra.rb
+++ fastlane/sinatra.rb
@@ -3,7 +3,7 @@ require 'fileutils'
 
 post '/push/:udid/:bundle_id' do
   push_data_file = 'push_payload.json'
-  File.open(push_data_file, 'w') { |f| f.write(request.body.read) }
+  File.write(push_data_file, request.body.read)
   puts `xcrun simctl push #{params['udid']} #{params['bundle_id']} #{push_data_file}`
 end
 
