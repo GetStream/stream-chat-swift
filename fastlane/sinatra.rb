af0b3a81d26cc22ff17c4fade11a2cb6eae70e0e
Alexey Alter-Pesotskiy <a.alterpesotskiy@yahoo.com>
2022-12-12 14:42:10 +0000

    Replace status with halt for better logs

--- fastlane/sinatra.rb
+++ fastlane/sinatra.rb
@@ -54,12 +54,12 @@ end
 
 post '/jwt/revoke/:udid' do
   jwt[:expiration_timeout] = install_jwt_timeout(udid: params['udid'], duration: params['duration'])
-  status 200
+  halt(200)
 end
 
 post '/jwt/break/:udid' do
   jwt[:generation_error_timeout] = install_jwt_timeout(udid: params['udid'], duration: params['duration'])
-  status 200
+  halt(200)
 end
 
 def install_jwt_timeout(udid:, duration:)
