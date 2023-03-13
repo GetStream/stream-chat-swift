ca0d8a76c25ad63f9bc03f65bdaffb46e07e4733
Nuno Vieira <nuno.fcvieira93@gmail.com>
2023-03-13 15:32:07 +0000

    Update channel list query doc in other init

--- Sources/StreamChat/Controllers/ChannelController/ChatClient+ChannelController.swift
+++ Sources/StreamChat/Controllers/ChannelController/ChatClient+ChannelController.swift
@@ -11,7 +11,7 @@ public extension ChatClient {
     ///
     /// - Parameters:
     ///   - cid: The id of the channel this controller represents.
-    ///   - channelListQuery: The channel list query the channel this controller represents is part of.
+    ///   - channelListQuery: The channel list query this controller is part of.
     ///   - messageOrdering: Describes the ordering the messages are presented.
     ///
     /// - Returns: A new instance of `ChatChannelController`.
