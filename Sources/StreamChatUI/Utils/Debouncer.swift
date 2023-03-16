1e2f4c4aa4d5c2c4a1522b01d662d8172dce91b4
Ilias Pavlidakis <ilias.pavlidakis@getstream.io>
2023-03-16 17:29:15 +0200

    Fix typo

--- Sources/StreamChatUI/Utils/Debouncer.swift
+++ Sources/StreamChatUI/Utils/Debouncer.swift
@@ -21,7 +21,7 @@ open class Debouncer: Debouncing {
     // The minimum amount of time that must elapse between calls to debounce.
     private var interval: TimeInterval
 
-    // The timer used to schwedule the handler closure.
+    // The timer used to schedule the handler closure.
     private var timer: Timer?
 
     public init(
