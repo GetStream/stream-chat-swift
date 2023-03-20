88120551746877f77a29397633b4f667d70066fb
Ilias Pavlidakis <ilias.pavlidakis@getstream.io>
2023-03-20 17:24:20 +0200

    [Async Messages]Implementation of Audio supporting components (#2528)

---
+++ Sources/StreamChat/AudioPlayer/AudioPlaybackRate.swift
@@ -0,0 +1,27 @@
+//
+// Copyright © 2023 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+
+/// Defines a struct that describes the audio playback rate with a raw value of Float
+public struct AudioPlaybackRate: Comparable, Equatable {
+    public let rawValue: Float
+
+    public init(rawValue: Float) {
+        self.rawValue = rawValue
+    }
+
+    public static func < (
+        lhs: AudioPlaybackRate,
+        rhs: AudioPlaybackRate
+    ) -> Bool {
+        lhs.rawValue < rhs.rawValue
+    }
+
+    /// SDK available playback rates
+    public static let zero = AudioPlaybackRate(rawValue: 0)
+    public static let half = AudioPlaybackRate(rawValue: 0.5)
+    public static let normal = AudioPlaybackRate(rawValue: 1)
+    public static let double = AudioPlaybackRate(rawValue: 2)
+}
