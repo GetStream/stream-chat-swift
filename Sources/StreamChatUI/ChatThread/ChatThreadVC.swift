1bf3797d592551853adbf9a5fcc99d46897c2ca0
Nuno Vieira <nuno.fcvieira93@gmail.com>
2023-03-21 16:38:47 +0000

    Remove forgotten `ThreadVC.appMovedToForeground()`

--- Sources/StreamChatUI/ChatThread/ChatThreadVC.swift
+++ Sources/StreamChatUI/ChatThread/ChatThreadVC.swift
@@ -59,14 +59,6 @@ open class ChatThreadVC: _ViewController,
     override open func setUp() {
         super.setUp()
 
-        let notificationCenter = NotificationCenter.default
-        notificationCenter.addObserver(
-            self,
-            selector: #selector(appMovedToForeground),
-            name: UIApplication.willEnterForegroundNotification,
-            object: nil
-        )
-
         messageListVC.delegate = self
         messageListVC.dataSource = self
         messageListVC.client = client
@@ -339,13 +331,6 @@ open class ChatThreadVC: _ViewController,
         default:
             break
         }
-    }
-
-    // When app becomes active, and channel is open, recreate the database observers and reload
-    // the data source so that any missed database updates from the NotificationService are refreshed.
-    @objc func appMovedToForeground() {
-        messageController.delegate = self
-        messageListVC.dataSource = self
     }
 
     private func updateMessages(with changes: [ListChange<ChatMessage>]) {
