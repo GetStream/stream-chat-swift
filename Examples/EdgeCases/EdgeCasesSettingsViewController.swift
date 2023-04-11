c784da6b251cc5b8da92a6d3ebbdf75a2f881c94
Pol Quintana <pol.quintana1@gmail.com>
2023-04-11 13:47:49 +0200

    Edge cases app (#2560)
    * Add EdgeCases app base
    
    * Simulate 401s in EdgeCases app

---
+++ Examples/EdgeCases/EdgeCasesSettingsViewController.swift
@@ -0,0 +1,56 @@
+//
+// Copyright © 2023 Stream.io Inc. All rights reserved.
+//
+
+import UIKit
+
+class EdgeCasesSettingsViewController: UITableViewController {
+    let coordinator: EdgeCasesCoordinator
+    let cases = CaseToCover.allCases
+
+    var selectedCases: [CaseToCover] {
+        coordinator.cases
+    }
+
+    init(coordinator: EdgeCasesCoordinator) {
+        self.coordinator = coordinator
+        super.init(style: .grouped)
+    }
+
+    // swiftlint:disable unavailable_function
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // swiftlint:enable unavailable_function
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        cases.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellCase = cases[indexPath.row]
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
+        cell.textLabel?.text = cellCase.title
+        cell.detailTextLabel?.text = cellCase.subtitle
+
+        let switchButton = SwitchButton()
+        switchButton.isOn = selectedCases.contains(cellCase)
+        switchButton.didChangeValue = { [weak self] in
+            self?.updateValue(for: cellCase, enable: $0)
+        }
+        cell.accessoryView = switchButton
+
+        return cell
+    }
+
+    private func updateValue(for updatedCase: CaseToCover, enable: Bool) {
+        if enable {
+            coordinator.cases.append(updatedCase)
+        } else {
+            guard let index = coordinator.cases.firstIndex(of: updatedCase) else { return }
+            coordinator.cases.remove(at: index)
+        }
+    }
+}
