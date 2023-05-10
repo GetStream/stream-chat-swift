7f405086a03b61c0d1ba062cc632d4a8d10b36f0
Nuno Vieira <nuno.fcvieira93@gmail.com>
2023-05-10 16:41:38 +0100

    Remove unmaintained ChatSampleApp (#2606)

--- hooks/pre-commit.sh
+++ hooks/pre-commit.sh
@@ -4,7 +4,6 @@ vale docusaurus
 
 ./hooks/git-format-staged --formatter 'mint run swiftformat --config .swiftformat stdin' 'Sources/*.swift' '!*Generated*' '!*StreamSwiftyMarkdown*' '!*StreamDifferenceKit*' '!*StreamNuke*' '!*StreamSwiftyGif*'
 ./hooks/git-format-staged --formatter 'mint run swiftformat --config .swiftformat stdin' 'Tests/*.swift'
-./hooks/git-format-staged --formatter 'mint run swiftformat --config .swiftformat stdin' 'StreamChatSample/*.swift'
 ./hooks/git-format-staged --formatter 'mint run swiftformat --config .swiftformat stdin' 'DemoApp/*.swift'
 ./hooks/git-format-staged --formatter 'mint run swiftformat --config .swiftformat stdin' 'Examples/*.swift'
 ./hooks/git-format-staged --formatter 'mint run swiftformat --config .swiftformat stdin' 'Integration/*.swift'
