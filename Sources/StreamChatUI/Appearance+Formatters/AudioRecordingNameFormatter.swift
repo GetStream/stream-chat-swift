a48bc1cf9b0dc12bb7a0d9b63ef6544928add132
Ilias Pavlidakis <ilias.pavlidakis@getstream.io>
2023-05-10 13:17:57 +0300

    [Async Messages]Audio Formatters (#2599)

---
+++ Sources/StreamChatUI/Appearance+Formatters/AudioRecordingNameFormatter.swift
@@ -0,0 +1,25 @@
+//
+// Copyright © 2023 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+
+/// A formatter that provides the name for a recording in a specified list of recordings.
+public protocol AudioRecordingNameFormatter {
+    func title(
+        forItemAtURL location: URL,
+        index: Int
+    ) -> String
+}
+
+/// The default video recording name formatter.
+open class DefaultAudioRecordingNameFormatter: AudioRecordingNameFormatter {
+    public init() {}
+
+    open func title(
+        forItemAtURL location: URL,
+        index: Int
+    ) -> String {
+        L10n.Recording.Presentation.name(index)
+    }
+}
