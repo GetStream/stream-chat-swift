0e800598b81e14805b3b1f91d2cb3cfa0be1e1a2
Ilias Pavlidakis <ilias.pavlidakis@getstream.io>
2023-05-15 17:10:00 +0300

    [Async Messages]VoiceRecordingVC
    commit-id:840d1648
    
    VoiceRecordingVC

---
+++ Sources/StreamChatUI/VoiceRecording/Views/RecordingIndicatorView.swift
@@ -0,0 +1,56 @@
+//
+// Copyright © 2023 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+/// A component that presents the active recording time during a recording flow.
+open class RecordingIndicatorView: _View, ThemeProvider {
+    public var content: TimeInterval = 0 {
+        didSet { updateContentIfNeeded() }
+    }
+
+    // MARK: - UI Components
+
+    /// The main container where all components will be added into.
+    open private(set) lazy var container: UIStackView = .init()
+        .withoutAutoresizingMaskConstraints
+
+    /// The imageView that shows by default a mic image, to indicate to the user we are currently
+    /// recording audio.
+    open private(set) lazy var recordingIndicator: UIImageView = .init()
+        .withoutAutoresizingMaskConstraints
+
+    /// The label that shows the duration of the active recording.
+    open private(set) lazy var durationLabel: UILabel = UILabel()
+        .withBidirectionalLanguagesSupport
+        .withoutAutoresizingMaskConstraints
+
+    // MARK: - Lifecycle
+
+    override open func setUpLayout() {
+        super.setUpLayout()
+        recordingIndicator.pin(anchors: [.width], to: 35)
+        recordingIndicator.pin(anchors: [.height], to: 40)
+
+        container.axis = .horizontal
+        container.spacing = 5
+        container.addArrangedSubview(recordingIndicator)
+        container.addArrangedSubview(durationLabel)
+
+        embed(container, insets: .zero)
+    }
+
+    override open func setUpAppearance() {
+        super.setUpAppearance()
+        recordingIndicator.contentMode = .center
+        recordingIndicator.image = appearance.images.mic.tinted(with: appearance.colorPalette.alert)
+        durationLabel.textColor = appearance.colorPalette.textLowEmphasis
+        durationLabel.font = .monospacedDigitSystemFont(ofSize: appearance.fonts.footnote.pointSize, weight: .medium)
+    }
+
+    override open func updateContent() {
+        durationLabel.text = appearance.formatters.videoDuration.format(content)
+    }
+}
---
+++ Tests/StreamChatUITests/SnapshotTests/VoiceRecording/Components/AudioQueuePlayerNextItemProvider_Tests.swift
@@ -0,0 +1,161 @@
+//
+// Copyright © 2023 Stream.io Inc. All rights reserved.
+//
+
+@testable import StreamChat
+@testable import StreamChatTestHelpers
+@testable import StreamChatUI
+import XCTest
+
+final class AudioQueuePlayerNextItemProvider_Tests: XCTestCase {
+    private lazy var userA: ChatUser! = .mock(id: .unique)
+    private lazy var userB: ChatUser! = .mock(id: .unique)
+    private lazy var userC: ChatUser! = .mock(id: .unique)
+    private lazy var encoder: JSONEncoder! = .init()
+    private lazy var subject: AudioQueuePlayerNextItemProvider! = .init()
+    private lazy var messages: [ChatMessage]! = [
+        .mock(author: userA),
+        .mock(author: userA),
+        .mock(author: userB),
+        .mock(author: userA),
+        .mock(author: userC, isSentByCurrentUser: true),
+        .mock(
+            author: userB,
+            attachments: [
+                makeAttachment(of: .voiceRecording)
+            ]
+        ),
+        .mock(
+            author: userB,
+            attachments: [
+                makeAttachment(of: .voiceRecording),
+                makeAttachment(of: .image),
+                makeAttachment(of: .voiceRecording),
+                makeAttachment(of: .file)
+            ]
+        ),
+        .mock(author: userC, isSentByCurrentUser: true),
+        .mock(author: userA),
+        .mock(author: userC, isSentByCurrentUser: true),
+        .mock(author: userB)
+    ]
+
+    private var messageFromUserBWithMoreThanOneVoiceRecording: ChatMessage { messages[6] }
+    private var messageFromUserBWithOneVoiceRecording: ChatMessage { messages[5] }
+
+    // MARK: - Lifecycle
+
+    override func tearDown() {
+        subject = nil
+        messages = nil
+        encoder = nil
+        userC = nil
+        userB = nil
+        userA = nil
+        super.tearDown()
+    }
+
+    // MARK: - findNextItem(in:currentVoiceRecordingURL:lookUpScope)
+
+    func test_findNextItem_messagesIsEmpty_returnsNil() {
+        XCTAssertNil(
+            subject.findNextItem(
+                in: [],
+                currentVoiceRecordingURL: .unique(),
+                lookUpScope: .sameMessage
+            )
+        )
+    }
+
+    func test_findNextItem_currentVoiceRecordingURLIsNil_returnsNil() {
+        XCTAssertNil(
+            subject.findNextItem(
+                in: messages,
+                currentVoiceRecordingURL: .unique(),
+                lookUpScope: .sameMessage
+            )
+        )
+    }
+
+    func test_findNextItem_lookUpScopeIsSameMessage_currentMessageContainsNextVoiceRecording_returnsExpectedValue() throws {
+        let currentVoiceRecordingURL = try XCTUnwrap(messageFromUserBWithMoreThanOneVoiceRecording.voiceRecordingAttachments.first?.voiceRecordingURL)
+        let expected = try XCTUnwrap(messageFromUserBWithMoreThanOneVoiceRecording.voiceRecordingAttachments.last?.voiceRecordingURL)
+
+        let actual = subject.findNextItem(
+            in: messages,
+            currentVoiceRecordingURL: currentVoiceRecordingURL,
+            lookUpScope: .sameMessage
+        )
+
+        XCTAssertEqual(expected, actual)
+    }
+
+    func test_findNextItem_lookUpScopeIsSameMessage_currentMessageDoesNotContainNextVoiceRecording_returnsNil() throws {
+        let currentVoiceRecordingURL = try XCTUnwrap(messageFromUserBWithMoreThanOneVoiceRecording.voiceRecordingAttachments.last?.voiceRecordingURL)
+
+        let actual = subject.findNextItem(
+            in: messages,
+            currentVoiceRecordingURL: currentVoiceRecordingURL,
+            lookUpScope: .sameMessage
+        )
+
+        XCTAssertNil(actual)
+    }
+
+    func test_findNextItem_lookUpScopeIsSubsequentMessagesFromUser_currentMessageContainsNextVoiceRecording_returnsExpectedValue() throws {
+        let currentVoiceRecordingURL = try XCTUnwrap(messageFromUserBWithMoreThanOneVoiceRecording.voiceRecordingAttachments.first?.voiceRecordingURL)
+        let expected = try XCTUnwrap(messageFromUserBWithMoreThanOneVoiceRecording.voiceRecordingAttachments.last?.voiceRecordingURL)
+
+        let actual = subject.findNextItem(
+            in: messages,
+            currentVoiceRecordingURL: currentVoiceRecordingURL,
+            lookUpScope: .subsequentMessagesFromUser
+        )
+
+        XCTAssertEqual(expected, actual)
+    }
+
+    func test_findNextItem_lookUpScopeIsSubsequentMessagesFromUser_currentMessageDoesNotContainNextVoiceRecording_returnsExpectedValue() throws {
+        let currentVoiceRecordingURL = try XCTUnwrap(messageFromUserBWithMoreThanOneVoiceRecording.voiceRecordingAttachments.last?.voiceRecordingURL)
+        let expected = try XCTUnwrap(messageFromUserBWithOneVoiceRecording.voiceRecordingAttachments.first?.voiceRecordingURL)
+
+        let actual = subject.findNextItem(
+            in: messages,
+            currentVoiceRecordingURL: currentVoiceRecordingURL,
+            lookUpScope: .subsequentMessagesFromUser
+        )
+
+        XCTAssertEqual(expected, actual)
+    }
+
+    func test_findNextItem_lookUpScopeIsSubsequentMessagesFromUser_currentMessageDoesNotContainNextVoiceRecordingAndNoValidSubsequentMessageDoes_returnsNil() throws {
+        let currentVoiceRecordingURL = try XCTUnwrap(messageFromUserBWithOneVoiceRecording.voiceRecordingAttachments.first?.voiceRecordingURL)
+
+        let actual = subject.findNextItem(
+            in: messages,
+            currentVoiceRecordingURL: currentVoiceRecordingURL,
+            lookUpScope: .subsequentMessagesFromUser
+        )
+
+        XCTAssertNil(actual)
+    }
+
+    // MARK: - Private Helpers
+
+    private func makeAttachment(of type: AttachmentType) -> AnyChatMessageAttachment {
+        let payload = VoiceRecordingAttachmentPayload(
+            title: .unique,
+            voiceRecordingRemoteURL: .unique(),
+            file: .init(type: .aac, size: 120, mimeType: nil),
+            duration: nil,
+            waveformData: nil,
+            extraData: nil
+        )
+        return AnyChatMessageAttachment(
+            id: .init(cid: .unique, messageId: .unique, index: 0),
+            type: type,
+            payload: try! encoder.encode(payload),
+            uploadingState: nil
+        )
+    }
+}
