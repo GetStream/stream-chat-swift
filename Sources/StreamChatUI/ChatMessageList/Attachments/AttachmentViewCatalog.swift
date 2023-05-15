a118686b0ac6193e6f9e2ca2c722a72b73dbba40
Ilias Pavlidakis <ilias.pavlidakis@getstream.io>
2023-05-15 20:32:12 +0300

    [Async Message]ChatMessageList & Composer
    commit-id:59699fd7
    
    ComposerVC

--- Sources/StreamChatUI/ChatMessageList/Attachments/AttachmentViewCatalog.swift
+++ Sources/StreamChatUI/ChatMessageList/Attachments/AttachmentViewCatalog.swift
@@ -20,17 +20,23 @@ open class AttachmentViewCatalog {
         let attachmentCounts = message.attachmentCounts
 
         if attachmentCounts.keys.contains(.image) || attachmentCounts.keys.contains(.video) {
-            if attachmentCounts.keys.contains(.file) {
+            if attachmentCounts.keys.contains(.file) || attachmentCounts.keys.contains(.voiceRecording) {
                 return components.mixedAttachmentInjector
             } else {
                 return components.galleryAttachmentInjector
             }
+        } else if attachmentCounts.keys.contains(.voiceRecording), attachmentCounts.count > 1 { // Contains voiceRecording and any other type
+            return components.mixedAttachmentInjector
         } else if attachmentCounts.keys.contains(.giphy) {
             return components.giphyAttachmentInjector
         } else if attachmentCounts.keys.contains(.file) {
             return components.filesAttachmentInjector
         } else if attachmentCounts.keys.contains(.linkPreview) {
             return components.linkAttachmentInjector
+        } else if attachmentCounts.keys.contains(.voiceRecording) {
+            return components.isVoiceRecordingEnabled
+                ? components.voiceRecordingAttachmentInjector
+                : components.filesAttachmentInjector
         } else {
             return nil
         }
