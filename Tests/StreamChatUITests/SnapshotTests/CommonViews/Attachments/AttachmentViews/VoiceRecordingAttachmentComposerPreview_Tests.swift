b2853f8483b1188a4674a998e3269479e90002a0
Ilias Pavlidakis <ilias.pavlidakis@getstream.io>
2023-05-15 20:20:57 +0300

    [Async Message]Preview views
    commit-id:76f4c714
    
    Previews

---
+++ Tests/StreamChatUITests/SnapshotTests/CommonViews/Attachments/AttachmentViews/VoiceRecordingAttachmentComposerPreview_Tests.swift
@@ -0,0 +1,42 @@
+//
+// Copyright © 2023 Stream.io Inc. All rights reserved.
+//
+
+import StreamChat
+import StreamChatTestTools
+@testable import StreamChatUI
+import XCTest
+
+final class VoiceRecordingAttachmentComposerPreview_Tests: XCTestCase {
+    private var subject: VoiceRecordingAttachmentComposerPreview! = .init().withoutAutoresizingMaskConstraints
+
+    // MARK: - Lifecycle
+
+    override func tearDown() {
+        subject = nil
+        super.tearDown()
+    }
+
+    // MARK: - audioPlayer
+
+    func test_audioPlayer_didSet_subscribesOnAudioPlayerForUpdates() {
+        let mockPlayer = MockAudioPlayer()
+
+        subject.audioPlayer = mockPlayer
+
+        XCTAssertTrue(mockPlayer.subscribeWasCalledWithSubscriber === subject)
+    }
+
+    // MARK: - Snapshot
+
+    func test_appearance_wasConfiguredAsExpected() {
+        subject.content = .init(
+            title: "Recording",
+            size: 120,
+            duration: 59,
+            audioAssetURL: .unique()
+        )
+
+        AssertSnapshot(subject)
+    }
+}
