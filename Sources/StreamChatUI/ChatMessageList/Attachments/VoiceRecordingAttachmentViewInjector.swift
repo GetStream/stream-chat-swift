8884ceb79229ff9e3019dfff8b4c632a78ab606c
Ilias Pavlidakis <ilias.pavlidakis@getstream.io>
2023-05-15 16:20:08 +0300

    [Async Messages]VoiceRecordingAttachmentInjector + ItemView
    commit-id:a3bdac90
    
    LLC Changes
    
    Presenter+ItemView

---
+++ Sources/StreamChatUI/ChatMessageList/Attachments/VoiceRecordingAttachmentViewInjector.swift
@@ -0,0 +1,58 @@
+//
+// Copyright © 2023 Stream.io Inc. All rights reserved.
+//
+
+import StreamChat
+import UIKit
+
+/// The delegate that will be assigned on an AudioView and will be responsible to handle user interactions
+/// from the view.
+public protocol VoiceRecordingAttachmentPresentationViewDelegate: ChatMessageContentViewDelegate {
+    /// Called when the user taps on the play button.
+    func voiceRecordingAttachmentPresentationViewConnect(
+        delegate: AudioPlayingDelegate
+    )
+
+    /// Called when the user taps on the play button.
+    func voiceRecordingAttachmentPresentationViewBeginPayback(
+        _ attachment: ChatMessageVoiceRecordingAttachment
+    )
+
+    /// Called when the user taps on the pause button.
+    func voiceRecordingAttachmentPresentationViewPausePayback()
+
+    /// Called when the user taps on the playback rate button.
+    func voiceRecordingAttachmentPresentationViewUpdatePlaybackRate(
+        _ audioPlaybackRate: AudioPlaybackRate
+    )
+
+    /// Called when the user scrubs the progress view.
+    func voiceRecordingAttachmentPresentationViewSeek(to timeInterval: TimeInterval)
+}
+
+public class VoiceRecordingAttachmentViewInjector: AttachmentViewInjector {
+    open lazy var voiceRecordingAttachmentView: ChatMessageVoiceRecordingAttachmentListView = {
+        let attachmentListView = contentView
+            .components
+            .voiceRecordingAttachmentListView
+            .init()
+
+        attachmentListView.playbackDelegate = contentView.delegate as? VoiceRecordingAttachmentPresentationViewDelegate
+
+        return attachmentListView.withoutAutoresizingMaskConstraints
+    }()
+
+    override open func contentViewDidLayout(options: ChatMessageLayoutOptions) {
+        contentView.bubbleContentContainer.insertArrangedSubview(voiceRecordingAttachmentView, at: 0, respectsLayoutMargins: false)
+    }
+
+    override open func contentViewDidUpdateContent() {
+        voiceRecordingAttachmentView.content = voiceRecordingAttachments
+    }
+}
+
+private extension VoiceRecordingAttachmentViewInjector {
+    var voiceRecordingAttachments: [ChatMessageVoiceRecordingAttachment] {
+        contentView.content?.voiceRecordingAttachments ?? []
+    }
+}
---
+++ Tests/StreamChatUITests/SnapshotTests/ChatMessageList/Attachments/VoiceRecordingAttachmentViewInjector_Tests.swift
@@ -0,0 +1,64 @@
+//
+// Copyright © 2023 Stream.io Inc. All rights reserved.
+//
+
+import StreamChat
+@testable import StreamChatTestTools
+@testable import StreamChatUI
+import UIKit
+import XCTest
+
+final class VoiceRecordingAttachmentViewInjector_Tests: XCTestCase {
+    private var contentView: ChatMessageContentView! = .init()
+    private lazy var subject: VoiceRecordingAttachmentViewInjector! = .init(contentView)
+
+    // MARK: - Lifecycle
+
+    override func tearDown() {
+        subject = nil
+        contentView = nil
+        super.tearDown()
+    }
+
+    // MARK: - voiceRecordingAttachmentView
+
+    func test_voiceRecordingAttachmentView_setsPlaybackDelegateAsExpected() {
+        let playbackDelegate = MockVoiceRecordingAttachmentPresentationViewDelegate()
+        subject.contentView.delegate = playbackDelegate
+
+        let voiceRecordingAttachmentView = subject.voiceRecordingAttachmentView
+
+        XCTAssertTrue(voiceRecordingAttachmentView.playbackDelegate === playbackDelegate)
+    }
+
+    // MARK: - contentViewDidLayout
+
+    func test_contentViewDidLayout_insertsViewAtExpectedPosition() {
+        subject.contentViewDidLayout(options: [])
+
+        XCTAssertTrue(contentView.bubbleContentContainer.subviews.first === subject.voiceRecordingAttachmentView)
+    }
+
+    // MARK: - contentViewDidUpdateContent
+
+    func test_contentViewDidUpdateContent_contentWasPassedDownToAttachmentView() throws {
+        let payload = VoiceRecordingAttachmentPayload(
+            title: nil,
+            voiceRecordingRemoteURL: .unique(),
+            file: .init(type: .generic, size: 120, mimeType: nil),
+            duration: nil,
+            waveformData: nil,
+            extraData: nil
+        )
+        subject.contentView.content = .mock(attachments: [
+            .dummy(),
+            .dummy(),
+            .dummy(),
+            .dummy(type: .voiceRecording, payload: try JSONEncoder().encode(payload))
+        ])
+
+        subject.contentViewDidUpdateContent()
+
+        XCTAssertEqual(subject.voiceRecordingAttachmentView.content.map(\.payload), [payload])
+    }
+}
