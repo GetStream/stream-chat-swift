079b7dae9ec331cb9519563fae68f5a8fbda618c
Ilias Pavlidakis <ilias.pavlidakis@getstream.io>
2023-05-15 15:59:00 +0300

    [Async Messages]MessageActionsTransitionController fix
    commit-id:718a087e

---
+++ Tests/StreamChatUITests/SnapshotTests/MessageActionsPopup/MessageActionsTransitionController_Tests.swift
@@ -0,0 +1,29 @@
+//
+// Copyright © 2023 Stream.io Inc. All rights reserved.
+//
+
+@testable import StreamChat
+@testable import StreamChatTestTools
+@testable import StreamChatUI
+import XCTest
+
+final class MessageActionsTransitionController_Tests: XCTestCase {
+    private lazy var subject: ChatMessageActionsTransitionController! = .init(messageListVC: nil)
+
+    override func tearDown() {
+        subject = nil
+        super.tearDown()
+    }
+
+    // MARK: - makeMessageContentView(fromOriginalView:)
+
+    func test_makeMessageContentView_delegateWasSetToOriginalViewDelegate() {
+        let originalView = ChatMessageContentView()
+        let delegate = ChatMessageContentViewDelegate_Mock()
+        originalView.delegate = delegate
+
+        let newMessageContentView = subject.makeMessageContentView(fromOriginalView: originalView)
+
+        XCTAssertTrue(newMessageContentView.delegate === delegate)
+    }
+}
