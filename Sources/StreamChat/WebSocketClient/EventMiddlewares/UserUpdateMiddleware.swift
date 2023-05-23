92de658ec80fe7838f1ec277c7327bdbec154add
Pol Quintana <pol.quintana1@gmail.com>
2023-05-23 14:07:10 +0200

    Create UserUpdateMiddleware to handle user updates

--- Sources/StreamChat/WebSocketClient/EventMiddlewares/UserUpdateMiddleware.swift
+++ Sources/StreamChat/WebSocketClient/EventMiddlewares/UserUpdateMiddleware.swift
@@ -3,3 +3,16 @@
 //
 
 import Foundation
+
+/// The middleware listens for `UserUpdatedEvent`s and updates `CurrentChatUserDTO` accordingly.
+struct UserUpdateMiddleware: EventMiddleware {
+    func handle(event: Event, session: DatabaseSession) -> Event? {
+        guard let userUpdatedEvent = event as? UserUpdatedEventDTO else { return event }
+        do {
+            try session.saveUser(payload: userUpdatedEvent.user)
+        } catch {
+            log.error("Failed to update user in the database, error: \(error)")
+        }
+        return event
+    }
+}
