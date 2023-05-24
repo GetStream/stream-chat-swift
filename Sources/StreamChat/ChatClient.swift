0b5f7aae0f7aba1e9cf7386c03a4a2fd33ed6a26
Pol Quintana <pol.quintana1@gmail.com>
2023-05-24 11:33:04 +0200

    Handle `user.updated` events (#2643)
    * Create User Profile view in Demo App
    
    * Create UserUpdateMiddleware to handle user updates
    
    * Remove unneeded userInfo parameter from ConnectionRepository's connect
    
    * Add tests for UserUpdatedMiddleware
    
    * Update CHANGELOG
    
    * Address PR comments
    
    * Fix leftover

--- Sources/StreamChat/ChatClient.swift
+++ Sources/StreamChat/ChatClient.swift
@@ -66,6 +66,7 @@ public class ChatClient {
             MemberEventMiddleware(),
             UserChannelBanEventsMiddleware(),
             UserWatchingEventMiddleware(),
+            UserUpdateMiddleware(),
             ChannelVisibilityEventMiddleware(),
             EventDTOConverterMiddleware()
         ]
---
+++ Sources/StreamChat/WebSocketClient/EventMiddlewares/UserUpdateMiddleware.swift
@@ -0,0 +1,18 @@
+//
+// Copyright © 2023 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+
+/// The middleware listens for `UserUpdatedEvent`s and updates the database accordingly.
+struct UserUpdateMiddleware: EventMiddleware {
+    func handle(event: Event, session: DatabaseSession) -> Event? {
+        guard let userUpdatedEvent = event as? UserUpdatedEventDTO else { return event }
+        do {
+            try session.saveUser(payload: userUpdatedEvent.user)
+        } catch {
+            log.error("Failed to update user in the database, error: \(error)")
+        }
+        return event
+    }
+}
