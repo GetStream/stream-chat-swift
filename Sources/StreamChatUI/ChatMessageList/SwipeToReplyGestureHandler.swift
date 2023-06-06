523b365d0ee65ca23d812ece671ea27b5c78010f
Nuno Vieira <nuno.fcvieira93@gmail.com>
2023-06-06 16:23:00 +0100

    Improve customisability of the SwipeToReplyGestureHandler

--- Sources/StreamChatUI/ChatMessageList/SwipeToReplyGestureHandler.swift
+++ Sources/StreamChatUI/ChatMessageList/SwipeToReplyGestureHandler.swift
@@ -14,40 +14,31 @@ open class SwipeToReplyGestureHandler {
     /// A feedback generator which will trigger whenever the threshold is surpassed.
     public private(set) var impactFeedbackGenerator: UIImpactFeedbackGenerator
 
-    /// The original message view position.
-    public private(set) var messageOriginalCenter = CGPoint()
-    /// The original reactions view position.
-    public private(set) var reactionsOriginalCenter = CGPoint()
-    /// The original reply icon position.
-    public private(set) var replyIconOriginalCenter = CGPoint()
-    /// The original thread info position.
-    public private(set) var threadInfoOriginalCenter = CGPoint()
-
     /// Whether the swipe threshold was surpassed or not. If yes, it means we should trigger a reply action.
     public private(set) var shouldReply = false
     /// A boolean to control when a feedback should be generated.
     /// It should be generated only one time, when it surpasses the threshold.
     public private(set) var shouldTriggerFeedback = true
 
-    /// The message bubble view.
-    public var messageBubbleView: ChatMessageBubbleView? {
-        messageCell?.messageContentView?.bubbleView
-    }
-
-    /// The message reactions bubble view.
-    public var reactionsBubbleView: ChatReactionBubbleBaseView? {
-        messageCell?.messageContentView?.reactionsBubbleView
-    }
-
-    /// The thread info of the message.
-    public var threadInfoContainer: ContainerStackView? {
-        messageCell?.messageContentView?.threadInfoContainer
-    }
-
     /// The reply icon view.
     public var replyIconImageView: UIImageView? {
         messageCell?.replyIconImageView
     }
+
+    /// The original reply icon position.
+    public private(set) var replyIconOriginalPosition = CGPoint()
+
+    /// The views which will animate when swiping.
+    open var swipeableViews: [UIView] {
+        [
+            messageCell?.messageContentView?.reactionsBubbleView,
+            messageCell?.messageContentView?.bubbleView,
+            messageCell?.messageContentView?.threadInfoContainer
+        ].compactMap { $0 }
+    }
+
+    /// The original message view position.
+    public private(set) var swipeableViewsOriginalPositions: [UIView: CGPoint] = [:]
 
     /// The message content.
     public var message: ChatMessage? {
@@ -61,7 +52,7 @@ open class SwipeToReplyGestureHandler {
     ///
     /// By default it is half the size of the message bubble view with a maximum of 50 points.
     open var swipeThreshold: CGFloat {
-        guard let messageAnimatableView = messageBubbleView else {
+        guard let messageAnimatableView = messageCell?.messageContentView?.bubbleView else {
             return 0
         }
         return min(50, messageAnimatableView.frame.width / 2.0)
@@ -83,10 +74,11 @@ open class SwipeToReplyGestureHandler {
         // When the gesture begins, record the original locations of the views
         if gesture.state == .began, let indexPath = listView.indexPathForRow(at: location) {
             messageCell = listView.cellForRow(at: indexPath) as? ChatMessageCell
-            messageOriginalCenter = messageBubbleView?.center ?? .zero
-            replyIconOriginalCenter = replyIconImageView?.center ?? .zero
-            reactionsOriginalCenter = reactionsBubbleView?.center ?? .zero
-            threadInfoOriginalCenter = threadInfoContainer?.center ?? .zero
+            replyIconOriginalPosition = replyIconImageView?.center ?? .zero
+            swipeableViewsOriginalPositions = [:]
+            swipeableViews.forEach {
+                swipeableViewsOriginalPositions[$0] = $0.center
+            }
         }
 
         guard let message = self.message, message.isInteractionEnabled else {
@@ -96,27 +88,7 @@ open class SwipeToReplyGestureHandler {
         // When we are swiping, move the message views and determine if it should reply
         if gesture.state == .changed {
             let translation = gesture.translation(in: messageCell)
-
-            messageBubbleView?.center = CGPoint(
-                x: max(messageOriginalCenter.x, messageOriginalCenter.x + translation.x),
-                y: messageOriginalCenter.y
-            )
-            reactionsBubbleView?.center = CGPoint(
-                x: max(reactionsOriginalCenter.x, reactionsOriginalCenter.x + translation.x),
-                y: reactionsOriginalCenter.y
-            )
-            threadInfoContainer?.center = CGPoint(
-                x: max(threadInfoOriginalCenter.x, threadInfoOriginalCenter.x + translation.x),
-                y: threadInfoOriginalCenter.y
-            )
-
-            let replyIconTranslation = max(0, min(translation.x, swipeThreshold))
-            messageCell?.replyIconImageView.center = CGPoint(
-                x: replyIconOriginalCenter.x + replyIconTranslation,
-                y: replyIconOriginalCenter.y
-            )
-            messageCell?.replyIconImageView.isHidden = false
-            messageCell?.replyIconImageView.alpha = replyIconTranslation / swipeThreshold
+            animateViews(with: translation)
 
             shouldReply = translation.x > swipeThreshold
 
@@ -132,19 +104,45 @@ open class SwipeToReplyGestureHandler {
 
         // When the gesture ends, animate back to the original position and trigger the reply if needed
         if gesture.state == .ended {
-            UIView.animate(withDuration: 0.4, animations: {
-                self.messageBubbleView?.center = self.messageOriginalCenter
-                self.reactionsBubbleView?.center = self.reactionsOriginalCenter
-                self.threadInfoContainer?.center = self.threadInfoOriginalCenter
-                self.replyIconImageView?.center = self.replyIconOriginalCenter
-                self.replyIconImageView?.alpha = 0.0
-            }, completion: { _ in
-                self.replyIconImageView?.isHidden = true
-            })
+            resetViewPositions()
 
             if shouldReply {
                 onReply?(message)
             }
+
+            swipeableViewsOriginalPositions = [:]
         }
+    }
+
+    /// Animates the views when a swiping is happening.
+    open func animateViews(with translation: CGPoint) {
+        swipeableViews.forEach {
+            guard let originalCenter = swipeableViewsOriginalPositions[$0] else { return }
+            $0.center = CGPoint(
+                x: originalCenter.x + translation.x,
+                y: originalCenter.y
+            )
+        }
+
+        let replyIconTranslation = max(0, min(translation.x, swipeThreshold))
+        messageCell?.replyIconImageView.center = CGPoint(
+            x: replyIconOriginalPosition.x + replyIconTranslation,
+            y: replyIconOriginalPosition.y
+        )
+        messageCell?.replyIconImageView.isHidden = false
+        messageCell?.replyIconImageView.alpha = replyIconTranslation / swipeThreshold
+    }
+
+    /// Animates the views to their original positions.
+    open func resetViewPositions() {
+        UIView.animate(withDuration: 0.4, animations: {
+            self.swipeableViews.forEach {
+                $0.center = self.swipeableViewsOriginalPositions[$0] ?? .zero
+            }
+            self.replyIconImageView?.center = self.replyIconOriginalPosition
+            self.replyIconImageView?.alpha = 0.0
+        }, completion: { _ in
+            self.replyIconImageView?.isHidden = true
+        })
     }
 }
