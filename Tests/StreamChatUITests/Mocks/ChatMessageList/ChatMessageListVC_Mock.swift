e350e67ba26a52d0ba2d1cfad9a8708b043b9772
Nuno Vieira <nuno.fcvieira93@gmail.com>
2023-06-23 11:31:24 +0100

    Add test coverage

--- Tests/StreamChatUITests/Mocks/ChatMessageList/ChatMessageListVC_Mock.swift
+++ Tests/StreamChatUITests/Mocks/ChatMessageList/ChatMessageListVC_Mock.swift
@@ -2,4 +2,18 @@
 // Copyright © 2023 Stream.io Inc. All rights reserved.
 //
 
-import Foundation
+@testable import StreamChat
+@testable import StreamChatUI
+import UIKit
+
+class ChatMessageListVC_Mock: ChatMessageListVC {
+    var scrollToOldestMessageCallCount = 0
+    override func scrollToOldestMessage(animated: Bool = true) {
+        scrollToOldestMessageCallCount += 1
+    }
+
+    var jumpToMessageCallCount = 0
+    override func jumpToMessage(id: MessageId, onHighlight: ((IndexPath) -> Void)? = nil) {
+        jumpToMessageCallCount += 1
+    }
+}
