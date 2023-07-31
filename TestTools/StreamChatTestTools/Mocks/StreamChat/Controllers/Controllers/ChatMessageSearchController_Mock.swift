5c01ef239898f88389ec053ad8266fee76548c13
Nuno Vieira <nuno.fcvieira93@gmail.com>
2023-07-31 22:15:06 +0100

    Add Channel and Message UI Test Coverage

--- TestTools/StreamChatTestTools/Mocks/StreamChat/Controllers/Controllers/ChatMessageSearchController_Mock.swift
+++ TestTools/StreamChatTestTools/Mocks/StreamChat/Controllers/Controllers/ChatMessageSearchController_Mock.swift
@@ -15,11 +15,29 @@ public class ChatMessageSearchController_Mock: ChatMessageSearchController {
         messages_mock ?? super.messages
     }
 
+    public var state_mock: DataController.State?
+    public override var state: DataController.State {
+        get {
+            state_mock ?? super.state
+        }
+        set {
+            state_mock = newValue
+        }
+    }
+
+    var loadNextMessagesCallCount = 0
     override public func loadNextMessages(limit: Int = 25, completion: ((Error?) -> Void)? = nil) {
+        loadNextMessagesCallCount += 1
         completion?(nil)
     }
 
+    var searchCallCount = 0
     override public func search(query: MessageSearchQuery, completion: ((Error?) -> Void)? = nil) {
+        searchCallCount += 1
         completion?(nil)
+    }
+
+    public override func search(text: String, completion: ((Error?) -> Void)? = nil) {
+        searchCallCount += 1
     }
 }
---
+++ Tests/StreamChatUITests/SnapshotTests/ChatChannelList/Search/ChatMessageSearchVC_Tests.swift
@@ -0,0 +1,116 @@
+//
+// Copyright © 2023 Stream.io Inc. All rights reserved.
+//
+
+@testable import StreamChat
+@testable import StreamChatTestTools
+@testable import StreamChatUI
+import XCTest
+
+final class ChatMessageSearchVC_Tests: XCTestCase {
+    var mockedClient: ChatClient_Mock!
+    var vc: ChatMessageSearchVC!
+    var mockedMessageSearchController: ChatMessageSearchController_Mock!
+    let channelId: ChannelId = .unique
+
+    override func setUpWithError() throws {
+        super.setUp()
+
+        mockedClient = ChatClient_Mock.mock
+        try mockedClient.databaseContainer.writeSynchronously { session in
+            try session.saveChannel(payload: .dummy(
+                channel: .dummy(cid: self.channelId, name: "Star Wars"))
+            )
+        }
+        mockedMessageSearchController = ChatMessageSearchController_Mock.mock(client: mockedClient)
+
+        vc = ChatMessageSearchVC()
+        vc.messageSearchController = mockedMessageSearchController
+    }
+
+    override func tearDown() {
+        vc = nil
+        mockedMessageSearchController = nil
+
+        super.tearDown()
+    }
+
+    func test_emptyAppearance() {
+        mockedMessageSearchController.messages_mock = []
+
+        vc.currentSearchText = "Some message"
+        vc.executeLifecycleMethods()
+        vc.controller(mockedMessageSearchController, didChangeState: .remoteDataFetched)
+
+        AssertSnapshot(vc, isEmbeddedInNavigationController: true)
+    }
+
+    func test_loadingAppearance() {
+        mockedMessageSearchController.messages_mock = []
+
+        vc.currentSearchText = "Some message"
+        vc.executeLifecycleMethods()
+        vc.controller(mockedMessageSearchController, didChangeState: .initialized)
+
+        AssertSnapshot(vc, isEmbeddedInNavigationController: true)
+    }
+
+    func test_defaultAppearance() {
+        mockedMessageSearchController.messages_mock = [
+            .mock(
+                cid: channelId,
+                text: "Some message 1",
+                author: .mock(id: .unique, name: "Yoda")
+            ),
+            .mock(
+                cid: channelId,
+                text: "Some message 2",
+                author: .mock(id: .unique, name: "Vader")
+            ),
+            .mock(
+                cid: channelId,
+                text: "Some message 3",
+                author: .mock(id: .unique, name: "R2")
+            )
+        ]
+
+        vc.currentSearchText = "Some message"
+        vc.executeLifecycleMethods()
+        vc.controller(mockedMessageSearchController, didChangeState: .remoteDataFetched)
+
+        AssertSnapshot(vc, isEmbeddedInNavigationController: true)
+    }
+
+    func test_hasEmptyResults() {
+        mockedMessageSearchController.messages_mock = []
+        XCTAssertEqual(vc.hasEmptyResults, true)
+
+        mockedMessageSearchController.messages_mock = [.mock()]
+        XCTAssertEqual(vc.hasEmptyResults, false)
+    }
+
+    func test_loadSearchResults() {
+        vc.loadSearchResults(with: "Dummy")
+
+        XCTAssertEqual(mockedMessageSearchController.searchCallCount, 1)
+    }
+
+    func test_loadMoreSearchResults() {
+        vc.loadMoreSearchResults()
+
+        XCTAssertEqual(mockedMessageSearchController.loadNextMessagesCallCount, 1)
+    }
+
+    func test_collectionViewDidSelectItemAt() {
+        mockedMessageSearchController.messages_mock = [.mock(cid: channelId), .mock(cid: channelId)]
+
+        var didSelectMessageCallCount = 0
+        vc.didSelectMessage = { (_, _) in
+            didSelectMessageCallCount += 1
+        }
+
+        vc.collectionView(vc.collectionView, didSelectItemAt: .init(item: 1, section: 0))
+
+        XCTAssertEqual(didSelectMessageCallCount, 1)
+    }
+}
