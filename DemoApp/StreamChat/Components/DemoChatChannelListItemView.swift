e7dda8fbea58899ea19ac40a6b4d26fcad2a097a
Nuno Vieira <nuno.fcvieira93@gmail.com>
2023-08-08 11:47:53 +0100

    Fix pinning channel background when searching messages

--- DemoApp/StreamChat/Components/DemoChatChannelListItemView.swift
+++ DemoApp/StreamChat/Components/DemoChatChannelListItemView.swift
@@ -7,6 +7,10 @@ import UIKit
 
 final class DemoChatChannelListItemView: ChatChannelListItemView {
     override var contentBackgroundColor: UIColor {
+        /// In case it is a message search, we don't want change the bg color.
+        if content?.searchResult?.message != nil {
+            return super.contentBackgroundColor
+        }
         if AppConfig.shared.demoAppConfig.isChannelPinningEnabled && content?.channel.isPinned == true {
             return appearance.colorPalette.pinnedMessageBackground
         }
