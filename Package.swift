4e0f7e5c7b364552843ce01b358e1c3dc13d5bff
Ilias Pavlidakis <ilias.pavlidakis@getstream.io>
2023-08-28 15:54:51 +0300

    Increase TestHelpers version

--- Package.swift
+++ Package.swift
@@ -28,7 +28,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(name: "StreamChatTestHelpers", url: "https://github.com/GetStream/stream-chat-swift-test-helpers.git", .exact("0.2.5")),
+        .package(name: "StreamChatTestHelpers", url: "https://github.com/GetStream/stream-chat-swift-test-helpers.git", .exact("0.2.11")),
         .package(name: "Swifter", url: "https://github.com/httpswift/swifter", .exact("1.5.0"))
     ],
     targets: [
