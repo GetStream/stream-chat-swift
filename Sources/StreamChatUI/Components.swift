5a971395df7266659477b0864846cf71682ff615
Nuno Vieira <nuno.fcvieira93@gmail.com>
2023-09-08 11:44:27 +0100

    Update typo in Components.swift

--- Sources/StreamChatUI/Components.swift
+++ Sources/StreamChatUI/Components.swift
@@ -118,7 +118,7 @@ public struct Components {
     /// of the list  when there are few messages. By default it is `false`.
     public var shouldMessagesStartAtTheTop: Bool = false
 
-    /// Wether it should animate when opening the channel with a given message around id.
+    /// Whether it should animate when opening the channel with a given message around id.
     /// Ex: When opening a channel from a push notification with a given message id.
     public var shouldAnimateJumpToMessageWhenOpeningChannel: Bool = true
 
