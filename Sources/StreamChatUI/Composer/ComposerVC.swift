72b09aae58f877a8040f4866760613d6d11cacf5
Nuno Vieira <nuno.fcvieira93@gmail.com>
2023-09-22 16:56:34 +0100

    Remove jumping to bottom when tapping in shrink input button

--- Sources/StreamChatUI/Composer/ComposerVC.swift
+++ Sources/StreamChatUI/Composer/ComposerVC.swift
@@ -703,7 +703,6 @@ open class ComposerVC: _ViewController,
 
             // Makes sure the text view remains in the same position when it changes size.
             self.composerView.inputMessageView.textView.setTextViewHeight()
-            self.composerView.inputMessageView.textView.scrollToCaretPosition(animated: true)
         }
     }
 
