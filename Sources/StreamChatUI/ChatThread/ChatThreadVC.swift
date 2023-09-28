10707a0abb1115848232104f8088365f5e78deab
Nuno Vieira <nuno.fcvieira93@gmail.com>
2023-09-28 16:37:16 +0100

    Fix skipping messages logic + performance improvements (#2809)
    * Fix skipping messages logic + performance improvements
    
    * Improve efficiency of threads
    
    * PR Feedback

--- Sources/StreamChatUI/ChatThread/ChatThreadVC.swift
+++ Sources/StreamChatUI/ChatThread/ChatThreadVC.swift
@@ -126,7 +126,7 @@ open class ChatThreadVC: _ViewController,
         }
 
         // Set the initial data
-        messages = getReplies(from: messageController)
+        messages = Array(getReplies(from: messageController))
 
         if messageController.message != nil {
             didFinishSynchronizing(with: nil)
@@ -434,15 +434,17 @@ open class ChatThreadVC: _ViewController,
         messageListVC.updateMessages(with: changes)
     }
 
-    private func getReplies(from messageController: ChatMessageController) -> [ChatMessage] {
+    private func getReplies(from messageController: ChatMessageController) -> LazyCachedMapCollection<ChatMessage> {
         guard shouldRenderParentMessage else {
-            return Array(messageController.replies)
+            return messageController.replies
         }
-        var messages = Array(messageController.replies)
+        var messages = messageController.replies
         let isFirstPage = messages.count < messageController.repliesPageSize
         let shouldAddRootMessageAtTheTop = isFirstPage || messageController.hasLoadedAllPreviousReplies
         if shouldAddRootMessageAtTheTop, let threadRootMessage = messageController.message {
-            messages.append(threadRootMessage)
+            if messages.last?.id != threadRootMessage.id {
+                messages.append(threadRootMessage)
+            }
         }
         return messages
     }
