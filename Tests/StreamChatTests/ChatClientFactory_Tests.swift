4e9703a546c659017d7dc4ad959e79a2969626ce
Nuno Vieira <nuno.fcvieira93@gmail.com>
2023-10-02 19:06:43 +0100

    Add test coverage

---
+++ Tests/StreamChatTests/ChatClientFactory_Tests.swift
@@ -0,0 +1,31 @@
+//
+// Copyright © 2023 Stream.io Inc. All rights reserved.
+//
+
+@testable import StreamChat
+@testable import StreamChatTestTools
+import XCTest
+
+final class ChatClientFactory_Tests: XCTestCase {
+    func test_makeUrlSessionConfiguration_whenCustomHeadersProvided() {
+        let config = ChatClientConfig(apiKeyString: "example")
+        config.urlSessionConfiguration.httpAdditionalHeaders = ["Custom": "Example"]
+        let factory = ChatClientFactory(config: config, environment: .mock)
+
+        let urlSessionConfiguration = factory.makeUrlSessionConfiguration()
+
+        XCTAssertNotNil(urlSessionConfiguration.httpAdditionalHeaders?["Custom"])
+        XCTAssertNotNil(urlSessionConfiguration.httpAdditionalHeaders?["X-Stream-Client"])
+    }
+
+    func test_makeUrlSessionConfiguration_whenCustomHeadersCollide() {
+        let config = ChatClientConfig(apiKeyString: "example")
+        config.urlSessionConfiguration.httpAdditionalHeaders = ["X-Stream-Client": "Fake"]
+        let factory = ChatClientFactory(config: config, environment: .mock)
+
+        let urlSessionConfiguration = factory.makeUrlSessionConfiguration()
+
+        XCTAssertNotNil(urlSessionConfiguration.httpAdditionalHeaders?["X-Stream-Client"])
+        XCTAssertTrue(urlSessionConfiguration.httpAdditionalHeaders?["X-Stream-Client"] as? String != "Fake")
+    }
+}
