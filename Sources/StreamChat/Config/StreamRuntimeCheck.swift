d58599325cd577f23abf6673009ec23d6e5b4ffa
Pol Quintana <pol.quintana1@gmail.com>
2023-10-27 08:38:38 +0200

    Background Mapping - Disable CoreDataLazy (#2844)
    * Avoid lazyness when fetching _readByCount
    
    * Remove CoreDataLazy(forceLazy:)
    
    * Avoid performance degradator logic in CoreDataLazy
    
    * Enable Background Mapping for internal Stream configuration
    
    * Add extra documentation
    
    * Fix existing CoreDataLazy specs
    
    * Add tests for relationshipAsModel

--- Sources/StreamChat/Config/StreamRuntimeCheck.swift
+++ Sources/StreamChat/Config/StreamRuntimeCheck.swift
@@ -14,4 +14,23 @@ public enum StreamRuntimeCheck {
     ///
     ///  Enables background mapping of DB models
     public static var _isBackgroundMappingEnabled = false
+
+    /// For *internal use* only
+    ///
+    ///  Established the maximum depth of relationships to fetch when performing a mapping
+    ///
+    ///  Eg.
+    ///  Relationship:    Message --->  QuotedMessage --->    QuotedMessage   ---X---     NIL
+    ///  Relationship:    Channel  --->      Message         --->     QuotedMessage  ---X---     NIL
+    ///  Depth:                     0                         1                                     2                               3
+    public static var _backgroundMappingRelationshipsMaxDepth = 2
+
+    /// For *internal use* only
+    ///
+    ///  Returns true if the maximum depth of relationships to fetch when performing a mapping is not yet met
+    public static func _canFetchRelationship(currentDepth: Int) -> Bool {
+        guard _isBackgroundMappingEnabled else { return true }
+
+        return currentDepth <= _backgroundMappingRelationshipsMaxDepth
+    }
 }
