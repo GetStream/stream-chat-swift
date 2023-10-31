46bde1df9bc8498fe71f218991ed27eebd59a255
Nuno Vieira <nuno.fcvieira93@gmail.com>
2023-10-31 12:50:17 +0000

    Update Sources/StreamChatUI/Utils/ChatMessage+Extensions.swift
    Co-authored-by: Pol Quintana <pol.quintana1@gmail.com>

--- Sources/StreamChatUI/Utils/ChatMessage+Extensions.swift
+++ Sources/StreamChatUI/Utils/ChatMessage+Extensions.swift
@@ -8,11 +8,7 @@ import StreamChat
 public extension ChatMessage {
     /// A boolean value that checks if actions are available on the message (e.g. `edit`, `delete`, `resend`, etc.).
     var isInteractionEnabled: Bool {
-        if isDeleted {
-            return false
-        }
-
-        if shouldRenderAsSystemMessage {
+        if isDeleted || shouldRenderAsSystemMessage {
             return false
         }
 
