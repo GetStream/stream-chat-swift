ad6be77122e1c17582adc1766e159bc18dee16f4
Alexey Alter-Pesotskiy <a.alterpesotskiy@mail.ru>
2023-11-13 19:46:46 +0000

    Remove overrideRecording

--- Tests/StreamChatUITests/SnapshotTests/Composer/ComposerVC_Tests.swift
+++ Tests/StreamChatUITests/SnapshotTests/Composer/ComposerVC_Tests.swift
@@ -16,7 +16,6 @@ final class ComposerVC_Tests: XCTestCase {
 
     /// Static setUp() is only run once. Which is what we want in this case to preload the images.
     override class func setUp() {
-        StreamChatTestTools.overrideRecording = true
         /// Dummy snapshot to preload the TestImages.yoda.url image
         /// This was the only workaround to make sure the image always appears in the snapshots.
         let view = UIImageView(frame: .init(center: .zero, size: .init(width: 100, height: 100)))
