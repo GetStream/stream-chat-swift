0dbcec9af57a2ac4f9353af384e0c4a26ff7feaa
martinmitrevski <martinmitrevski.oh@gmail.com>
2023-11-15 13:50:30 +0100

    Testing new parsing

--- Tests/StreamChatTests/APIClient/Endpoints/Payloads/ChannelListPayload_Tests.swift
+++ Tests/StreamChatTests/APIClient/Endpoints/Payloads/ChannelListPayload_Tests.swift
@@ -24,7 +24,28 @@ final class ChannelListPayload_Tests: XCTestCase {
 
         measure {
             do {
+                let start = Date()
                 _ = try JSONDecoder.default.decode(ChannelListPayload.self, from: url)
+                let end = Date()
+                let diff = end.timeIntervalSince(start)
+                print("time: \(diff)")
+            } catch {
+                XCTFail("Failed to parse JSON: \(error)")
+            }
+        }
+    }
+    
+    func test_decode_bigChannelListPayload_V3() {
+        // 3MB JSON Channel List from Watercooler
+        let url = XCTestCase.mockData(fromJSONFile: "new_format_v3")
+
+        measure {
+            do {
+                let start = Date()
+                let data = try JSONDecoder.default.decode(QueryChannels_V3.self, from: url)
+                let end = Date()
+                let diff = end.timeIntervalSince(start)
+                print("time: \(diff)")
             } catch {
                 XCTFail("Failed to parse JSON: \(error)")
             }
@@ -371,4 +392,119 @@ final class ChannelPayload_Tests: XCTestCase {
         // THEN
         XCTAssertEqual(payload.newestMessage?.id, laterMessage.id)
     }
+}
+
+struct QueryChannels_V3: Decodable {
+    let entities: Entity_V3
+    let channels: [ChannelPayload_V3]
+    let duration: String
+}
+
+struct Entity_V3: Decodable {
+    let users: [String: User_V3]
+    let configs: [String: Config_V3]
+    let own_capabilities: [String: [String]]
+}
+
+struct User_V3: Decodable {
+    let id: String
+    let role: String
+    let created_at: TimeInterval
+    let updated_at: TimeInterval
+    let last_active: TimeInterval?
+    let banned: Bool
+    let online: Bool
+    let custom: [String: RawJSON]
+}
+
+struct Config_V3: Decodable {
+    let created_at: TimeInterval
+    let updated_at: TimeInterval
+    let name: String
+    let typing_events: Bool
+    let read_events: Bool
+    let connect_events: Bool
+    let search: Bool
+    let reactions: Bool
+    let replies: Bool
+    let quotes: Bool
+    let mutes: Bool
+    let uploads: Bool
+    let url_enrichment: Bool
+    let custom_events: Bool
+    let push_notifications: Bool
+    let reminders: Bool
+    let mark_messages_pending: Bool
+    let message_retention: String
+    let max_message_length: Int
+    let automod: String
+    let automod_behavior: String
+    let blocklist_behavior: String
+    let commands: [[String: String]]
+}
+
+struct Channel_V3: Decodable {
+    let id: String
+    let type: String
+    let cid: String
+    let last_message_at: TimeInterval
+    let created_at: TimeInterval
+    let updated_at: TimeInterval
+    let frozen: Bool
+    let disabled: Bool
+    let member_count: Int
+    let config: String
+    let own_capabilities: String
+    let hidden: Bool
+    let custom: [String: RawJSON]
+}
+
+struct Message_V3: Decodable {
+    let id: String
+    let text: String
+    let html: String
+    let type: String
+    let attachments: [MessageAttachmentPayload]
+    let latest_reactions: [MessageReactionPayload]
+    let own_reactions: [MessageReactionPayload]
+//    let reaction_counts: [MessageReactionType: Int]
+//    let reaction_scores: [MessageReactionType: Int]
+    let reply_count: Int
+    let deleted_reply_count: Int
+    let created_at: TimeInterval
+    let updated_at: TimeInterval
+    let shadowed: Bool
+    let mentioned_users: [String]
+    let silent: Bool
+    let pinned: Bool
+    let pinned_at: TimeInterval?
+    let pinned_by: [String]?
+    let pin_expires: TimeInterval?
+}
+
+struct Read_V3: Decodable {
+    let last_read: TimeInterval
+    let unread_messages: Int
+}
+
+struct Member_V3: Decodable {
+    let user_id: String
+    let status: String
+    let created_at: TimeInterval
+    let updated_at: TimeInterval
+    let banned: Bool
+    let shadow_banned: Bool
+    let role: String
+    let channel_role: String
+    let notifications_muted: Bool
+}
+
+struct ChannelPayload_V3: Decodable {
+    let channel: Channel_V3
+    let messages: [Message_V3]
+    let pinned_messages: [Message_V3]
+    let watcher_count: Int
+    let read: [Read_V3]
+    let members: [Member_V3]
+    let membership: Member_V3?
 }
