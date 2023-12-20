c4b2473453c4aa5906869794abf25c7093ac4d83
Nuno Vieira <nuno.fcvieira93@gmail.com>
2023-12-20 16:24:19 +0000

    Improve support for custom mixed attachments

--- Sources/StreamChatUI/ChatMessageList/Attachments/MixedAttachmentViewInjector.swift
+++ Sources/StreamChatUI/ChatMessageList/Attachments/MixedAttachmentViewInjector.swift
@@ -3,14 +3,35 @@
 //
 
 import Foundation
+import StreamChat
 
 /// The injector used to combine multiple types of attachment views.
 open class MixedAttachmentViewInjector: AttachmentViewInjector {
-    open lazy var injectors = [
-        contentView.components.galleryAttachmentInjector.init(contentView),
-        contentView.components.filesAttachmentInjector.init(contentView),
-        contentView.components.voiceRecordingAttachmentInjector.init(contentView)
+    /// The injectors that should support mixed attachment rendering.
+    static var injectorsRegistry: [AttachmentType: AttachmentViewInjector.Type] = [
+        .file: FilesAttachmentViewInjector.self,
+        .video: GalleryAttachmentViewInjector.self,
+        .image: GalleryAttachmentViewInjector.self,
+        .voiceRecording: VoiceRecordingAttachmentViewInjector.self
     ]
+
+    private var _injectors: [AttachmentViewInjector] {
+        contentView.content?.attachmentCounts.keys
+            .compactMap { Self.injectorsRegistry[$0] }
+            .map { $0.init(contentView) }
+            ?? []
+    }
+
+    open lazy var injectors: [AttachmentViewInjector] = _injectors
+
+    public required init(_ contentView: ChatMessageContentView) {
+        super.init(contentView)
+    }
+
+    /// Register a custom attachment injector if the attachment can be mixed with other types of attachments.
+    public static func register(type: AttachmentType, for injector: AttachmentViewInjector.Type) {
+        injectorsRegistry[type] = injector
+    }
 
     override open func contentViewDidLayout(options: ChatMessageLayoutOptions) {
         injectors.forEach { $0.contentViewDidLayout(options: options) }
