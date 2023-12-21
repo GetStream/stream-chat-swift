0aead4cb24f87b04dfee4997769b5f5d8f4b0d2e
martinmitrevski <martinmitrevski.oh@gmail.com>
2023-12-21 22:12:50 +0100

    Added sending message wrapper

--- Sources/StreamChat/State/Chat.swift
+++ Sources/StreamChat/State/Chat.swift
@@ -7,14 +7,48 @@ import Foundation
 @available(iOS 13.0, *)
 public class Chat {
     private let channelController: ChatChannelController
+    private let eventsController: EventsController
+    
+    private var pendingMessageContinuations = [MessageId: CheckedContinuation<MessageId, Error>]()
+    private var pendingPaginationContinuations = [CheckedContinuation<Void, Error>]()
         
     init(chatClient: ChatClient, channelId: ChannelId) {
         channelController = chatClient.channelController(for: channelId)
+        eventsController = chatClient.eventsController()
+        eventsController.delegate = self
         channelController.delegate = self
         channelController.synchronize()
     }
     
     @MainActor public internal(set) var state = ChatState()
+    
+    public func sendMessage(text: String) async throws -> MessageId {
+        try await withCheckedThrowingContinuation { [weak self] continuation in
+            guard let self else { return }
+            channelController.createNewMessage(text: text) { result in
+                switch result {
+                case let .success(messageId):
+                    self.pendingMessageContinuations[messageId] = continuation
+                case let .failure(error):
+                    return continuation.resume(throwing: error)
+                }
+            }
+        }
+    }
+    
+    public func loadPreviousMessages() async throws {
+        try await withCheckedThrowingContinuation { [weak self] continuation in
+            guard let self else { return }
+            channelController.loadPreviousMessages { error in
+                print("===== query updated in the db \(Date())")
+                if let error {
+                    continuation.resume(throwing: error)
+                } else {
+                    continuation.resume()
+                }
+            }
+        }
+    }
 }
 
 @available(iOS 13.0, *)
@@ -25,6 +59,18 @@ extension Chat: ChatChannelControllerDelegate {
     ) {
         Task {
             await state.update(messages: channelController.messages)
+        }
+    }
+}
+
+@available(iOS 13.0, *)
+extension Chat: EventsControllerDelegate {
+    public func eventsController(_ controller: EventsController, didReceiveEvent event: Event) {
+        if let newMessageEvent = event as? MessageNewEvent {
+            let id = newMessageEvent.message.id
+            let continuation = pendingMessageContinuations[id]
+            continuation?.resume(returning: id)
+            pendingMessageContinuations.removeValue(forKey: id)
         }
     }
 }
