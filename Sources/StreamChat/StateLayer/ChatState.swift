533a10f6b850ad4b04b6fd1cec502a0fdbe66506
Toomas Vahter <toomas.vahter@getstream.io>
2024-02-26 17:09:42 +0200

    Start observing channel updates in ChatState and add markRead and markUnread to Chat

--- Sources/StreamChat/StateLayer/ChatState.swift
+++ Sources/StreamChat/StateLayer/ChatState.swift
@@ -7,12 +7,19 @@ import Foundation
 @available(iOS 13.0, *)
 public final class ChatState: ObservableObject {
     private let cid: ChannelId
+    private var channelObserver: EntityDatabaseObserverWrapper<ChatChannel, ChannelDTO>?
     let messageOrder: MessageOrdering
     
-    init(cid: ChannelId, messageOrder: MessageOrdering) {
+    init(cid: ChannelId, database: DatabaseContainer, messageOrder: MessageOrdering) {
         self.cid = cid
         self.messageOrder = messageOrder
+        startObservingChannel(with: cid, in: database)
     }
+    
+    // MARK: Represented Channel
+    
+    // TODO: Exposing it as non-nil? Requires one DB fetch on Chat creation
+    @Published private(set) var channel: ChatChannel?
     
     // MARK: Messages
     
@@ -20,5 +27,29 @@ public final class ChatState: ObservableObject {
     
     @MainActor func setMessages(_ messages: [ChatMessage]) {
         self.messages = messages
+    }
+}
+
+// MARK: Observing the channel
+
+@available(iOS 13.0, *)
+extension ChatState {
+    private func startObservingChannel(with cid: ChannelId, in database: DatabaseContainer) {
+        channelObserver = { [weak self] in
+            let observer = EntityDatabaseObserverWrapper(
+                isBackground: true,
+                database: database,
+                fetchRequest: ChannelDTO.fetchRequest(for: cid),
+                itemCreator: { try $0.asModel() as ChatChannel }
+            ).onChange { [weak self] change in
+                self?.channel = change.item
+            }
+            return observer
+        }()
+        do {
+            try channelObserver?.startObserving()
+        } catch {
+            log.error("Failed to start the channel observer for \(cid)")
+        }
     }
 }
