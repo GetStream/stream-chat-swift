e7559311cb9791c88daf6b9021068661796f26d4
Toomas Vahter <toomas.vahter@getstream.io>
2024-02-28 16:27:31 +0200

    Add pinMessage and unpinMessage to Chat which waits until API request has finished

--- Sources/StreamChat/Workers/ChannelUpdater.swift
+++ Sources/StreamChat/Workers/ChannelUpdater.swift
@@ -894,7 +894,7 @@ extension ChannelUpdater {
 }
 
 @available(iOS 13.0, *)
-private extension CheckedContinuation where T == Void, E == Error {
+extension CheckedContinuation where T == Void, E == Error {
     func resume(with error: Error?) {
         if let error {
             resume(throwing: error)
