9f248af1bf79e8ad8f1da388948c8140618d102b
martinmitrevski <martinmitrevski.oh@gmail.com>
2024-02-29 13:15:41 +0100

    Added RequestEncoder tests

--- Tests/StreamChatTests/APIClient/RequestEncoder_Tests.swift
+++ Tests/StreamChatTests/APIClient/RequestEncoder_Tests.swift
@@ -6,7 +6,6 @@
 @testable import StreamChatTestTools
 import XCTest
 
-// TODO: rewrite these.
 final class RequestEncoder_Tests: XCTestCase {
     var encoder: RequestEncoder!
     var baseURL: URL!
@@ -253,283 +252,191 @@ final class RequestEncoder_Tests: XCTestCase {
         XCTAssertEqual(request.allHTTPHeaderFields?["Authorization"], token.rawValue)
     }
 
-//
-//    func test_encodingRequestURL() throws {
-//        let testStringValue = String.unique
-//
-//        // Prepare a request with query items
-//        let endpoint = Endpoint<Data>(
-//            path: .guest,
-//            method: .post,
-//            queryItems: ["stringValue": testStringValue],
-//            requiresConnectionId: false,
-//            requiresToken: false,
-//            body: nil
-//        )
-//
-//        // Encode the request and wait for the result
-//        let request = try waitFor { encoder.encodeRequest(for: endpoint, completion: $0) }.get()
-//
-//        // Check the URL is set up correctly
-//        XCTAssertEqual(request.httpMethod, endpoint.method.rawValue)
-//        XCTAssertEqual(request.url?.scheme, baseURL.scheme)
-//        XCTAssertEqual(request.url?.host, baseURL.host)
-//        XCTAssertEqual(request.url?.port, baseURL.port)
-//        XCTAssertEqual(request.url?.path, "/" + endpoint.path.value)
-//
-//        // Check custom query items
-//        let urlComponenets = try XCTUnwrap(URLComponents(url: request.url!, resolvingAgainstBaseURL: false))
-//        XCTAssertEqual(urlComponenets.queryItems?["stringValue"], testStringValue)
-//    }
-//
-//    func test_encodingRequestBody_POST() throws {
-//        // Prepare a POST endpoint with JSON body
-//        let endpoint = Endpoint<Data>(
-//            path: .guest,
-//            method: .post,
-//            queryItems: nil,
-//            requiresConnectionId: false,
-//            requiresToken: false,
-//            body: TestUser(name: "Luke", age: 22)
-//        )
-//
-//        // Encode the request and wait for the result
-//        let request = try waitFor { encoder.encodeRequest(for: endpoint, completion: $0) }.get()
-//
-//        // Check the body is present
-//        let body = try XCTUnwrap(request.httpBody)
-//        let serializedBody = try JSONDecoder.stream.decode(TestUser.self, from: body)
-//
-//        XCTAssertEqual(serializedBody, endpoint.body as! TestUser)
-//    }
-//
-//    func test_encodingRequestBodyAsData_POST() throws {
-//        // Prepare a POST endpoint with JSON body
-//        let bodyAsData = try JSONEncoder.stream.encode(TestUser(name: "Luke", age: 22))
-//
-//        let endpoint = Endpoint<Data>(
-//            path: .guest,
-//            method: .post,
-//            queryItems: nil,
-//            requiresConnectionId: false,
-//            requiresToken: false,
-//            body: bodyAsData
-//        )
-//
-//        // Encode the request and wait for the result
-//        let request = try waitFor { encoder.encodeRequest(for: endpoint, completion: $0) }.get()
-//
-//        // Check the body is sent as is
-//        let sentBody = try XCTUnwrap(request.httpBody)
-//        XCTAssertEqual(sentBody, bodyAsData)
-//    }
-//
-//    func test_encodingRequestWithoutBody_POST() throws {
-//        // Our backend expects all POST requests will have a body, even if empty
-//        // nil body is not acceptable (causes invalid json - 400 error)
-//
-//        // Prepare a POST endpoint with JSON body
-//        let endpoint = Endpoint<Data>(
-//            path: .guest,
-//            method: .post,
-//            queryItems: nil,
-//            requiresConnectionId: false,
-//            requiresToken: false,
-//            body: nil
-//        )
-//
-//        // Encode the request and wait for the result
-//        let request = try waitFor { encoder.encodeRequest(for: endpoint, completion: $0) }.get()
-//
-//        // Check the body is present (and empty)
-//        let body = try XCTUnwrap(request.httpBody)
-//        let serializedBody = try JSONDecoder.stream.decode(EmptyBody.self, from: body)
-//
-//        XCTAssertEqual(serializedBody, EmptyBody())
-//    }
-//
-//    func test_encodingRequestBody_PATCH() throws {
-//        // Prepare a PATCH endpoint with JSON body
-//        let endpoint = Endpoint<Data>(
-//            path: .guest,
-//            method: .patch,
-//            queryItems: nil,
-//            requiresConnectionId: false,
-//            requiresToken: false,
-//            body: TestUser(name: "Luke", age: 22)
-//        )
-//
-//        // Encode the request and wait for the result
-//        let request = try waitFor { encoder.encodeRequest(for: endpoint, completion: $0) }.get()
-//
-//        // Check the body is present
-//        let body = try XCTUnwrap(request.httpBody)
-//        let serializedBody = try JSONDecoder.stream.decode(TestUser.self, from: body)
-//
-//        XCTAssertEqual(serializedBody, endpoint.body as! TestUser)
-//    }
-//
-//    func test_encodingRequestBodyAsData_PATCH() throws {
-//        // Prepare a PATCH endpoint with JSON body
-//        let bodyAsData = try JSONEncoder.stream.encode(TestUser(name: "Luke", age: 22))
-//
-//        let endpoint = Endpoint<Data>(
-//            path: .guest,
-//            method: .patch,
-//            queryItems: nil,
-//            requiresConnectionId: false,
-//            requiresToken: false,
-//            body: bodyAsData
-//        )
-//
-//        // Encode the request and wait for the result
-//        let request = try waitFor { encoder.encodeRequest(for: endpoint, completion: $0) }.get()
-//
-//        // Check the body is present
-//        let sentBody = try XCTUnwrap(request.httpBody)
-//        XCTAssertEqual(sentBody, bodyAsData)
-//    }
-//
-//    func test_encodingRequestWithoutBody_PATCH() throws {
-//        // Prepare a PATCH endpoint without JSON body
-//        let endpoint = Endpoint<Data>(
-//            path: .guest,
-//            method: .patch,
-//            queryItems: nil,
-//            requiresConnectionId: false,
-//            requiresToken: false,
-//            body: nil
-//        )
-//
-//        // Encode the request and wait for the result
-//        let request = try waitFor { encoder.encodeRequest(for: endpoint, completion: $0) }.get()
-//
-//        // Check the body is present (and empty)
-//        let body = try XCTUnwrap(request.httpBody)
-//        let serializedBody = try JSONDecoder.stream.decode(EmptyBody.self, from: body)
-//
-//        XCTAssertEqual(serializedBody, EmptyBody())
-//    }
-//
-//    func test_encodingRequestBody_GET() throws {
-//        // Prepare a GET endpoint with JSON body
-//        let endpoint = Endpoint<Data>(
-//            path: .guest,
-//            method: .get,
-//            queryItems: nil,
-//            requiresConnectionId: false,
-//            requiresToken: false,
-//            body: [
-//                "user1": TestUser(name: "Luke", age: 22),
-//                // Test non-alphanumeric characters, too
-//                "user2": TestUser(name: "Leia is the best! + ♥️", age: 22)
-//            ]
-//        )
-//
-//        // Encode the request and wait for the result
-//        let request = try waitFor { encoder.encodeRequest(for: endpoint, completion: $0) }.get()
-//
-//        // Check both JSONs are present in the query items
-//        let urlComponents = try XCTUnwrap(URLComponents(url: request.url!, resolvingAgainstBaseURL: false))
-//
-//        let user1String = try XCTUnwrap(urlComponents.queryItems?["user1"])
-//        let user1JSON = try JSONDecoder.default.decode(TestUser.self, from: user1String.data(using: .utf8)!)
-//        XCTAssertEqual(user1JSON, TestUser(name: "Luke", age: 22))
-//
-//        let user2String = try XCTUnwrap(urlComponents.queryItems?["user2"])
-//        let user2JSON = try JSONDecoder.default.decode(TestUser.self, from: user2String.data(using: .utf8)!)
-//        XCTAssertEqual(user2JSON, TestUser(name: "Leia is the best! + ♥️", age: 22))
-//
-//        // Check the + sign is encoded properly in the query
-//        XCTAssertFalse(urlComponents.url!.query!.contains("+"))
-//        XCTAssertTrue(urlComponents.url!.query!.contains("%2B"))
-//    }
-//
-//    func test_encodingRequestBodyAsData_GET() throws {
-//        // Prepare a GET endpoint with Data body
-//        let bodyAsData = try JSONEncoder.stream.encode([
-//            "user1": TestUser(name: "Luke", age: 22),
-//            // Test non-alphanumeric characters, too
-//            "user2": TestUser(name: "Leia is the best! + ♥️", age: 22)
-//        ])
-//        let endpoint = Endpoint<Data>(
-//            path: .guest,
-//            method: .get,
-//            queryItems: nil,
-//            requiresConnectionId: false,
-//            requiresToken: false,
-//            body: bodyAsData
-//        )
-//
-//        // Encode the request and wait for the result
-//        let request = try waitFor { encoder.encodeRequest(for: endpoint, completion: $0) }.get()
-//
-//        // Check both JSONs are present in the query items
-//        let urlComponents = try XCTUnwrap(URLComponents(url: request.url!, resolvingAgainstBaseURL: false))
-//
-//        let user1String = try XCTUnwrap(urlComponents.queryItems?["user1"])
-//        let user1JSON = try JSONDecoder.default.decode(TestUser.self, from: user1String.data(using: .utf8)!)
-//        XCTAssertEqual(user1JSON, TestUser(name: "Luke", age: 22))
-//
-//        let user2String = try XCTUnwrap(urlComponents.queryItems?["user2"])
-//        let user2JSON = try JSONDecoder.default.decode(TestUser.self, from: user2String.data(using: .utf8)!)
-//        XCTAssertEqual(user2JSON, TestUser(name: "Leia is the best! + ♥️", age: 22))
-//
-//        // Check the + sign is encoded properly in the query
-//        XCTAssertFalse(urlComponents.url!.query!.contains("+"))
-//        XCTAssertTrue(urlComponents.url!.query!.contains("%2B"))
-//    }
-//
-//    func test_encodingGETRequestBody_withQueryItems() throws {
-//        // Prepare a GET endpoint with both, the query items and JSON body
-//        let endpoint = Endpoint<Data>(
-//            path: .guest,
-//            method: .get,
-//            queryItems: ["father": "Anakin"],
-//            requiresConnectionId: false,
-//            requiresToken: false,
-//            body: ["user": TestUser(name: "Luke", age: 22)]
-//        )
-//
-//        // Encode the request and wait for the result
-//        let request = try waitFor { encoder.encodeRequest(for: endpoint, completion: $0) }.get()
-//
-//        let urlComponents = try XCTUnwrap(URLComponents(url: request.url!, resolvingAgainstBaseURL: false))
-//
-//        // Check the query item
-//        XCTAssertEqual(urlComponents.queryItems?["father"], "Anakin")
-//
-//        // Check the query-item encoded body
-//        let userString = try XCTUnwrap(urlComponents.queryItems?["user"])
-//        let userJSON = try JSONDecoder.default.decode(TestUser.self, from: userString.data(using: .utf8)!)
-//        XCTAssertEqual(userJSON, TestUser(name: "Luke", age: 22))
-//    }
-//
-//    func test_encodingGETRequestBodyAsData_withQueryItemsAsData() throws {
-//        // Prepare a GET endpoint with both, the query items and body as Data
-//        let queryItemsData = try JSONEncoder.stream.encode(["father": "Anakin"])
-//        let bodyAsData = try JSONEncoder.stream.encode(["user": TestUser(name: "Luke", age: 22)])
-//
-//        let endpoint = Endpoint<Data>(
-//            path: .guest,
-//            method: .get,
-//            queryItems: queryItemsData,
-//            requiresConnectionId: false,
-//            requiresToken: false,
-//            body: bodyAsData
-//        )
-//
-//        // Encode the request and wait for the result
-//        let request = try waitFor { encoder.encodeRequest(for: endpoint, completion: $0) }.get()
-//
-//        let urlComponents = try XCTUnwrap(URLComponents(url: request.url!, resolvingAgainstBaseURL: false))
-//
-//        // Check the query item
-//        XCTAssertEqual(urlComponents.queryItems?["father"], "Anakin")
-//
-//        // Check the query-item encoded body
-//        let userString = try XCTUnwrap(urlComponents.queryItems?["user"])
-//        let userJSON = try JSONDecoder.default.decode(TestUser.self, from: userString.data(using: .utf8)!)
-//        XCTAssertEqual(userJSON, TestUser(name: "Luke", age: 22))
-//    }
+    func test_encodingRequestURL() throws {
+        let testStringValue = String.unique
+
+        // Prepare a request with query items
+        var components = URLComponents(url: .unique(), resolvingAgainstBaseURL: true)!
+        components.queryItems = [.init(name: "stringValue", value: testStringValue)]
+        var urlRequest = URLRequest(url: components.url ?? .unique())
+        urlRequest.httpMethod = "POST"
+
+        // Encode the request and wait for the result
+        let request = try waitFor {
+            encoder.encode(
+                request: urlRequest,
+                requiresConnectionId: false,
+                requiresToken: false,
+                completion: $0
+            )
+        }.get()
+
+        // Check the URL is set up correctly
+        XCTAssertEqual(request.httpMethod, urlRequest.httpMethod)
+        XCTAssertEqual(request.url?.scheme, baseURL.scheme)
+        XCTAssertEqual(request.url?.host, urlRequest.url?.host)
+        XCTAssertEqual(request.url?.port, baseURL.port)
+        XCTAssertEqual(request.url?.path, urlRequest.url!.relativePath)
+
+        // Check custom query items
+        let urlComponenets = try XCTUnwrap(URLComponents(url: request.url!, resolvingAgainstBaseURL: false))
+        XCTAssertEqual(urlComponenets.queryItems?["stringValue"], testStringValue)
+    }
+
+    func test_encodingRequestBody_POST() throws {
+        // Prepare a POST endpoint with JSON body
+        let testUser = TestUser(name: "Luke", age: 22)
+        var initialRequest = URLRequest(url: .unique())
+        initialRequest.httpBody = try JSONEncoder.default.encode(testUser)
+        initialRequest.httpMethod = "POST"
+
+        // Encode the request and wait for the result
+        let request = try waitFor {
+            encoder.encode(
+                request: initialRequest,
+                requiresConnectionId: false,
+                requiresToken: false,
+                completion: $0
+            )
+        }.get()
+
+        // Check the body is present
+        let body = try XCTUnwrap(request.httpBody)
+        let serializedBody = try JSONDecoder.stream.decode(TestUser.self, from: body)
+
+        XCTAssertEqual(serializedBody, testUser)
+    }
+
+    func test_encodingRequestBodyAsData_POST() throws {
+        // Prepare a POST endpoint with JSON body
+        let bodyAsData = try JSONEncoder.stream.encode(TestUser(name: "Luke", age: 22))
+        var initialRequest = URLRequest(url: .unique())
+        initialRequest.httpBody = bodyAsData
+        initialRequest.httpMethod = "POST"
+
+        // Encode the request and wait for the result
+        let request = try waitFor {
+            encoder.encode(
+                request: initialRequest,
+                requiresConnectionId: false,
+                requiresToken: false,
+                completion: $0
+            )
+        }.get()
+
+        // Check the body is sent as is
+        let sentBody = try XCTUnwrap(request.httpBody)
+        XCTAssertEqual(sentBody, bodyAsData)
+    }
+
+    func test_encodingRequestWithoutBody_POST() throws {
+        // Our backend expects all POST requests will have a body, even if empty
+        // nil body is not acceptable (causes invalid json - 400 error)
+
+        // Prepare a POST endpoint without JSON body
+        var initialRequest = URLRequest(url: .unique())
+        initialRequest.httpMethod = "POST"
+
+        // Encode the request and wait for the result
+        let request = try waitFor {
+            encoder.encode(
+                request: initialRequest,
+                requiresConnectionId: false,
+                requiresToken: false,
+                completion: $0
+            )
+        }.get()
+
+        // Check the body is nil
+        XCTAssertNil(request.httpBody)
+    }
+
+    func test_encodingRequestBody_PATCH() throws {
+        // Prepare a PATCH endpoint with JSON body
+        let testUser = TestUser(name: "Luke", age: 22)
+        let bodyAsData = try JSONEncoder.stream.encode(testUser)
+        var initialRequest = URLRequest(url: .unique())
+        initialRequest.httpBody = bodyAsData
+        initialRequest.httpMethod = "PATCH"
+
+        // Encode the request and wait for the result
+        let request = try waitFor {
+            encoder.encode(
+                request: initialRequest,
+                requiresConnectionId: false,
+                requiresToken: false,
+                completion: $0
+            )
+        }.get()
+
+        // Check the body is present
+        let body = try XCTUnwrap(request.httpBody)
+        let serializedBody = try JSONDecoder.stream.decode(TestUser.self, from: body)
+
+        XCTAssertEqual(serializedBody, testUser)
+    }
+
+    func test_encodingRequestBodyAsData_PATCH() throws {
+        // Prepare a PATCH endpoint with JSON body
+        let bodyAsData = try JSONEncoder.stream.encode(TestUser(name: "Luke", age: 22))
+
+        var initialRequest = URLRequest(url: .unique())
+        initialRequest.httpBody = bodyAsData
+        initialRequest.httpMethod = "PATCH"
+
+        // Encode the request and wait for the result
+        let request = try waitFor {
+            encoder.encode(
+                request: initialRequest,
+                requiresConnectionId: false,
+                requiresToken: false,
+                completion: $0
+            )
+        }.get()
+
+        // Check the body is present
+        let sentBody = try XCTUnwrap(request.httpBody)
+        XCTAssertEqual(sentBody, bodyAsData)
+    }
+
+    func test_encodingRequestWithoutBody_PATCH() throws {
+        // Prepare a PATCH endpoint without JSON body
+        var initialRequest = URLRequest(url: .unique())
+        initialRequest.httpMethod = "PATCH"
+
+        // Encode the request and wait for the result
+        let request = try waitFor {
+            encoder.encode(
+                request: initialRequest,
+                requiresConnectionId: false,
+                requiresToken: false,
+                completion: $0
+            )
+        }.get()
+
+        // Check the body is nil
+        XCTAssertNil(request.httpBody)
+    }
+
+    func test_encodingGETRequestBody_withQueryItems() throws {
+        // Prepare a GET endpoint with both, the query items and JSON body
+        var components = URLComponents(url: .unique(), resolvingAgainstBaseURL: true)!
+        components.queryItems = [.init(name: "father", value: "Anakin")]
+        let urlRequest = URLRequest(url: components.url ?? .unique())
+
+        // Encode the request and wait for the result
+        let request = try waitFor {
+            encoder.encode(
+                request: urlRequest,
+                requiresConnectionId: false,
+                requiresToken: false,
+                completion: $0
+            )
+        }.get()
+
+        let urlComponents = try XCTUnwrap(URLComponents(url: request.url!, resolvingAgainstBaseURL: false))
+
+        // Check the query item
+        XCTAssertEqual(urlComponents.queryItems?["father"], "Anakin")
+    }
 }
