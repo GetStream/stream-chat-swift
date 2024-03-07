4b17bebb061bde2465907c848ac19d8b0620e785
Toomas Vahter <toomas.vahter@getstream.io>
2024-03-07 15:22:02 +0200

    Disband helper loaders and coordinators and move them to designated updaters

--- Sources/StreamChat/Controllers/ChannelController/ChannelController.swift
+++ Sources/StreamChat/Controllers/ChannelController/ChannelController.swift
@@ -199,6 +199,7 @@ public class ChatChannelController: DataController, DelegateCallable, DataStoreP
         updater = self.environment.channelUpdaterBuilder(
             client.channelRepository,
             client.callRepository,
+            client.messageRepository,
             client.makeMessagesPaginationStateHandler(),
             client.databaseContainer,
             client.apiClient
@@ -1308,6 +1309,7 @@ extension ChatChannelController {
         var channelUpdaterBuilder: (
             _ channelRepository: ChannelRepository,
             _ callRepository: CallRepository,
+            _ messageRepository: MessageRepository,
             _ paginationStateHandler: MessagesPaginationStateHandling,
             _ database: DatabaseContainer,
             _ apiClient: APIClient
--- Sources/StreamChat/StateLayer/ChatState.swift
+++ Sources/StreamChat/StateLayer/ChatState.swift
@@ -21,9 +21,8 @@ public final class ChatState: ObservableObject {
         observer.start(
             with: .init(
                 channelDidChange: { [weak self] in await self?.setValue($0, for: \.channel) },
-                messagesDidChange: { [weak self] changes in
-                    guard let self else { return }
-                    await self.setValue(self.orderedMessages.withListChanges(changes), for: \.messages)
+                messagesDidChange: { _ in
+                    // TODO: messages did change
                 },
                 typingUsersDidChange: { [weak self] in await self?.setValue($0, for: \.typingUsers) }
             )
@@ -109,13 +108,5 @@ public final class ChatState: ObservableObject {
     // Force mutations on main actor since ChatState is meant to be used by UI.
     @MainActor func setValue<Value>(_ value: Value, for keyPath: ReferenceWritableKeyPath<ChatState, Value>) {
         self[keyPath: keyPath] = value
-    }
-    
-    @MainActor var orderedMessages: OrderedMessages {
-        OrderedMessages(messageOrdering: messageOrder, orderedMessages: messages)
-    }
-    
-    @MainActor func setSortedMessages(_ sortedMessages: [ChatMessage]) {
-        setValue(sortedMessages, for: \.messages)
     }
 }
