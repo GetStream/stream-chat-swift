ef630df2b87e3a3e4227f3db24eda9e61f4ef644
Toomas Vahter <toomas.vahter@getstream.io>
2024-03-12 13:56:47 +0200

    Add linking and unlinking channels from channel list query when channel related events are triggered (#3069)

--- Sources/StreamChat/Workers/EventObservers/EventObserver.swift
+++ Sources/StreamChat/Workers/EventObservers/EventObserver.swift
@@ -22,6 +22,23 @@ class EventObserver {
             notificationCenter?.removeObserver(observer)
         }
     }
+    
+    @available(iOS 13.0, *)
+    convenience init<EventType>(
+        notificationCenter: NotificationCenter,
+        transform: @escaping (Event) -> EventType?,
+        callback: @escaping (EventType) async throws -> Void
+    ) {
+        self.init(notificationCenter: notificationCenter, transform: transform) { event in
+            Task {
+                do {
+                    try await callback(event)
+                } catch {
+                    log.debug("Event observer failed to handle event \(event) with error \(error)")
+                }
+            }
+        }
+    }
 
     deinit {
         stopObserving()
