4f7a90eb04b479546216c0e037fc64a956becf49
Toomas Vahter <toomas.vahter@getstream.io>
2024-03-13 15:16:46 +0200

    Rename ban methods to use the term member

--- Sources/StreamChat/StateLayer/Chat.swift
+++ Sources/StreamChat/StateLayer/Chat.swift
@@ -180,8 +180,6 @@ public final class Chat {
         try await channelUpdater.removeMembers(currentUserId: currentUserId, cid: cid, userIds: Set(members), message: systemMessage)
     }
     
-    // MARK: - Member List
-    
     /// Loads an array of members for the specified query.
     ///
     /// - Parameters:
@@ -212,6 +210,57 @@ public final class Chat {
         query.pagination = pagination
         let members = try await memberListUpdater.load(query)
         return MemberListState(members: members, query: query, database: databaseContainer)
+    }
+    
+    // MARK: - Member Moderation
+    
+    /// Bans the specified member from the channel.
+    ///
+    /// When the channel member is banned, they will not be allowed to post messages until the ban is removed
+    /// or expired.
+    ///
+    /// Learn more about [banning and moderation tools](https://getstream.io/chat/docs/ios-swift/moderation/?language=swift#ban).
+    ///
+    ///  - Note: Channel watchers cannot be banned.
+    ///  - Note: In most cases, only admins or moderators are allowed to ban other users from the channel.
+    ///
+    /// - Parameters:
+    ///   - userId: The user id of the channel member.
+    ///   - reason: The reason that the ban was created.
+    ///   - timeoutInMinutes: The number of minutes the user should be banned for. Nil means that the user is banned forever or until the user is unbanned explicitly.
+    ///
+    /// - Throws: An error while communicating with the Stream API or missing required capabilities.
+    public func banMember(_ userId: UserId, reason: String? = nil, timeout timeoutInMinutes: Int? = nil) async throws {
+        try state.channel?.requireCapability(of: .banChannelMembers)
+        try await memberUpdater.banMember(userId, in: cid, shadow: false, for: timeoutInMinutes, reason: reason)
+    }
+    
+    /// Shadow bans the specified member from the channel.
+    ///
+    /// When the channel member is shadow banned, they will still be allowed to post messages, but any message
+    /// sent during the ban will only be visible to the author of the message and invisible to other users of the app.
+    ///
+    /// Learn more about [shadow banning and moderation tools](https://getstream.io/chat/docs/ios-swift/moderation/?language=swift#shadow-ban).
+    ///
+    /// - Parameters:
+    ///   - userId: The user id of the channel member.
+    ///   - reason: The reason that the ban was created.
+    ///   - timeoutInMinutes: The number of minutes the user should be banned for. Nil means that the user is banned forever or until the user is unbanned explicitly.
+    ///
+    /// - Throws: An error while communicating with the Stream API or missing required capabilities.
+    public func shadowBanMember(_ userId: UserId, reason: String? = nil, timeout timeoutInMinutes: Int? = nil) async throws {
+        try state.channel?.requireCapability(of: .banChannelMembers)
+        try await memberUpdater.banMember(userId, in: cid, shadow: true, for: timeoutInMinutes, reason: reason)
+    }
+    
+    /// Removes the channel member from the ban list.
+    ///
+    /// - Parameter userId: The user id of the channel member.
+    ///
+    /// - Throws: An error while communicating with the Stream API or missing required capabilities.
+    public func unbanMember(_ userId: UserId) async throws {
+        try state.channel?.requireCapability(of: .banChannelMembers)
+        try await memberUpdater.unbanMember(userId, in: cid)
     }
     
     // MARK: - Messages
@@ -650,57 +699,6 @@ public final class Chat {
     /// - Throws: An error while communicating with the Stream API
     @discardableResult public func translateMessage(_ message: MessageId, to language: TranslationLanguage) async throws -> ChatMessage {
         try await messageUpdater.translate(messageId: message, to: language)
-    }
-    
-    // MARK: - Moderation
-    
-    /// Bans the specified user from the channel.
-    ///
-    /// When the user is banned, they will not be allowed to post messages until the ban is removed
-    /// or expired but will be able to connect to Chat and to channels as before.
-    ///
-    /// Learn more about [banning and moderation tools](https://getstream.io/chat/docs/ios-swift/moderation/?language=swift#ban).
-    ///
-    ///  - Note: Channel watchers cannot be banned.
-    ///  - Note: In most cases, only admins or moderators are allowed to ban other users from the channel.
-    ///
-    /// - Parameters:
-    ///   - userId: The user id of the channel member.
-    ///   - reason: The reason that the ban was created.
-    ///   - timeoutInMinutes: The number of minutes the user should be banned for. Nil means that the user is banned forever or until the user is unbanned explicitly.
-    ///
-    /// - Throws: An error while communicating with the Stream API or missing required capabilities.
-    public func banUser(_ userId: UserId, reason: String? = nil, timeout timeoutInMinutes: Int? = nil) async throws {
-        try state.channel?.requireCapability(of: .banChannelMembers)
-        try await memberUpdater.banMember(userId, in: cid, shadow: false, for: timeoutInMinutes, reason: reason)
-    }
-    
-    /// Shadow bans the specified user from the channel.
-    ///
-    /// When the user is shadow banned, they will still be allowed to post messages, but any message
-    /// sent during the ban will only be visible to the author of the message and invisible to other users of the app.
-    ///
-    /// Learn more about [shadow banning and moderation tools](https://getstream.io/chat/docs/ios-swift/moderation/?language=swift#shadow-ban).
-    ///
-    /// - Parameters:
-    ///   - userId: The user id of the channel member.
-    ///   - reason: The reason that the ban was created.
-    ///   - timeoutInMinutes: The number of minutes the user should be banned for. Nil means that the user is banned forever or until the user is unbanned explicitly.
-    ///
-    /// - Throws: An error while communicating with the Stream API or missing required capabilities.
-    public func shadowBanUser(_ userId: UserId, reason: String? = nil, timeout timeoutInMinutes: Int? = nil) async throws {
-        try state.channel?.requireCapability(of: .banChannelMembers)
-        try await memberUpdater.banMember(userId, in: cid, shadow: true, for: timeoutInMinutes, reason: reason)
-    }
-    
-    /// Removes the user from the ban list.
-    ///
-    /// - Parameter userId: The user id of the channel member.
-    ///
-    /// - Throws: An error while communicating with the Stream API or missing required capabilities.
-    public func unbanUser(_ userId: UserId) async throws {
-        try state.channel?.requireCapability(of: .banChannelMembers)
-        try await memberUpdater.unbanMember(userId, in: cid)
     }
     
     // MARK: - Muting or Hiding the Channel
