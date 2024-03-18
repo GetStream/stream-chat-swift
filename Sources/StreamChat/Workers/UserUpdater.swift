5d7b35ee32cbb050fc626824dad71008aa794d52
Toomas Vahter <toomas.vahter@getstream.io>
2024-03-18 15:25:15 +0200

    Add global user flagging and muting support (#3096)

--- Sources/StreamChat/Workers/UserUpdater.swift
+++ Sources/StreamChat/Workers/UserUpdater.swift
@@ -105,3 +105,38 @@ extension ClientError {
         }
     }
 }
+
+@available(iOS 13.0, *)
+extension UserUpdater {
+    func muteUser(_ userId: UserId) async throws {
+        try await withCheckedThrowingContinuation { continuation in
+            muteUser(userId) { error in
+                continuation.resume(with: error)
+            }
+        }
+    }
+    
+    func unmuteUser(_ userId: UserId) async throws {
+        try await withCheckedThrowingContinuation { continuation in
+            unmuteUser(userId) { error in
+                continuation.resume(with: error)
+            }
+        }
+    }
+    
+    func flag(_ userId: UserId) async throws {
+        try await withCheckedThrowingContinuation { continuation in
+            flagUser(true, with: userId) { error in
+                continuation.resume(with: error)
+            }
+        }
+    }
+    
+    func unflag(_ userId: UserId) async throws {
+        try await withCheckedThrowingContinuation { continuation in
+            flagUser(false, with: userId) { error in
+                continuation.resume(with: error)
+            }
+        }
+    }
+}
