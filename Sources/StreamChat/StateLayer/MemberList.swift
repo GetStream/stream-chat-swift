43d717822d16b7b4386ad636a5221a8ac0ed8170
Toomas Vahter <toomas.vahter@getstream.io>
2024-03-20 09:19:19 +0200

    [ChatStateLayer] Member banning and member lists (#3079)
    * Add member banning methods 
    
    * Add observable MemberState type with MemberList
    
    * Allow creating member lists separately and at the same time provide a default channel members loading support in the Chat interface

---
+++ Sources/StreamChat/StateLayer/MemberList.swift
@@ -0,0 +1,76 @@
+//
+// Copyright © 2024 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+
+/// An object which represents a list of `ChatChannelMember` for the specified channel.
+@available(iOS 13.0, *)
+public struct MemberList {
+    private let query: ChannelMemberListQuery
+    private let memberListUpdater: ChannelMemberListUpdater
+    
+    init(query: ChannelMemberListQuery, client: ChatClient, environment: Environment = .init()) {
+        self.query = query
+        memberListUpdater = environment.memberListUpdaterBuilder(
+            client.databaseContainer,
+            client.apiClient
+        )
+        state = environment.stateBuilder(
+            [],
+            query,
+            client.databaseContainer
+        )
+    }
+    
+    /// An observable object representing the current state of the member list.
+    public let state: MemberListState
+    
+    /// Loads channel members for the specified pagination parameters and updates ``MemberListState.members``.
+    ///
+    /// - Parameters:
+    ///   - pagination: The pagination configuration which includes a limit and an offset or a cursor.
+    ///
+    /// - Throws: An error while communicating with the Stream API.
+    /// - Returns: An array of channel members for the pagination.
+    @discardableResult public func loadMembers(with pagination: Pagination) async throws -> [ChatChannelMember] {
+        try await memberListUpdater.load(query.withPagination(pagination))
+    }
+    
+    /// Loads more channel members and updates ``MemberListState.members``.
+    ///
+    /// - Parameters
+    ///   - limit: The limit for the page size. The default limit is 30.
+    ///
+    /// - Throws: An error while communicating with the Stream API.
+    /// - Returns: An array of channel members.
+    @discardableResult public func loadNextMembers(with limit: Int? = nil) async throws -> [ChatChannelMember] {
+        let pageSize = limit ?? Int.channelMembersPageSize
+        let pagination = Pagination(pageSize: pageSize, offset: state.members.count)
+        return try await loadMembers(with: pagination)
+    }
+}
+
+@available(iOS 13.0, *)
+extension MemberList {
+    struct Environment {
+        var memberListUpdaterBuilder: (
+            _ database: DatabaseContainer,
+            _ apiClient: APIClient
+        ) -> ChannelMemberListUpdater = ChannelMemberListUpdater.init
+        
+        var stateBuilder: (
+            _ members: [ChatChannelMember],
+            _ query: ChannelMemberListQuery,
+            _ database: DatabaseContainer
+        ) -> MemberListState = MemberListState.init
+    }
+}
+
+private extension ChannelMemberListQuery {
+    func withPagination(_ pagination: Pagination) -> Self {
+        var result = self
+        result.pagination = pagination
+        return result
+    }
+}
