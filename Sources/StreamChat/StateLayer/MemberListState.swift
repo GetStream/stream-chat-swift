d1ad8f3763de176caa49bb3a842a4239873ffac4
Toomas Vahter <toomas.vahter@getstream.io>
2024-03-21 16:23:16 +0200

    Move observers to separate files

--- Sources/StreamChat/StateLayer/MemberListState.swift
+++ Sources/StreamChat/StateLayer/MemberListState.swift
@@ -26,38 +26,3 @@ public final class MemberListState: ObservableObject {
         self[keyPath: keyPath] = value
     }
 }
-
-// MARK: - Observing the Local State
-
-@available(iOS 13.0, *)
-extension MemberListState {
-    struct Observer {
-        private let memberListObserver: BackgroundListDatabaseObserver<ChatChannelMember, MemberDTO>
-        
-        init(query: ChannelMemberListQuery, database: DatabaseContainer) {
-            memberListObserver = BackgroundListDatabaseObserver(
-                context: database.backgroundReadOnlyContext,
-                fetchRequest: MemberDTO.members(matching: query),
-                itemCreator: { try $0.asModel() as ChatChannelMember },
-                sorting: []
-            )
-        }
-        
-        struct Handlers {
-            let membersDidChange: (StreamCollection<ChatChannelMember>) async -> Void
-        }
-        
-        func start(with handlers: Handlers) {
-            memberListObserver.onDidChange = { [weak memberListObserver] _ in
-                guard let items = memberListObserver?.items else { return }
-                let collection = StreamCollection(items)
-                Task { await handlers.membersDidChange(collection) }
-            }
-            do {
-                try memberListObserver.startObserving()
-            } catch {
-                log.error("Failed to start the member list observer with error \(error)")
-            }
-        }
-    }
-}
