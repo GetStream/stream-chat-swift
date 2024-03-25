cae2d8b757699fa78591829ff141dab858f8c147
Toomas Vahter <toomas.vahter@getstream.io>
2024-03-25 10:12:25 +0200

    [ChatStateLayer] Client, connection and channel events (#3102)
    * Add subscribe methods for listening to events

--- Sources/StreamChat/Workers/EventSender.swift
+++ Sources/StreamChat/Workers/EventSender.swift
@@ -22,3 +22,14 @@ class EventSender: Worker {
         }
     }
 }
+
+@available(iOS 13.0, *)
+extension EventSender {
+    func sendEvent<Payload: CustomEventPayload>(_ payload: Payload, to cid: ChannelId) async throws {
+        try await withCheckedThrowingContinuation { continuation in
+            sendEvent(payload, to: cid) { error in
+                continuation.resume(with: error)
+            }
+        }
+    }
+}
