31c0f72f029304e970d7dea8ab3accaae104d38c
Nuno Vieira <nuno.fcvieira93@gmail.com>
2024-04-01 19:00:30 +0100

    Add Thread Payloads Parsing

---
+++ Sources/StreamChat/APIClient/Endpoints/Payloads/ThreadListPayload.swift
@@ -0,0 +1,88 @@
+//
+// Copyright © 2024 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+
+struct ThreadListPayload: Decodable {
+    enum CodingKeys: String, CodingKey {
+        case threads
+        case next
+    }
+
+    let threads: [ThreadPayload]
+    let next: String?
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let threads = try container.decodeArrayIgnoringFailures([ThreadPayload].self, forKey: .threads)
+        let next = try container.decodeIfPresent(String.self, forKey: .next)
+
+        self.threads = threads
+        self.next = next
+    }
+}
+
+struct ThreadPayload: Decodable {
+    enum CodingKeys: String, CodingKey {
+        case channel
+        case parentMessage = "parent_message"
+        case createdBy = "created_by"
+        case replyCount = "reply_count"
+        case participantCount = "participant_count"
+        case threadParticipants = "thread_participants"
+        case lastMessageAt = "last_message_at"
+        case createdAt = "created_at"
+        case updatedAt = "updated_at"
+        case title
+        case latestReplies = "latest_replies"
+        case read
+    }
+
+    let channel: ChannelDetailPayload
+    let parentMessage: MessagePayload
+    let createdBy: UserPayload
+    let replyCount: Int
+    let participantCount: Int
+    let threadParticipants: [ThreadParticipant]
+    let lastMessageAt: Date?
+    let createdAt: Date
+    let updatedAt: Date?
+    let title: String?
+    let latestReplies: [MessagePayload]
+    let read: [ChannelReadPayload]
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        channel = try container.decode(ChannelDetailPayload.self, forKey: .channel)
+        parentMessage = try container.decode(MessagePayload.self, forKey: .parentMessage)
+        createdBy = try container.decode(UserPayload.self, forKey: .createdBy)
+        replyCount = try container.decode(Int.self, forKey: .replyCount)
+        participantCount = try container.decode(Int.self, forKey: .participantCount)
+        threadParticipants = try container.decode(
+            [ThreadParticipant].self,
+            forKey: .threadParticipants
+        )
+        lastMessageAt = try container.decodeIfPresent(Date.self, forKey: .lastMessageAt)
+        createdAt = try container.decode(Date.self, forKey: .createdAt)
+        updatedAt = try container.decodeIfPresent(Date.self, forKey: .updatedAt)
+        title = try container.decodeIfPresent(String.self, forKey: .title)
+        latestReplies = try container.decodeArrayIgnoringFailures([MessagePayload].self, forKey: .latestReplies)
+        read = try container.decodeArrayIgnoringFailures([ChannelReadPayload].self, forKey: .read)
+    }
+}
+
+struct ThreadParticipant: Decodable {
+    enum CodingKeys: String, CodingKey {
+        case user
+        case threadId = "thread_id"
+        case lastReadAt = "last_read_at"
+        case createdAt = "created_at"
+    }
+    
+    let user: UserPayload
+    let threadId: String
+    let createdAt: Date
+    let lastReadAt: Date?
+}
---
+++ Tests/StreamChatTests/APIClient/Endpoints/Payloads/ThreadListPayload_Tests.swift
@@ -0,0 +1,37 @@
+//
+// Copyright © 2024 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+
+@testable import StreamChat
+@testable import StreamChatTestTools
+import XCTest
+
+final class ThreadListPayload_Tests: XCTestCase {
+    func test_threadList_decoding() throws {
+        let url = XCTestCase.mockData(fromJSONFile: "ThreadList")
+        let payload = try JSONDecoder.default.decode(ThreadListPayload.self, from: url)
+        // The JSON Payload actually has 10 threads, but 2 have invalid `parentMessage`.
+        // The goal is to also test that if there are invalid threads, we still parse the remaining ones.
+        XCTAssertEqual(payload.threads.count, 8)
+    }
+
+    func test_thread_decoding() throws {
+        let url = XCTestCase.mockData(fromJSONFile: "Thread")
+        let payload = try JSONDecoder.default.decode(ThreadPayload.self, from: url)
+        XCTAssertEqual(payload.channel.cid.rawValue, "messaging:4AB11F2F-4")
+        XCTAssertEqual(payload.parentMessage.id, "488bba2a-193d-48d2-95ae-3aa4b8e34960")
+        XCTAssertEqual(payload.parentMessage.text, "msg: 24")
+        XCTAssertEqual(payload.createdBy.id, "han_solo")
+        XCTAssertEqual(payload.replyCount, 60)
+        XCTAssertEqual(payload.participantCount, 3)
+        XCTAssertEqual(payload.threadParticipants.count, 3)
+        XCTAssertEqual(payload.lastMessageAt, "2024-03-26T12:25:07.25741Z".toDate())
+        XCTAssertEqual(payload.createdAt, "2024-03-26T12:14:10.87779Z".toDate())
+        XCTAssertEqual(payload.updatedAt, "2024-03-26T12:25:07.25741Z".toDate())
+        XCTAssertEqual(payload.title, "msg: 24")
+        XCTAssertEqual(payload.latestReplies.count, 2)
+        XCTAssertEqual(payload.read.count, 3)
+    }
+}
