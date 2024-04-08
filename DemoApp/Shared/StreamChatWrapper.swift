3eac68609ec32741d40bcf1168acbd53594e38a3
Nuno Vieira <nuno.fcvieira93@gmail.com>
2024-04-08 15:52:10 +0100

    Fix not able to logout when user does not have device registered in the DemoApp

--- DemoApp/Shared/StreamChatWrapper.swift
+++ DemoApp/Shared/StreamChatWrapper.swift
@@ -112,11 +112,14 @@ extension StreamChatWrapper {
         if let deviceId = currentUserController.currentUser?.currentDevice?.id {
             currentUserController.removeDevice(id: deviceId) { error in
                 if let error = error {
-                    log.warning("removing the device failed with an error \(error)")
+                    log.error("Removing the device failed with an error \(error)")
                 }
 
                 client.logout(completion: completion)
             }
+        } else {
+            log.error("No deviceId has been found from the current user.")
+            client.logout(completion: completion)
         }
     }
 }
