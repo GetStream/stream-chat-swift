59adaf0b1659a703057142a8454d7ed1c061c363
Toomas Vahter <toomas.vahter@getstream.io>
2024-04-16 13:13:32 +0300

    [ChatStateLayer] Add test coverage for ConnectedUser (#3151)
    * Add test coverage for ConnectedUser
    
    * Fix tests in CurrentUserUpdater which did not handle completion handlers

--- TestTools/StreamChatTestTools/Mocks/StreamChat/Workers/UserUpdater_Mock.swift
+++ TestTools/StreamChatTestTools/Mocks/StreamChat/Workers/UserUpdater_Mock.swift
@@ -9,51 +9,63 @@ import XCTest
 final class UserUpdater_Mock: UserUpdater {
     @Atomic var muteUser_userId: UserId?
     @Atomic var muteUser_completion: ((Error?) -> Void)?
+    @Atomic var muteUser_completion_result: Result<Void, Error>?
 
     @Atomic var unmuteUser_userId: UserId?
     @Atomic var unmuteUser_completion: ((Error?) -> Void)?
+    @Atomic var unmuteUser_completion_result: Result<Void, Error>?
 
     @Atomic var loadUser_userId: UserId?
     @Atomic var loadUser_completion: ((Error?) -> Void)?
+    @Atomic var loadUser_completion_result: Result<Void, Error>?
 
     @Atomic var flagUser_flag: Bool?
     @Atomic var flagUser_userId: UserId?
     @Atomic var flagUser_completion: ((Error?) -> Void)?
+    @Atomic var flagUser_completion_result: Result<Void, Error>?
 
     override func muteUser(_ userId: UserId, completion: ((Error?) -> Void)? = nil) {
         muteUser_userId = userId
         muteUser_completion = completion
+        muteUser_completion_result?.invoke(with: completion)
     }
 
     override func unmuteUser(_ userId: UserId, completion: ((Error?) -> Void)? = nil) {
         unmuteUser_userId = userId
         unmuteUser_completion = completion
+        unmuteUser_completion_result?.invoke(with: completion)
     }
 
     override func loadUser(_ userId: UserId, completion: ((Error?) -> Void)? = nil) {
         loadUser_userId = userId
         loadUser_completion = completion
+        loadUser_completion_result?.invoke(with: completion)
     }
 
     override func flagUser(_ flag: Bool, with userId: UserId, completion: ((Error?) -> Void)? = nil) {
         flagUser_flag = flag
         flagUser_userId = userId
         flagUser_completion = completion
+        flagUser_completion_result?.invoke(with: completion)
     }
 
     // Cleans up all recorded values
     func cleanUp() {
         muteUser_userId = nil
         muteUser_completion = nil
+        muteUser_completion_result = nil
 
         unmuteUser_userId = nil
         unmuteUser_completion = nil
+        unmuteUser_completion_result = nil
 
         loadUser_userId = nil
         loadUser_completion = nil
+        loadUser_completion_result = nil
 
         flagUser_flag = nil
         flagUser_userId = nil
         flagUser_completion = nil
+        flagUser_completion_result = nil
     }
 }
