d6a5b25770b65f2c34ace8c178e057ac32cbc942
Toomas Vahter <toomas.vahter@getstream.io>
2024-04-30 15:46:53 +0300

    Remove duplicate casting in observers

--- Sources/StreamChat/StateLayer/MemberListState+Observer.swift
+++ Sources/StreamChat/StateLayer/MemberListState+Observer.swift
@@ -13,7 +13,7 @@ extension MemberListState {
             memberListObserver = StateLayerDatabaseObserver(
                 databaseContainer: database,
                 fetchRequest: MemberDTO.members(matching: query),
-                itemCreator: { try $0.asModel() as ChatChannelMember }
+                itemCreator: { try $0.asModel() }
             )
         }
         
