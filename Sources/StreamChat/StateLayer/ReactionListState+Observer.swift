c97a682c2f0c79d5ab28df5cce0bffe56611dbdd
Toomas Vahter <toomas.vahter@getstream.io>
2024-05-03 10:03:59 +0300

    Add ReactionList for querying reactions filtered in a specific way (#3175)

---
+++ Sources/StreamChat/StateLayer/ReactionListState+Observer.swift
@@ -0,0 +1,33 @@
+//
+// Copyright © 2024 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+
+@available(iOS 13.0, *)
+extension ReactionListState {
+    struct Observer {
+        private let reactionListObserver: StateLayerDatabaseObserver<ListResult, ChatMessageReaction, MessageReactionDTO>
+        
+        init(query: ReactionListQuery, database: DatabaseContainer) {
+            reactionListObserver = StateLayerDatabaseObserver(
+                databaseContainer: database,
+                fetchRequest: MessageReactionDTO.reactionListFetchRequest(query: query),
+                itemCreator: { try $0.asModel() }
+            )
+        }
+        
+        struct Handlers {
+            let reactionsDidChange: (StreamCollection<ChatMessageReaction>) async -> Void
+        }
+        
+        func start(with handlers: Handlers) -> StreamCollection<ChatMessageReaction> {
+            do {
+                return try reactionListObserver.startObserving(didChange: handlers.reactionsDidChange)
+            } catch {
+                log.error("Failed to start the reaction list observer with error \(error)")
+                return StreamCollection([])
+            }
+        }
+    }
+}
