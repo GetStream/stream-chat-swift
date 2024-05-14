42739887c9f5f1c016086c203e6018a69fc8aaa1
Nuno Vieira <nuno.fcvieira93@gmail.com>
2024-05-14 16:09:17 +0100

    Provide default arguments to the thread queries to make it less confusing

--- Sources/StreamChat/Query/ThreadQuery.swift
+++ Sources/StreamChat/Query/ThreadQuery.swift
@@ -17,11 +17,11 @@ internal struct ThreadQuery: Encodable {
     /// The parent message ID which the thread belongs to.
     internal var messageId: MessageId
     /// A boolean indicating whether to watch for changes in the thread or not.
-    internal var watch: Bool?
+    internal var watch: Bool = false
     /// The amount of replies fetched from the thread. Default is 2.
-    internal var replyLimit: Int?
+    internal var replyLimit: Int = 2
     /// The amount of participants fetched per thread. Default is 100.
-    internal var participantLimit: Int?
+    internal var participantLimit: Int = 100
 
     internal init(messageId: MessageId) {
         self.messageId = messageId
