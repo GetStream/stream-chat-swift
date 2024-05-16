ef55352798e6da8c9c6467a286facb918f8f4855
Nuno Vieira <nuno.fcvieira93@gmail.com>
2024-05-16 19:56:26 +0100

    Add SwiftUI and Combine Helpers

---
+++ Sources/StreamChat/Controllers/ThreadListController/ChatClient+ThreadListController.swift
@@ -0,0 +1,13 @@
+//
+// Copyright © 2024 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+
+extension ChatClient {
+    /// Creates a new `ThreadListController` with the provided thread query.
+    /// - Returns: A new instance of `ChatThreadListController`.
+    internal func threadListController(query: ThreadListQuery) -> ChatThreadListController {
+        .init(query: query, client: self)
+    }
+}
