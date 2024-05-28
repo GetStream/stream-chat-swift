7f7b5ff0d08a48d3d413d19b5a48dee993a81dd2
Nuno Vieira <nuno.fcvieira93@gmail.com>
2024-05-28 18:26:53 +0100

    Make the default ThreadListQuery more optimised for performance

--- Sources/StreamChat/Query/ThreadListQuery.swift
+++ Sources/StreamChat/Query/ThreadListQuery.swift
@@ -16,18 +16,20 @@ public struct ThreadListQuery: Encodable {
 
     /// A boolean indicating whether to watch for changes in the thread or not.
     public var watch: Bool
-    /// The amount of replies fetched per thread. Default is 2.
-    public var replyLimit: Int = 2
-    /// The amount of participants fetched per thread. Default is 100.
-    public var participantLimit: Int = 100
-    /// The amount of threads fetched per page. Default is 10.
-    public var limit: Int = 10
+    /// The amount of threads fetched per page. Default is 20.
+    public var limit: Int
+    /// The amount of replies fetched per thread. Default is 3.
+    public var replyLimit: Int
+    /// The amount of participants fetched per thread. Default is 10.
+    public var participantLimit: Int
     /// The pagination token from the previous response to fetch the next page.
     public var next: String?
 
-    public init(
-        watch: Bool
-    ) {
+    public init(watch: Bool, limit: Int = 20, replyLimit: Int = 3, participantLimit: Int = 10, next: String? = nil) {
         self.watch = watch
+        self.limit = limit
+        self.replyLimit = replyLimit
+        self.participantLimit = participantLimit
+        self.next = next
     }
 }
