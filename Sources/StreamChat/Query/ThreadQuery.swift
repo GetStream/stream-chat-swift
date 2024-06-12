10ec8650400fef989144a965512bc87e28eb43c7
Nuno Vieira <nuno.fcvieira93@gmail.com>
2024-06-12 19:18:22 +0100

    Expose ThreadQuery

--- Sources/StreamChat/Query/ThreadQuery.swift
+++ Sources/StreamChat/Query/ThreadQuery.swift
@@ -6,7 +6,7 @@ import Foundation
 
 /// A query to fetch information about a thread.
 /// To fetch all the replies from a thread and paginate the replies, the `ChatMessageController` should be used instead.
-internal struct ThreadQuery: Encodable {
+public struct ThreadQuery: Encodable {
     private enum CodingKeys: String, CodingKey {
         case messageId = "message_id"
         case watch
@@ -15,15 +15,23 @@ internal struct ThreadQuery: Encodable {
     }
     
     /// The parent message ID which the thread belongs to.
-    internal var messageId: MessageId
+    public var messageId: MessageId
     /// A boolean indicating whether to watch for changes in the thread or not.
-    internal var watch: Bool = false
-    /// The amount of replies fetched from the thread. Default is 2.
-    internal var replyLimit: Int = 2
-    /// The amount of participants fetched per thread. Default is 100.
-    internal var participantLimit: Int = 100
+    public var watch: Bool
+    /// The amount of replies fetched from the thread. Default is 3.
+    public var replyLimit: Int
+    /// The amount of participants fetched per thread. Default is 10.
+    public var participantLimit: Int
 
-    internal init(messageId: MessageId) {
+    public init(
+        messageId: MessageId,
+        watch: Bool = false,
+        replyLimit: Int = 3,
+        participantLimit: Int = 10
+    ) {
         self.messageId = messageId
+        self.watch = watch
+        self.replyLimit = replyLimit
+        self.participantLimit = participantLimit
     }
 }
