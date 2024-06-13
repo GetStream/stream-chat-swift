0c7fd2276be47d287ed102bcf6e64981905f0491
Alexey Alter-Pesotskiy <a.alterpesotskiy@mail.ru>
2024-06-13 12:41:48 +0100

    Add an option to switch API keys in the tests

--- StreamChatUITestsApp/StreamChat/User.swift
+++ StreamChatUITestsApp/StreamChat/User.swift
@@ -8,10 +8,22 @@ import StreamChat
 extension UserCredentials {
 
     static var `default`: UserCredentials {
+        ProcessInfo.processInfo.arguments.contains("SWIFTUI_API_KEY") ? .swiftUI : .swift
+    }
+
+    static var swift: UserCredentials {
         UserCredentials(id: "luke_skywalker",
                         name: "Luke Skywalker",
                         avatarURL: URL(string: "https://vignette.wikia.nocookie.net/starwars/images/2/20/LukeTLJ.jpg")!,
                         token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoibHVrZV9za3l3YWxrZXIifQ.kFSLHRB5X62t0Zlc7nwczWUfsQMwfkpylC6jCUZ6Mc0",
+                        birthLand: "Tatooine")
+    }
+    
+    static var swiftUI: UserCredentials {
+        UserCredentials(id: "luke_skywalker",
+                        name: "Luke Skywalker",
+                        avatarURL: URL(string: "https://vignette.wikia.nocookie.net/starwars/images/2/20/LukeTLJ.jpg")!,
+                        token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoibHVrZV9za3l3YWxrZXIifQ.b6EiC8dq2AHk0JPfI-6PN-AM9TVzt8JV-qB1N9kchlI",
                         birthLand: "Tatooine")
     }
 }
