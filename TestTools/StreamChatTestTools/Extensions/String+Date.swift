a97c3b52bf6b570843a69a292df06b6a0bd4c5f0
Toomas Vahter <toomas.vahter@getstream.io>
2024-10-04 15:39:01 +0300

    Unit-tests were crashing on iOS 13 and 14 when toDate() was used

--- TestTools/StreamChatTestTools/Extensions/String+Date.swift
+++ TestTools/StreamChatTestTools/Extensions/String+Date.swift
@@ -6,9 +6,11 @@ import Foundation
 @testable import StreamChat
 
 public extension String {
-    /// Converst a string to `Date`. Only for testing!
+    /// Converts a string to `Date`. Only for testing!
     func toDate() -> Date {
-        if let date = JSONDecoder.stream.iso8601formatter.dateWithMicroseconds(from: self) {
+        let iso8601Formatter = ISO8601DateFormatter()
+        iso8601Formatter.formatOptions = [.withFractionalSeconds, .withInternetDateTime]
+        if let date = iso8601Formatter.dateWithMicroseconds(from: self) {
             return date
         }
         return DateFormatter.Stream.rfc3339Date(from: self)!
