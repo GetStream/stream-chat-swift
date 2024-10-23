568577471ee0dab22df39ebc45de637b0eff49a0
Nuno Vieira <nuno.fcvieira93@gmail.com>
2024-10-23 18:03:31 +0100

    Add `ChatChannelMemberController.partialUpdate()` to partially update a channel member including extra data

--- Sources/StreamChat/APIClient/Endpoints/EndpointPath+OfflineRequest.swift
+++ Sources/StreamChat/APIClient/Endpoints/EndpointPath+OfflineRequest.swift
@@ -9,12 +9,13 @@ extension EndpointPath {
         switch self {
         case .sendMessage, .editMessage, .deleteMessage, .pinMessage, .unpinMessage, .addReaction, .deleteReaction:
             return true
-        case .createChannel, .connect, .sync, .users, .guest, .members, .search, .devices, .channels, .updateChannel,
+        case .createChannel, .connect, .sync, .users, .guest, .members, .partialMemberUpdate, .search, .devices, .channels, .updateChannel,
              .deleteChannel, .channelUpdate, .muteChannel, .showChannel, .truncateChannel, .markChannelRead, .markChannelUnread,
              .markAllChannelsRead, .channelEvent, .stopWatchingChannel, .pinnedMessages, .uploadAttachment, .message,
              .replies, .reactions, .messageAction, .banMember, .flagUser, .flagMessage, .muteUser, .translateMessage,
-             .callToken, .createCall, .deleteFile, .deleteImage, .og, .appSettings, .threads, .thread, .markThreadRead, .markThreadUnread, .polls, .pollsQuery,
-             .poll, .pollOption, .pollOptions, .pollVotes, .pollVoteInMessage, .pollVote, .unread, .blockUser, .unblockUser:
+             .callToken, .createCall, .deleteFile, .deleteImage, .og, .appSettings, .threads, .thread, .markThreadRead, .markThreadUnread,
+             .polls, .pollsQuery, .poll, .pollOption, .pollOptions, .pollVotes, .pollVoteInMessage, .pollVote,
+             .unread, .blockUser, .unblockUser:
             return false
         }
     }
--- Sources/StreamChat/APIClient/Endpoints/MemberEndpoints.swift
+++ Sources/StreamChat/APIClient/Endpoints/MemberEndpoints.swift
@@ -16,4 +16,35 @@ extension Endpoint {
             body: ["payload": query]
         )
     }
+
+    static func partialMemberUpdate(
+        userId: UserId,
+        cid: ChannelId,
+        extraData: [String: RawJSON]?,
+        unset: [String]?
+    ) -> Endpoint<PartialMemberUpdateResponse> {
+        var body: [String: AnyEncodable] = [:]
+        if let extraData {
+            body["set"] = AnyEncodable(extraData)
+        }
+        if let unset {
+            body["unset"] = AnyEncodable(unset)
+        }
+
+        return .init(
+            path: .partialMemberUpdate(userId: userId, cid: cid),
+            method: .patch,
+            queryItems: nil,
+            requiresConnectionId: false,
+            body: body
+        )
+    }
+}
+
+struct PartialMemberUpdateResponse: Decodable {
+    var channelMember: MemberPayload
+
+    enum CodingKeys: String, CodingKey {
+        case channelMember = "channel_member"
+    }
 }
