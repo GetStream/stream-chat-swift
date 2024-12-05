69225cb4dce07de6d9a07a74724f73e302783f45
Toomas Vahter <toomas.vahter@getstream.io>
2024-12-05 10:39:25 +0200

    Move the pinning scope to a separate file

--- Sources/StreamChat/Controllers/ChannelController/ChannelController.swift
+++ Sources/StreamChat/Controllers/ChannelController/ChannelController.swift
@@ -1716,12 +1716,6 @@ private extension ChatChannelController {
     }
 }
 
-/// The scope of the channel pinning action.
-public enum ChannelPinningScope: String {
-    /// Channel is pinned only for the currently connected user. Other channel members do not see channels as pinned.
-    case me
-}
-
 // MARK: - Errors
 
 extension ClientError {
