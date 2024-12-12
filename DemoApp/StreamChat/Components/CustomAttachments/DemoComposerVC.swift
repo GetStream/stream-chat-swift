afc3f681b8412c684281408c0dff4eb1be00ddaa
Nuno Vieira <nuno.fcvieira93@gmail.com>
2024-12-12 16:26:34 +0000

    Create a CurrentUserLocationProvider to make it easier to fetch the current user location

--- DemoApp/StreamChat/Components/CustomAttachments/DemoComposerVC.swift
+++ DemoApp/StreamChat/Components/CustomAttachments/DemoComposerVC.swift
@@ -8,14 +8,7 @@ import StreamChatUI
 import UIKit
 
 class DemoComposerVC: ComposerVC {
-    private lazy var locationManager = CLLocationManager()
-
-    var sharingType: LocationSharingType = .addToAttachments
-
-    enum LocationSharingType {
-        case addToAttachments
-        case instant
-    }
+    private lazy var currentUserLocationProvider = CurrentUserLocationProvider()
 
     override var attachmentsPickerActions: [UIAlertAction] {
         var actions = super.attachmentsPickerActions
@@ -26,8 +19,7 @@ class DemoComposerVC: ComposerVC {
                 title: "Add Current Location",
                 style: .default,
                 handler: { [weak self] _ in
-                    self?.sharingType = .addToAttachments
-                    self?.requestLocationPermission()
+                    self?.addStaticLocationToAttachments()
                 }
             )
             actions.append(addLocationAction)
@@ -36,34 +28,81 @@ class DemoComposerVC: ComposerVC {
                 title: "Send Current Location",
                 style: .default,
                 handler: { [weak self] _ in
-                    self?.sharingType = .instant
-                    self?.requestLocationPermission()
+                    self?.sendInstantStaticLocation()
                 }
             )
             actions.append(sendLocationAction)
+
+            let sendLiveLocationAction = UIAlertAction(
+                title: "Share Live Location",
+                style: .default,
+                handler: { [weak self] _ in
+                    self?.sendInstantLiveLocation()
+                }
+            )
+            actions.append(sendLiveLocationAction)
         }
 
         return actions
     }
 
-    override func setUp() {
-        super.setUp()
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
 
-        locationManager = CLLocationManager()
+        currentUserLocationProvider.stopMonitoringLocation()
     }
 
-    private func requestLocationPermission() {
-        locationManager.delegate = self
+    func addStaticLocationToAttachments() {
+        currentUserLocationProvider.getCurrentLocation { [weak self] result in
+            switch result {
+            case .success(let location):
+                let staticLocationPayload = StaticLocationAttachmentPayload(
+                    latitude: location.coordinate.latitude,
+                    longitude: location.coordinate.longitude
+                )
+                self?.content.attachments.append(AnyAttachmentPayload(payload: staticLocationPayload))
+            case .failure(let error):
+                if error is LocationPermissionError {
+                    self?.showLocationPermissionAlert()
+                }
+            }
+        }
+    }
 
-        switch locationManager.authorizationStatus {
-        case .notDetermined:
-            locationManager.requestWhenInUseAuthorization()
-        case .authorizedWhenInUse, .authorizedAlways:
-            locationManager.startUpdatingLocation()
-        case .denied, .restricted:
-            showLocationPermissionAlert()
-        @unknown default:
-            break
+    func sendInstantStaticLocation() {
+        currentUserLocationProvider.getCurrentLocation { [weak self] result in
+            switch result {
+            case .success(let location):
+                let staticLocationPayload = StaticLocationAttachmentPayload(
+                    latitude: location.coordinate.latitude,
+                    longitude: location.coordinate.longitude
+                )
+                self?.channelController?.sendStaticLocation(staticLocationPayload)
+            case .failure(let error):
+                if error is LocationPermissionError {
+                    self?.showLocationPermissionAlert()
+                }
+            }
+        }
+    }
+
+    var throttler = Throttler(interval: 5)
+    var messageId: MessageId?
+
+    func sendInstantLiveLocation() {
+        currentUserLocationProvider.startMonitoringLocation()
+        currentUserLocationProvider.didUpdateLocation = { [weak self] location in
+            self?.throttler.execute {
+                let liveLocation = LiveLocationAttachmentPayload(
+                    latitude: location.coordinate.latitude,
+                    longitude: location.coordinate.longitude,
+                    stoppedSharing: false
+                )
+                debugPrint("newLiveLocation: \(liveLocation)")
+                self?.channelController?.updateLiveLocation(liveLocation, messageId: self?.messageId) {
+                    self?.messageId = try? $0.get()
+                }
+            }
         }
     }
 
@@ -85,9 +124,73 @@ class DemoComposerVC: ComposerVC {
     }
 }
 
-// MARK: - CLLocationManagerDelegate
+enum LocationPermissionError: Error {
+    case permissionDenied
+    case permissionRestricted
+}
 
-extension DemoComposerVC: CLLocationManagerDelegate {
+class CurrentUserLocationProvider: NSObject {
+    private let locationManager: CLLocationManager
+    private var onCurrentLocationFetch: ((Result<CLLocation, Error>) -> Void)?
+
+    var didUpdateLocation: ((CLLocation) -> Void)?
+    var lastLocation: CLLocation?
+    var onError: ((Error) -> Void)?
+
+    init(locationManager: CLLocationManager = CLLocationManager()) {
+        self.locationManager = locationManager
+        super.init()
+        self.locationManager.delegate = self
+    }
+
+    func startMonitoringLocation() {
+        locationManager.allowsBackgroundLocationUpdates = true
+        locationManager.startMonitoringSignificantLocationChanges()
+        requestPermission { [weak self] error in
+            guard let error else { return }
+            self?.onError?(error)
+        }
+    }
+
+    func stopMonitoringLocation() {
+        locationManager.allowsBackgroundLocationUpdates = false
+        locationManager.stopUpdatingLocation()
+        locationManager.stopMonitoringSignificantLocationChanges()
+    }
+
+    func getCurrentLocation(completion: @escaping (Result<CLLocation, Error>) -> Void) {
+        onCurrentLocationFetch = completion
+        if let lastLocation = lastLocation {
+            onCurrentLocationFetch?(.success(lastLocation))
+            onCurrentLocationFetch = nil
+        } else {
+            requestPermission { [weak self] error in
+                guard let error else { return }
+                self?.onCurrentLocationFetch?(.failure(error))
+                self?.onCurrentLocationFetch = nil
+            }
+        }
+    }
+
+    func requestPermission(completion: @escaping (Error?) -> Void) {
+        switch locationManager.authorizationStatus {
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+            completion(nil)
+        case .authorizedWhenInUse, .authorizedAlways:
+            locationManager.startUpdatingLocation()
+            completion(nil)
+        case .denied:
+            completion(LocationPermissionError.permissionDenied)
+        case .restricted:
+            completion(LocationPermissionError.permissionRestricted)
+        @unknown default:
+            break
+        }
+    }
+}
+
+extension CurrentUserLocationProvider: CLLocationManagerDelegate {
     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
         let status = manager.authorizationStatus
         if status == .authorizedWhenInUse || status == .authorizedAlways {
@@ -97,25 +200,13 @@ extension DemoComposerVC: CLLocationManagerDelegate {
 
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let location = locations.first else { return }
-        
-        let locationPayload = StaticLocationAttachmentPayload(
-            latitude: location.coordinate.latitude,
-            longitude: location.coordinate.longitude
-        )
-
-        switch sharingType {
-        case .addToAttachments:
-            content.attachments.append(AnyAttachmentPayload(payload: locationPayload))
-        case .instant:
-            channelController?.sendStaticLocation(locationPayload)
-        }
-
-        // We only send the location once so we can stop updating the location.
-        locationManager.stopUpdatingLocation()
-        locationManager.delegate = nil
+        didUpdateLocation?(location)
+        lastLocation = location
+        onCurrentLocationFetch?(.success(location))
+        onCurrentLocationFetch = nil
     }
-    
-    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        print("Location manager error: \(error.localizedDescription)")
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: any Error) {
+        onError?(error)
     }
 }
