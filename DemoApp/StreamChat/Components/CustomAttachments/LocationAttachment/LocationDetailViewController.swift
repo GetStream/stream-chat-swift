f3460bec131b1f352d4415bd6fb15fc94fda117c
Nuno Vieira <nuno.fcvieira93@gmail.com>
2025-01-02 19:10:22 +0000

    Refactor code structure of the map detail view controller

--- DemoApp/StreamChat/Components/CustomAttachments/LocationAttachment/LocationDetailViewController.swift
+++ DemoApp/StreamChat/Components/CustomAttachments/LocationAttachment/LocationDetailViewController.swift
@@ -68,6 +68,29 @@ class LocationDetailViewController: UIViewController, ThemeProvider {
 
         view = mapView
     }
+
+    func updateUserLocation(
+        _ coordinate: CLLocationCoordinate2D
+    ) {
+        if let existingAnnotation = userAnnotation {
+            // By setting the duration to 5, and since we update the location every 3s
+            // this will make sure the annotation moves smoothly and has a constant animation.
+            UIView.animate(withDuration: 5) {
+                existingAnnotation.coordinate = coordinate
+            }
+            UIView.animate(withDuration: 5, delay: 0.2, options: .curveEaseOut) {
+                self.mapView.setCenter(coordinate, animated: true)
+            }
+        } else if let author = messageController.message?.author {
+            // Create new annotation
+            let userAnnotation = UserAnnotation(
+                coordinate: coordinate,
+                user: author
+            )
+            mapView.addAnnotation(userAnnotation)
+            self.userAnnotation = userAnnotation
+        }
+    }
 }
 
 extension LocationDetailViewController: ChatMessageControllerDelegate {
@@ -87,27 +110,6 @@ extension LocationDetailViewController: ChatMessageControllerDelegate {
         updateUserLocation(
             locationCoordinate
         )
-    }
-
-    func updateUserLocation(
-        _ coordinate: CLLocationCoordinate2D
-    ) {
-        if let existingAnnotation = userAnnotation {
-            UIView.animate(withDuration: 5) {
-                existingAnnotation.coordinate = coordinate
-            }
-            UIView.animate(withDuration: 5, delay: 0.2, options: .curveEaseOut) {
-                self.mapView.setCenter(coordinate, animated: true)
-            }
-        } else if let author = messageController.message?.author {
-            // Create new annotation
-            let userAnnotation = UserAnnotation(
-                coordinate: coordinate,
-                user: author
-            )
-            mapView.addAnnotation(userAnnotation)
-            self.userAnnotation = userAnnotation
-        }
     }
 }
 
@@ -136,92 +138,5 @@ extension LocationDetailViewController: MKMapViewDelegate {
             annotationView?.stopPulsingAnimation()
         }
         return annotationView
-    }
-}
-
-class UserAnnotation: NSObject, MKAnnotation {
-    dynamic var coordinate: CLLocationCoordinate2D
-    var user: ChatUser
-
-    init(coordinate: CLLocationCoordinate2D, user: ChatUser) {
-        self.coordinate = coordinate
-        self.user = user
-        super.init()
-    }
-}
-
-class UserAnnotationView: MKAnnotationView {
-    private lazy var avatarView: ChatUserAvatarView = {
-        let view = ChatUserAvatarView()
-        view.shouldShowOnlineIndicator = false
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.layer.masksToBounds = true
-        return view
-    }()
-
-    private var size: CGSize = .init(width: 40, height: 40)
-
-    private var pulseLayer: CALayer?
-
-    override init(annotation: MKAnnotation?, reuseIdentifier: String?) {
-        super.init(annotation: annotation, reuseIdentifier: reuseIdentifier)
-        backgroundColor = .gray
-        frame = CGRect(x: 0, y: 0, width: size.width, height: size.height)
-        layer.cornerRadius = 20
-        layer.masksToBounds = false
-        layer.borderWidth = 2
-        layer.borderColor = UIColor.white.cgColor
-        addSubview(avatarView)
-        avatarView.width(size.width)
-        avatarView.height(size.height)
-    }
-
-    @available(*, unavailable)
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) not implemented")
-    }
-
-    func setUser(_ user: ChatUser) {
-        avatarView.content = user
-    }
-
-    func startPulsingAnimation() {
-        guard pulseLayer == nil else {
-            return
-        }
-        let pulseLayer = CALayer()
-        pulseLayer.masksToBounds = false
-        pulseLayer.frame = bounds
-        pulseLayer.cornerRadius = bounds.width / 2
-        pulseLayer.backgroundColor = UIColor.systemBlue.withAlphaComponent(0.5).cgColor
-        layer.insertSublayer(pulseLayer, below: avatarView.layer)
-
-        let animationScale = CABasicAnimation(keyPath: "transform.scale")
-        animationScale.fromValue = 1.0
-        animationScale.toValue = 1.5
-        animationScale.duration = 2.0
-        animationScale.timingFunction = CAMediaTimingFunction(name: .easeOut)
-        animationScale.autoreverses = false
-        animationScale.repeatCount = .infinity
-
-        let animationOpacity = CABasicAnimation(keyPath: "opacity")
-        animationOpacity.fromValue = 1.0
-        animationOpacity.toValue = 0
-        animationOpacity.duration = 2.0
-        animationOpacity.timingFunction = CAMediaTimingFunction(name: .easeOut)
-        animationOpacity.autoreverses = false
-        animationOpacity.repeatCount = .infinity
-
-        pulseLayer.add(animationScale, forKey: "pulseScale")
-        pulseLayer.add(animationOpacity, forKey: "pulseOpacity")
-        self.pulseLayer = pulseLayer
-    }
-
-    func stopPulsingAnimation() {
-        guard pulseLayer != nil else {
-            return
-        }
-        pulseLayer?.removeFromSuperlayer()
-        pulseLayer = nil
     }
 }
