e489c1488931856456cb90965330c9d0e8874cda
Nuno Vieira <nuno.fcvieira93@gmail.com>
2025-01-22 12:30:48 +0000

    Add test coverage to channel member transformation

--- Tests/StreamChatTests/Models/Member_Tests.swift
+++ Tests/StreamChatTests/Models/Member_Tests.swift
@@ -39,4 +39,60 @@ final class MemberRole_Tests: XCTestCase {
         json = try embed("some_unknown_role")
         XCTAssertEqual((try? decoder.decode(RoleContainer.self, from: json))?.role, "some_unknown_role")
     }
+
+    func test_replacing_correctlyUpdatesFields() {
+        // GIVEN
+        let originalMember = ChatChannelMember.mock(
+            id: .unique,
+            name: "original name",
+            imageURL: URL(string: "http://original.com"),
+            isOnline: true,
+            isBanned: false,
+            userRole: .user,
+            language: .english,
+            extraData: ["original": .string("data")],
+            memberExtraData: ["original": .string("member_data")]
+        )
+        
+        // WHEN
+        let newName = "new name"
+        let newImageURL = URL(string: "http://new.com")
+        let newUserExtraData: [String: RawJSON] = ["new": .string("data")]
+        let newMemberExtraData: [String: RawJSON] = ["new": .string("member_data")]
+        
+        let updatedMember = originalMember.replacing(
+            name: newName,
+            imageURL: newImageURL,
+            userExtraData: newUserExtraData,
+            memberExtraData: newMemberExtraData
+        )
+        
+        // THEN
+        // Updated fields
+        XCTAssertEqual(updatedMember.name, newName)
+        XCTAssertEqual(updatedMember.imageURL, newImageURL)
+        XCTAssertEqual(updatedMember.extraData, newUserExtraData)
+        XCTAssertEqual(updatedMember.memberExtraData, newMemberExtraData)
+        
+        // Unchanged fields
+        XCTAssertEqual(updatedMember.id, originalMember.id)
+        XCTAssertEqual(updatedMember.isOnline, originalMember.isOnline)
+        XCTAssertEqual(updatedMember.isBanned, originalMember.isBanned)
+        XCTAssertEqual(updatedMember.userRole, originalMember.userRole)
+        XCTAssertEqual(updatedMember.language, originalMember.language)
+
+        // Test replacing some fields while erasing others
+        let partialUpdatedMember = originalMember.replacing(
+            name: "replaced",
+            imageURL: nil,
+            userExtraData: nil,
+            memberExtraData: nil
+        )
+
+        XCTAssertEqual(partialUpdatedMember.id, originalMember.id)
+        XCTAssertEqual(partialUpdatedMember.name, "replaced")
+        XCTAssertEqual(partialUpdatedMember.imageURL, nil)
+        XCTAssertEqual(partialUpdatedMember.extraData, [:])
+        XCTAssertEqual(partialUpdatedMember.memberExtraData, [:])
+    }
 }
