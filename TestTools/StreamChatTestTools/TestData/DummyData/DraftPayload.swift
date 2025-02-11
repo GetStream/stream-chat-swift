8c5902e72e3b569e4cf49958eb2c109a29b37b41
Nuno Vieira <nuno.fcvieira93@gmail.com>
2025-02-11 18:29:57 +0000

    Add Draft Messages Repository Tests

---
+++ TestTools/StreamChatTestTools/TestData/DummyData/DraftPayload.swift
@@ -0,0 +1,55 @@
+//
+// Copyright © 2025 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+@testable import StreamChat
+
+extension DraftPayload {
+    /// Returns dummy draft payload with the given values.
+    static func dummy(
+        cid: ChannelId? = nil,
+        channelPayload: ChannelDetailPayload? = nil,
+        createdAt: Date = .unique,
+        message: DraftMessagePayload = .dummy(),
+        quotedMessage: MessagePayload? = nil,
+        parentId: String? = nil,
+        parentMessage: MessagePayload? = nil
+    ) -> Self {
+        .init(
+            cid: cid,
+            channelPayload: channelPayload,
+            createdAt: createdAt,
+            message: message,
+            quotedMessage: quotedMessage,
+            parentId: parentId,
+            parentMessage: parentMessage
+        )
+    }
+}
+
+extension DraftMessagePayload {
+    static func dummy(
+        id: String = .unique,
+        text: String = .unique,
+        command: String? = nil,
+        args: String? = nil,
+        showReplyInChannel: Bool = false,
+        mentionedUsers: [UserPayload]? = nil,
+        extraData: [String: RawJSON] = [:],
+        attachments: [MessageAttachmentPayload]? = nil,
+        isSilent: Bool = false
+    ) -> Self {
+        .init(
+            id: id,
+            text: text,
+            command: command,
+            args: args,
+            showReplyInChannel: showReplyInChannel,
+            mentionedUsers: mentionedUsers,
+            extraData: extraData,
+            attachments: attachments,
+            isSilent: isSilent
+        )
+    }
+}
