5a7065d1e8ceacd74f0216363502d6e943c3568e
Martin Mitrevski <martinmitrevski.oh@gmail.com>
2025-02-14 13:31:26 +0100

    Added restricted visibility to the state layer

--- Sources/StreamChat/Workers/ChannelUpdater.swift
+++ Sources/StreamChat/Workers/ChannelUpdater.swift
@@ -745,6 +745,7 @@ extension ChannelUpdater {
         quotedMessageId: MessageId?,
         skipPush: Bool,
         skipEnrichUrl: Bool,
+        restrictedVisibility: [UserId],
         extraData: [String: RawJSON]
     ) async throws -> ChatMessage {
         try await withCheckedThrowingContinuation { continuation in
@@ -762,6 +763,7 @@ extension ChannelUpdater {
                 quotedMessageId: quotedMessageId,
                 skipPush: skipPush,
                 skipEnrichUrl: skipEnrichUrl,
+                restrictedVisibility: restrictedVisibility,
                 extraData: extraData
             ) { result in
                 continuation.resume(with: result)
