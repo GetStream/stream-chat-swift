99b5a047fae050c21692d8aacc5c28d0a209b8a8
Toomas Vahter <toomas.vahter@getstream.io>
2025-02-19 10:16:07 +0200

    Remove SwiftyMarkdown (#3591)

--- hooks/pre-commit.sh
+++ hooks/pre-commit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eo pipefail
 
-./hooks/git-format-staged --formatter 'mint run swiftformat --config .swiftformat stdin' 'Sources/*.swift' '!*Generated*' '!*StreamSwiftyMarkdown*' '!*StreamDifferenceKit*' '!*StreamNuke*' '!*StreamSwiftyGif*'
+./hooks/git-format-staged --formatter 'mint run swiftformat --config .swiftformat stdin' 'Sources/*.swift' '!*Generated*' '!*StreamDifferenceKit*' '!*StreamNuke*' '!*StreamSwiftyGif*'
 ./hooks/git-format-staged --formatter 'mint run swiftformat --config .swiftformat stdin' 'Tests/*.swift'
 ./hooks/git-format-staged --formatter 'mint run swiftformat --config .swiftformat stdin' 'DemoApp/*.swift'
 ./hooks/git-format-staged --formatter 'mint run swiftformat --config .swiftformat stdin' 'Examples/*.swift'
