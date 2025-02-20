05c6be8f101ed94f4007c75e8f02cdf063961e94
Toomas Vahter <toomas.vahter@getstream.io>
2025-02-20 20:34:08 +0200

    Revert test code

--- Sources/StreamChat/Workers/Background/ConnectionRecoveryHandler.swift
+++ Sources/StreamChat/Workers/Background/ConnectionRecoveryHandler.swift
@@ -130,22 +130,20 @@ extension DefaultConnectionRecoveryHandler {
             return
         }
 
-        disconnectIfNeeded()
-        
-//        guard let scheduler = backgroundTaskScheduler else { return }
-//
-//        let succeed = scheduler.beginTask { [weak self] in
-//            log.debug("Background task -> ❌", subsystems: .webSocket)
-//
-//            self?.disconnectIfNeeded()
-//        }
-//
-//        if succeed {
-//            log.debug("Background task -> ✅", subsystems: .webSocket)
-//        } else {
-//            // Can't initiate a background task, close the connection
-//            disconnectIfNeeded()
-//        }
+        guard let scheduler = backgroundTaskScheduler else { return }
+
+        let succeed = scheduler.beginTask { [weak self] in
+            log.debug("Background task -> ❌", subsystems: .webSocket)
+
+            self?.disconnectIfNeeded()
+        }
+
+        if succeed {
+            log.debug("Background task -> ✅", subsystems: .webSocket)
+        } else {
+            // Can't initiate a background task, close the connection
+            disconnectIfNeeded()
+        }
     }
 
     @objc private func internetConnectionAvailabilityDidChange(_ notification: Notification) {
