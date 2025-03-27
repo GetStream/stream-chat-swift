e2bd1954ef17c7259d6e4a520e5be911e6e14aff
Toomas Vahter <toomas.vahter@getstream.io>
2025-03-27 14:11:14 +0200

    Add documentation

--- Sources/StreamChat/Utils/MainQueue+Synchronous.swift
+++ Sources/StreamChat/Utils/MainQueue+Synchronous.swift
@@ -6,6 +6,10 @@ import Foundation
 
 extension MainActor {
     /// Synchronously performs the provided action on the main thread.
+    ///
+    /// Used for ensuring we are on the main thread when compiler can't know it. For example,
+    /// controller completion handlers by default are called from main thread, but one can
+    /// configure controller to use background thread for completions instead.
     static func ensureIsolated<T>(_ action: @MainActor @Sendable() throws -> T) rethrows -> T where T: Sendable {
         if Thread.current.isMainThread {
             return try MainActor.assumeIsolated {
