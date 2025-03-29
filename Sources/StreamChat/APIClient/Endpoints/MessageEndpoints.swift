f0d95b7c876a208fb4e1753d2bd2cb7e70eede17
Nuno Vieira <nuno.fcvieira93@gmail.com>
2025-03-29 01:00:20 +0000

    Move reminder endpoints to a separate file

--- Sources/StreamChat/APIClient/Endpoints/MessageEndpoints.swift
+++ Sources/StreamChat/APIClient/Endpoints/MessageEndpoints.swift
@@ -94,54 +94,6 @@ extension Endpoint {
     }
 }
 
-// MARK: - Reminder Endpoints
-
-extension Endpoint {
-    // Creates or updates a reminder for a message
-    static func createReminder(messageId: MessageId, request: ReminderRequestBody) -> Endpoint<ReminderResponsePayload> {
-        .init(
-            path: .reminder(messageId),
-            method: .post,
-            queryItems: nil,
-            requiresConnectionId: false,
-            body: request
-        )
-    }
-    
-    // Updates an existing reminder for a message
-    static func updateReminder(messageId: MessageId, request: ReminderRequestBody) -> Endpoint<ReminderResponsePayload> {
-        .init(
-            path: .reminder(messageId),
-            method: .patch,
-            queryItems: nil,
-            requiresConnectionId: false,
-            body: request
-        )
-    }
-    
-    // Deletes a reminder for a message
-    static func deleteReminder(messageId: MessageId) -> Endpoint<EmptyResponse> {
-        .init(
-            path: .reminder(messageId),
-            method: .delete,
-            queryItems: nil,
-            requiresConnectionId: false,
-            body: nil
-        )
-    }
-    
-    // Queries reminders with the provided parameters
-    static func queryReminders(query: MessageReminderListQuery) -> Endpoint<RemindersQueryPayload> {
-        .init(
-            path: .reminders,
-            method: .post,
-            queryItems: nil,
-            requiresConnectionId: false,
-            body: query
-        )
-    }
-}
-
 // MARK: - Helper data structures
 
 struct MessagePartialUpdateRequest: Encodable {
