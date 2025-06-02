ef66246e0ac00d602f0d46afe8469ed0dd26a8d7
Nuno Vieira <nuno.fcvieira93@gmail.com>
2025-06-02 22:26:09 +0100

    Add InMemoryRecorderLogDestination

---
+++ Sources/StreamChat/Utils/Logger/Destination/InMemoryRecorderLogDestination.swift
@@ -0,0 +1,59 @@
+//
+// Copyright © 2025 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+
+/// A class that provides an in-memory store for recording log entries.
+public class InMemoryLogEntryStoreProvider {
+    @Published public private(set) var logs: [LogEntry] = []
+
+    public static let shared = InMemoryLogEntryStoreProvider()
+
+    private let queue = DispatchQueue(label: "com.getstream.in-memory-log-entry-store")
+
+    private init() {}
+
+    public func addLog(_ log: LogEntry) {
+        queue.async { [weak self] in
+            self?.logs.append(log)
+        }
+    }
+
+    public func deleteLog(with id: UUID) {
+        queue.async { [weak self] in
+            self?.logs.removeAll(where: { $0.id == id })
+        }
+    }
+
+    public func clear() {
+        queue.async { [weak self] in
+            self?.logs.removeAll()
+        }
+    }
+}
+
+/// A log destination that records logs in memory.
+public class InMemoryRecorderLogDestination: BaseLogDestination {
+    private let logsStoreProvider = InMemoryLogEntryStoreProvider.shared
+
+    override public func process(logDetails: LogDetails) {
+        let entry = LogEntry(
+            timestamp: Date(),
+            level: logDetails.level,
+            subsystems: logDetails.subsystems,
+            functionName: "\(logDetails.functionName)",
+            description: logDetails.message
+        )
+        logsStoreProvider.addLog(entry)
+    }
+}
+
+public struct LogEntry: Identifiable {
+    public let id = UUID()
+    public let timestamp: Date
+    public let level: LogLevel
+    public let subsystems: LogSubsystem
+    public let functionName: String
+    public let description: String
+}
