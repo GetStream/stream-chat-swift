0b12e2c67101ce4ef2b6259e6d67b3147df3b457
Nuno Vieira <nuno.fcvieira93@gmail.com>
2025-08-01 13:10:02 +0100

    Use reactions from livestream controller in the demo app livestream UI

---
+++ DemoApp/Screens/Livestream/DemoLivestreamChatMessageListVC.swift
@@ -0,0 +1,69 @@
+//
+// Copyright © 2025 Stream.io Inc. All rights reserved.
+//
+
+import StreamChat
+import StreamChatUI
+import UIKit
+
+/// A custom message list view controller for livestream channels that uses LivestreamChannelController
+/// instead of MessageController and shows a custom bottom sheet for message actions.
+class DemoLivestreamChatMessageListVC: ChatMessageListVC {
+    /// The livestream channel controller for managing channel operations
+    public weak var livestreamChannelController: LivestreamChannelController?
+
+    override func didSelectMessageCell(at indexPath: IndexPath) {
+        guard
+            let cell = listView.cellForRow(at: indexPath) as? ChatMessageCell,
+            let messageContentView = cell.messageContentView,
+            let message = messageContentView.content,
+            message.isInteractionEnabled == true,
+            let livestreamChannelController = livestreamChannelController
+        else { return }
+
+        if message.isBounced {
+            showActions(forDebouncedMessage: message)
+            return
+        }
+
+        // Create the custom livestream actions view controller
+        let actionsController = DemoLivestreamMessageActionsVC()
+        actionsController.message = message
+        actionsController.livestreamChannelController = livestreamChannelController
+        actionsController.delegate = self
+        
+        // Present as bottom sheet
+        actionsController.modalPresentationStyle = .pageSheet
+        
+        if #available(iOS 16.0, *) {
+            if let sheetController = actionsController.sheetPresentationController {
+                sheetController.detents = [
+                    .custom { _ in
+                        return 180
+                    }
+                ]
+                sheetController.prefersGrabberVisible = true
+                sheetController.preferredCornerRadius = 16
+            }
+        }
+        
+        present(actionsController, animated: true)
+    }
+}
+
+// MARK: - LivestreamMessageActionsVCDelegate
+
+extension DemoLivestreamChatMessageListVC: LivestreamMessageActionsVCDelegate {
+    public func livestreamMessageActionsVCDidFinish(_ vc: DemoLivestreamMessageActionsVC) {
+        dismiss(animated: true)
+    }
+    
+    func livestreamMessageActionsVC(
+        _ vc: DemoLivestreamMessageActionsVC,
+        message: ChatMessage,
+        didTapOnActionItem actionItem: ChatMessageActionItem
+    ) {
+        // Handle action items that need to be delegated to the parent
+        delegate?.chatMessageListVC(self, didTapOnAction: actionItem, for: message)
+    }
+}
