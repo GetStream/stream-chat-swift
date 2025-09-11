227370c32a32ddcb4d1d52b8c808def962b7a5e8
Nuno Vieira <nuno.fcvieira93@gmail.com>
2025-09-11 13:06:42 +0100

    Move the masking to update content to avoid cell reuse breaking the masking

--- Sources/StreamChatUI/ChatMessageList/Attachments/Gallery/GalleryAttachmentViewInjector.swift
+++ Sources/StreamChatUI/ChatMessageList/Attachments/Gallery/GalleryAttachmentViewInjector.swift
@@ -43,29 +43,6 @@ open class GalleryAttachmentViewInjector: AttachmentViewInjector {
         contentView.bubbleView?.clipsToBounds = true
         contentView.bubbleContentContainer.insertArrangedSubview(galleryView, at: 0, respectsLayoutMargins: false)
 
-        // We need to apply corners to the left and right containers because the previewsContainerView
-        // is applying extra layout margins and the rounded corners wouldn't match the margins.
-        let leftCorners: CACornerMask = [.layerMinXMaxYCorner, .layerMinXMinYCorner]
-        let leftMaskedCorners = options.roundedCorners(for: galleryView.effectiveUserInterfaceLayoutDirection)
-            .intersection(leftCorners)
-        galleryView.leftPreviewsContainerView.layer.maskedCorners = leftMaskedCorners
-        galleryView.leftPreviewsContainerView.layer.cornerRadius = 16
-        galleryView.leftPreviewsContainerView.layer.masksToBounds = true
-
-        let rightCorners: CACornerMask = [.layerMaxXMaxYCorner, .layerMaxXMinYCorner]
-        let rightMaskedCorners = options.roundedCorners(for: galleryView.effectiveUserInterfaceLayoutDirection)
-            .intersection(rightCorners)
-
-        // If there is only one attachment, only the left container is used, so we need to apply
-        // the right corners to it as well.
-        if contentView.content?.allAttachments.count == 1 {
-            galleryView.leftPreviewsContainerView.layer.maskedCorners = rightMaskedCorners.union(leftMaskedCorners)
-        } else {
-            galleryView.rightPreviewsContainerView.layer.maskedCorners = rightMaskedCorners
-            galleryView.rightPreviewsContainerView.layer.cornerRadius = 16
-            galleryView.rightPreviewsContainerView.layer.masksToBounds = true
-        }
-
         if let ratio = galleryViewAspectRatio {
             galleryView
                 .widthAnchor
@@ -76,6 +53,31 @@ open class GalleryAttachmentViewInjector: AttachmentViewInjector {
 
     override open func contentViewDidUpdateContent() {
         super.contentViewDidUpdateContent()
+
+        if let options = contentView.layoutOptions {
+            // We need to apply corners to the left and right containers because the previewsContainerView
+            // is applying extra layout margins and the rounded corners wouldn't match the margins.
+            let leftCorners: CACornerMask = [.layerMinXMaxYCorner, .layerMinXMinYCorner]
+            let leftMaskedCorners = options.roundedCorners(for: galleryView.effectiveUserInterfaceLayoutDirection)
+                .intersection(leftCorners)
+            let rightCorners: CACornerMask = [.layerMaxXMaxYCorner, .layerMaxXMinYCorner]
+            let rightMaskedCorners = options.roundedCorners(for: galleryView.effectiveUserInterfaceLayoutDirection)
+                .intersection(rightCorners)
+
+            galleryView.leftPreviewsContainerView.layer.cornerRadius = 16
+            galleryView.leftPreviewsContainerView.layer.masksToBounds = true
+            if contentView.content?.allAttachments.count == 1 {
+                // If there is only one attachment, only the left container is used, so we need to apply
+                // the right corners to it as well.
+                galleryView.leftPreviewsContainerView.layer.maskedCorners = rightMaskedCorners.union(leftMaskedCorners)
+            } else {
+                galleryView.leftPreviewsContainerView.layer.maskedCorners = leftMaskedCorners
+            }
+
+            galleryView.rightPreviewsContainerView.layer.maskedCorners = rightMaskedCorners
+            galleryView.rightPreviewsContainerView.layer.cornerRadius = 16
+            galleryView.rightPreviewsContainerView.layer.masksToBounds = true
+        }
 
         let videos = attachments(payloadType: VideoAttachmentPayload.self)
         let images = attachments(payloadType: ImageAttachmentPayload.self)
