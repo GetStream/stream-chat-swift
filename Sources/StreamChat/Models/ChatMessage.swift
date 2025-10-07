9b381b5465619db2415f54333ee169d27c6a9651
Nuno Vieira <nuno.fcvieira93@gmail.com>
2025-10-07 19:19:15 +0100

    Fix forgotten deletedForMe in ChatMessage Equality

--- Sources/StreamChat/Models/ChatMessage.swift
+++ Sources/StreamChat/Models/ChatMessage.swift
@@ -571,6 +571,7 @@ extension ChatMessage: Hashable {
         guard lhs.localState == rhs.localState else { return false }
         guard lhs.updatedAt == rhs.updatedAt else { return false }
         guard lhs.deletedAt == rhs.deletedAt else { return false }
+        guard lhs.deletedForMe == rhs.deletedForMe else { return false }
         guard lhs.allAttachments == rhs.allAttachments else { return false }
         guard lhs.poll == rhs.poll else { return false }
         guard lhs.author == rhs.author else { return false }
