7282a8a7106fadaf6caf257d1fee81d263f5a94a
Toomas Vahter <toomas.vahter@getstream.io>
2025-10-16 12:00:48 +0300

    Add typealiases for avoiding importing StreamCore

--- Sources/StreamChat/Extensions/StreamCore+Extensions.swift
+++ Sources/StreamChat/Extensions/StreamCore+Extensions.swift
@@ -8,3 +8,12 @@ import StreamCore
 public var log: Logger {
     LogConfig.logger
 }
+
+public typealias BaseLogDestination = StreamCore.BaseLogDestination
+public typealias ConsoleLogDestination = StreamCore.ConsoleLogDestination
+public typealias LogConfig = StreamCore.LogConfig
+public typealias LogDestination = StreamCore.LogDestination
+public typealias LogFormatter = StreamCore.LogFormatter
+public typealias LogLevel = StreamCore.LogLevel
+public typealias LogSubsystem = StreamCore.LogSubsystem
+public typealias PrefixLogFormatter = StreamCore.PrefixLogFormatter
