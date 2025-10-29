3cd1e8c28318e2964ff126b7a57fbe05b52156e5
Toomas Vahter <toomas.vahter@getstream.io>
2025-10-29 12:48:12 +0200

    Fix tests after merge

--- Sources/StreamChat/Controllers/PollController/PollVoteListController.swift
+++ Sources/StreamChat/Controllers/PollController/PollVoteListController.swift
@@ -163,16 +163,26 @@ public class PollVoteListController: DataController, DelegateCallable, DataStore
         super.init()
         eventsObserver = client.subscribe { [weak self] event in
             guard let self else { return }
-            var vote: PollVote?
-            if let event = event as? PollVoteCastedEvent {
-                vote = event.vote
-            } else if let event = event as? PollVoteChangedEvent {
-                vote = event.vote
-            }
-            guard let vote else { return }
-            if vote.isAnswer == true && self.query.pollId == vote.pollId && self.query.optionId == nil {
-                self.pollsRepository.link(pollVote: vote, to: query)
-            }
+            self.didReceiveEvent(event)
+        }
+    }
+    
+    func didReceiveEvent(_ event: Event) {
+        var vote: PollVote?
+        if let event = event as? PollVoteCastedEvent {
+            vote = event.vote
+        } else if let event = event as? PollVoteChangedEvent {
+            vote = event.vote
+        }
+        guard let vote else { return }
+        if vote.isAnswer == true
+            && query.pollId == vote.pollId
+            && query.optionId == nil {
+            pollsRepository.link(pollVote: vote, to: query)
+        } else if vote.isAnswer == false
+            && query.pollId == vote.pollId
+            && query.optionId == vote.optionId {
+            pollsRepository.link(pollVote: vote, to: query)
         }
     }
 
@@ -267,27 +277,6 @@ extension PollVoteListController {
                 itemCreator: $2,
                 fetchedResultsControllerType: $3
             )
-        }
-    }
-}
-
-extension PollVoteListController: EventsControllerDelegate {
-    public func eventsController(_ controller: EventsController, didReceiveEvent event: any Event) {
-        var vote: PollVote?
-        if let event = event as? PollVoteCastedEvent {
-            vote = event.vote
-        } else if let event = event as? PollVoteChangedEvent {
-            vote = event.vote
-        }
-        guard let vote else { return }
-        if vote.isAnswer == true
-            && query.pollId == vote.pollId
-            && query.optionId == nil {
-            pollsRepository.link(pollVote: vote, to: query)
-        } else if vote.isAnswer == false
-            && query.pollId == vote.pollId
-            && query.optionId == vote.optionId {
-            pollsRepository.link(pollVote: vote, to: query)
         }
     }
 }
