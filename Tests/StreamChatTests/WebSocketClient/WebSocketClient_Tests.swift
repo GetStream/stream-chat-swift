f2ee33930179c81b4b34d3acde98883c3972627b
Toomas Vahter <toomas.vahter@getstream.io>
2025-12-02 12:59:10 +0200

    Fix rare crash in WebSocketClient when updating the engine

--- Tests/StreamChatTests/WebSocketClient/WebSocketClient_Tests.swift
+++ Tests/StreamChatTests/WebSocketClient/WebSocketClient_Tests.swift
@@ -378,6 +378,15 @@ final class WebSocketClient_Tests: XCTestCase {
             self.webSocketClient.connectEndpoint = .webSocketConnect(userInfo: UserInfo(id: "user_\(index)"))
         })
     }
+    
+    func test_recreatingEngineConcurrently() {
+        DispatchQueue.concurrentPerform(iterations: 100, execute: { _ in
+            self.webSocketClient.initialize()
+            // Change mocked request so that connect recreates the engine
+            requestEncoder.encodeRequest = .success(.init(url: .unique()))
+            self.webSocketClient.connect()
+        })
+    }
 
     // MARK: - Event handling tests
 
