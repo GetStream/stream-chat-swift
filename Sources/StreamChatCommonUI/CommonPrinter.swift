25da2d7be26913e28c306e8fb6ec62a8731000b6
Nuno Vieira <nuno.fcvieira93@gmail.com>
2025-12-16 16:43:35 +0000

    Hello World from StreamChatCommonUI

---
+++ Sources/StreamChatCommonUI/CommonPrinter.swift
@@ -0,0 +1,13 @@
+//
+// Copyright © 2025 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+
+public class ThePrinter {
+    public init() {}
+
+    public func echo() {
+        print("Hello World")
+    }
+}
