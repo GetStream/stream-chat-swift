a931ff1057155f1b71a0665b1572d33c563bc683
Nuno Vieira <nuno.fcvieira93@gmail.com>
2025-12-16 16:33:01 +0000

    Add StreamChatCommonUI module

---
+++ Sources/StreamChatCommonUI/StreamChatCommonUI.h
@@ -0,0 +1,18 @@
+//
+//  StreamChatCommonUI.h
+//  StreamChatCommonUI
+//
+// Copyright © 2025 Stream.io Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StreamChatCommonUI.
+FOUNDATION_EXPORT double StreamChatCommonUIVersionNumber;
+
+//! Project version string for StreamChatCommonUI.
+FOUNDATION_EXPORT const unsigned char StreamChatCommonUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StreamChatCommonUI/PublicHeader.h>
+
+
