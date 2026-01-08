d8a2c7707b192132acc122cf37251e7cdea78611
Nuno Vieira <nuno.fcvieira93@gmail.com>
2026-01-08 18:05:45 +0000

    Revert "Fix UI Tests not compiling because of duplicated extensions"
    This reverts commit 32c9b128117aeea4499101a5359444c01c9d6fbd.

--- Sources/StreamChatUI/Utils/Extensions/UIImage+Extensions.swift
+++ Sources/StreamChatUI/Utils/Extensions/UIImage+Extensions.swift
@@ -5,6 +5,22 @@
 import UIKit
 
 extension UIImage {
+    func tinted(with fillColor: UIColor) -> UIImage? {
+        let image = withRenderingMode(.alwaysTemplate)
+        UIGraphicsBeginImageContextWithOptions(size, false, scale)
+        fillColor.set()
+        image.draw(in: CGRect(origin: .zero, size: size))
+
+        guard let imageColored = UIGraphicsGetImageFromCurrentImageContext() else {
+            return nil
+        }
+
+        UIGraphicsEndImageContext()
+        return imageColored
+    }
+}
+
+extension UIImage {
     func temporaryLocalFileUrl() throws -> URL? {
         guard let imageData = jpegData(compressionQuality: 1.0) else { return nil }
         let imageName = "\(UUID().uuidString).jpg"
