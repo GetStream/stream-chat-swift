7e2c73832458744bd4330ec62702a196b07fadbb
Alexey Alter-Pesotskiy <alex@testableapple.com>
2026-01-15 07:01:23 +0000

    [CI] Resolve backend test issue (#3923)

--- StreamChatUITestsAppUITests/Tests/Performance/MessageListScrollTime.swift
+++ StreamChatUITestsAppUITests/Tests/Performance/MessageListScrollTime.swift
@@ -6,11 +6,25 @@ import XCTest
 
 @available(iOS 15.0, *)
 class MessageListScrollTime: StreamTestCase {
+    override func setUpWithError() throws {
+        useMockServer = false
+        switchApiKey = "zcgvnykxsfm8"
+        try super.setUpWithError()
+    }
+    
     func testMessageListScrollTime() {
-        WHEN("user opens the message list") {
-            backendRobot.generateChannels(channelsCount: 1, messagesCount: 100, attachments: true)
-            participantRobot.addReaction(type: .like)
-            userRobot.login().openChannel()
+        WHEN("user logs in") {
+            userRobot.login()
+        }
+        AND("user opens a channel with many messages") {
+            let minMessageCount = 20
+            for channelIndex in 0..<minMessageCount {
+                userRobot.openChannel(channelCellIndex: channelIndex)
+                if MessageListPage.cells.count >= minMessageCount {
+                    break
+                }
+                userRobot.tapOnBackButton()
+            }
         }
         THEN("user scrolls the message list") {
             let measureOptions = XCTMeasureOptions()
