b864614aa175f0994575529648d96224b1c6c147
Alexey Alter-Pesotskiy <alex@testableapple.com>
2026-01-15 15:03:04 +0000

    Remove STATIC_LIBRARY from code

--- Sources/StreamChat/Utils/Bundle+Extensions.swift
+++ Sources/StreamChat/Utils/Bundle+Extensions.swift
@@ -10,10 +10,6 @@ extension Bundle {
     static var streamChat: Bundle {
         #if SWIFT_PACKAGE
         return Bundle.module
-        #elseif STATIC_LIBRARY
-        return Bundle.main
-            .url(forResource: "StreamChat", withExtension: "bundle")
-            .flatMap(Bundle.init(url:))!
         #else
         return Bundle(for: BundleIdentifyingClass.self)
         #endif
