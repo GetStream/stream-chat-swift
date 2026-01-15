b07808647250b860b016907398d5d125fcc8de8a
Martin Mitrevski <martinmitrevski.oh@gmail.com>
2026-01-15 17:11:21 +0100

    Fix font scale issues on SwiftUI

--- Sources/StreamChatCommonUI/Appearance+Fonts.swift
+++ Sources/StreamChatCommonUI/Appearance+Fonts.swift
@@ -25,72 +25,43 @@ public extension Appearance {
     }
     
     struct FontsSwiftUI {
-        public var caption1: Font
-        public var footnoteBold: Font
-        public var footnote: Font
-        public var subheadline: Font
-        public var subheadlineBold: Font
-        public var body: Font
-        public var bodyBold: Font
-        public var bodyItalic: Font
-        public var headline: Font
-        public var headlineBold: Font
-        public var title: Font
-        public var title2: Font
-        public var title3: Font
-        public var emoji: Font
-        
-        init(fonts: Fonts) {
-            self.caption1 = fonts.caption1.toFont
-            self.footnoteBold = fonts.footnoteBold.toFont
-            self.footnote = fonts.footnote.toFont
-            self.subheadline = fonts.subheadline.toFont
-            self.subheadlineBold = fonts.subheadlineBold.toFont
-            self.body = fonts.body.toFont
-            self.bodyBold = fonts.bodyBold.toFont
-            self.bodyItalic = fonts.bodyItalic.toFont
-            self.headline = fonts.headline.toFont
-            self.headlineBold = fonts.headlineBold.toFont
-            self.title = fonts.title.toFont
-            self.title2 = fonts.title2.toFont
-            self.title3 = fonts.title3.toFont
-            self.emoji = fonts.emoji.toFont
-        }
-    }
-}
+        public init() {}
 
-extension UIFont {
-    var inferredTextStyle: UIFont.TextStyle {
-        fontDescriptor.object(forKey: .textStyle) as? UIFont.TextStyle ?? .body
-    }
+        public var caption1: Font = .caption
+        public var footnoteBold: Font = .footnote.bold()
+        public var footnote: Font = .footnote
+        public var subheadline: Font = .subheadline
+        public var subheadlineBold: Font = .subheadline.bold()
+        public var body: Font = .body
+        public var bodyBold: Font = .body.bold()
+        public var bodyItalic: Font = .body.italic()
+        public var headline: Font = .headline
+        public var headlineBold: Font = .headline.bold()
+        public var title: Font = .title
 
-    var toFont: Font {
-        if #available(iOS 14.0, *) {
-            Font.custom(fontName,
-                        size: pointSize,
-                        relativeTo: inferredTextStyle.toSwiftUITextStyle)
-        } else {
-            Font(self)
-        }
-    }
-}
+        // Backing storage (optional overrides)
+        private var _title2: Font? = nil
+        private var _title3: Font? = nil
 
-@available(iOS 14.0, *)
-extension UIFont.TextStyle {
-    var toSwiftUITextStyle: Font.TextStyle {
-        switch self {
-        case .largeTitle: return .largeTitle
-        case .title1:     return .title
-        case .title2:     return .title2
-        case .title3:     return .title3
-        case .headline:   return .headline
-        case .subheadline:return .subheadline
-        case .callout:    return .callout
-        case .caption1:   return .caption
-        case .caption2:   return .caption2
-        case .footnote:   return .footnote
-        case .body:       return .body
-        default:          return .body
+        // Publicly mutable properties
+        public var title2: Font {
+            get {
+                if let v = _title2 { return v }
+                if #available(iOS 14.0, *) { return .title2 }
+                return .title
+            }
+            set { _title2 = newValue }
         }
+
+        public var title3: Font {
+            get {
+                if let v = _title3 { return v }
+                if #available(iOS 14.0, *) { return .title3 }
+                return .title
+            }
+            set { _title3 = newValue }
+        }
+
+        public var emoji: Font = .system(size: 50)
     }
 }
