2a96d4d9ec3987cf1f2d0f23e0f4fd51976deedb
Toomas Vahter <toomas.vahter@getstream.io>
2026-01-19 18:01:45 +0200

    V5: Add token based color definitions (#3925)
    * Add token based color definitions
    
    * Convenience for color conversion like we have for font
    
    * Do not use lazy ColorPalette
    
    * Switch back to lazy var because there are many self references in the generated code, requires ColorPalette to be class
    
    * Include DesignSystemTokens
    
    * Remove font related tokens since text styles are coming
    
    * [CI] Snapshots (#3927)

---
+++ Sources/StreamChatCommonUI/Generated/DesignSystemTokens.swift
@@ -0,0 +1,108 @@
+//
+// Copyright © 2026 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+
+public enum DesignSystemTokens {
+    // MARK: Foundations
+    static let radius0: CGFloat = 0
+    static let radius12: CGFloat = 12
+    static let radius16: CGFloat = 16
+    static let radius2: CGFloat = 2
+    static let radius20: CGFloat = 20
+    static let radius24: CGFloat = 24
+    static let radius32: CGFloat = 32
+    static let radius4: CGFloat = 4
+    static let radius6: CGFloat = 6
+    static let radius8: CGFloat = 8
+    static let radiusFull: CGFloat = 9999
+    static let size12: CGFloat = 12
+    static let size128: CGFloat = 128
+    static let size16: CGFloat = 16
+    static let size2: CGFloat = 2
+    static let size20: CGFloat = 20
+    static let size24: CGFloat = 24
+    static let size240: CGFloat = 240
+    static let size28: CGFloat = 28
+    static let size32: CGFloat = 32
+    static let size320: CGFloat = 320
+    static let size4: CGFloat = 4
+    static let size40: CGFloat = 40
+    static let size48: CGFloat = 48
+    static let size480: CGFloat = 480
+    static let size560: CGFloat = 560
+    static let size6: CGFloat = 6
+    static let size64: CGFloat = 64
+    static let size640: CGFloat = 640
+    static let size8: CGFloat = 8
+    static let size80: CGFloat = 80
+    static let space0: CGFloat = 0
+    static let space12: CGFloat = 12
+    static let space16: CGFloat = 16
+    static let space2: CGFloat = 2
+    static let space20: CGFloat = 20
+    static let space24: CGFloat = 24
+    static let space32: CGFloat = 32
+    static let space4: CGFloat = 4
+    static let space40: CGFloat = 40
+    static let space48: CGFloat = 48
+    static let space64: CGFloat = 64
+    static let space8: CGFloat = 8
+    static let space80: CGFloat = 80
+    static let w100: CGFloat = 1
+    static let w120: CGFloat = 1.2
+    static let w150: CGFloat = 1.5
+    static let w200: CGFloat = 2
+    static let w300: CGFloat = 3
+    static let w400: CGFloat = 4
+
+    // MARK: Chat
+    public static let buttonHitTargetMinHeight: CGFloat = size48
+    public static let buttonHitTargetMinWidth: CGFloat = size48
+    public static let buttonRadiusFull: CGFloat = radiusFull
+    public static let buttonRadiusLg: CGFloat = radiusFull
+    public static let buttonRadiusMd: CGFloat = radiusFull
+    public static let buttonRadiusSm: CGFloat = radiusFull
+    public static let buttonVisualHeightLg: CGFloat = size48
+    public static let buttonVisualHeightMd: CGFloat = size40
+    public static let buttonVisualHeightSm: CGFloat = size32
+    public static let composerRadiusFixed: CGFloat = radius3xl
+    public static let composerRadiusFloating: CGFloat = radius3xl
+    public static let deviceRadius: CGFloat = 62
+    public static let deviceSafeAreaBottom: CGFloat = space32
+    public static let deviceSafeAreaTop: CGFloat = 62
+    public static let iconSizeLg: CGFloat = size32
+    public static let iconSizeMd: CGFloat = size20
+    public static let iconSizeSm: CGFloat = size16
+    public static let iconSizeXs: CGFloat = size12
+    public static let iconStrokeDefault: CGFloat = w150
+    public static let iconStrokeEmphasis: CGFloat = w200
+    public static let iconStrokeSubtle: CGFloat = w120
+    public static let messageBubbleRadiusAttachment: CGFloat = radiusLg
+    public static let messageBubbleRadiusAttachmentInline: CGFloat = radiusMd
+    public static let messageBubbleRadiusGroupBottom: CGFloat = radius2xl
+    public static let messageBubbleRadiusGroupMiddle: CGFloat = radius2xl
+    public static let messageBubbleRadiusGroupTop: CGFloat = radius2xl
+    public static let messageBubbleRadiusTail: CGFloat = radiusNone
+    public static let radius2xl: CGFloat = radius20
+    public static let radius3xl: CGFloat = radius24
+    public static let radius4xl: CGFloat = radius32
+    public static let radiusLg: CGFloat = radius12
+    public static let radiusMax: CGFloat = radiusFull
+    public static let radiusMd: CGFloat = radius8
+    public static let radiusNone: CGFloat = radius0
+    public static let radiusSm: CGFloat = radius6
+    public static let radiusXl: CGFloat = radius16
+    public static let radiusXs: CGFloat = radius4
+    public static let radiusXxs: CGFloat = radius2
+    public static let spacing2xl: CGFloat = space32
+    public static let spacing3xl: CGFloat = space40
+    public static let spacingLg: CGFloat = space20
+    public static let spacingMd: CGFloat = space16
+    public static let spacingNone: CGFloat = space0
+    public static let spacingSm: CGFloat = space12
+    public static let spacingXl: CGFloat = space24
+    public static let spacingXs: CGFloat = space8
+    public static let spacingXxs: CGFloat = space4
+}
