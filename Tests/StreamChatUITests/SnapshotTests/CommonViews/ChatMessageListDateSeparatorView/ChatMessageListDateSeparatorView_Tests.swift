84f57210c4feaf212da6cdb2d8df4e90477ae8b6
Toomas Vahter <toomas.vahter@getstream.io>
2026-04-01 14:57:23 +0300

    Fix tests

--- Tests/StreamChatUITests/SnapshotTests/CommonViews/ChatMessageListDateSeparatorView/ChatMessageListDateSeparatorView_Tests.swift
+++ Tests/StreamChatUITests/SnapshotTests/CommonViews/ChatMessageListDateSeparatorView/ChatMessageListDateSeparatorView_Tests.swift
@@ -33,9 +33,9 @@ import XCTest
         subject.setUpAppearance()
 
         XCTAssertNil(subject.backgroundColor)
-        XCTAssertEqual(subject.container.backgroundColor, subject.appearance.colorPalette.accentNeutral)
+        XCTAssertEqual(subject.container.backgroundColor, subject.appearance.colorPalette.backgroundCoreSurfaceSubtle)
         XCTAssertEqual(subject.textLabel.font, subject.appearance.fonts.footnote)
-        XCTAssertEqual(subject.textLabel.textColor, subject.appearance.colorPalette.textOnAccent)
+        XCTAssertEqual(subject.textLabel.textColor, subject.appearance.colorPalette.chatTextSystem)
     }
 
     // MARK: - updateContent
