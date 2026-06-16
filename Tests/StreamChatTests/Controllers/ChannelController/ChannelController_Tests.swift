8d1fb1e5d33dcb2d6e56aec538158f8c87754800
Toomas Vahter <toomas.vahter@getstream.io>
2026-06-16 14:00:59 +0300

    Update default typing events test to expect no send when channel not loaded

--- Tests/StreamChatTests/Controllers/ChannelController/ChannelController_Tests.swift
+++ Tests/StreamChatTests/Controllers/ChannelController/ChannelController_Tests.swift
@@ -5333,7 +5333,7 @@ final class ChannelController_Tests: XCTestCase {
         }
 
         let isEnabled = try waitFor { controller.shouldSendTypingEvents(completion: $0) }
-        XCTAssertEqual(isEnabled, true)
+        XCTAssertEqual(isEnabled, false)
     }
 
     func test_shouldSendTypingEvents_whenChannelEnabled_whenUserEnabled() throws {
