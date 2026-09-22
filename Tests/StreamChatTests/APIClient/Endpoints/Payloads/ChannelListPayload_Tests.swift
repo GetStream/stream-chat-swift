4f1845fac2e292cc35354ce6131f345eeb0f08af
Toomas Vahter <toomas.vahter@getstream.io>
2026-09-22 14:47:18 +0300

    Migrate query channels to OpenAPI generated v2 endpoint

--- Tests/StreamChatTests/APIClient/Endpoints/Payloads/ChannelListPayload_Tests.swift
+++ Tests/StreamChatTests/APIClient/Endpoints/Payloads/ChannelListPayload_Tests.swift
@@ -63,9 +63,9 @@ final class ChannelListPayload_Tests: XCTestCase {
         XCTAssertEqual(predefined.name, "user_per_channel_type_channels")
         XCTAssertEqual(predefined.filter["type"], .string("messaging"))
         XCTAssertEqual(predefined.filter["members"], .dictionary(["$in": .array([.string("r2-d2")])]))
-        XCTAssertEqual(predefined.sort.count, 2)
-        XCTAssertEqual(predefined.sort.first?["field"], .string("last_message_at"))
-        XCTAssertEqual(predefined.sort.first?["direction"], .number(-1))
+        XCTAssertEqual(predefined.sort?.count, 2)
+        XCTAssertEqual(predefined.sort?.first?.field, "last_message_at")
+        XCTAssertEqual(predefined.sort?.first?.direction, -1)
     }
 
     func test_channelListPayload_predefinedFilter_isNilWhenAbsent() throws {
